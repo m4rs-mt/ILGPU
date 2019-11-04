5f83ee946b961bbd19458d838dadcbaa59d88d66
Marcel Koester <m4rs@m4rs.net>
2019-11-04 01:01:32 +0100

    Added OpenCL tests.

---
+++ Src/ILGPU.Tests.OpenCL/ContextProvider.cs
@@ -0,0 +1,19 @@
+﻿using ILGPU.IR.Transformations;
+using ILGPU.Runtime;
+using ILGPU.Runtime.OpenCL;
+
+namespace ILGPU.Tests.OpenCL
+{
+    sealed class OpenCLContextProvider : ContextProvider
+    {
+        public OpenCLContextProvider(OptimizationLevel optimizationLevel)
+            : base(optimizationLevel)
+        { }
+
+        public override Accelerator CreateAccelerator(Context context)
+        {
+            var mainAccelerator = CLAccelerator.CLAccelerators[0];
+            return new CLAccelerator(context, mainAccelerator);
+        }
+    }
+}
