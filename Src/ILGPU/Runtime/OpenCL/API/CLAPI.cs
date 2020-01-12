m4rs-mt/ILGPU

commit 5f0209caeb4819c46dd1d22880691d10ca7c5bbc

Increased the error log size in the scope of the CLAPI.

Src/ILGPU/Runtime/OpenCL/API/CLAPI.cs
@@ -463,8 +463,8 @@ namespace ILGPU.Runtime.OpenCL.API
             IntPtr device,
             out string buildLog)
         {
-            const int LogSize = 4096;
-            var log = stackalloc sbyte[LogSize];
+            const int LogSize = 32_000;
+            var log = new sbyte[LogSize];
             var error = GetProgramBuildInfo(
                 program,
                 device,

commit b4c9e25f0580b3c0c23478ba556e198d17d37d15

Fixed compilation issues introduced by #57.

Src/ILGPU/Runtime/OpenCL/API/CLAPI.cs
@@ -465,21 +465,24 @@ namespace ILGPU.Runtime.OpenCL.API
         {
             const int LogSize = 32_000;
             var log = new sbyte[LogSize];
-            var error = GetProgramBuildInfo(
-                program,
-                device,
-                CLProgramBuildInfo.CL_PROGRAM_BUILD_LOG,
-                new IntPtr(LogSize),
-                log,
-                out IntPtr logLength);
-            buildLog = string.Empty;
-            if (error == CLError.CL_SUCCESS)
-                buildLog = new string(
-                    log,
-                    0,
-                    logLength.ToInt32(),
-                    System.Text.Encoding.ASCII);
-            return error;
+            fixed (sbyte* logPtr = &log[0])
+            {
+                var error = GetProgramBuildInfo(
+                    program,
+                    device,
+                    CLProgramBuildInfo.CL_PROGRAM_BUILD_LOG,
+                    new IntPtr(LogSize),
+                    logPtr,
+                    out IntPtr logLength);
+                buildLog = string.Empty;
+                if (error == CLError.CL_SUCCESS)
+                    buildLog = new string(
+                        logPtr,
+                        0,
+                        logLength.ToInt32(),
+                        System.Text.Encoding.ASCII);
+                return error;
+            }
         }
 
         /// <summary>