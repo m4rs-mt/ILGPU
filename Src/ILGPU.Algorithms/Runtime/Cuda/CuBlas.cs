a683d5aea261a7cb0351d168c26133daf213735b
Marcel Koester <m4rs@m4rs.net>
2020-01-19 22:18:04 +0100

    Added new CuBlas API bindings.

---
+++ Src/ILGPU.Algorithms/Runtime/Cuda/CuBlas.cs
@@ -0,0 +1,157 @@
+﻿// -----------------------------------------------------------------------------
+//                             ILGPU.Algorithms
+//                  Copyright (c) 2020 ILGPU Algorithms Project
+//                                www.ilgpu.net
+//
+// File: CuBlas.cs
+//
+// This file is part of ILGPU and is distributed under the University of
+// Illinois Open Source License. See LICENSE.txt for details
+// -----------------------------------------------------------------------------
+
+using ILGPU.Util;
+using System;
+
+namespace ILGPU.Runtime.Cuda
+{
+    /// <summary>
+    /// Wraps library calls to the external native Nvidia cuBlas library.
+    /// </summary>
+    public sealed unsafe partial class CuBlas : DisposeBase
+    {
+        #region Instance
+
+        /// <summary>
+        /// The underlying associated stream.
+        /// </summary>
+        private CudaStream stream;
+
+        /// <summary>
+        /// Constructs a new CuBlas instance to access the Nvidia cublas library.
+        /// </summary>
+        /// <param name="accelerator">The associated cuda accelerator.</param>
+        public CuBlas(CudaAccelerator accelerator)
+        {
+            if (accelerator == null)
+                throw new ArgumentNullException(nameof(accelerator));
+
+            accelerator.Bind();
+            CuBlasException.ThrowIfFailed(
+                NativeMethods.Create(out IntPtr handle));
+            Handle = handle;
+
+            CuBlasException.ThrowIfFailed(
+                NativeMethods.GetVersion(handle, out int version));
+            Version = version;
+
+            Stream = accelerator.DefaultStream as CudaStream;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The native CuBlas library handle.
+        /// </summary>
+        public IntPtr Handle { get; private set; }
+
+        /// <summary>
+        /// Returns the current library version.
+        /// </summary>
+        public int Version { get; }
+
+        /// <summary>
+        /// Gets or sets the current <see cref="CuBlasPointerMode"/> value.
+        /// </summary>
+        public CuBlasPointerMode PointerMode
+        {
+            get
+            {
+                CuBlasException.ThrowIfFailed(
+                    NativeMethods.GetPointerMode(Handle, out var mode));
+                return mode;
+            }
+            set
+            {
+                CuBlasException.ThrowIfFailed(
+                    NativeMethods.SetPointerMode(Handle, value));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current <see cref="CuBlasAtomicsMode"/> value.
+        /// </summary>
+        public CuBlasAtomicsMode AtomicsMode
+        {
+            get
+            {
+                CuBlasException.ThrowIfFailed(
+                    NativeMethods.GetAtomicsMode(Handle, out var mode));
+                return mode;
+            }
+            set
+            {
+                CuBlasException.ThrowIfFailed(
+                    NativeMethods.SetAtomicsMode(Handle, value));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current <see cref="CuBlasMathMode"/> value.
+        /// </summary>
+        public CuBlasMathMode MathMode
+        {
+            get
+            {
+                CuBlasException.ThrowIfFailed(
+                    NativeMethods.GetMathMode(Handle, out var mode));
+                return mode;
+            }
+            set
+            {
+                CuBlasException.ThrowIfFailed(
+                    NativeMethods.SetMathMode(Handle, value));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the associated accelerator stream.
+        /// </summary>
+        public CudaStream Stream
+        {
+            get => stream;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+                CuBlasException.ThrowIfFailed(
+                    NativeMethods.SetStream(Handle, value.StreamPtr));
+                stream = value;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region IDisposable
+
+        /// <summary cref="DisposeBase.Dispose(bool)"/>
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (Handle != IntPtr.Zero)
+            {
+                CuBlasException.ThrowIfFailed(
+                    NativeMethods.Free(Handle));
+                Handle = IntPtr.Zero;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Src/ILGPU.Algorithms/Runtime/Cuda/Enums.cs
@@ -0,0 +1,81 @@
+﻿// -----------------------------------------------------------------------------
+//                             ILGPU.Algorithms
+//                  Copyright (c) 2020 ILGPU Algorithms Project
+//                                www.ilgpu.net
+//
+// File: Enums.cs
+//
+// This file is part of ILGPU and is distributed under the University of
+// Illinois Open Source License. See LICENSE.txt for details
+// -----------------------------------------------------------------------------
+
+#pragma warning disable IDE1006 // Naming Styles
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace ILGPU.Runtime.Cuda
+{
+    public enum CuBlasStatus : int
+    {
+        CUBLAS_STATUS_SUCCESS = 0,
+        CUBLAS_STATUS_NOT_INITIALIZED = 1,
+        CUBLAS_STATUS_ALLOC_FAILED = 3,
+        CUBLAS_STATUS_INVALID_VALUE = 7,
+        CUBLAS_STATUS_ARCH_MISMATCH = 8,
+        CUBLAS_STATUS_MAPPING_ERROR = 11,
+        CUBLAS_STATUS_EXECUTION_FAILED = 13,
+        CUBLAS_STATUS_INTERNAL_ERROR = 14,
+        CUBLAS_STATUS_NOT_SUPPORTED = 15,
+        CUBLAS_STATUS_LICENSE_ERROR = 16
+    }
+
+    public enum CuBlasFillMode
+    {
+        Lower = 0,
+        Upper = 1,
+        Full = 2
+    }
+
+    public enum CuBlasSideMode
+    {
+        Left = 0,
+        Right = 1
+    }
+
+    public enum CuBlasDiagType : int
+    {
+        NonUnit = 0,
+        Unit = 1
+    }
+
+    public enum CuBlasOperation : int
+    {
+        NonTranspose = 0,
+        Transpose = 1,
+        ConjugateTranspose = 2,
+        Hermitan = 2,
+        Conjugate = 3
+    }
+
+    public enum CuBlasPointerMode : int
+    {
+        Host = 0,
+        Device = 1
+    }
+
+    public enum CuBlasAtomicsMode : int
+    {
+        NotAllowed = 0,
+        Allowed = 1
+    }
+
+    public enum CuBlasMathMode : int
+    {
+        DefaultMath = 0,
+        TensorOpMath = 1
+    }
+}
+
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+#pragma warning restore IDE1006 // Naming Styles
