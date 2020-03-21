eadb22d2102c8322eb6f69f764a115dfa3e0c6e9
Marcel Koester <m4rs@m4rs.net>
2020-03-21 13:48:36 +0100

    Redesigned SSAConstruction transformation to support new Rewriter API.

--- Src/ILGPU/IR/Transformations/SSAConstruction.cs
+++ Src/ILGPU/IR/Transformations/SSAConstruction.cs
@@ -11,11 +11,10 @@
 
 using ILGPU.IR.Analyses;
 using ILGPU.IR.Construction;
+using ILGPU.IR.Rewriting;
 using ILGPU.IR.Values;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using static ILGPU.IR.Types.StructureType;
 
 namespace ILGPU.IR.Transformations
 {
@@ -24,132 +23,14 @@ namespace ILGPU.IR.Transformations
     /// </summary>
     public sealed class SSAConstruction : UnorderedTransformation
     {
-        /// <summary>
-        /// Constructs a new SSA construction pass.
-        /// </summary>
-        public SSAConstruction() { }
-
-        /// <summary cref="UnorderedTransformation.PerformTransformation(Method.Builder)"/>
-        protected override bool PerformTransformation(Method.Builder builder)
-        {
-            // Create scope and try to find SSA-convertible alloca nodes
-            var scope = builder.CreateScope(ScopeFlags.AddAlreadyVisitedNodes);
-            var allocas = FindConvertibleAllocas(scope);
-            if (allocas.Count < 1)
-                return false;
-
-            // Perform SSA construction
-            var cfg = scope.CreateCFG();
-            var ssaBuilder = SSABuilder<Value>.Create(builder, cfg);
-
-            var convertedNodes = new Dictionary<Value, FieldRef>();
-            foreach (var block in scope)
-            {
-                if (!ssaBuilder.ProcessAndSeal(block))
-                    continue;
-
-                var blockBuilder = builder[block];
-                foreach (var valueEntry in blockBuilder)
-                {
-                    // Move insert position to the current instruction
-                    blockBuilder.SetupInsertPosition(valueEntry);
-
-                    Value value = valueEntry;
-                    switch (value)
-                    {
-                        case Alloca alloca:
-                            if (allocas.Contains(alloca))
-                            {
-                                Debug.Assert(!alloca.IsArrayAllocation, "Unsupported dynamic allocation");
-                                ssaBuilder.SetValue(
-                                    block,
-                                    alloca,
-                                    blockBuilder.CreateNull(alloca.AllocaType));
-
-                                convertedNodes.Add(alloca, new FieldRef(alloca));
-                                blockBuilder.Remove(alloca);
-                            }
-                            break;
-                        case Load load:
-                            if (convertedNodes.TryGetValue(load.Source, out FieldRef loadRef))
-                            {
-                                var ssaValue = ssaBuilder.GetValue(block, loadRef.Source);
-                                ssaValue = blockBuilder.CreateGetField(ssaValue, loadRef.AccessChain);
-
-                                load.Replace(ssaValue);
-                                blockBuilder.Remove(load);
-                            }
-                            else if (!load.Uses.HasAny)
-                            {
-                                blockBuilder.Remove(load);
-                            }
-                            break;
-                        case Store store:
-                            if (convertedNodes.TryGetValue(store.Target, out FieldRef storeRef))
-                            {
-                                var ssaValue = ssaBuilder.GetValue(block, storeRef.Source);
-                                ssaValue = blockBuilder.CreateSetField(
-                                    ssaValue,
-                                    storeRef.AccessChain,
-                                    store.Value);
-
-                                ssaBuilder.SetValue(block, storeRef.Source, ssaValue);
-                                blockBuilder.Remove(store);
-                            }
-                            break;
-                        case LoadFieldAddress loadFieldAddress:
-                            if (convertedNodes.TryGetValue(loadFieldAddress.Source, out FieldRef fieldRef))
-                            {
-                                convertedNodes.Add(
-                                    loadFieldAddress,
-                                    fieldRef.Access(loadFieldAddress.FieldIndex));
-                                blockBuilder.Remove(loadFieldAddress);
-                            }
-                            break;
-                        case AddressSpaceCast addressSpaceCast:
-                            if (convertedNodes.TryGetValue(addressSpaceCast.Value, out FieldRef castRef))
-                            {
-                                convertedNodes.Add(
-                                    addressSpaceCast,
-                                    castRef);
-                                blockBuilder.Remove(addressSpaceCast);
-                            }
-                            break;
-                    }
-                }
-            }
-
-            return true;
-        }
+        #region Utility Methods
 
         /// <summary>
-        /// Finds all SSA-convertible alloca nodes.
-        /// </summary>
-        /// <param name="scope">The scope in which to search for allocas.</param>
-        /// <returns>A set containing all detected alloca nodes.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static HashSet<Alloca> FindConvertibleAllocas(Scope scope)
-        {
-            var result = new HashSet<Alloca>();
-            foreach (Value value in scope.Values)
-            {
-                if (value is Alloca alloca &&
-                    !alloca.IsArrayAllocation &&
-                    alloca.AddressSpace == MemoryAddressSpace.Local &&
-                    !RequiresAddress(alloca))
-                    result.Add(alloca);
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Returns false iff the given node cannot be transformed
-        /// into a SSA variable.
+        /// Returns false if the given node cannot be transformed into an SSA value.
         /// </summary>
         /// <param name="node">The current node.</param>
         /// <returns>
-        /// False, iff the given node cannot be transformed into a SSA variable.
+        /// False, if the given node cannot be transformed into an SSA value.
         /// </returns>
         private static bool RequiresAddress(Value node)
         {
@@ -160,8 +41,8 @@ namespace ILGPU.IR.Transformations
                     case Load _:
                     case Store _:
                         continue;
-                    case LoadFieldAddress loadFieldAddress:
-                        if (RequiresAddress(loadFieldAddress))
+                    case LoadFieldAddress lfa:
+                        if (RequiresAddress(lfa))
                             return true;
                         break;
                     case AddressSpaceCast addressSpaceCast:
@@ -173,6 +54,216 @@ namespace ILGPU.IR.Transformations
                 }
             }
             return false;
+        }
+
+        #endregion
+
+        #region Nested Types
+
+        /// <summary>
+        /// Data that is using during SSA construction.
+        /// </summary>
+        private readonly struct ConstructionData
+        {
+            /// <summary>
+            /// Initializes the data structure.
+            /// </summary>
+            public ConstructionData(HashSet<Alloca> allocas)
+            {
+                Allocas = allocas;
+                ConvertedValues = new Dictionary<Value, FieldRef>();
+            }
+
+            /// <summary>
+            /// The set of all allocas to be converted into SSA value.
+            /// </summary>
+            private HashSet<Alloca> Allocas { get; }
+
+            /// <summary>
+            /// Maps converted values to their associated field references.
+            /// </summary>
+            private Dictionary<Value, FieldRef> ConvertedValues { get; }
+
+            /// <summary>
+            /// Returns true if the given alloca should be converted.
+            /// </summary>
+            /// <param name="alloca">The alloca to check.</param>
+            public bool ContainsAlloca(Alloca alloca) => Allocas.Contains(alloca);
+
+            /// <summary>
+            /// Tries to get a converted value entry.
+            /// </summary>
+            /// <param name="value">The value to lookup.</param>
+            /// <param name="fieldRef">The resolved field reference (if any).</param>
+            public bool TryGetConverted(Value value, out FieldRef fieldRef) =>
+                ConvertedValues.TryGetValue(value, out fieldRef);
+
+            /// <summary>
+            /// Adds the given value and the field reference to the mapping of converted values.
+            /// </summary>
+            /// <param name="value">The value to register.</param>
+            /// <param name="fieldRef">The field reference.</param>
+            public void AddConverted(Value value, FieldRef fieldRef) =>
+                ConvertedValues.Add(value, fieldRef);
+        }
+
+        #endregion
+
+        #region Rewriter Methods
+
+        /// <summary>
+        /// Converts an alloca node to its initial SSA value.
+        /// </summary>
+        private static void Convert(
+            SSARewriterContext<Value> context,
+            ConstructionData data,
+            Alloca alloca)
+        {
+            if (!data.ContainsAlloca(alloca))
+                return;
+
+            Debug.Assert(!alloca.IsArrayAllocation, "Unsupported dynamic allocation");
+            var initValue = context.Builder.CreateNull(alloca.AllocaType);
+            context.SetValue(context.Block, alloca, initValue);
+
+            data.AddConverted(alloca, new FieldRef(alloca));
+            context.Remove(alloca);
+        }
+
+        /// <summary>
+        /// Converts a load node into an SSA value.
+        /// </summary>
+        private static void Convert(
+            SSARewriterContext<Value> context,
+            ConstructionData data,
+            Load load)
+        {
+            if (data.TryGetConverted(load.Source, out var loadRef))
+            {
+                var ssaValue = context.GetValue(context.Block, loadRef.Source);
+                if (!loadRef.IsDirect)
+                {
+                    ssaValue = context.Builder.CreateGetField(
+                         ssaValue,
+                         loadRef.FieldSpan);
+                }
+
+                context.ReplaceAndRemove(load, ssaValue);
+            }
+            else if (!load.Uses.HasAny)
+            {
+                context.Remove(load);
+            }
+        }
+
+        /// <summary>
+        /// Converts a store node into an SSA value.
+        /// </summary>
+        private static void Convert(
+            SSARewriterContext<Value> context,
+            ConstructionData data,
+            Store store)
+        {
+            if (!data.TryGetConverted(store.Target, out var storeRef))
+                return;
+
+            Value ssaValue = store.Value;
+            if (!storeRef.IsDirect)
+            {
+                ssaValue = context.GetValue(context.Block, storeRef.Source);
+                ssaValue = context.Builder.CreateSetField(
+                    ssaValue,
+                    storeRef.FieldSpan,
+                    store.Value);
+            }
+
+            context.SetValue(context.Block, storeRef.Source, ssaValue);
+            context.Remove(store);
+        }
+
+        /// <summary>
+        /// Converts a field-address operation into an SSA binding.
+        /// </summary>
+        private static void Convert(
+            SSARewriterContext<Value> context,
+            ConstructionData data,
+            LoadFieldAddress loadFieldAddress)
+        {
+            if (!data.TryGetConverted(loadFieldAddress.Source, out var fieldRef))
+                return;
+
+            data.AddConverted(
+                loadFieldAddress,
+                fieldRef.Access(loadFieldAddress.FieldSpan));
+            context.Remove(loadFieldAddress);
+        }
+
+        /// <summary>
+        /// Converts an address-space cast into an SSA binding.
+        /// </summary>
+        private static void Convert(
+            SSARewriterContext<Value> context,
+            ConstructionData data,
+            AddressSpaceCast addressSpaceCast)
+        {
+            if (!data.TryGetConverted(addressSpaceCast.Value, out var castRef))
+                return;
+
+            data.AddConverted(addressSpaceCast, castRef);
+            context.Remove(addressSpaceCast);
+        }
+
+        #endregion
+
+        #region Rewriter
+
+        /// <summary>
+        /// The internal rewriter.
+        /// </summary>
+        private static readonly SSARewriter<Value, ConstructionData> Rewriter =
+            new SSARewriter<Value, ConstructionData>();
+
+        /// <summary>
+        /// Registers all rewriting patterns.
+        /// </summary>
+        static SSAConstruction()
+        {
+            Rewriter.Add<Alloca>(Convert);
+            Rewriter.Add<Load>(Convert);
+            Rewriter.Add<Store>(Convert);
+            Rewriter.Add<LoadFieldAddress>(Convert);
+            Rewriter.Add<AddressSpaceCast>(Convert);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Constructs a new SSA construction pass.
+        /// </summary>
+        public SSAConstruction() { }
+
+        /// <summary cref="UnorderedTransformation.PerformTransformation(Method.Builder)"/>
+        protected override bool PerformTransformation(Method.Builder builder)
+        {
+            // Create scope and try to find SSA-convertible alloca nodes
+            var scope = builder.CreateScope(ScopeFlags.AddAlreadyVisitedNodes);
+
+            // Search for convertible allocas
+            var allocas = new HashSet<Alloca>();
+            scope.ForEachValue<Alloca>(alloca =>
+            {
+                if (alloca.IsArrayAllocation ||
+                    alloca.AddressSpace != MemoryAddressSpace.Local ||
+                    RequiresAddress(alloca))
+                    return;
+                allocas.Add(alloca);
+            });
+            if (allocas.Count < 1)
+                return false;
+
+            // Perform SSA construction
+            var ssaBuilder = SSABuilder<Value>.Create(builder, scope);
+            return Rewriter.Rewrite(ssaBuilder, new ConstructionData(allocas));
         }
     }
 }
