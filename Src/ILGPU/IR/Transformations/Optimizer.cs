db47db3985a93ad651911fe981ffb3cf381d211e
Marcel Koester <m4rs@m4rs.net>
2020-03-22 20:57:20 +0100

    Added new optimization configuration.

--- Src/ILGPU/IR/Transformations/Optimizer.cs
+++ Src/ILGPU/IR/Transformations/Optimizer.cs
@@ -9,26 +9,40 @@
 // Illinois Open Source License. See LICENSE.txt for details
 // -----------------------------------------------------------------------------
 
+using ILGPU.Backends.PointerViews;
 using System;
-using System.Runtime.CompilerServices;
 
 namespace ILGPU.IR.Transformations
 {
     /// <summary>
-    /// Represetns an optimization level.
+    /// Represent an optimization level.
     /// </summary>
     public enum OptimizationLevel
     {
         /// <summary>
-        /// Lightweight (required) transformations only.
+        /// Defaults to O0.
         /// </summary>
-        Debug,
+        Debug = O0,
 
         /// <summary>
-        /// All transformation passes with seveal optimization
-        /// iterations.
+        /// Defaults to O1.
         /// </summary>
-        Release,
+        Release = O1,
+
+        /// <summary>
+        /// Lightweight (required) transformations only.
+        /// </summary>
+        O0 = 0,
+
+        /// <summary>
+        /// Default release mode transformations.
+        /// </summary>
+        O1 = 1,
+
+        /// <summary>
+        /// Expensive transformations.
+        /// </summary>
+        O2 = 2,
     }
 
     /// <summary>
@@ -40,7 +54,17 @@ namespace ILGPU.IR.Transformations
         /// <summary>
         /// Returns the number of known optimization levels.
         /// </summary>
-        public const int NumOptimizationLevels = 2;
+        public const int NumOptimizationLevels = 3;
+
+        /// <summary>
+        /// Internal mapping from optimization levels to handlers.
+        /// </summary>
+        private static readonly Action<Transformer.Builder, ContextFlags>[] OptimizationHandlers =
+        {
+            AddO0Optimizations,
+            AddO1Optimizations,
+            AddO2Optimizations,
+        };
 
         /// <summary>
         /// Populates the given transformation manager with the required
@@ -50,58 +74,100 @@ namespace ILGPU.IR.Transformations
         /// <param name="contextFlags">The context flags.</param>
         /// <param name="level">The desired optimization level.</param>
         /// <returns>The maximum number of iterations.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void AddOptimizations(
             this Transformer.Builder builder,
             ContextFlags contextFlags,
             OptimizationLevel level)
         {
-            switch (level)
+            if (level < OptimizationLevel.O0 || level > OptimizationLevel.O2)
+                throw new ArgumentOutOfRangeException(nameof(level));
+            OptimizationHandlers[(int)level](builder, contextFlags);
+        }
+
+        /// <summary>
+        /// Adds basic optimization transformations.
+        /// </summary>
+        /// <param name="builder">The transformation manager to populate.</param>
+        /// <param name="contextFlags">The context flags.</param>
+        public static void AddBasicOptimizations(
+            this Transformer.Builder builder,
+            ContextFlags contextFlags)
+        {
+            if (!contextFlags.HasFlags(ContextFlags.NoInlining))
+                builder.Add(new Inliner());
+            builder.Add(new SimplifyControlFlow());
+        }
+
+        /// <summary>
+        /// Adds general backend optimizations.
+        /// </summary>
+        /// <param name="builder">The transformation manager to populate.</param>
+        /// <param name="acceleratorSpecializer">
+        /// An instance of an <see cref="AcceleratorSpecializer"/> class.
+        /// </param>
+        /// <param name="level">The desired optimization level.</param>
+        public static void AddBackendOptimizations(
+            this Transformer.Builder builder,
+            AcceleratorSpecializer acceleratorSpecializer,
+            OptimizationLevel level)
+        {
+            // Specialize accelerator properties and views
+            builder.Add(new LowerPointerViews());
+            builder.Add(acceleratorSpecializer);
+
+            // Lower structures
+            if (level > OptimizationLevel.O1)
             {
-                case OptimizationLevel.Debug:
-                    AddDebugOptimizations(builder, contextFlags);
-                    break;
-                case OptimizationLevel.Release:
-                    AddReleaseOptimizations(builder, contextFlags);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(level));
+                builder.Add(new LowerStructures());
+                builder.Add(new DeadCodeElimination());
             }
         }
 
         /// <summary>
-        /// Populates the given transformation manager with the required
-        /// debug optimization transformations.
+        /// Populates the given transformation manager with O0 optimizations.
         /// </summary>
         /// <param name="builder">The transformation manager to populate.</param>
         /// <param name="contextFlags">The context flags.</param>
-        public static void AddDebugOptimizations(
+        public static void AddO0Optimizations(
             this Transformer.Builder builder,
             ContextFlags contextFlags)
         {
-            if (!contextFlags.HasFlags(ContextFlags.NoInlining))
-                builder.Add(new Inliner());
-            builder.Add(new SimplifyControlFlow());
+            builder.AddBasicOptimizations(contextFlags);
             builder.Add(new SSAConstruction());
             builder.Add(new DeadCodeElimination());
         }
 
         /// <summary>
-        /// Populates the given transformation manager with the required
-        /// release optimization transformations.
+        /// Populates the given transformation manager with O1 optimizations.
         /// </summary>
         /// <param name="builder">The transformation manager to populate.</param>
         /// <param name="contextFlags">The context flags.</param>
-        public static void AddReleaseOptimizations(
+        public static void AddO1Optimizations(
             this Transformer.Builder builder,
             ContextFlags contextFlags)
         {
-            if (!contextFlags.HasFlags(ContextFlags.NoInlining))
-                builder.Add(new Inliner());
-            builder.Add(new SimplifyControlFlow());
-            builder.Add(new SSAConstruction());
-            builder.Add(new InferAddressSpaces());
+            builder.AddBasicOptimizations(contextFlags);
             builder.Add(new DeadCodeElimination());
+            builder.Add(new SSAConstruction());
+            builder.Add(new DeadCodeElimination());
+            builder.Add(new InferAddressSpaces());
+        }
+
+        /// <summary>
+        /// Populates the given transformation manager with O2 optimizations.
+        /// </summary>
+        /// <param name="builder">The transformation manager to populate.</param>
+        /// <param name="contextFlags">The context flags.</param>
+        public static void AddO2Optimizations(
+            this Transformer.Builder builder,
+            ContextFlags contextFlags)
+        {
+            builder.AddBasicOptimizations(contextFlags);
+            builder.Add(new DeadCodeElimination());
+            builder.Add(new SSAConstruction());
+            builder.Add(new LowerStructures());
+            builder.Add(new DeadCodeElimination());
+            builder.Add(new InferAddressSpaces());
         }
 
         /// <summary>
