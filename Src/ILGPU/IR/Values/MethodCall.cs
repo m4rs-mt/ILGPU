12758e42366724431f8c0bcd9794dc78d1d1f2b2
Marcel Koester <m4rs@m4rs.net>
2020-06-01 11:40:08 +0200

    Added new builder types for several Values to simplify their construction.

--- Src/ILGPU/IR/Values/MethodCall.cs
+++ Src/ILGPU/IR/Values/MethodCall.cs
@@ -23,6 +23,83 @@ namespace ILGPU.IR.Values
     [ValueKind(ValueKind.MethodCall)]
     public sealed class MethodCall : Value
     {
+        #region Nested Types
+
+        /// <summary>
+        /// An instance builder for method calls.
+        /// </summary>
+        public struct Builder
+        {
+            #region Instance
+
+            private ValueList builder;
+
+            /// <summary>
+            /// Initializes a new call builder.
+            /// </summary>
+            /// <param name="irBuilder">The current IR builder.</param>
+            /// <param name="location">The current location.</param>
+            /// <param name="target">The target method to call.</param>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal Builder(IRBuilder irBuilder, Location location, Method target)
+            {
+                builder = ValueList.Create(target.NumParameters);
+                IRBuilder = irBuilder;
+                Location = location;
+                Target = target;
+            }
+
+            #endregion
+
+            #region Properties
+
+            /// <summary>
+            /// Returns the parent builder.
+            /// </summary>
+            public IRBuilder IRBuilder { get; }
+
+            /// <summary>
+            /// Returns the current location.
+            /// </summary>
+            public Location Location { get; }
+
+            /// <summary>
+            /// Returns the call target.
+            /// </summary>
+            public Method Target { get; }
+
+            /// <summary>
+            /// The number of arguments.
+            /// </summary>
+            public int Count => builder.Count;
+
+            #endregion
+
+            #region Methods
+
+            /// <summary>
+            /// Adds the given value to the call builder.
+            /// </summary>
+            /// <param name="value">The value to add.</param>
+            public void Add(Value value)
+            {
+                IRBuilder.AssertNotNull(value);
+                builder.Add(value);
+            }
+
+            /// <summary>
+            /// Constructs a new value that represents the current method call.
+            /// </summary>
+            /// <returns>The resulting value reference.</returns>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public MethodCall Seal() =>
+                IRBuilder.CreateCall(Location, Target, ref builder);
+
+            #endregion
+        }
+
+        #endregion
+
         #region Instance
 
         /// <summary>
@@ -66,14 +143,11 @@ namespace ILGPU.IR.Values
             IRBuilder builder,
             IRRebuilder rebuilder)
         {
-            var args = ImmutableArray.CreateBuilder<ValueReference>(Nodes.Length);
-            foreach (var arg in Nodes)
-                args.Add(rebuilder.Rebuild(arg));
             var target = rebuilder.LookupCallTarget(Target);
-            return builder.CreateCall(
-                Location,
-                target,
-                args.ToImmutable());
+            var call = builder.CreateCall(Location, target);
+            foreach (var arg in Nodes)
+                call.Add(rebuilder.Rebuild(arg));
+            return call.Seal();
         }
 
         /// <summary cref="Value.Accept"/>
