145e2f0d833032565b08c06f3496b1a7acf44dea
Marcel Koester <m4rs@m4rs.net>
2020-06-02 19:09:14 +0200

    Adapted Runtime classes to support the new InlineList type.

--- Src/ILGPU/Runtime/SpecializationCache.cs
+++ Src/ILGPU/Runtime/SpecializationCache.cs
@@ -137,8 +137,7 @@ namespace ILGPU.Runtime
                 var blockBuilder = methodBuilder.EntryBlockBuilder;
 
                 // Append all parameters
-                var targetValues = ImmutableArray.CreateBuilder<ValueReference>(
-                    kernelMethod.NumParameters);
+                var callBuilder = blockBuilder.CreateCall(location, kernelMethod);
                 var specializedParameters = Entry.Parameters.SpecializedParameters;
                 int paramOffset = Entry.KernelIndexParameterOffset;
                 for (
@@ -164,18 +163,15 @@ namespace ILGPU.Runtime
                             managedArgument,
                             managedArgument.GetType());
 
-                        targetValues.Add(irValue);
+                        callBuilder.Add(irValue);
                         ++specialParamIdx;
                     }
                     else
                     {
-                        targetValues.Add(newParam);
+                        callBuilder.Add(newParam);
                     }
                 }
-                blockBuilder.CreateCall(
-                    location,
-                    kernelMethod,
-                    targetValues.MoveToImmutable());
+                callBuilder.Seal();
                 blockBuilder.CreateReturn(location);
             }
             return targetMethod;
