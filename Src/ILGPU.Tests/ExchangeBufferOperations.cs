7d8d68e335c46c4bee2191a11b35a28e7fa22fca
Yey007 <yey007tech@gmail.com>
2020-06-14 22:27:23 -0400

    Fixed warnings, added more tests and converted them to theory

--- Src/ILGPU.Tests/ExchangeBufferOperations.cs
+++ Src/ILGPU.Tests/ExchangeBufferOperations.cs
@@ -12,7 +12,6 @@ namespace ILGPU.Tests
     {
 
         private const int Length = 32;
-        private static Random random = new Random();
 
         protected ExchangeBufferOperations(ITestOutputHelper output, TestContext testContext)
             : base(output, testContext)
@@ -47,19 +46,22 @@ namespace ILGPU.Tests
 
         internal static void Copy2DKernel(Index2 index, ArrayView<int, Index2> data)
         {
-            data[index] = 5;
+            data[index] -= 5;
         }
 
-        [Fact]
+        [Theory]
+        [InlineData(10)]
+        [InlineData(int.MaxValue)]
+        [InlineData(int.MinValue)]
         [KernelMethod(nameof(Copy2DKernel))]
-        public void Copy2D()
+        public void Copy2D(int constant)
         {
             using var exchangeBuffer = Accelerator.AllocateExchangeBuffer<int>(new Index2(Length, Length));
             for(int i = 0; i < Length; i++)
             {
                 for(int j = 0; j < Length; j++)
                 {
-                    exchangeBuffer[new Index2(i, j)] = 10;
+                    exchangeBuffer[new Index2(i, j)] = constant;
                 }
             }
             exchangeBuffer.CopyToAccelerator();
@@ -69,13 +71,41 @@ namespace ILGPU.Tests
             {
                 for (int j = 0; j < Length; j++)
                 {
-                    expected[new Index2(i, j)] = 5;
+                    expected[new Index2(i, j)] = constant - 5;
                 }
             }
 
             Execute(exchangeBuffer.Extent, exchangeBuffer.View);
             exchangeBuffer.CopyFromAccelerator();
             Assert.Equal(exchangeBuffer.CPUView, expected);
+        }
+
+        internal static void GetAsArray2DKernel(Index2 index, ArrayView<int, Index2> data, int c)
+        {
+            data[index] = c;
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(int.MaxValue)]
+        [InlineData(int.MinValue)]
+        [KernelMethod(nameof(GetAsArray2DKernel))]
+        public void GetAsArray2D(int constant)
+        {
+            using var exchangeBuffer = Accelerator.AllocateExchangeBuffer<int>(new Index2(Length, Length));
+            Execute(exchangeBuffer.Extent, exchangeBuffer.View, constant);
+
+            var expected = new int[Length][];
+            for (int i = 0; i < Length; i++)
+            {
+                expected[i] = new int[Length];
+                for (int j = 0; j < Length; j++)
+                {
+                    expected[i][j] = constant;
+                }
+            }
+
+            Assert.Equal(expected, exchangeBuffer.GetAs2DArray(Accelerator.DefaultStream));
         }
     }
 }
