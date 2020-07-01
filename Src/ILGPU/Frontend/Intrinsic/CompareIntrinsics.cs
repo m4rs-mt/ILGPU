eb066e02210b38dc7977f37652d2aa7c0a266932
Marcel Koester <m4rs@m4rs.net>
2020-07-01 17:46:13 +0200

    Added support for convert and compare intrinsics in frontend.

---
+++ Src/ILGPU/Frontend/Intrinsic/CompareIntrinsics.cs
@@ -0,0 +1,64 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2016-2020 Marcel Koester
+//                                    www.ilgpu.net
+//
+// File: CompareIntrinsics.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details
+// ---------------------------------------------------------------------------------------
+
+using ILGPU.IR.Values;
+using System;
+
+namespace ILGPU.Frontend.Intrinsic
+{
+    /// <summary>
+    /// Marks compare intrinsics that are built in.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    sealed class CompareIntriniscAttribute : IntrinsicAttribute
+    {
+        public CompareIntriniscAttribute(CompareKind kind)
+            : this(kind, CompareFlags.None)
+        { }
+
+        public CompareIntriniscAttribute(CompareKind kind, CompareFlags flags)
+        {
+            IntrinsicKind = kind;
+            IntrinsicFlags = flags;
+        }
+
+        public override IntrinsicType Type => IntrinsicType.Compare;
+
+        /// <summary>
+        /// Returns the associated intrinsic kind.
+        /// </summary>
+        public CompareKind IntrinsicKind { get; }
+
+        /// <summary>
+        /// Returns the associated intrinsic flags.
+        /// </summary>
+        public CompareFlags IntrinsicFlags { get; }
+    }
+
+    partial class Intrinsics
+    {
+        /// <summary>
+        /// Handles compare operations.
+        /// </summary>
+        /// <param name="context">The current invocation context.</param>
+        /// <param name="attribute">The intrinsic attribute.</param>
+        /// <returns>The resulting value.</returns>
+        private static ValueReference HandleCompareOperation(
+            ref InvocationContext context,
+            CompareIntriniscAttribute attribute) =>
+            context.Builder.CreateCompare(
+                context.Location,
+                context[0],
+                context[1],
+                attribute.IntrinsicKind,
+                attribute.IntrinsicFlags);
+    }
+}
---
+++ Src/ILGPU/Frontend/Intrinsic/ConvertIntrinsics.cs
@@ -0,0 +1,62 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2016-2020 Marcel Koester
+//                                    www.ilgpu.net
+//
+// File: ConvertIntrinsics.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details
+// ---------------------------------------------------------------------------------------
+
+using ILGPU.IR.Values;
+using ILGPU.Util;
+using System;
+
+namespace ILGPU.Frontend.Intrinsic
+{
+    /// <summary>
+    /// Marks compare intrinsics that are built in.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    sealed class ConvertIntriniscAttribute : IntrinsicAttribute
+    {
+        public ConvertIntriniscAttribute()
+            : this(ConvertFlags.None)
+        { }
+
+        public ConvertIntriniscAttribute(ConvertFlags flags)
+        {
+            IntrinsicFlags = flags;
+        }
+
+        public override IntrinsicType Type => IntrinsicType.Convert;
+
+        /// <summary>
+        /// Returns the associated intrinsic flags.
+        /// </summary>
+        public ConvertFlags IntrinsicFlags { get; }
+    }
+
+    partial class Intrinsics
+    {
+        /// <summary>
+        /// Handles convert operations.
+        /// </summary>
+        /// <param name="context">The current invocation context.</param>
+        /// <param name="attribute">The intrinsic attribute.</param>
+        /// <returns>The resulting value.</returns>
+        private static ValueReference HandleConvertOperation(
+            ref InvocationContext context,
+            ConvertIntriniscAttribute attribute)
+        {
+            var returnType = context.Method.GetReturnType();
+            var typeNode = context.Builder.CreateType(returnType);
+            return context.Builder.CreateConvert(
+                context.Location,
+                context[0],
+                typeNode,
+                attribute.IntrinsicFlags);
+        }
+    }
+}
