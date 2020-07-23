97babdf036771c4b3e418e89080f6f4ea9a42a3a
Marcel Koester <m4rs@m4rs.net>
2020-07-24 01:03:56 +0200

    Fixed invalid offset mapping of prefixed IL instructions.

--- Src/ILGPU.Tests/DisassemblerTests.cs
+++ Src/ILGPU.Tests/DisassemblerTests.cs
@@ -1,6 +1,8 @@
 ﻿using ILGPU.Frontend;
 using ILGPU.Frontend.DebugInformation;
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Reflection;
 using Xunit;
 using Xunit.Abstractions;
@@ -9,9 +11,56 @@ namespace ILGPU.Tests
 {
     public abstract class DisassemblerTests : TestBase
     {
+        private const int Length = 32;
+
         protected DisassemblerTests(ITestOutputHelper output, TestContext testContext)
             : base(output, testContext)
         { }
+
+        internal interface IPrefixOffsetFunc
+        {
+            int Apply(int value);
+        }
+
+        internal struct IdentityImpl : IPrefixOffsetFunc, IXunitSerializable
+        {
+            public int Apply(int value) => value;
+
+            public void Serialize(IXunitSerializationInfo info) { }
+
+            public void Deserialize(IXunitSerializationInfo info) { }
+        }
+
+        public static TheoryData<object> PrefixOffsetsData => new TheoryData<object>
+        {
+            { default(IdentityImpl) }
+        };
+
+        internal static void InstructionPrefixOffsetsKernel<TFunc>(
+            Index1 index,
+            ArrayView<int> data)
+            where TFunc : struct, IPrefixOffsetFunc
+        {
+            TFunc func = default;
+            data[index] = func.Apply(Group.IsFirstThread ? default : default(int));
+        }
+
+        [Theory]
+        [MemberData(nameof(PrefixOffsetsData))]
+        [KernelMethod(nameof(InstructionPrefixOffsetsKernel))]
+        [SuppressMessage(
+            "Usage",
+            "xUnit1026:Theory methods should use all of their parameters",
+            Justification = "Required to infer generic type argument")]
+        public void InstructionPrefixOffsets<T>(T _)
+            where T : unmanaged
+        {
+            using var buffer = Accelerator.Allocate<int>(Length);
+            Execute<Index1, T>(buffer.Length, buffer.View);
+
+            var expected = Enumerable.Repeat(0, Length).ToArray();
+            Verify(buffer, expected);
+        }
 
         internal static void StelemLdtokenKernel<T>(
             Index1 index,
