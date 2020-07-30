22924c45ae80b061a81a68211b0c54a92f93bbc9
albosc <alebos@web.de>
2020-07-30 10:32:36 +0200

    Added new test cases for the algorithms library. (#30)

---
+++ Src/ILGPU.Algorithms.Tests/InitializeTests.cs
@@ -0,0 +1,114 @@
+﻿using ILGPU.Tests;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ILGPU.Algorithms.Tests
+{
+    public abstract partial class InitializeTests : TestBase
+    {
+        protected InitializeTests(ITestOutputHelper output, TestContext testContext)
+            : base(output, testContext)
+        { }
+
+        #region MemberData
+
+        public static TheoryData<object, object> SimpleTestData =>
+            new TheoryData<object, object>
+        {
+            { sbyte.MinValue, 1 },
+            { byte.MaxValue, 1 },
+            { short.MinValue, 1 },
+            { ushort.MaxValue, 1 },
+            { int.MinValue, 1 },
+            { uint.MaxValue, 1 },
+            { long.MinValue, 1 },
+            { ulong.MaxValue, 1 },
+            { float.Epsilon, 1 },
+            { double.Epsilon, 1 },
+            { default(EmptyStruct), 1 },
+            { default(TestStruct), 1 },
+            { default(TestStruct<TestStruct<byte>>), 1 },
+
+            { byte.MaxValue, 2 },
+            { short.MinValue, 2 },
+            { int.MinValue, 2 },
+            { long.MinValue, 2 },
+            { default(EmptyStruct), 2 },
+            { default(TestStruct), 2 },
+            { default(TestStruct<TestStruct<byte>>), 2 },
+
+            { byte.MaxValue, 31 },
+            { short.MinValue, 31 },
+            { int.MinValue, 31 },
+            { long.MinValue, 31 },
+            { default(EmptyStruct), 31 },
+            { default(TestStruct<TestStruct<ushort>>), 31 },
+
+            { byte.MaxValue, 32 },
+            { short.MinValue, 32 },
+            { int.MinValue, 32 },
+            { long.MinValue, 32 },
+            { default(EmptyStruct), 32 },
+            { default(TestStruct<TestStruct<float>>), 32 },
+
+            { byte.MaxValue, 33 },
+            { short.MinValue, 33 },
+            { int.MinValue, 33 },
+            { long.MinValue, 33 },
+            { default(EmptyStruct), 33 },
+            { default(TestStruct<TestStruct<int>>), 33 },
+
+            { byte.MaxValue, 65 },
+            { short.MinValue, 65 },
+            { int.MinValue, 65 },
+            { long.MinValue, 65 },
+            { default(EmptyStruct), 65 },
+            { default(TestStruct<TestStruct<short>>), 65 },
+
+            { byte.MaxValue, 127 },
+            { short.MinValue, 127 },
+            { int.MinValue, 127 },
+            { long.MinValue, 127 },
+            { default(EmptyStruct), 127 },
+            { default(TestStruct<TestStruct<long>>), 127 },
+
+            { byte.MaxValue, 1000 },
+            { short.MinValue, 1000 },
+            { int.MinValue, 1000 },
+            { long.MinValue, 1000 },
+            { default(EmptyStruct), 1000 },
+            { default(TestStruct<TestStruct<long>>), 1000 },
+
+            { byte.MaxValue, 10000 },
+            { short.MinValue, 10000 },
+            { int.MinValue, 10000 },
+            { long.MinValue, 10000 },
+            { default(EmptyStruct), 10000 },
+            { default(TestStruct<TestStruct<long>>), 10000 },
+
+            { byte.MaxValue, 100000 },
+            { short.MinValue, 100000 },
+            { int.MinValue, 100000 },
+            { long.MinValue, 100000 },
+            { default(EmptyStruct), 100000 },
+            { default(TestStruct<TestStruct<long>>), 100000 },
+        };
+
+        #endregion
+
+        [Theory]
+        [MemberData(nameof(SimpleTestData))]
+        public void InitializeSimple<T, TArraySize>(T value, int size)
+            where T : unmanaged
+        {
+            using var buffer = Accelerator.Allocate<T>(size);
+            using var stream = Accelerator.CreateStream();
+            Accelerator.Initialize(stream, buffer.View, value);
+
+            stream.Synchronize();
+            var expected = Enumerable.Repeat(value, size).ToArray();
+            Verify(buffer, expected);
+        }
+    }
+}
---
+++ Src/ILGPU.Algorithms.Tests/VectorTests.cs
@@ -0,0 +1,190 @@
+﻿using ILGPU.Runtime;
+using ILGPU.Tests;
+using System.Numerics;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ILGPU.Algorithms.Tests
+{
+    public abstract partial class VectorTests : TestBase
+    {
+        protected VectorTests(ITestOutputHelper output, TestContext testContext)
+            : base(output, testContext)
+        { }
+
+        #region MemberData
+
+        public static TheoryData<object, object> Vector2dTestData =>
+            new TheoryData<object, object>
+            {
+                { 1, default(Vector2Zero) },
+                { 32, default(Vector2One) },
+                { 64, default(Vector2UnitX) },
+                { 128, default(Vector2UnitY) },
+            };
+
+        public static TheoryData<object, object> Vector3dTestData =>
+            new TheoryData<object, object>
+            {
+                { 1, default(Vector3Zero) },
+                { 32, default(Vector3One) },
+                { 64, default(Vector3UnitX) },
+                { 128, default(Vector3UnitY) },
+                { 65, default(Vector3UnitZ) },
+            };
+
+        public static TheoryData<object, object> Vector4dTestData =>
+            new TheoryData<object, object>
+            {
+                { 1, default(Vector4Zero) },
+                { 32, default(Vector4One) },
+                { 64, default(Vector4UnitX) },
+                { 128, default(Vector4UnitY) },
+                { 127, default(Vector4UnitZ) },
+                { 48, default(Vector4UnitW) },
+            };
+
+        #endregion
+
+        #region Kernels
+
+        internal static void Vector2dAddKernel(
+            Index1 index,
+            ArrayView<Vector2> input,
+            Vector2 operand)
+        {
+            var target = input.GetVariableView(index);
+            target.AtomicAdd(operand);
+        }
+
+        internal static void Vector3dAddKernel(
+            Index1 index,
+            ArrayView<Vector3> input,
+            Vector3 operand)
+        {
+            var target = input.GetVariableView(index);
+            target.AtomicAdd(operand);
+        }
+
+        internal static void Vector4dAddKernel(
+            Index1 index,
+            ArrayView<Vector4> input,
+            Vector4 operand)
+        {
+            var target = input.GetVariableView(index);
+            target.AtomicAdd(operand);
+        }
+
+        #endregion
+
+        [Theory]
+        [MemberData(nameof(Vector2dTestData))]
+        [KernelMethod(nameof(Vector2dAddKernel))]
+        public void Vector2dAdd<TVector>(int size, TVector vector)
+            where TVector : struct, IVector<Vector2>
+        {
+            using var stream = Accelerator.CreateStream();
+            using var targetBuffer = Accelerator.Allocate<Vector2>(size);
+
+            var sequencer = new Vector2DSequencer();
+            var sequence = sequencer.ComputeSequence(
+                new Vector2(0, size - 1),
+                new Vector2(1, -1),
+                size);
+            targetBuffer.CopyFrom(stream, sequence, 0, 0, size);
+            stream.Synchronize();
+            
+            Execute(targetBuffer.Length, targetBuffer.View, vector.GetVector());
+
+            var expected = new Vector2[size];
+            for (int i = 0; i < size; ++i)
+                expected[i] = new Vector2(i, size - 1 - i) + vector.GetVector();
+
+            Verify(targetBuffer, expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(Vector3dTestData))]
+        [KernelMethod(nameof(Vector3dAddKernel))]
+        public void Vector3dAdd<TVector>(int size, TVector vector)
+            where TVector : struct, IVector<Vector3>
+        {
+            using var targetBuffer = Accelerator.Allocate<Vector3>(size);
+            using var stream = Accelerator.CreateStream();
+
+            var sequencer = new Vector3DSequencer();
+            var sequence = sequencer.ComputeSequence(
+                new Vector3(0, size - 1, 0),
+                new Vector3(1, -1, 1),
+                size);
+            targetBuffer.CopyFrom(stream, sequence, 0, 0, size);
+            stream.Synchronize();
+
+            Execute(targetBuffer.Length, targetBuffer.View, vector.GetVector());
+
+            var expected = new Vector3[size];
+            for (int i = 0; i < size; ++i)
+                expected[i] = new Vector3(i, size - 1 - i, i) + vector.GetVector();
+
+            Verify(targetBuffer, expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(Vector4dTestData))]
+        [KernelMethod(nameof(Vector4dAddKernel))]
+        public void Vector4dAdd<TVector>(int size, TVector vector)
+            where TVector : struct, IVector<Vector4>
+        {
+            using var targetBuffer = Accelerator.Allocate<Vector4>(size);
+            using var stream = Accelerator.CreateStream();
+
+            var sequencer = new Vector4DSequencer();
+            var sequence = sequencer.ComputeSequence(
+                new Vector4(0, size - 1, 0, 0),
+                new Vector4(1, -1, 1, 0),
+                size);
+            targetBuffer.CopyFrom(stream, sequence, 0, 0, size);
+            stream.Synchronize();
+
+            Execute(targetBuffer.Length, targetBuffer.View, vector.GetVector());
+
+            var expected = new Vector4[size];
+            for (int i = 0; i < size; ++i)
+                expected[i] = new Vector4(i, size - 1 - i, i, 0) + vector.GetVector();
+
+            Verify(targetBuffer, expected);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 2)]
+        [InlineData(2, 1)]
+        public void Index2Vector2Conv(float x, float y)
+        {
+            Vector2 initVector = new Vector2(x, y);
+            Index2 initIndex = new Index2((int)x, (int)y);
+
+            Index2 index = initVector.ToIndex();
+            Vector2 vector = index.ToVector();
+
+            Assert.Equal(initVector, vector);
+            Assert.Equal(initIndex, index);
+        }
+
+        [Theory]
+        [InlineData(0,0,0)]
+        [InlineData(1,2,3)]
+        [InlineData(3,2,1)]
+        public void Index3Vector3Conv(float x, float y, float z)
+        {
+            Vector3 initVector = new Vector3(x, y, z);
+            Index3 initIndex = new Index3((int)x, (int)y, (int)z);
+
+            Index3 index = initVector.ToIndex();
+            Vector3 vector = index.ToVector();
+
+            Assert.Equal(initVector, vector);
+            Assert.Equal(initIndex, index);
+        }
+    }
+}
