4449062d3303a52ee8aaa75d924d6c715a768c92
Marcel Koester <m4rs@m4rs.net>
2020-10-21 01:21:27 +0200

    Added new PTXBlockSchedule implementation to compute and use PTX-specific block schedules.

---
+++ Src/ILGPU/Backends/PTX/Analyses/DefaultPTXBlockSchedule.cs
@@ -0,0 +1,67 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2016-2020 Marcel Koester
+//                                    www.ilgpu.net
+//
+// File: DefaultPTXBlockSchedule.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details
+// ---------------------------------------------------------------------------------------
+
+using ILGPU.IR;
+using ILGPU.IR.Analyses.ControlFlowDirection;
+using ILGPU.IR.Analyses.TraversalOrders;
+
+namespace ILGPU.Backends.PTX.Analyses
+{
+    /// <summary>
+    /// Represents a default PTX-specific block schedule.
+    /// </summary>
+    public sealed class DefaultPTXBlockSchedule :
+        PTXBlockSchedule<ReversePostOrder, Forwards>
+    {
+        #region Instance
+
+        /// <summary>
+        /// Constructs a new PTX block schedule.
+        /// </summary>
+        /// <param name="blocks">The underlying block collection.</param>
+        internal DefaultPTXBlockSchedule(
+            in BasicBlockCollection<ReversePostOrder, Forwards> blocks)
+            : base(blocks)
+        { }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Returns false to ensure that all branches will be generated.
+        /// </summary>
+        /// <returns>False.</returns>
+        public override bool IsImplicitSuccessor(
+            BasicBlock source,
+            BasicBlock successor) => false;
+
+        /// <summary>
+        /// Returns true to ensure that all blocks will receive a branch target.
+        /// </summary>
+        /// <returns>True.</returns>
+        public override bool NeedBranchTarget(BasicBlock block) => true;
+
+        #endregion
+    }
+
+    public partial class PTXBlockScheduleExtensions
+    {
+        /// <summary>
+        /// Creates a new default block schedule using the given blocks.
+        /// </summary>
+        /// <param name="blocks">The input blocks.</param>
+        /// <returns>The created block schedule.</returns>
+        public static PTXBlockSchedule CreateDefaultPTXSchedule(
+            this BasicBlockCollection<ReversePostOrder, Forwards> blocks) =>
+            new DefaultPTXBlockSchedule(blocks);
+    }
+}
