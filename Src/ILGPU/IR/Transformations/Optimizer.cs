6c0df847507663bcf99aada6a5ac3ffbb2347458
Marcel Koester <m4rs@m4rs.net>
2020-11-22 14:51:05 +0100

    Added new SSA structure conversion transformation to O2 pipeline.

--- Src/ILGPU/IR/Transformations/Optimizer.cs
+++ Src/ILGPU/IR/Transformations/Optimizer.cs
@@ -204,6 +204,7 @@ namespace ILGPU.IR.Transformations
             builder.Add(dce);
             builder.Add(scf);
 
+            builder.Add(new SSAStructureConstruction());
             builder.Add(new IfConversion());
             builder.Add(dce);
             builder.Add(scf);
