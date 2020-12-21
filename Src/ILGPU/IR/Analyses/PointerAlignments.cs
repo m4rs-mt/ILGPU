99b9acf62614e3e55c1f2168a3a1c569d3a01e43
Marcel Koester <m4rs@m4rs.net>
2020-12-21 06:38:25 +0100

    Adapted PointerAlignments analysis to the latest analysis framework updates.

--- Src/ILGPU/IR/Analyses/PointerAlignments.cs
+++ Src/ILGPU/IR/Analyses/PointerAlignments.cs
@@ -13,39 +13,49 @@ using ILGPU.IR.Analyses.ControlFlowDirection;
 using ILGPU.IR.Types;
 using ILGPU.IR.Values;
 using System;
-using System.Collections.Generic;
 
 namespace ILGPU.IR.Analyses
 {
     /// <summary>
     /// An analysis to determine safe alignment information for all pointer values.
     /// </summary>
-    public sealed class PointerAlignments
+    public class PointerAlignments : GlobalFixPointAnalysis<int, Forwards>
     {
         #region Nested Types
 
         /// <summary>
-        /// Stores alignment information of a specific method.
+        /// Stores alignment information of an alignment analysis run.
         /// </summary>
         public readonly struct AlignmentInfo
         {
-            internal AlignmentInfo(
-                Method method,
-                AnalysisValueMapping<int> alignments)
+            #region Static
+
+            /// <summary>
+            /// Empty allocation information.
+            /// </summary>
+            public static readonly AlignmentInfo Empty =
+                new AlignmentInfo(GlobalAnalysisValueResult<int>.Empty);
+
+            #endregion
+
+            #region Instance
+
+            /// <summary>
+            /// Constructs a new alignment analysis.
+            /// </summary>
+            internal AlignmentInfo(GlobalAnalysisValueResult<int> analysisResult)
             {
-                Method = method;
-                Alignments = alignments;
+                AnalysisResult = analysisResult;
             }
 
-            /// <summary>
-            /// Returns the underlying alignments object.
-            /// </summary>
-            private AnalysisValueMapping<int> Alignments { get; }
+            #endregion
+
+            #region Properties
 
             /// <summary>
-            /// Returns the associated method.
+            /// Stores a method value-alignment mapping.
             /// </summary>
-            public Method Method { get; }
+            public GlobalAnalysisValueResult<int> AnalysisResult { get; }
 
             /// <summary>
             /// Returns pointer alignment information for the given value.
@@ -53,82 +63,120 @@ namespace ILGPU.IR.Analyses
             /// <param name="value">The value to get alignment information for.</param>
             /// <returns>Pointer alignment in bytes (can be 1 byte).</returns>
             public readonly int this[Value value] =>
-                Alignments.TryGetValue(value, out var alignment)
-                ? alignment.Data
+                AnalysisResult.TryGetData(value, out var data)
+                ? data.Data
                 : 1;
-        }
-
-        /// <summary>
-        /// Models the internal pointer alignment analysis.
-        /// </summary>
-        private sealed class AnalysisImplementation :
-            GlobalFixPointAnalysis<int, Forwards>
-        {
-            private readonly AllocaAlignments allocaAlignments =
-                AllocaAlignments.Create();
 
             /// <summary>
-            /// Constructs a new analysis implementation.
+            /// Returns true if this alignment information object is empty.
             /// </summary>
-            /// <param name="globalAlignment">The global alignment information.</param>
-            public AnalysisImplementation(int globalAlignment)
-                : base(
-                      defaultValue: 1,
-                      initialValue: globalAlignment)
-            { }
+            public readonly bool IsEmpty => AnalysisResult.IsEmpty;
+
+            #endregion
+
+            #region Methods
 
             /// <summary>
-            /// Creates initial analysis data.
+            /// Returns the alignment information determined and used for the given
+            /// alloca.
             /// </summary>
-            protected override AnalysisValue<int> CreateData(Value node) =>
-                Create(
-                    node is Alloca alloca
-                        ? allocaAlignments.ComputeAllocaAlignment(alloca)
-                        : GetInitialAlignment(node),
-                    node.Type);
+            /// <param name="alloca">
+            /// The alloca to get the alignment information for.
+            /// </param>
+            /// <returns>The determined and used alignment in bytes.</returns>
+            public readonly int GetAllocaAlignment(Alloca alloca) =>
+                AnalysisResult.TryGetData(alloca, out var data)
+                ? data.Data
+                : AllocaAlignments.GetInitialAlignment(alloca);
 
             /// <summary>
-            /// Returns the minimum of the first and the second value.
+            /// Returns safe alignment information.
             /// </summary>
-            protected override int Merge(int first, int second) =>
-                Math.Min(first, second);
+            /// <param name="value">
+            /// The value for which to compute the alignment for.
+            /// </param>
+            /// <param name="safeMinAlignment">
+            /// The safe minimum alignment in bytes.
+            /// </param>
+            /// <returns>The computed alignment.</returns>
+            public readonly int GetAlignment(Value value, int safeMinAlignment) =>
+                Math.Max(this[value], safeMinAlignment);
 
             /// <summary>
-            /// Returns merged information about <see cref="LoadFieldAddress"/> and
-            /// <see cref="LoadElementAddress"/> IR nodes.
+            /// Returns safe alignment information.
             /// </summary>
-            protected override AnalysisValue<int>? TryMerge<TContext>(
+            /// <param name="value">
+            /// The value for which to compute the alignment for.
+            /// </param>
+            /// <param name="safeMinTypeAlignment">
+            /// The safe minimum type alignment.
+            /// </param>
+            /// <returns>The computed alignment.</returns>
+            public readonly int GetAlignment(
                 Value value,
-                TContext context) =>
-                value switch
-                {
-                    LoadFieldAddress lfa =>
-                        Create(
-                            Math.Min(
-                                context[lfa.Source].Data,
-                                lfa.StructureType[lfa.FieldSpan.Access].Alignment),
-                                lfa.Type),
-                    LoadElementAddress lea =>
-                        Create(
-                            Math.Max(
-                                context[lea.Source].Data,
-                                (lea.Type as IAddressSpaceType).ElementType.Alignment),
-                            lea.Type),
-                    _ => null,
-                };
+                TypeNode safeMinTypeAlignment) =>
+                GetAlignment(value, safeMinTypeAlignment.Alignment);
 
-            /// <summary>
-            /// Creates alignment information for global pointer and view types.
-            /// </summary>
-            protected override AnalysisValue<int>? TryProvide(TypeNode typeNode) =>
-                typeNode is IAddressSpaceType
-                ? Create(InitialValue, typeNode)
-                : (AnalysisValue<int>?)null;
+            #endregion
         }
 
         #endregion
 
         #region Static
+
+        /// <summary>
+        /// Creates a new alignment analysis.
+        /// </summary>
+        /// <param name="globalAlignment">
+        /// The initial alignment information of all pointers and views of the root
+        /// method.
+        /// </param>
+        public static PointerAlignments Create(int globalAlignment) =>
+            new PointerAlignments(globalAlignment);
+
+        /// <summary>
+        /// Applies a new alignment analysis to the given root method.
+        /// </summary>
+        /// <param name="rootMethod">The root (entry) method.</param>
+        /// <param name="globalAlignment">
+        /// The initial alignment information of all pointers and views of the root
+        /// method.
+        /// </param>
+        public static AlignmentInfo Apply(Method rootMethod, int globalAlignment)
+        {
+            var analysis = Create(globalAlignment);
+            var result = analysis.AnalyzeGlobalMethod(rootMethod, globalAlignment);
+            return new AlignmentInfo(result);
+        }
+
+        #endregion
+
+        #region Instance
+
+        private readonly AllocaAlignments allocaAlignments = AllocaAlignments.Create();
+
+        /// <summary>
+        /// Constructs a new analysis implementation.
+        /// </summary>
+        /// <param name="globalAlignment">The global alignment information.</param>
+        protected PointerAlignments(int globalAlignment)
+            : base(defaultValue: 1)
+        {
+            GlobalAlignment = globalAlignment;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Returns the global alignment in bytes.
+        /// </summary>
+        public int GlobalAlignment { get; }
+
+        #endregion
+
+        #region Methods
 
         /// <summary>
         /// Returns initial and unconstrained alignment information.
@@ -165,103 +213,53 @@ namespace ILGPU.IR.Analyses
         }
 
         /// <summary>
-        /// An empty value mapping.
+        /// Creates initial analysis data.
         /// </summary>
-        private readonly static AnalysisValueMapping<int> EmptyMapping =
-            AnalysisValueMapping.Create<int>();
+        protected override AnalysisValue<int> CreateData(Value node) =>
+            CreateValue(
+                node is Alloca alloca
+                    ? allocaAlignments.ComputeAllocaAlignment(alloca)
+                    : GetInitialAlignment(node),
+                node.Type);
 
         /// <summary>
-        /// Represents no pointer alignment information.
+        /// Returns the minimum of the first and the second value.
         /// </summary>
-        public static readonly PointerAlignments Empty = new PointerAlignments();
+        protected override int Merge(int first, int second) =>
+            Math.Min(first, second);
 
         /// <summary>
-        /// Creates a new alignment analysis.
+        /// Returns merged information about <see cref="LoadFieldAddress"/> and
+        /// <see cref="LoadElementAddress"/> IR nodes.
         /// </summary>
-        /// <param name="rootMethod">The root (entry) method.</param>
-        /// <param name="globalAlignment">
-        /// The initial alignment information of all pointers and views of the root
-        /// method.
-        /// </param>
-        public static PointerAlignments Create(Method rootMethod, int globalAlignment) =>
-            new PointerAlignments(rootMethod, globalAlignment);
-
-        #endregion
-
-        #region Instance
+        protected override AnalysisValue<int>? TryMerge<TContext>(
+            Value value,
+            TContext context) =>
+            value switch
+            {
+                LoadFieldAddress lfa =>
+                    CreateValue(
+                        Math.Min(
+                            context[lfa.Source].Data,
+                            lfa.StructureType[lfa.FieldSpan.Access].Alignment),
+                            lfa.Type),
+                LoadElementAddress lea =>
+                    CreateValue(
+                        Math.Max(
+                            context[lea.Source].Data,
+                            (lea.Type as IAddressSpaceType).ElementType.Alignment),
+                        lea.Type),
+                _ => null,
+            };
 
         /// <summary>
-        /// Stores a method value-alignment mapping.
+        /// Creates alignment information for global pointer and view types.
         /// </summary>
-        private readonly Dictionary<Method, AnalysisValueMapping<int>> alignments;
-
-        /// <summary>
-        /// Constructs an empty pointer alignment analysis.
-        /// </summary>
-        private PointerAlignments()
-        {
-            alignments = new Dictionary<Method, AnalysisValueMapping<int>>();
-        }
-
-        /// <summary>
-        /// Constructs a new alignment analysis.
-        /// </summary>
-        private PointerAlignments(Method rootMethod, int globalAlignment)
-        {
-            var impl = new AnalysisImplementation(globalAlignment);
-            alignments = impl.AnalyzeGlobal(rootMethod);
-        }
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Returns pointer alignment information for the given method.
-        /// </summary>
-        /// <param name="method">The method to get alignment information for.</param>
-        /// <returns>Resolved pointer alignment information.</returns>
-        public AlignmentInfo this[Method method] =>
-            alignments.TryGetValue(method, out var result)
-            ? new AlignmentInfo(method, result)
-            : new AlignmentInfo(method, EmptyMapping);
-
-        /// <summary>
-        /// Returns pointer alignment information for the given value.
-        /// </summary>
-        /// <param name="value">The value to get alignment information for.</param>
-        /// <returns>Pointer alignment in bytes (can be 1 byte).</returns>
-        public int this[Value value] => this[value.Method][value];
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Returns the alignment information determined and used for the given alloca.
-        /// </summary>
-        /// <param name="alloca">The alloca to get the alignment information for.</param>
-        /// <returns>The determined and used alignment in bytes.</returns>
-        public int GetAllocaAlignment(Alloca alloca) => this[alloca];
-
-        /// <summary>
-        /// Returns safe alignment information.
-        /// </summary>
-        /// <param name="value">The value for which to compute the alignment for.</param>
-        /// <param name="safeMinAlignment">The safe minimum alignment in bytes.</param>
-        /// <returns>The computed alignment.</returns>
-        public int GetAlignment(Value value, int safeMinAlignment) =>
-            Math.Max(this[value], safeMinAlignment);
-
-        /// <summary>
-        /// Returns safe alignment information.
-        /// </summary>
-        /// <param name="value">The value for which to compute the alignment for.</param>
-        /// <param name="safeMinTypeAlignment">The safe minimum type alignment.</param>
-        /// <returns>The computed alignment.</returns>
-        public int GetAlignment(Value value, TypeNode safeMinTypeAlignment) =>
-            GetAlignment(value, safeMinTypeAlignment.Alignment);
-
-        #endregion
+        protected override AnalysisValue<int>? TryProvide(TypeNode typeNode) =>
+            typeNode is IAddressSpaceType
+            ? CreateValue(GlobalAlignment, typeNode)
+            : default(AnalysisValue<int>?);
     }
+
+    #endregion
 }
