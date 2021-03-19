34fa37838752bb818c1f2daf842f105c987188e2
MoFtZ <MoFtZ@users.noreply.github.com>
2021-03-20 10:29:13 +1100

    Added support for formattable strings in OpenCL code generator.

---
+++ Src/ILGPU/Util/RawString.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2016-2020 Marcel Koester
+//                                    www.ilgpu.net
+//
+// File: RawString.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details
+// ---------------------------------------------------------------------------------------
+
+using System;
+
+// disable: max_line_length
+
+namespace ILGPU.Util
+{
+    /// <summary>
+    /// Helper to coerce the C# compiler into preferencing the FormattableString
+    /// overload of a function, and to also accept a regular string.
+    ///
+    /// https://www.damirscorner.com/blog/posts/20180921-FormattableStringAsMethodParameter.html
+    /// </summary>
+    public struct RawString
+    {
+        /// <summary>
+        /// The string value.
+        /// </summary>
+        public string Value { get; }
+
+        private RawString(string value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Implicit conversion from string.
+        /// </summary>
+        /// <param name="value">The string value.</param>
+        public static implicit operator RawString(string value) =>
+            new RawString(value);
+
+        /// <summary>
+        /// Implicit conversion from <see cref="FormattableString"/>.
+        /// </summary>
+        /// <remarks>
+        /// This should not be used, but is necessary to coerce the C# compiler.
+        /// </remarks>
+        /// <param name="value">The string value.</param>
+        public static implicit operator RawString(FormattableString value) =>
+            new RawString(value.ToString());
+    }
+}
