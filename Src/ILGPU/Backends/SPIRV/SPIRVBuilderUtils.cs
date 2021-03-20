d133c73a39c4594aa0ae3a5c32c5604cbb07b735
Yey007 <yey007tech@gmail.com>
2021-03-19 23:23:20 -0400

    Working SPIRVBuilder

--- Src/ILGPU/Backends/SPIRV/SPIRVBuilderUtils.cs
+++ Src/ILGPU/Backends/SPIRV/SPIRVBuilderUtils.cs
@@ -2,11 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 
 namespace ILGPU.Backends.SPIRV
 {
+    /// <summary>
+    /// Utilities for generated functions in SPIRVBuilder.cs
+    /// </summary>
     public static class SPIRVBuilderUtils
     {
+        /// <summary>
+        /// Attempts to convert an object to a SPIRV compatible representation
+        /// </summary>
+        /// <param name="obj">The object to convert</param>
+        /// <returns>A list of words (uints) representing the object</returns>
+        /// <exception cref="NotImplementedException"></exception>
         public static List<uint> ToUintList(object obj)
         {
             if (obj == null)
@@ -24,7 +34,7 @@ namespace ILGPU.Backends.SPIRV
                     BindingFlags.Public))
                 {
                     // Infinite loop is impossible because all spirv structs have primitive bases
-                    list.AddRange(ToUintList(field));
+                    list.AddRange(ToUintList(field.GetValue(obj)));
                 }
 
                 return list;
@@ -35,8 +45,9 @@ namespace ILGPU.Backends.SPIRV
                 case uint u:
                     return new List<uint> {u};
                 case string s:
-                    var bytes = System.Text.Encoding.Unicode.GetBytes(s + "\0");
-                    uint[] uints = new uint[bytes.Length / 4];
+                    var bytes = Encoding.UTF8.GetBytes(s + "\0");
+                    int roundedUp = (bytes.Length - 1) / 4 + 1;
+                    uint[] uints = new uint[roundedUp];
                     Buffer.BlockCopy(bytes, 0, uints, 0, bytes.Length);
                     return uints.ToList();
                 case Array a:
@@ -52,6 +63,22 @@ namespace ILGPU.Backends.SPIRV
                 default:
                     throw new NotImplementedException();
             }
+        }
+
+        /// <summary>
+        /// Joins an Opcode and word count into one word
+        /// </summary>
+        /// <param name="opCode"></param>
+        /// <param name="wordCount"></param>
+        /// <returns></returns>
+        public static uint JoinOpCodeWordCount(ushort opCode, ushort wordCount)
+        {
+            uint opCodeUint = opCode;
+            uint wordCountUint = wordCount;
+
+            uint shiftedWordCount = wordCountUint << 16;
+
+            return shiftedWordCount | opCodeUint;
         }
     }
 }
