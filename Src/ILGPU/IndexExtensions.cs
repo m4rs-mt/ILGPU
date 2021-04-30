119f47328091e8216193be513309d56f71dd0d75
Marcel Koester <m4rs@m4rs.net>
2021-04-30 02:20:37 +0200

    Removed implicit long->int index casts (#451).

--- Src/ILGPU/IndexExtensions.cs
+++ Src/ILGPU/IndexExtensions.cs
@@ -11,28 +11,6 @@
 
 namespace ILGPU
 {
-    partial struct Index1D
-    {
-        /// <summary>
-        /// Converts the given 64-bit index into its 32-bit representation.
-        /// </summary>
-        /// <param name="index">The long index value.</param>
-        /// <remarks>Required due to backwards compatibility.</remarks>
-        public static explicit operator Index1D(long index) =>
-            new LongIndex1D(index).ToIntIndex();
-    }
-
-    partial struct Index2D
-    {
-        /// <summary>
-        /// Converts the given 64-bit index into its 32-bit representation.
-        /// </summary>
-        /// <param name="index">The long index value.</param>
-        /// <remarks>Required due to backwards compatibility.</remarks>
-        public static explicit operator Index2D(LongIndex2D index) =>
-            index.ToIntIndex();
-    }
-
     partial struct Index3D
     {
         /// <summary>
@@ -62,14 +40,6 @@ namespace ILGPU
         /// Returns the YZ components.
         /// </summary>
         public readonly Index2D YZ => new Index2D(Y, Z);
-
-        /// <summary>
-        /// Converts the given 64-bit index into its 32-bit representation.
-        /// </summary>
-        /// <param name="index">The long index value.</param>
-        /// <remarks>Required due to backwards compatibility.</remarks>
-        public static explicit operator Index3D(LongIndex3D index) =>
-            index.ToIntIndex();
     }
 
     partial struct LongIndex3D
