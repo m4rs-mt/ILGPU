9158314821aefa4fa931db0985fddd7b17d77c62
Marcel Koester <m4rs@m4rs.net>
2021-04-30 02:20:46 +0200

    Added new CudaMemoryBuffer allocation support to Cuda runtime.

--- Src/ILGPU/Runtime/Cuda/CudaAccelerator.cs
+++ Src/ILGPU/Runtime/Cuda/CudaAccelerator.cs
@@ -28,6 +28,15 @@ namespace ILGPU.Runtime.Cuda
     public sealed class CudaAccelerator :
         KernelAccelerator<PTXCompiledKernel, CudaKernel>
     {
+        #region Constants
+
+        /// <summary>
+        /// The default pitched allocation alignment in bytes (128) for all Cuda devices.
+        /// </summary>
+        public static readonly int PitchedAllocationAlignmentInBytes = 128;
+
+        #endregion
+
         #region Static
 
         /// <summary>
@@ -356,30 +365,25 @@ namespace ILGPU.Runtime.Cuda
 
         #region Methods
 
-        /// <summary cref="Accelerator.CreateExtension{TExtension, TExtensionProvider}(
-        /// TExtensionProvider)"/>
+        /// <inheritdoc/>
         public override TExtension CreateExtension<TExtension, TExtensionProvider>(
             TExtensionProvider provider) =>
             provider.CreateCudaExtension(this);
 
-        /// <summary cref="Accelerator.Allocate{T, TIndex}(TIndex)"/>
-        protected override MemoryBuffer<T, TIndex> AllocateInternal<T, TIndex>(
-            TIndex extent) =>
-            new CudaMemoryBuffer<T, TIndex>(this, extent);
-
-        /// <summary cref="KernelAccelerator{TCompiledKernel, TKernel}.CreateKernel(
-        /// TCompiledKernel)"/>
+        /// <inheritdoc/>
         protected override CudaKernel CreateKernel(PTXCompiledKernel compiledKernel) =>
             new CudaKernel(this, compiledKernel, null);
 
-        /// <summary cref="KernelAccelerator{TCompiledKernel, TKernel}.CreateKernel(
-        /// TCompiledKernel, MethodInfo)"/>
+        /// <inheritdoc/>
         protected override CudaKernel CreateKernel(
             PTXCompiledKernel compiledKernel,
             MethodInfo launcher) =>
             new CudaKernel(this, compiledKernel, launcher);
 
-        /// <summary cref="Accelerator.CreateStream()"/>
+        /// <summary>
+        /// Create a Cuda stream with the flag
+        /// <see cref="StreamFlags.CU_STREAM_NON_BLOCKING"/>.
+        /// </summary>
         protected override AcceleratorStream CreateStreamInternal() =>
             new CudaStream(this, StreamFlags.CU_STREAM_NON_BLOCKING);
 
@@ -428,6 +432,48 @@ namespace ILGPU.Runtime.Cuda
                 CurrentAPI.GetMemoryInfo(out long free, out long _));
             return free;
         }
+
+        #endregion
+
+        #region Allocation
+
+        /// <inheritdoc/>
+        protected override MemoryBuffer AllocateRawInternal(
+            long length,
+            int elementSize) =>
+            new CudaMemoryBuffer(this, length, elementSize);
+
+        /// <summary>
+        /// Allocates a pitched 2D buffer with X being the leading dimension using an
+        /// alignment of <see cref="PitchedAllocationAlignmentInBytes"/>.
+        /// </summary>
+        /// <typeparam name="T">The element type.</typeparam>
+        /// <param name="extent">The number of elements to allocate.</param>
+        /// <returns>An allocated 2D buffer on the this accelerator.</returns>
+        /// <remarks>
+        /// Since X is the leading dimension, X must be less or equal to
+        /// <see cref="int.MaxValue"/>.
+        /// </remarks>
+        public MemoryBuffer<ArrayView2D<T, Stride2D.DenseX>> Allocate2DPitchedX<T>(
+            LongIndex2 extent)
+            where T : unmanaged =>
+            Allocate2DPitchedX<T>(extent, PitchedAllocationAlignmentInBytes);
+
+        /// <summary>
+        /// Allocates a pitched 2D buffer with Y being the leading dimension using an
+        /// alignment of <see cref="PitchedAllocationAlignmentInBytes"/>.
+        /// </summary>
+        /// <typeparam name="T">The element type.</typeparam>
+        /// <param name="extent">The number of elements to allocate.</param>
+        /// <returns>An allocated 2D buffer on the this accelerator.</returns>
+        /// <remarks>
+        /// Since Y is the leading dimension, Y must be less or equal to
+        /// <see cref="int.MaxValue"/>.
+        /// </remarks>
+        public MemoryBuffer<ArrayView2D<T, Stride2D.DenseY>> Allocate2DPitchedY<T>(
+            LongIndex2 extent)
+            where T : unmanaged =>
+            Allocate2DPitchedY<T>(extent, PitchedAllocationAlignmentInBytes);
 
         #endregion
 
--- Src/ILGPU/Runtime/Cuda/CudaMemoryBuffer.cs
+++ Src/ILGPU/Runtime/Cuda/CudaMemoryBuffer.cs
@@ -18,26 +18,108 @@ namespace ILGPU.Runtime.Cuda
     /// <summary>
     /// Represents an unmanaged Cuda buffer.
     /// </summary>
-    /// <typeparam name="T">The element type.</typeparam>
-    /// <typeparam name="TIndex">The index type.</typeparam>
-    public sealed class CudaMemoryBuffer<T, TIndex> : MemoryBuffer<T, TIndex>
-        where T : unmanaged
-        where TIndex : unmanaged, IIndex, IGenericIndex<TIndex>
+    public sealed class CudaMemoryBuffer : MemoryBuffer
     {
+        #region Static
+
+        /// <summary>
+        /// Performs a Cuda memset operation.
+        /// </summary>
+        /// <typeparam name="T">The element type.</typeparam>
+        /// <param name="stream">The Cuda stream to use (must not be null)</param>
+        /// <param name="value">The value to write into the buffer.</param>
+        /// <param name="targetView">The target view to write to.</param>
+        public static void CudaMemSet<T>(
+            CudaStream stream,
+            byte value,
+            in ArrayView<T> targetView)
+            where T : unmanaged
+        {
+            if (stream is null)
+                throw new ArgumentNullException(nameof(stream));
+
+            if (targetView.GetAcceleratorType() != AcceleratorType.Cuda)
+            {
+                throw new NotSupportedException(
+                    RuntimeErrorMessages.NotSupportedTargetAccelerator);
+            }
+
+            var binding = stream.Accelerator.BindScoped();
+
+            CudaException.ThrowIfFailed(
+                CurrentAPI.Memset(
+                    targetView.LoadEffectiveAddressAsPtr(),
+                    value,
+                    new IntPtr(targetView.LengthInBytes),
+                    stream));
+
+            binding.Recover();
+        }
+
+        /// <summary>
+        /// Performs a Cuda copy operation.
+        /// </summary>
+        /// <typeparam name="T">The element type.</typeparam>
+        /// <param name="stream">The Cuda stream to use (must not be null)</param>
+        /// <param name="sourceView">The source view to copy from.</param>
+        /// <param name="targetView">The target view to copy to.</param>
+        public static void CudaCopy<T>(
+            CudaStream stream,
+            in ArrayView<T> sourceView,
+            in ArrayView<T> targetView)
+            where T : unmanaged
+        {
+            if (stream is null)
+                throw new ArgumentNullException(nameof(stream));
+
+            using var binding = stream.Accelerator.BindScoped();
+
+            var sourceType = sourceView.GetAcceleratorType();
+            var targetType = targetView.GetAcceleratorType();
+
+            if (sourceType == AcceleratorType.OpenCL ||
+                targetType == AcceleratorType.OpenCL)
+            {
+                throw new NotSupportedException(
+                    RuntimeErrorMessages.NotSupportedTargetAccelerator);
+            }
+
+            var sourceAddress = sourceView.LoadEffectiveAddressAsPtr();
+            var targetAddress = targetView.LoadEffectiveAddressAsPtr();
+
+            var length = new IntPtr(targetView.LengthInBytes);
+
+            // a) Copy from CPU to GPU
+            // b) Copy from GPU to CPU
+            // c) Copy from GPU to GPU
+            CudaException.ThrowIfFailed(
+                CurrentAPI.MemcpyAsync(
+                    targetAddress,
+                    sourceAddress,
+                    length,
+                    stream));
+        }
+
+        #endregion
+
         #region Instance
 
         /// <summary>
         /// Constructs a new Cuda buffer.
         /// </summary>
         /// <param name="accelerator">The accelerator.</param>
-        /// <param name="extent">The extent.</param>
-        internal CudaMemoryBuffer(CudaAccelerator accelerator, TIndex extent)
-            : base(accelerator, extent)
+        /// <param name="length">The length of this buffer.</param>
+        /// <param name="elementSize">The element size.</param>
+        public CudaMemoryBuffer(
+            CudaAccelerator accelerator,
+            long length,
+            int elementSize)
+            : base(accelerator, length, elementSize)
         {
             CudaException.ThrowIfFailed(
                 CurrentAPI.AllocateMemory(
                     out IntPtr resultPtr,
-                    new IntPtr(extent.Size * ElementSize)));
+                    new IntPtr(LengthInBytes)));
             NativePtr = resultPtr;
         }
 
@@ -45,86 +127,35 @@ namespace ILGPU.Runtime.Cuda
 
         #region Methods
 
-        /// <summary cref="MemoryBuffer{T, TIndex}.CopyToView(
-        /// AcceleratorStream, ArrayView{T}, LongIndex1)"/>
-        protected internal unsafe override void CopyToView(
+        /// <inheritdoc/>
+        public override unsafe void MemSet(
             AcceleratorStream stream,
-            ArrayView<T> target,
-            LongIndex1 sourceOffset)
+            byte value,
+            long targetOffsetInBytes,
+            long length)
         {
-            var binding = Accelerator.BindScoped();
-
-            var targetBuffer = target.Source;
-            var sourceAddress = new IntPtr(ComputeEffectiveAddress(sourceOffset));
-            var targetAddress = new IntPtr(target.LoadEffectiveAddress());
-            var lengthInBytes = new IntPtr(target.LengthInBytes);
-            switch (targetBuffer.AcceleratorType)
-            {
-                case AcceleratorType.CPU:
-                case AcceleratorType.Cuda:
-                    CudaException.ThrowIfFailed(
-                        CurrentAPI.MemcpyAsync(
-                            targetAddress,
-                            sourceAddress,
-                            lengthInBytes,
-                            stream));
-                    break;
-                default:
-                    throw new NotSupportedException(
-                        RuntimeErrorMessages.NotSupportedTargetAccelerator);
-            }
-
-            binding.Recover();
-        }
-
-        /// <summary cref="MemoryBuffer{T, TIndex}.CopyFromView(
-        /// AcceleratorStream, ArrayView{T}, LongIndex1)"/>
-        protected internal unsafe override void CopyFromView(
-            AcceleratorStream stream,
-            ArrayView<T> source,
-            LongIndex1 targetOffset)
-        {
-            var binding = Accelerator.BindScoped();
-
-            var sourceAddress = new IntPtr(source.LoadEffectiveAddress());
-            var targetAddress = new IntPtr(ComputeEffectiveAddress(targetOffset));
-            var lengthInBytes = new IntPtr(source.LengthInBytes);
-            switch (source.AcceleratorType)
-            {
-                case AcceleratorType.CPU:
-                case AcceleratorType.Cuda:
-                    CudaException.ThrowIfFailed(
-                        CurrentAPI.MemcpyAsync(
-                            targetAddress,
-                            sourceAddress,
-                            lengthInBytes,
-                            stream));
-                    break;
-                default:
-                    throw new NotSupportedException(
-                        RuntimeErrorMessages.NotSupportedTargetAccelerator);
-            }
-
-            binding.Recover();
+            var targetView = AsRawArrayView(targetOffsetInBytes, length);
+            CudaMemSet(stream as CudaStream, value, targetView);
         }
 
         /// <inheritdoc/>
-        protected internal override unsafe void MemSetInternal(
+        public override void CopyFrom(
             AcceleratorStream stream,
-            byte value,
-            long offsetInBytes,
-            long lengthInBytes)
+            in ArrayView<byte> sourceView,
+            long targetOffsetInBytes)
         {
-            var binding = Accelerator.BindScoped();
+            var targetView = AsRawArrayView(targetOffsetInBytes);
+            CudaCopy(stream as CudaStream, sourceView, targetView);
+        }
 
-            CudaException.ThrowIfFailed(
-                CurrentAPI.Memset(
-                    new IntPtr(NativePtr.ToInt64() + offsetInBytes),
-                    value,
-                    new IntPtr(lengthInBytes),
-                    stream));
-
-            binding.Recover();
+        /// <inheritdoc/>
+        public override unsafe void CopyTo(
+            AcceleratorStream stream,
+            long sourceOffsetInBytes,
+            in ArrayView<byte> targetView)
+        {
+            var sourceView = AsRawArrayView(sourceOffsetInBytes);
+            CudaCopy(stream as CudaStream, sourceView, targetView);
         }
 
         #endregion
