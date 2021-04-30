3fdf3d08f86ff4de1e557142fa2abc109a771db4
Marcel Koester <m4rs@m4rs.net>
2021-04-30 02:20:37 +0200

    Renamed index types to make all type names consistent.

--- Src/ILGPU/IndexExtensions.cs
+++ Src/ILGPU/IndexExtensions.cs
@@ -11,36 +11,36 @@
 
 namespace ILGPU
 {
-    partial struct Index1
+    partial struct Index1D
     {
         /// <summary>
         /// Converts the given 64-bit index into its 32-bit representation.
         /// </summary>
         /// <param name="index">The long index value.</param>
         /// <remarks>Required due to backwards compatibility.</remarks>
-        public static implicit operator Index1(long index) =>
-            new LongIndex1(index).ToIntIndex();
+        public static explicit operator Index1D(long index) =>
+            new LongIndex1D(index).ToIntIndex();
     }
 
-    partial struct Index2
+    partial struct Index2D
     {
         /// <summary>
         /// Converts the given 64-bit index into its 32-bit representation.
         /// </summary>
         /// <param name="index">The long index value.</param>
         /// <remarks>Required due to backwards compatibility.</remarks>
-        public static implicit operator Index2(LongIndex2 index) =>
+        public static explicit operator Index2D(LongIndex2D index) =>
             index.ToIntIndex();
     }
 
-    partial struct Index3
+    partial struct Index3D
     {
         /// <summary>
         /// Constructs a new index.
         /// </summary>
         /// <param name="xy">The x and y values.</param>
         /// <param name="z">The z value.</param>
-        public Index3(Index2 xy, int z)
+        public Index3D(Index2D xy, int z)
             : this(xy.X, xy.Y, z)
         { }
 
@@ -49,37 +49,37 @@ namespace ILGPU
         /// </summary>
         /// <param name="x">The x value.</param>
         /// <param name="yz">The x and y values.</param>
-        public Index3(int x, Index2 yz)
+        public Index3D(int x, Index2D yz)
             : this(x, yz.X, yz.Y)
         { }
 
         /// <summary>
         /// Returns the XY components.
         /// </summary>
-        public readonly Index2 XY => new Index2(X, Y);
+        public readonly Index2D XY => new Index2D(X, Y);
 
         /// <summary>
         /// Returns the YZ components.
         /// </summary>
-        public readonly Index2 YZ => new Index2(Y, Z);
+        public readonly Index2D YZ => new Index2D(Y, Z);
 
         /// <summary>
         /// Converts the given 64-bit index into its 32-bit representation.
         /// </summary>
         /// <param name="index">The long index value.</param>
         /// <remarks>Required due to backwards compatibility.</remarks>
-        public static implicit operator Index3(LongIndex3 index) =>
+        public static explicit operator Index3D(LongIndex3D index) =>
             index.ToIntIndex();
     }
 
-    partial struct LongIndex3
+    partial struct LongIndex3D
     {
         /// <summary>
         /// Constructs a new index.
         /// </summary>
         /// <param name="xy">The x and y values.</param>
         /// <param name="z">The z value.</param>
-        public LongIndex3(LongIndex2 xy, long z)
+        public LongIndex3D(LongIndex2D xy, long z)
             : this(xy.X, xy.Y, z)
         { }
 
@@ -88,18 +88,18 @@ namespace ILGPU
         /// </summary>
         /// <param name="x">The x value.</param>
         /// <param name="yz">The x and y values.</param>
-        public LongIndex3(long x, LongIndex2 yz)
+        public LongIndex3D(long x, LongIndex2D yz)
             : this(x, yz.X, yz.Y)
         { }
 
         /// <summary>
         /// Returns the XY components.
         /// </summary>
-        public readonly LongIndex2 XY => new LongIndex2(X, Y);
+        public readonly LongIndex2D XY => new LongIndex2D(X, Y);
 
         /// <summary>
         /// Returns the YZ components.
         /// </summary>
-        public readonly LongIndex2 YZ => new LongIndex2(Y, Z);
+        public readonly LongIndex2D YZ => new LongIndex2D(Y, Z);
     }
 }
