5b695c83341a412654e2284b1909b92376f3c89d
Marcel Koester <m4rs@m4rs.net>
2021-04-30 02:20:47 +0200

    Removed obsolete allocation methods from LocalMemory and SharedMemory classes.

--- Src/ILGPU/LocalMemory.cs
+++ Src/ILGPU/LocalMemory.cs
@@ -10,7 +10,7 @@
 // ---------------------------------------------------------------------------------------
 
 using ILGPU.Frontend.Intrinsic;
-using ILGPU.Runtime;
+using ILGPU.Runtime.CPU;
 using System;
 using System.Diagnostics;
 using System.Reflection;
@@ -21,10 +21,10 @@ namespace ILGPU
     /// <summary>
     /// Contains methods to allocate and manage local memory.
     /// </summary>
-    public static class LocalMemory
+    public static partial class LocalMemory
     {
         /// <summary>
-        /// A readonly reference to the <see cref="AllocateZero{T}(long)"/> method.
+        /// A readonly reference to the <see cref="AllocateZero{T}(int)"/> method.
         /// </summary>
         private static readonly MethodInfo AllocateZeroMethod =
             typeof(LocalMemory).GetMethod(
@@ -32,7 +32,7 @@ namespace ILGPU
                 BindingFlags.NonPublic | BindingFlags.Static);
 
         /// <summary>
-        /// Creates a typed <see cref="AllocateZero{T}(long)"/> method instance to invoke.
+        /// Creates a typed <see cref="AllocateZero{T}(int)"/> method instance to invoke.
         /// </summary>
         /// <param name="elementType">The array element type.</param>
         /// <returns>The typed method instance.</returns>
@@ -46,7 +46,7 @@ namespace ILGPU
         /// <param name="extent">The extent (number of elements to allocate).</param>
         /// <returns>An allocated region of local memory.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static ArrayView<T> AllocateZero<T>(long extent)
+        private static ArrayView<T> AllocateZero<T>(int extent)
             where T : unmanaged
         {
             Trace.Assert(extent >= 0, "Invalid extent");
@@ -63,86 +63,15 @@ namespace ILGPU
         /// <returns>An allocated element in local memory.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [LocalMemoryIntrinsic(LocalMemoryIntrinsicKind.Allocate)]
-        public static ArrayView<T> Allocate<T>(long extent)
+        public static ArrayView<T> Allocate<T>(int extent)
             where T : unmanaged
         {
             Trace.Assert(extent >= 0, "Invalid extent");
+            int elementSize = Interop.SizeOf<T>();
             return new ArrayView<T>(
-                UnmanagedMemoryViewSource.Create(
-                    Interop.SizeOf<T>() * extent),
+                CPUMemoryBuffer.Create(extent * elementSize, elementSize),
                 0,
                 extent);
         }
-
-        /// <summary>
-        /// Allocates a chunk of local memory with the specified number of elements.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <typeparam name="TIndex">The index type.</typeparam>
-        /// <param name="extent">The extent (number of elements to allocate).</param>
-        /// <returns>An allocated region of local memory.</returns>
-        public static ArrayView<T, TIndex> Allocate<T, TIndex>(TIndex extent)
-            where T : unmanaged
-            where TIndex : unmanaged, IIntIndex, IGenericIndex<TIndex>
-        {
-            var baseView = Allocate<T>((long)extent.Size);
-            return new ArrayView<T, TIndex>(baseView, extent);
-        }
-
-        /// <summary>
-        /// Allocates a 1D chunk of local memory with the specified number of elements.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="length">The number of elements to allocate.</param>
-        /// <returns>An allocated region of local memory.</returns>
-        public static ArrayView<T> Allocate<T>(Index1 length)
-            where T : unmanaged =>
-            Allocate<T, Index1>(length).AsLinearView();
-
-        /// <summary>
-        /// Allocates a 2D chunk of local memory with the specified number of elements.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="width">The width of the 2D buffer.</param>
-        /// <param name="height">The height of the 2D buffer.</param>
-        /// <returns>An allocated region of local memory.</returns>
-        public static ArrayView2D<T> Allocate2D<T>(Index1 width, Index1 height)
-            where T : unmanaged =>
-            Allocate2D<T>(new Index2(width, height));
-
-        /// <summary>
-        /// Allocates a 2D chunk of local memory with the specified number of elements.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="extent">The extent (number of elements to allocate).</param>
-        /// <returns>An allocated region of local memory.</returns>
-        public static ArrayView2D<T> Allocate2D<T>(Index2 extent)
-            where T : unmanaged =>
-            Allocate<T, Index2>(extent);
-
-        /// <summary>
-        /// Allocates a 3D chunk of local memory with the specified number of elements.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="width">The width of the 3D buffer.</param>
-        /// <param name="height">The height of the 3D buffer.</param>
-        /// <param name="depth">The depth of the 3D buffer.</param>
-        /// <returns>An allocated region of local memory.</returns>
-        public static ArrayView3D<T> Allocate3D<T>(
-            Index1 width,
-            Index1 height,
-            Index1 depth)
-            where T : unmanaged =>
-            Allocate3D<T>(new Index3(width, height, depth));
-
-        /// <summary>
-        /// Allocates a 3D chunk of local memory with the specified number of elements.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="extent">The extent (number of elements to allocate).</param>
-        /// <returns>An allocated region of local memory.</returns>
-        public static ArrayView3D<T> Allocate3D<T>(Index3 extent)
-            where T : unmanaged =>
-            Allocate<T, Index3>(extent);
     }
 }
--- Src/ILGPU/SharedMemory.cs
+++ Src/ILGPU/SharedMemory.cs
@@ -18,7 +18,7 @@ namespace ILGPU
     /// <summary>
     /// Contains methods to allocate and managed shared memory.
     /// </summary>
-    public static class SharedMemory
+    public static partial class SharedMemory
     {
         /// <summary>
         /// Allocates a single element in shared memory.
@@ -41,77 +41,6 @@ namespace ILGPU
         public static ArrayView<T> Allocate<T>(int extent)
             where T : unmanaged =>
             CPURuntimeGroupContext.Current.AllocateSharedMemory<T>(extent);
-
-        /// <summary>
-        /// Allocates a chunk of shared memory with the specified number of elements.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <typeparam name="TIndex">The index type.</typeparam>
-        /// <param name="extent">The extent (number of elements to allocate).</param>
-        /// <returns>An allocated region of shared memory.</returns>
-        public static ArrayView<T, TIndex> Allocate<T, TIndex>(TIndex extent)
-            where T : unmanaged
-            where TIndex : unmanaged, IIntIndex, IGenericIndex<TIndex>
-        {
-            var baseView = Allocate<T>(extent.Size);
-            return new ArrayView<T, TIndex>(baseView, extent);
-        }
-
-        /// <summary>
-        /// Allocates a 1D chunk of shared memory with the specified number of elements.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="length">The number of elements to allocate.</param>
-        /// <returns>An allocated region of shared memory.</returns>
-        public static ArrayView<T> Allocate<T>(Index1 length)
-            where T : unmanaged =>
-            Allocate<T, Index1>(length).AsLinearView();
-
-        /// <summary>
-        /// Allocates a 2D chunk of shared memory with the specified number of elements.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="width">The width of the 2D buffer.</param>
-        /// <param name="height">The height of the 2D buffer.</param>
-        /// <returns>An allocated region of shared memory.</returns>
-        public static ArrayView2D<T> Allocate2D<T>(Index1 width, Index1 height)
-            where T : unmanaged =>
-            Allocate2D<T>(new Index2(width, height));
-
-        /// <summary>
-        /// Allocates a 2D chunk of shared memory with the specified number of elements.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="extent">The extent (number of elements to allocate).</param>
-        /// <returns>An allocated region of shared memory.</returns>
-        public static ArrayView2D<T> Allocate2D<T>(Index2 extent)
-            where T : unmanaged =>
-            Allocate<T, Index2>(extent);
-
-        /// <summary>
-        /// Allocates a 3D chunk of shared memory with the specified number of elements.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="width">The width of the 3D buffer.</param>
-        /// <param name="height">The height of the 3D buffer.</param>
-        /// <param name="depth">The depth of the 3D buffer.</param>
-        /// <returns>An allocated region of shared memory.</returns>
-        public static ArrayView3D<T> Allocate3D<T>(
-            Index1 width,
-            Index1 height,
-            Index1 depth)
-            where T : unmanaged =>
-            Allocate3D<T>(new Index3(width, height, depth));
-
-        /// <summary>
-        /// Allocates a 3D chunk of shared memory with the specified number of elements.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="extent">The extent (number of elements to allocate).</param>
-        /// <returns>An allocated region of shared memory.</returns>
-        public static ArrayView3D<T> Allocate3D<T>(Index3 extent)
-            where T : unmanaged =>
-            Allocate<T, Index3>(extent);
 
         /// <summary>
         /// Gets a chunk of dynamically allocated shared memory as typed memory view
