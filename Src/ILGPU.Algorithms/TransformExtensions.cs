42a60054687f954e3a3e0205e357f7d1dd0e43bc
Marcel Koester <m4rs@m4rs.net>
2021-05-16 17:06:27 +0200

    Ported Transform extension to new ILGPU memory API.

--- Src/ILGPU.Algorithms/TransformExtensions.cs
+++ Src/ILGPU.Algorithms/TransformExtensions.cs
@@ -13,7 +13,7 @@
 using ILGPU.Algorithms.Resources;
 using ILGPU.Runtime;
 using System;
-using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace ILGPU.Algorithms
 {
@@ -57,28 +57,6 @@ namespace ILGPU.Algorithms
     }
 
     /// <summary>
-    /// </summary>
-    /// <typeparam name="TSource">The source value type of the transformation.</typeparam>
-    /// <typeparam name="TTarget">The target value type of the transformation.</typeparam>
-    /// <typeparam name="TTransformer">
-    /// The transformer to transform elements from the source type to the target type.
-    /// </typeparam>
-    static class TransformImpl<TSource, TTarget, TTransformer>
-        where TSource : unmanaged
-        where TTarget : unmanaged
-        where TTransformer : struct, ITransformer<TSource, TTarget>
-    {
-        /// <summary>
-        /// Represents a transform kernel.
-        /// </summary>
-        public static readonly MethodInfo KernelMethod =
-            typeof(TransformImpl<TSource, TTarget, TTransformer>).GetMethod(
-                nameof(Kernel),
-                BindingFlags.NonPublic | BindingFlags.Static);
-
-    }
-
-    /// <summary>
     /// Represents an element transformer that Transforms elements in the source view into
     /// elements in the target view using the given transformer.
     /// </summary>
@@ -103,6 +81,39 @@ namespace ILGPU.Algorithms
         where TTransformer : struct, ITransformer<TSource, TTarget>;
 
     /// <summary>
+    /// Represents an element transformer that Transforms elements in the source view into
+    /// elements in the target view using the given transformer.
+    /// </summary>
+    /// <typeparam name="TSource">The source value type of the transformation.</typeparam>
+    /// <typeparam name="TSourceStride">The 1D stride of the source view.</typeparam>
+    /// <typeparam name="TTarget">The target value type of the transformation.</typeparam>
+    /// <typeparam name="TTargetStride">The 1D stride of the target view.</typeparam>
+    /// <typeparam name="TTransformer">
+    /// The transformer to transform elements from the source type to the target type.
+    /// </typeparam>
+    /// <param name="stream">The accelerator stream.</param>
+    /// <param name="source">The source elements to transform</param>
+    /// <param name="target">
+    /// The target elements that will contain the transformed values.
+    /// </param>
+    /// <param name="transformer">The used transformer.</param>
+    public delegate void Transformer<
+        TSource,
+        TSourceStride,
+        TTarget,
+        TTargetStride,
+        TTransformer>(
+        AcceleratorStream stream,
+        ArrayView1D<TSource, TSourceStride> source,
+        ArrayView1D<TTarget, TTargetStride> target,
+        TTransformer transformer)
+        where TSource : unmanaged
+        where TSourceStride : struct, IStride1D
+        where TTarget : unmanaged
+        where TTargetStride : struct, IStride1D
+        where TTransformer : struct, ITransformer<TSource, TTarget>;
+
+    /// <summary>
     /// Transformer functionality for accelerators.
     /// </summary>
     public static class TransformExtensions
@@ -110,33 +121,74 @@ namespace ILGPU.Algorithms
         #region Transform Implementation
 
         /// <summary>
-        /// Implements a transformer algorithm.
+        /// A actual raw transform loop body.
         /// </summary>
-        /// <typeparam name="TSource">
-        /// The source value type of the transformation.
-        /// </typeparam>
-        /// <typeparam name="TTarget">
-        /// The target value type of the transformation.
-        /// </typeparam>
-        /// <typeparam name="TTransformer">
-        /// The transformer to transform elements from the source type to the target type.
-        /// </typeparam>
-        /// <param name="index"></param>
-        /// <param name="source"></param>
-        /// <param name="target"></param>
-        /// <param name="transformer"></param>
-        internal static void TransformKernel<TSource, TTarget, TTransformer>(
-            Index1 index,
-            ArrayView<TSource> source,
-            ArrayView<TTarget> target,
-            TTransformer transformer)
+        /// <typeparam name="TSource">The source element type.</typeparam>
+        /// <typeparam name="TSourceStride">The 1D stride of the source view.</typeparam>
+        /// <typeparam name="TTarget">The target element type.</typeparam>
+        /// <typeparam name="TTargetStride">The 1D stride of the target view.</typeparam>
+        /// <typeparam name="TTransformer">The type of the transformer to use.</typeparam>
+        internal readonly struct TransformImplementation<
+            TSource,
+            TSourceStride,
+            TTarget,
+            TTargetStride,
+            TTransformer> : IGridStrideKernelBody
             where TSource : unmanaged
+            where TSourceStride : struct, IStride1D
             where TTarget : unmanaged
+            where TTargetStride : struct, IStride1D
             where TTransformer : struct, ITransformer<TSource, TTarget>
         {
-            var stride = GridExtensions.GridStrideLoopStride;
-            for (var idx = index; idx < source.Length; idx += stride)
-                target[idx] = transformer.Transform(source[idx]);
+            /// <summary>
+            /// Creates a new transform instance.
+            /// </summary>
+            /// <param name="source">The parent source view.</param>
+            /// <param name="target">The parent target view.</param>
+            /// <param name="transformer">The transformer instance.</param>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public TransformImplementation(
+                ArrayView1D<TSource, TSourceStride> source,
+                ArrayView1D<TTarget, TTargetStride> target,
+                TTransformer transformer)
+            {
+                Source = source;
+                Target = target;
+                Transformer = transformer;
+            }
+
+            /// <summary>
+            /// Returns the source view.
+            /// </summary>
+            public ArrayView1D<TSource, TSourceStride> Source { get; }
+
+            /// <summary>
+            /// Returns the target view.
+            /// </summary>
+            public ArrayView1D<TTarget, TTargetStride> Target { get; }
+
+            /// <summary>
+            /// Returns the transformer instance.
+            /// </summary>
+            public TTransformer Transformer { get; }
+
+            /// <summary>
+            /// Executes this transform wrapper.
+            /// </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public readonly void Execute(LongIndex1D linearIndex)
+            {
+                if (linearIndex >= Source.Length)
+                    return;
+
+                Target[linearIndex] = Transformer.Transform(Source[linearIndex]);
+            }
+
+            /// <summary>
+            /// Performs no operation.
+            /// </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public readonly void Finish() { }
         }
 
         /// <summary>
@@ -146,37 +198,43 @@ namespace ILGPU.Algorithms
         /// <typeparam name="TSource">
         /// The source value type of the transformation.
         /// </typeparam>
+        /// <typeparam name="TSourceStride">The 1D stride of the source view.</typeparam>
         /// <typeparam name="TTarget">
         /// The target value type of the transformation.
         /// </typeparam>
+        /// <typeparam name="TTargetStride">The 1D stride of the target view.</typeparam>
         /// <typeparam name="TTransformer">
         /// The transformer to transform elements from the source type to the target type.
         /// </typeparam>
         /// <param name="accelerator">The accelerator.</param>
-        /// <param name="minDataSize">The minimum data size for maximum occupancy.</param>
         /// <returns>The loaded transformer.</returns>
         private static Action<
             AcceleratorStream,
-            Index1,
-            ArrayView<TSource>,
-            ArrayView<TTarget>,
-            TTransformer> CreateRawTransformer<TSource, TTarget, TTransformer>(
-            this Accelerator accelerator,
-            out Index1 minDataSize)
-            where TSource : unmanaged
-            where TTarget : unmanaged
-            where TTransformer : struct, ITransformer<TSource, TTarget>
-        {
-            var result = accelerator.LoadAutoGroupedKernel<
-                Index1,
-                ArrayView<TSource>,
-                ArrayView<TTarget>,
+            LongIndex1D,
+            TransformImplementation<
+                TSource,
+                TSourceStride,
+                TTarget,
+                TTargetStride,
+                TTransformer>>
+            CreateRawTransformer<
+                TSource,
+                TSourceStride,
+                TTarget,
+                TTargetStride,
                 TTransformer>(
-                TransformKernel,
-                out var info);
-            minDataSize = info.MinGroupSize.Value * info.MinGridSize.Value;
-            return result;
-        }
+            this Accelerator accelerator)
+            where TSource : unmanaged
+            where TSourceStride : struct, IStride1D
+            where TTarget : unmanaged
+            where TTargetStride : struct, IStride1D
+            where TTransformer : struct, ITransformer<TSource, TTarget> =>
+            accelerator.LoadGridStrideKernel<TransformImplementation<
+                TSource,
+                TSourceStride,
+                TTarget,
+                TTargetStride,
+                TTransformer>>();
 
         #endregion
 
@@ -202,10 +260,58 @@ namespace ILGPU.Algorithms
             where TTarget : unmanaged
             where TTransformer : struct, ITransformer<TSource, TTarget>
         {
+            var transformerLauncher = accelerator.CreateTransformer<
+                TSource,
+                Stride1D.Dense,
+                TTarget,
+                Stride1D.Dense,
+                TTransformer>();
+            return (stream, source, target, transformer) =>
+                transformerLauncher(stream, source, target, transformer);
+        }
+
+        /// <summary>
+        /// Creates a raw transformer that is defined by the given source and target type
+        /// and the specified transformer type.
+        /// </summary>
+        /// <typeparam name="TSource">
+        /// The source value type of the transformation.
+        /// </typeparam>
+        /// <typeparam name="TSourceStride">The 1D stride of the source view.</typeparam>
+        /// <typeparam name="TTarget">
+        /// The target value type of the transformation.
+        /// </typeparam>
+        /// <typeparam name="TTargetStride">The 1D stride of the target view.</typeparam>
+        /// <typeparam name="TTransformer">
+        /// The transformer to transform elements from the source type to the target type.
+        /// </typeparam>
+        /// <param name="accelerator">The accelerator.</param>
+        /// <returns>The loaded transformer.</returns>
+        public static Transformer<
+            TSource,
+            TSourceStride,
+            TTarget,
+            TTargetStride,
+            TTransformer>
+            CreateTransformer<
+                TSource,
+                TSourceStride,
+                TTarget,
+                TTargetStride,
+                TTransformer>(
+            this Accelerator accelerator)
+            where TSource : unmanaged
+            where TSourceStride : struct, IStride1D
+            where TTarget : unmanaged
+            where TTargetStride : struct, IStride1D
+            where TTransformer : struct, ITransformer<TSource, TTarget>
+        {
             var rawTransformer = accelerator.CreateRawTransformer<
                 TSource,
+                TSourceStride,
                 TTarget,
-                TTransformer>(out Index1 minDataSize);
+                TTargetStride,
+                TTransformer>();
             return (stream, source, target, transformer) =>
             {
                 if (!source.IsValid)
@@ -215,20 +321,26 @@ namespace ILGPU.Algorithms
                 if (!target.IsValid)
                     throw new ArgumentNullException(nameof(source));
                 if (target.Length < source.Length)
+                {
                     throw new ArgumentOutOfRangeException(
                         nameof(target),
-                        "The source view is larger than the target view");
-                if (source.Length > int.MaxValue)
-                {
-                    throw new NotSupportedException(
-                        ErrorMessages.NotSupportedArrayView64);
+                        string.Format(
+                            ErrorMessages.ViewOutOfRange,
+                            nameof(source),
+                            nameof(target)));
                 }
                 rawTransformer(
                     stream,
-                    Math.Min(source.Length, minDataSize),
-                    source,
-                    target,
-                    transformer);
+                    source.Length,
+                    new TransformImplementation<
+                        TSource,
+                        TSourceStride,
+                        TTarget,
+                        TTargetStride,
+                        TTransformer>(
+                        source,
+                        target,
+                        transformer));
             };
         }
 
@@ -245,10 +357,8 @@ namespace ILGPU.Algorithms
         public static Transformer<T, T, TTransformer> CreateTransformer<T, TTransformer>(
             this Accelerator accelerator)
             where T : unmanaged
-            where TTransformer : struct, ITransformer<T, T>
-        {
-            return accelerator.CreateTransformer<T, T, TTransformer>();
-        }
+            where TTransformer : struct, ITransformer<T, T> =>
+            accelerator.CreateTransformer<T, T, TTransformer>();
 
         /// <summary>
         /// Transforms elements in the source view into elements in the target view using
@@ -272,14 +382,12 @@ namespace ILGPU.Algorithms
             ArrayView<T> target,
             TTransformer transformer)
             where T : unmanaged
-            where TTransformer : struct, ITransformer<T, T>
-        {
+            where TTransformer : struct, ITransformer<T, T> =>
             accelerator.CreateTransformer<T, TTransformer>()(
                 stream,
                 source,
                 target,
                 transformer);
-        }
 
         /// <summary>
         /// Transforms elements in the source view into elements in the target view using
@@ -309,13 +417,91 @@ namespace ILGPU.Algorithms
             TTransformer transformer)
             where TSource : unmanaged
             where TTarget : unmanaged
-            where TTransformer : struct, ITransformer<TSource, TTarget>
-        {
+            where TTransformer : struct, ITransformer<TSource, TTarget> =>
             accelerator.CreateTransformer<TSource, TTarget, TTransformer>()(
                 stream,
                 source,
                 target,
                 transformer);
-        }
+
+        /// <summary>
+        /// Transforms elements in the source view into elements in the target view using
+        /// the given transformer.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements to transform.</typeparam>
+        /// <typeparam name="TStride">The 1D stride of all views.</typeparam>
+        /// <typeparam name="TTransformer">
+        /// The transformer to transform elements from the source type to the target type.
+        /// </typeparam>
+        /// <param name="accelerator">The accelerator.</param>
+        /// <param name="stream">The accelerator stream.</param>
+        /// <param name="source">The source elements to transform</param>
+        /// <param name="target">
+        /// The target elements that will contain the transformed values.
+        /// </param>
+        /// <param name="transformer">The used transformer.</param>
+        public static void Transform<T, TStride, TTransformer>(
+            this Accelerator accelerator,
+            AcceleratorStream stream,
+            ArrayView1D<T, TStride> source,
+            ArrayView1D<T, TStride> target,
+            TTransformer transformer)
+            where T : unmanaged
+            where TStride : struct, IStride1D
+            where TTransformer : struct, ITransformer<T, T> =>
+            accelerator.CreateTransformer<T, TStride, T, TStride, TTransformer>()(
+                stream,
+                source,
+                target,
+                transformer);
+
+        /// <summary>
+        /// Transforms elements in the source view into elements in the target view using
+        /// the given transformer.
+        /// </summary>
+        /// <typeparam name="TSource">
+        /// The source type of the elements to transform.
+        /// </typeparam>
+        /// <typeparam name="TSourceStride">The 1D stride of the source view.</typeparam>
+        /// <typeparam name="TTarget">
+        /// The target type of the elements that have been transformed.
+        /// </typeparam>
+        /// <typeparam name="TTargetStride">The 1D stride of the target view.</typeparam>
+        /// <typeparam name="TTransformer">
+        /// The transformer to transform elements from the source type to the target type.
+        /// </typeparam>
+        /// <param name="accelerator">The accelerator.</param>
+        /// <param name="stream">The accelerator stream.</param>
+        /// <param name="source">The source elements to transform</param>
+        /// <param name="target">
+        /// The target elements that will contain the transformed values.
+        /// </param>
+        /// <param name="transformer">The used transformer.</param>
+        public static void Transform<
+            TSource,
+            TSourceStride,
+            TTarget,
+            TTargetStride,
+            TTransformer>(
+            this Accelerator accelerator,
+            AcceleratorStream stream,
+            ArrayView1D<TSource, TSourceStride> source,
+            ArrayView1D<TTarget, TTargetStride> target,
+            TTransformer transformer)
+            where TSource : unmanaged
+            where TSourceStride : struct, IStride1D
+            where TTarget : unmanaged
+            where TTargetStride : struct, IStride1D
+            where TTransformer : struct, ITransformer<TSource, TTarget> =>
+            accelerator.CreateTransformer<
+                TSource,
+                TSourceStride,
+                TTarget,
+                TTargetStride,
+                TTransformer>()(
+                stream,
+                source,
+                target,
+                transformer);
     }
 }
