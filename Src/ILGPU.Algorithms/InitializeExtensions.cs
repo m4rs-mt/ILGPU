89446ec701287532ef50fe3b2f7055f03b910787
Marcel Koester <m4rs@m4rs.net>
2021-05-16 17:06:07 +0200

    Ported Initializer extension to new ILGPU memory API.

--- Src/ILGPU.Algorithms/InitializeExtensions.cs
+++ Src/ILGPU.Algorithms/InitializeExtensions.cs
@@ -10,9 +10,9 @@
 // Source License. See LICENSE.txt for details
 // ---------------------------------------------------------------------------------------
 
-using ILGPU.Algorithms.Resources;
 using ILGPU.Runtime;
 using System;
+using System.Runtime.CompilerServices;
 
 namespace ILGPU.Algorithms
 {
@@ -20,14 +20,16 @@ namespace ILGPU.Algorithms
     /// Performs an initialization on the given view.
     /// </summary>
     /// <typeparam name="T">The element type.</typeparam>
+    /// <typeparam name="TStride">The 1D stride of the target view.</typeparam>
     /// <param name="stream">The accelerator stream.</param>
     /// <param name="view">The element view.</param>
     /// <param name="value">The target value.</param>
-    public delegate void Initializer<T>(
+    public delegate void Initializer<T, TStride>(
         AcceleratorStream stream,
-        ArrayView<T> view,
+        ArrayView1D<T, TStride> view,
         T value)
-        where T : unmanaged;
+        where T : unmanaged
+        where TStride : struct, IStride1D;
 
     /// <summary>
     /// Initialize functionality for accelerators.
@@ -37,64 +39,100 @@ namespace ILGPU.Algorithms
         #region Initialize Implementation
 
         /// <summary>
-        /// The actual initialize implementation.
+        /// A actual raw initializer implementation.
         /// </summary>
         /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="index">The current thread index.</param>
-        /// <param name="view">The target view.</param>
-        /// <param name="value">The value.</param>
-        internal static void InitializeKernel<T>(Index1 index, ArrayView<T> view, T value)
+        /// <typeparam name="TStride">The 1D stride of the target view.</typeparam>
+        internal readonly struct InitializerImplementation<T, TStride> :
+            IGridStrideKernelBody
             where T : unmanaged
+            where TStride : struct, IStride1D
         {
-            var stride = GridExtensions.GridStrideLoopStride;
-            for (var idx = index; idx < view.Length; idx += stride)
-                view[idx] = value;
+            /// <summary>
+            /// Creates a new initializer implementation.
+            /// </summary>
+            /// <param name="view">The parent target view.</param>
+            /// <param name="value">The initializer value.</param>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public InitializerImplementation(ArrayView1D<T, TStride> view, T value)
+            {
+                View = view;
+                Value = value;
+            }
+
+            /// <summary>
+            /// Returns the target view.
+            /// </summary>
+            public ArrayView1D<T, TStride> View { get; }
+
+            /// <summary>
+            /// Returns the initializer value.
+            /// </summary>
+            public T Value { get; }
+
+            /// <summary>
+            /// Executes this sequencer wrapper.
+            /// </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public readonly void Execute(LongIndex1D linearIndex)
+            {
+                if (linearIndex >= View.Length)
+                    return;
+
+                View[linearIndex] = Value;
+            }
+
+            /// <summary>
+            /// Performs no operation.
+            /// </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public readonly void Finish() { }
         }
 
         /// <summary>
         /// Creates a raw initializer that is defined by the given element type.
         /// </summary>
         /// <typeparam name="T">The element type.</typeparam>
+        /// <typeparam name="TStride">The 1D stride of the target view.</typeparam>
         /// <param name="accelerator">The accelerator.</param>
-        /// <param name="minDataSize">The minimum data size for maximum occupancy.</param>
         /// <returns>The loaded initializer.</returns>
-        private static Action<AcceleratorStream, Index1, ArrayView<T>, T>
-            CreateRawInitializer<T>(
-            this Accelerator accelerator,
-            out Index1 minDataSize)
+        private static Action<
+            AcceleratorStream,
+            LongIndex1D,
+            InitializerImplementation<T, TStride>>
+            CreateRawInitializer<T, TStride>(
+            this Accelerator accelerator)
             where T : unmanaged
-        {
-            var result = accelerator.LoadAutoGroupedKernel(
-                (Action<Index1, ArrayView<T>, T>)InitializeKernel,
-                out var info);
-            minDataSize = info.MinGroupSize.Value * info.MinGridSize.Value;
-            return result;
-        }
+            where TStride : struct, IStride1D =>
+            accelerator.LoadGridStrideKernel<
+                InitializerImplementation<T, TStride>>();
 
         /// <summary>
         /// Creates an initializer that is defined by the given element type.
         /// </summary>
         /// <typeparam name="T">The element type.</typeparam>
+        /// <typeparam name="TStride">The 1D stride of the target view.</typeparam>
         /// <param name="accelerator">The accelerator.</param>
         /// <returns>The loaded transformer.</returns>
-        public static Initializer<T> CreateInitializer<T>(
+        public static Initializer<T, TStride> CreateInitializer<T, TStride>(
             this Accelerator accelerator)
             where T : unmanaged
+            where TStride : struct, IStride1D
         {
-            var rawInitializer = accelerator.CreateRawInitializer<T>(
-                out Index1 minDataSize);
+            var rawInitializer = accelerator.CreateRawInitializer<T, TStride>();
             return (stream, view, value) =>
             {
                 if (!view.IsValid)
                     throw new ArgumentNullException(nameof(view));
                 if (view.Length < 1)
                     throw new ArgumentOutOfRangeException(nameof(view));
-                if (view.Length > int.MaxValue)
-                {
-                    throw new NotSupportedException(
-                        ErrorMessages.NotSupportedArrayView64);
-                }
-                rawInitializer(stream, Math.Min(view.Length, minDataSize), view, value);
+
+                rawInitializer(
+                    stream,
+                    view.Length,
+                    new InitializerImplementation<T, TStride>(
+                        view,
+                        value));
             };
         }
 
@@ -111,13 +149,32 @@ namespace ILGPU.Algorithms
             AcceleratorStream stream,
             ArrayView<T> view,
             T value)
-            where T : unmanaged
-        {
-            accelerator.CreateInitializer<T>()(
+            where T : unmanaged =>
+            accelerator.CreateInitializer<T, Stride1D.Dense>()(
                 stream,
                 view,
                 value);
-        }
+
+        /// <summary>
+        /// Performs an initialization on the given view.
+        /// </summary>
+        /// <typeparam name="T">The element type.</typeparam>
+        /// <typeparam name="TStride">The 1D stride of the target view.</typeparam>
+        /// <param name="accelerator">The accelerator.</param>
+        /// <param name="stream">The accelerator stream.</param>
+        /// <param name="view">The element view.</param>
+        /// <param name="value">The target value.</param>
+        public static void Initialize<T, TStride>(
+            this Accelerator accelerator,
+            AcceleratorStream stream,
+            ArrayView1D<T, TStride> view,
+            T value)
+            where T : unmanaged
+            where TStride : struct, IStride1D =>
+            accelerator.CreateInitializer<T, TStride>()(
+                stream,
+                view,
+                value);
 
         #endregion
     }
