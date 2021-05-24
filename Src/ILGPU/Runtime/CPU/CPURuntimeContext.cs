cb04bbb85a054f90a6a5b19b85aead8af5ec90ea
Marcel Koester <m4rs@m4rs.net>
2021-05-25 00:27:34 +0200

    Refined CPURuntimeContext implementation to support the new SM model.

--- Src/ILGPU/Runtime/CPU/CPURuntimeContext.cs
+++ Src/ILGPU/Runtime/CPU/CPURuntimeContext.cs
@@ -47,6 +47,18 @@ namespace ILGPU.Runtime.CPU
         }
 
         /// <summary>
+        /// A parent object that has a barrier function.
+        /// </summary>
+        public interface IParent
+        {
+            /// <summary>
+            /// Executes a thread barrier.
+            /// </summary>
+            /// <returns>The number of participating threads.</returns>
+            int Barrier();
+        }
+
+        /// <summary>
         /// Represents an operation that allocates and managed broadcast memory.
         /// </summary>
         /// <typeparam name="T">The element type.</typeparam>
@@ -102,11 +114,6 @@ namespace ILGPU.Runtime.CPU
         #region Instance
 
         /// <summary>
-        /// The general barrier.
-        /// </summary>
-        private readonly Barrier barrier;
-
-        /// <summary>
         /// The global memory lock variable.
         /// </summary>
         private volatile int memoryLock;
@@ -124,14 +131,13 @@ namespace ILGPU.Runtime.CPU
         /// <summary>
         /// Constructs a new CPU-based runtime context for parallel processing.
         /// </summary>
-        /// <param name="accelerator">The target CPU accelerator.</param>
-        protected CPURuntimeContext(CPUAccelerator accelerator)
+        /// <param name="multiprocessor">The target CPU multiprocessor.</param>
+        protected CPURuntimeContext(CPUMultiprocessor multiprocessor)
         {
-            Accelerator = accelerator
-                ?? throw new ArgumentNullException(nameof(accelerator));
-            barrier = new Barrier(0);
+            Multiprocessor = multiprocessor
+                ?? throw new ArgumentNullException(nameof(multiprocessor));
 
-            broadcastBuffer = new MemoryBufferCache(accelerator);
+            broadcastBuffer = new MemoryBufferCache(multiprocessor.Accelerator);
             broadcastBuffer.Allocate<int>(16);
         }
 
@@ -140,44 +146,22 @@ namespace ILGPU.Runtime.CPU
         #region Properties
 
         /// <summary>
-        /// Returns the associated accelerator.
+        /// Returns the parent multiprocessor instance.
         /// </summary>
-        public CPUAccelerator Accelerator { get; }
-
-        /// <summary>
-        /// Returns the number of barrier participants.
-        /// </summary>
-        protected int NumParticipants => barrier.ParticipantCount;
+        public CPUMultiprocessor Multiprocessor { get; }
 
         #endregion
 
         #region Methods
 
         /// <summary>
-        /// Executes a thread barrier.
-        /// </summary>
-        public abstract void Barrier();
-
-        /// <summary>
-        /// Executes a thread barrier.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        protected void WaitForAllThreads()
-        {
-            // Issue a thread memory barrier first to ensure that no IO operations
-            // will be reorder across this barrier
-            Thread.MemoryBarrier();
-
-            // Wait for all other participants
-            barrier.SignalAndWait();
-        }
-
-        /// <summary>
         /// Performs the given operation locked with respect to all other operating
         /// threads that are currently active.
         /// </summary>
+        /// <typeparam name="TParent">The parent object type.</typeparam>
         /// <typeparam name="TOperation">The operation type.</typeparam>
         /// <typeparam name="T">The result type.</typeparam>
+        /// <param name="parent">The parent object instance.</param>
         /// <param name="operation">The operation to perform.</param>
         /// <returns>The determined result value for all threads.</returns>
         /// <remarks>
@@ -187,15 +171,18 @@ namespace ILGPU.Runtime.CPU
         /// the synchronized operation and the main thread releases its lock.
         /// </remarks>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        protected T PerformLocked<TOperation, T>(TOperation operation)
+        protected T PerformLocked<TParent, TOperation, T>(
+            TParent parent,
+            TOperation operation)
+            where TParent : IParent
             where TOperation : ILockedOperation<T>
         {
             bool isMainThread = AquireLock();
             if (isMainThread)
                 operation.ApplySyncInMainThread();
-            Barrier();
+            parent.Barrier();
             var result = operation.Result;
-            ReleaseLock(isMainThread);
+            ReleaseLock(parent, isMainThread);
             return result;
         }
 
@@ -211,82 +198,70 @@ namespace ILGPU.Runtime.CPU
         /// <summary>
         /// Release the internal lock.
         /// </summary>
+        /// <typeparam name="TParent">The parent object type.</typeparam>
+        /// <param name="parent">The parent object instance.</param>
         /// <param name="isMainThread">True, if this thread is the main thread.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        protected void ReleaseLock(bool isMainThread)
+        protected void ReleaseLock<TParent>(
+            TParent parent,
+            bool isMainThread)
+            where TParent : IParent
         {
             // If we are the main thread, release the lock by issuing an atomic
             // exchange operation in order to be visible by another AquireLock
             // operation that might be executed in the future.
             if (isMainThread)
                 Interlocked.Exchange(ref memoryLock, 0);
-            Barrier();
+            parent.Barrier();
         }
 
         /// <summary>
         /// Executes a broadcast operation.
         /// </summary>
+        /// <typeparam name="TParent">The parent object type.</typeparam>
+        /// <typeparam name="T">The element type to broadcast.</typeparam>
+        /// <param name="parent">The parent object instance.</param>
         /// <param name="value">The desired group index.</param>
+        /// <param name="threadIndex">The current thread index.</param>
         /// <param name="groupIndex">The source thread index within the group.</param>
-        public T Broadcast<T>(T value, int groupIndex)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected T Broadcast<TParent, T>(
+            TParent parent,
+            T value,
+            int threadIndex,
+            int groupIndex)
+            where TParent : IParent
             where T : unmanaged
         {
             // Allocate a compatible view to perform the actual broadcast operation
             var view = PerformLocked<
+                TParent,
                 GetBroadcastMemory<T>,
                 ArrayView<T>>(
+                parent,
                 new GetBroadcastMemory<T>(this, groupIndex));
 
             // Fill the shared view and verify the passed group index
             Trace.Assert(
                 broadcastIndex == groupIndex,
                 "Broadcast lanes must be the same for all participating threads");
-            if (Group.LinearIndex == groupIndex)
+            if (threadIndex == groupIndex)
                 view[0] = value;
-            Barrier();
+            parent.Barrier();
 
             // Get the actual broadcast result
             var result = view[0];
-            Barrier();
+            parent.Barrier();
             return result;
         }
 
         /// <summary>
-        /// Removes a single participant from the current barrier.
+        /// Initializes the internal runtime context.
         /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        protected void RemoveBarrierParticipant() =>
-            barrier.RemoveParticipant();
-
-        /// <summary>
-        /// Initializes the internal runtime context using the given number of
-        /// participants.
-        /// </summary>
-        /// <param name="participants">The number of participants to use.</param>
-        protected void Initialize(int participants)
-        {
+        protected void Initialize() =>
             // Initialize the lock using the interlocked API the ensure visibility
             // across all participants
             Interlocked.Exchange(ref memoryLock, 0);
-            EnsureBarrierParticipants(participants);
-
-            // Make sure that all memory operations are visible
-            Thread.MemoryBarrier();
-        }
-
-        /// <summary>
-        /// Ensures that the internal barrier has the given number of participants.
-        /// </summary>
-        /// <param name="participants">The number of participants.</param>
-        protected void EnsureBarrierParticipants(int participants)
-        {
-            Debug.Assert(participants >= 0, "Invalid number of participants");
-            var currentBarrierCount = barrier.ParticipantCount;
-            if (currentBarrierCount > participants)
-                barrier.RemoveParticipants(currentBarrierCount - participants);
-            else if (currentBarrierCount < participants)
-                barrier.AddParticipants(participants - currentBarrierCount);
-        }
 
         #endregion
 
@@ -296,10 +271,7 @@ namespace ILGPU.Runtime.CPU
         protected override void Dispose(bool disposing)
         {
             if (disposing)
-            {
-                barrier.Dispose();
                 broadcastBuffer.Dispose();
-            }
             base.Dispose(disposing);
         }
 
