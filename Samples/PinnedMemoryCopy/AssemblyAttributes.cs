5fa623c206446f14f01f1042d966fd4e159c816f
MoFtZ <MoFtZ@users.noreply.github.com>
2021-08-05 08:57:14 +1000

    Fixed compiler warnings in ILGPU samples.

--- Samples/PinnedMemoryCopy/AssemblyAttributes.cs
+++ Samples/PinnedMemoryCopy/AssemblyAttributes.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: CLSCompliant(true)]
 
 // Mark all internals to be visible to the ILGPU runtime
 [assembly: InternalsVisibleTo(ILGPU.Context.RuntimeAssemblyName)]
