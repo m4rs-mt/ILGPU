22cf3cb1e53d3d94ae75c74404ee7f880eabde96
Marcel Koester <m4rs@m4rs.net>
2021-10-02 15:44:26 +0200

    Adapted ILGPU.Samples copyright headers.

--- Samples/Mandelbrot/Program.cs
+++ Samples/Mandelbrot/Program.cs
@@ -1,13 +1,13 @@
-﻿// -----------------------------------------------------------------------------
-//                                ILGPU Samples
-//                 Copyright (c) 2017-2019 ILGPU Samples Project
-//                                www.ilgpu.net
+﻿// ---------------------------------------------------------------------------------------
+//                                    ILGPU Samples
+//                           Copyright (c) 2021 ILGPU Project
+//                                    www.ilgpu.net
 //
 // File: Program.cs
 //
-// This file is part of ILGPU and is distributed under the University of
-// Illinois Open Source License. See LICENSE.txt for details.
-// -----------------------------------------------------------------------------
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
 
 using System;
 using System.Windows.Forms;
