c3ee6231c381e29b8adf3224b538ad4a4c25a769
Marcel Koester <m4rs@m4rs.net>
2021-10-02 15:35:49 +0200

    Adapted ILGPU.Algorithms copyright headers.

--- Src/ILGPU.Algorithms/Random/RNG.cs
+++ Src/ILGPU.Algorithms/Random/RNG.cs
@@ -1,13 +1,12 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU.Algorithms
-//                      Copyright (c) 2019 ILGPU Algorithms Project
-//                     Copyright (c) 2017-2018 ILGPU Samples Project
+//                                   ILGPU Algorithms
+//                           Copyright (c) 2021 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: RNG.cs
 //
 // This file is part of ILGPU and is distributed under the University of Illinois Open
-// Source License. See LICENSE.txt for details
+// Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
 using ILGPU.Algorithms.Random.Operations;
--- Src/ILGPU.Algorithms/Runtime/Cuda/API/CuBlasAPI.cs
+++ Src/ILGPU.Algorithms/Runtime/Cuda/API/CuBlasAPI.cs
@@ -1,12 +1,12 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU.Algorithms
-//                      Copyright (c) 2020 ILGPU Algorithms Project
+//                                   ILGPU Algorithms
+//                        Copyright (c) 2020-2021 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: CuBlasAPI.cs
 //
 // This file is part of ILGPU and is distributed under the University of Illinois Open
-// Source License. See LICENSE.txt for details
+// Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
 using System;
