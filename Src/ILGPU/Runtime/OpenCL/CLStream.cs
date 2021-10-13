71297702e49255cbbe5093d68c1fb98bd7fc5a78
MoFtZ <MoFtZ@users.noreply.github.com>
2021-10-14 09:15:37 +1100

    Ensure that profiling markers are binding the current accelerator.

--- Src/ILGPU/Runtime/OpenCL/CLStream.cs
+++ Src/ILGPU/Runtime/OpenCL/CLStream.cs
@@ -76,6 +76,8 @@ namespace ILGPU.Runtime.OpenCL
         /// <inheritdoc/>
         protected unsafe override ProfilingMarker AddProfilingMarkerInternal()
         {
+            using var binding = Accelerator.BindScoped();
+
             IntPtr* profilingEvent = stackalloc IntPtr[1];
             CLException.ThrowIfFailed(
                 CurrentAPI.EnqueueBarrierWithWaitList(
@@ -86,7 +88,7 @@ namespace ILGPU.Runtime.OpenCL
             // WORKAROUND: The OpenCL event needs to be awaited now, otherwise
             // it does not contain the correct timing - it appears to have the timing
             // of whenever it gets awaited.
-            var marker = new CLProfilingMarker(*profilingEvent);
+            var marker = new CLProfilingMarker(Accelerator, *profilingEvent);
             marker.Synchronize();
             return marker;
         }
