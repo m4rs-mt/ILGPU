50ef462d38b97f280f442529ab838da5f24f2e49
MoFtZ <MoFtZ@users.noreply.github.com>
2021-12-10 08:46:31 +1100

    Added CuFFT wrapper.

---
+++ Src/ILGPU.Algorithms/Runtime/Cuda/CuFFT.cs
@@ -0,0 +1,270 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                   ILGPU Algorithms
+//                           Copyright (c) 2021 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: CuFFT.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+using ILGPU.Runtime.Cuda.API;
+using System;
+
+namespace ILGPU.Runtime.Cuda
+{
+    /// <summary>
+    /// Wrapper over cuFFT to simplify integration with ILGPU.
+    /// </summary>
+    [CLSCompliant(false)]
+    public sealed class CuFFT
+    {
+        /// <summary>
+        /// Constructs a new CuFFT instance.
+        /// </summary>
+        public CuFFT()
+            : this(default)
+        { }
+
+        /// <summary>
+        /// Constructs a new CuFFT instance.
+        /// </summary>
+        public CuFFT(CuFFTAPIVersion? version)
+        {
+            API = CuFFTAPI.Create(version);
+        }
+
+        /// <summary>
+        /// The underlying API wrapper.
+        /// </summary>
+        public CuFFTAPI API { get; }
+
+        #region Basic Plans
+
+        /// <summary>
+        /// Creates a 1D plan.
+        /// </summary>
+        /// <param name="plan">Filled in with the created plan.</param>
+        /// <param name="nx">The transform size.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="batch">The number of transforms.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult Plan1D(out CuFFTPlan plan, int nx, CuFFTType type, int batch)
+        {
+            var errorCode = API.Plan1D(out var planHandle, nx, type, batch);
+            plan = errorCode == CuFFTResult.CUFFT_SUCCESS
+                ? new CuFFTPlan(API, planHandle)
+                : default;
+            return errorCode;
+        }
+
+        /// <summary>
+        /// Creates a 2D plan.
+        /// </summary>
+        /// <param name="plan">Filled in with the created plan.</param>
+        /// <param name="nx">The transform size in the x dimension.</param>
+        /// <param name="ny">The transform size in the y dimension.</param>
+        /// <param name="type">The transform type.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult Plan2D(out CuFFTPlan plan, int nx, int ny, CuFFTType type)
+        {
+            var errorCode = API.Plan2D(out var planHandle, nx, ny, type);
+            plan = errorCode == CuFFTResult.CUFFT_SUCCESS
+                ? new CuFFTPlan(API, planHandle)
+                : default;
+            return errorCode;
+        }
+
+        /// <summary>
+        /// Creates a 3D plan.
+        /// </summary>
+        /// <param name="plan">Filled in with the created plan.</param>
+        /// <param name="nx">The transform size in the x dimension.</param>
+        /// <param name="ny">The transform size in the y dimension.</param>
+        /// <param name="nz">The transform size in the z dimension.</param>
+        /// <param name="type">The transform type.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult Plan3D(
+            out CuFFTPlan plan,
+            int nx,
+            int ny,
+            int nz,
+            CuFFTType type)
+        {
+            var errorCode = API.Plan3D(out var planHandle, nx, ny, nz, type);
+            plan = errorCode == CuFFTResult.CUFFT_SUCCESS
+                ? new CuFFTPlan(API, planHandle)
+                : default;
+            return errorCode;
+        }
+
+        /// <summary>
+        /// Creates a custom plan.
+        /// </summary>
+        /// <param name="plan">Filled in with the created plan.</param>
+        /// <param name="rank">The transform.</param>
+        /// <param name="n">The transform dimensions.</param>
+        /// <param name="inembed">The storage dimensions of the input data.</param>
+        /// <param name="istride">The stride of the input data.</param>
+        /// <param name="idist">The distance of the input data.</param>
+        /// <param name="onembed">The storage dimensions of the output data.</param>
+        /// <param name="ostride">The stride of the output data.</param>
+        /// <param name="odist">The distance of the output data.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="batch">The number of transforms.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult PlanMany(
+            out CuFFTPlan plan,
+            int rank,
+            int[] n,
+            int[] inembed,
+            int istride,
+            int idist,
+            int[] onembed,
+            int ostride,
+            int odist,
+            CuFFTType type,
+            int batch)
+        {
+            var errorCode = API.PlanMany(
+                out var planHandle,
+                rank,
+                n,
+                inembed,
+                istride,
+                idist,
+                onembed,
+                ostride,
+                odist,
+                type,
+                batch);
+            plan = errorCode == CuFFTResult.CUFFT_SUCCESS
+                ? new CuFFTPlan(API, planHandle)
+                : default;
+            return errorCode;
+        }
+
+        #endregion
+
+        #region Extensible Plans
+
+        /// <summary>
+        /// Creates an extensible plan.
+        /// </summary>
+        public CuFFTResult CreatePlan(out CuFFTPlan plan)
+        {
+            var errorCode = API.Create(out var planHandle);
+            plan = errorCode == CuFFTResult.CUFFT_SUCCESS
+                ? new CuFFTPlan(API, planHandle)
+                : default;
+            return errorCode;
+        }
+
+        #endregion
+
+        #region Estimated Size Of Work Area
+
+        /// <summary>
+        /// Estimates the work area for a 1D plan.
+        /// </summary>
+        /// <param name="nx">The transform size.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="batch">The number of transforms.</param>
+        /// <param name="workSize">Filled in with the estimated size in bytes.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult Estimate1D(
+            int nx,
+            CuFFTType type,
+            int batch,
+            UIntPtr[] workSize) =>
+            API.Estimate1D(
+                nx,
+                type,
+                batch,
+                workSize);
+
+        /// <summary>
+        /// Estimates the work area for a 2D plan.
+        /// </summary>
+        /// <param name="nx">The transform size in the x dimension.</param>
+        /// <param name="ny">The transform size in the y dimension.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="workSize">Filled in with the estimated size in bytes.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult Estimate2D(
+            int nx,
+            int ny,
+            CuFFTType type,
+            UIntPtr[] workSize) =>
+            API.Estimate2D(
+                nx,
+                ny,
+                type,
+                workSize);
+
+        /// <summary>
+        /// Estimates the work area for a 3D plan.
+        /// </summary>
+        /// <param name="nx">The transform size in the x dimension.</param>
+        /// <param name="ny">The transform size in the y dimension.</param>
+        /// <param name="nz">The transform size in the z dimension.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="workSize">Filled in with the estimated size in bytes.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult Estimate3D(
+            int nx,
+            int ny,
+            int nz,
+            CuFFTType type,
+            UIntPtr[] workSize) =>
+            API.Estimate3D(
+                nx,
+                ny,
+                nz,
+                type,
+                workSize);
+
+        /// <summary>
+        /// Estimates the work area for a custom plan.
+        /// </summary>
+        /// <param name="rank">The transform.</param>
+        /// <param name="n">The transform dimensions.</param>
+        /// <param name="inembed">The storage dimensions of the input data.</param>
+        /// <param name="istride">The stride of the input data.</param>
+        /// <param name="idist">The distance of the input data.</param>
+        /// <param name="onembed">The storage dimensions of the output data.</param>
+        /// <param name="ostride">The stride of the output data.</param>
+        /// <param name="odist">The distance of the output data.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="batch">The number of transforms.</param>
+        /// <param name="workSize">Filled in with the estimated size in bytes.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult EstimateMany(
+            int rank,
+            int[] n,
+            int[] inembed,
+            int istride,
+            int idist,
+            int[] onembed,
+            int ostride,
+            int odist,
+            CuFFTType type,
+            int batch,
+            UIntPtr[] workSize) =>
+            API.EstimateMany(
+                rank,
+                n,
+                inembed,
+                istride,
+                idist,
+                onembed,
+                ostride,
+                odist,
+                type,
+                batch,
+                workSize);
+
+        #endregion
+    }
+}
---
+++ Src/ILGPU.Algorithms/Runtime/Cuda/CuFFTPlan.cs
@@ -0,0 +1,390 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                   ILGPU Algorithms
+//                           Copyright (c) 2021 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: CuFFTPlan.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+using ILGPU.Runtime.Cuda.API;
+using ILGPU.Util;
+using System;
+
+namespace ILGPU.Runtime.Cuda
+{
+    /// <summary>
+    /// Represents a cuFFT plan.
+    /// </summary>
+    [CLSCompliant(false)]
+    public sealed partial class CuFFTPlan : DisposeBase
+    {
+        /// <summary>
+        /// The underlying API wrapper.
+        /// </summary>
+        public CuFFTAPI API { get; }
+
+        /// <summary>
+        /// The native plan handle.
+        /// </summary>
+        public IntPtr PlanHandle { get; private set; }
+
+        /// <summary>
+        /// Constructs a new instance to wrap a cuFFT plan.
+        /// </summary>
+        public CuFFTPlan(CuFFTAPI api, IntPtr plan)
+        {
+            API = api;
+            PlanHandle = plan;
+        }
+
+        /// <inheritdoc />
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                CuFFTException.ThrowIfFailed(
+                    API.Destroy(PlanHandle));
+                PlanHandle = IntPtr.Zero;
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Associates a CUDA stream with a cuFFT plan.
+        /// </summary>
+        public void SetStream(CudaStream cudaStream)
+        {
+            CuFFTException.ThrowIfFailed(
+                API.SetStream(PlanHandle, cudaStream));
+        }
+
+        #region Caller Allocated Work Area Support
+
+        /// <summary>
+        /// Indicates whether to allocate work area.
+        /// </summary>
+        public void SetAutoAllocate(bool autoAllocate) =>
+            CuFFTException.ThrowIfFailed(
+                API.SetAutoAllocation(PlanHandle, autoAllocate ? 1 : 0));
+
+        /// <summary>
+        /// Overrides the work area associated with a plan.
+        /// </summary>
+        public void SetWorkArea(ArrayView<byte> workArea) =>
+            CuFFTException.ThrowIfFailed(
+                API.SetWorkArea(PlanHandle, workArea));
+
+        #endregion
+
+        #region Extensible Plans
+
+        /// <summary>
+        /// Configures a 1D plan.
+        /// </summary>
+        /// <param name="nx">The transform size.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="batch">The number of transforms.</param>
+        /// <param name="workSize">The work size.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult MakePlan1D(
+            int nx,
+            CuFFTType type,
+            int batch,
+            UIntPtr[] workSize) =>
+            API.MakePlan1D(
+                PlanHandle,
+                nx,
+                type,
+                batch,
+                workSize);
+
+        /// <summary>
+        /// Configures a 2D plan.
+        /// </summary>
+        /// <param name="nx">The transform size in the x dimension.</param>
+        /// <param name="ny">The transform size in the y dimension.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="workSize">The work size.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult MakePlan2D(
+            int nx,
+            int ny,
+            CuFFTType type,
+            UIntPtr[] workSize) =>
+            API.MakePlan2D(
+                PlanHandle,
+                nx,
+                ny,
+                type,
+                workSize);
+
+        /// <summary>
+        /// Configures a 3D plan.
+        /// </summary>
+        /// <param name="nx">The transform size in the x dimension.</param>
+        /// <param name="ny">The transform size in the y dimension.</param>
+        /// <param name="nz">The transform size in the z dimension.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="workSize">The work size.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult MakePlan3D(
+            int nx,
+            int ny,
+            int nz,
+            CuFFTType type,
+            UIntPtr[] workSize) =>
+            API.MakePlan3D(
+                PlanHandle,
+                nx,
+                ny,
+                nz,
+                type,
+                workSize);
+
+        /// <summary>
+        /// Configures a custom plan.
+        /// </summary>
+        /// <param name="rank">The transform.</param>
+        /// <param name="n">The transform dimensions.</param>
+        /// <param name="inembed">The storage dimensions of the input data.</param>
+        /// <param name="istride">The stride of the input data.</param>
+        /// <param name="idist">The distance of the input data.</param>
+        /// <param name="onembed">The storage dimensions of the output data.</param>
+        /// <param name="ostride">The stride of the output data.</param>
+        /// <param name="odist">The distance of the output data.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="batch">The number of transforms.</param>
+        /// <param name="workSize">The work size.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult MakePlanMany(
+            int rank,
+            int[] n,
+            int[] inembed,
+            int istride,
+            int idist,
+            int[] onembed,
+            int ostride,
+            int odist,
+            CuFFTType type,
+            int batch,
+            UIntPtr[] workSize) =>
+            API.MakePlanMany(
+                PlanHandle,
+                rank,
+                n,
+                inembed,
+                istride,
+                idist,
+                onembed,
+                ostride,
+                odist,
+                type,
+                batch,
+                workSize);
+
+        /// <summary>
+        /// Configures a custom plan.
+        /// </summary>
+        /// <param name="rank">The transform.</param>
+        /// <param name="n">The transform dimensions.</param>
+        /// <param name="inembed">The storage dimensions of the input data.</param>
+        /// <param name="istride">The stride of the input data.</param>
+        /// <param name="idist">The distance of the input data.</param>
+        /// <param name="onembed">The storage dimensions of the output data.</param>
+        /// <param name="ostride">The stride of the output data.</param>
+        /// <param name="odist">The distance of the output data.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="batch">The number of transforms.</param>
+        /// <param name="workSize">The work size.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult MakePlanMany(
+            int rank,
+            long[] n,
+            long[] inembed,
+            long istride,
+            long idist,
+            long[] onembed,
+            long ostride,
+            long odist,
+            CuFFTType type,
+            long batch,
+            UIntPtr[] workSize) =>
+            API.MakePlanMany(
+                PlanHandle,
+                rank,
+                n,
+                inembed,
+                istride,
+                idist,
+                onembed,
+                ostride,
+                odist,
+                type,
+                batch,
+                workSize);
+
+        #endregion
+
+        #region Refined Estimated Size Of Work Area
+
+        /// <summary>
+        /// Estimates the work area for a 1D plan.
+        /// </summary>
+        /// <param name="nx">The transform size.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="batch">The number of transforms.</param>
+        /// <param name="workSize">Populated with the estimated size in bytes.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult GetSize1D(
+            int nx,
+            CuFFTType type,
+            int batch,
+            UIntPtr[] workSize) =>
+            API.GetSize1D(
+                PlanHandle,
+                nx,
+                type,
+                batch,
+                workSize);
+
+        /// <summary>
+        /// Estimates the work area for a 2D plan.
+        /// </summary>
+        /// <param name="nx">The transform size in the x dimension.</param>
+        /// <param name="ny">The transform size in the y dimension.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="workSize">Populated with the estimated size in bytes.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult GetSize2D(
+            int nx,
+            int ny,
+            CuFFTType type,
+            UIntPtr[] workSize) =>
+            API.GetSize2D(
+                PlanHandle,
+                nx,
+                ny,
+                type,
+                workSize);
+
+        /// <summary>
+        /// Estimates the work area for a 3D plan.
+        /// </summary>
+        /// <param name="nx">The transform size in the x dimension.</param>
+        /// <param name="ny">The transform size in the y dimension.</param>
+        /// <param name="nz">The transform size in the z dimension.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="workSize">Populated with the estimated size in bytes.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult GetSize3D(
+            int nx,
+            int ny,
+            int nz,
+            CuFFTType type,
+            UIntPtr[] workSize) =>
+            API.GetSize3D(
+                PlanHandle,
+                nx,
+                ny,
+                nz,
+                type,
+                workSize);
+
+        /// <summary>
+        /// Estimates the work area for a custom plan.
+        /// </summary>
+        /// <param name="rank">The transform.</param>
+        /// <param name="n">The transform dimensions.</param>
+        /// <param name="inembed">The storage dimensions of the input data.</param>
+        /// <param name="istride">The stride of the input data.</param>
+        /// <param name="idist">The distance of the input data.</param>
+        /// <param name="onembed">The storage dimensions of the output data.</param>
+        /// <param name="ostride">The stride of the output data.</param>
+        /// <param name="odist">The distance of the output data.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="batch">The number of transforms.</param>
+        /// <param name="workSize">Populated with the estimated size in bytes.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult GetSizeMany(
+            int rank,
+            int[] n,
+            int[] inembed,
+            int istride,
+            int idist,
+            int[] onembed,
+            int ostride,
+            int odist,
+            CuFFTType type,
+            int batch,
+            UIntPtr[] workSize) =>
+            API.GetSizeMany(
+                PlanHandle,
+                rank,
+                n,
+                inembed,
+                istride,
+                idist,
+                onembed,
+                ostride,
+                odist,
+                type,
+                batch,
+                workSize);
+
+        /// <summary>
+        /// Estimates the work area for a custom plan.
+        /// </summary>
+        /// <param name="rank">The transform.</param>
+        /// <param name="n">The transform dimensions.</param>
+        /// <param name="inembed">The storage dimensions of the input data.</param>
+        /// <param name="istride">The stride of the input data.</param>
+        /// <param name="idist">The distance of the input data.</param>
+        /// <param name="onembed">The storage dimensions of the output data.</param>
+        /// <param name="ostride">The stride of the output data.</param>
+        /// <param name="odist">The distance of the output data.</param>
+        /// <param name="type">The transform type.</param>
+        /// <param name="batch">The number of transforms.</param>
+        /// <param name="workSize">The work size.</param>
+        /// <returns>The error code.</returns>
+        public CuFFTResult GetSizeMany(
+            int rank,
+            long[] n,
+            long[] inembed,
+            long istride,
+            long idist,
+            long[] onembed,
+            long ostride,
+            long odist,
+            CuFFTType type,
+            long batch,
+            UIntPtr[] workSize) =>
+            API.GetSizeMany(
+                PlanHandle,
+                rank,
+                n,
+                inembed,
+                istride,
+                idist,
+                onembed,
+                ostride,
+                odist,
+                type,
+                batch,
+                workSize);
+
+        /// <summary>
+        /// Returns the work size.
+        /// </summary>
+        /// <param name="workSize">Populated with the estimated size in bytes.</param>
+        public void GetSize(UIntPtr[] workSize) =>
+            CuFFTException.ThrowIfFailed(
+                API.GetSize(
+                    PlanHandle,
+                    workSize));
+
+        #endregion
+    }
+}
