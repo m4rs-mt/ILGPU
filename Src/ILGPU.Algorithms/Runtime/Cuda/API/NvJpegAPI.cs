1dfaa0b9ebac800fd381dcde05f0871675929ead
MoFtZ <MoFtZ@users.noreply.github.com>
2021-12-16 12:56:22 +1100

    Added bindings for NvJpeg Single Image Decoding.

---
+++ Src/ILGPU.Algorithms/Runtime/Cuda/API/NvJpegAPI.cs
@@ -0,0 +1,165 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                   ILGPU.Algorithms
+//                      Copyright (c) 2021 ILGPU Algorithms Project
+//                                    www.ilgpu.net
+//
+// File: NvJpegAPI.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details
+// ---------------------------------------------------------------------------------------
+
+using System;
+
+namespace ILGPU.Runtime.Cuda.API
+{
+    /// <summary>
+    /// An implementation of the nvJpeg API.
+    /// </summary>
+    [CLSCompliant(false)]
+    public abstract partial class NvJpegAPI
+    {
+        #region Static
+
+        /// <summary>
+        /// Creates a new API wrapper.
+        /// </summary>
+        /// <param name="version">The nvJPEG version to use.</param>
+        /// <returns>The created API wrapper.</returns>
+        public static NvJpegAPI Create(NvJpegAPIVersion? version) =>
+            version.HasValue
+            ? CreateInternal(version.Value)
+            : CreateLatest();
+
+        /// <summary>
+        /// Creates a new API wrapper using the latest installed version.
+        /// </summary>
+        /// <returns>The created API wrapper.</returns>
+        private static NvJpegAPI CreateLatest()
+        {
+            Exception firstException = null;
+            var versions = Enum.GetValues(typeof(CuFFTAPIVersion));
+
+            for (var i = versions.Length - 1; i >= 0; i--)
+            {
+                var version = (NvJpegAPIVersion)versions.GetValue(i);
+                var api = CreateInternal(version);
+                if (api is null)
+                    continue;
+
+                try
+                {
+                    var status = api.GetProperty(
+                        LibraryPropertyType.MAJOR_VERSION,
+                        out _);
+                    if (status == NvJpegStatus.NVJPEG_STATUS_SUCCESS)
+                        return api;
+                }
+                catch (Exception ex) when (
+                    ex is DllNotFoundException ||
+                    ex is EntryPointNotFoundException)
+                {
+                    firstException ??= ex;
+                }
+            }
+
+            throw firstException ?? new DllNotFoundException(nameof(NvJpegAPI));
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Retrieves information about the supplied JPEG.
+        /// </summary>
+        /// <param name="libHandle">The NvJPEG library handle.</param>
+        /// <param name="imageBytes">The JPEG image bytes.</param>
+        /// <param name="numComponents">Filled in with the number of components.</param>
+        /// <param name="subsampling">Filled in with the subsampling.</param>
+        /// <param name="widths">Filled in with the widths.</param>
+        /// <param name="heights">Filled in with the heights.</param>
+        /// <returns>The error code.</returns>
+        public unsafe NvJpegStatus GetImageInfo(
+            IntPtr libHandle,
+            ReadOnlySpan<byte> imageBytes,
+            out int numComponents,
+            out NvJpegChromaSubsampling subsampling,
+            out int[] widths,
+            out int[] heights)
+        {
+            widths = new int[NvJpegConstants.NVJPEG_MAX_COMPONENT];
+            heights = new int[NvJpegConstants.NVJPEG_MAX_COMPONENT];
+
+            fixed (byte* imageBytesPtr = imageBytes)
+            fixed (int* widthsPtr = widths)
+            fixed (int* heightsPtr = heights)
+            {
+                return GetImageInfo(
+                    libHandle,
+                    imageBytesPtr,
+                    (ulong)imageBytes.Length,
+                    out numComponents,
+                    out subsampling,
+                    widthsPtr,
+                    heightsPtr);
+            }
+        }
+
+        /// <summary>
+        /// Performs single image decode.
+        /// </summary>
+        /// <param name="libHandle">The NvJPEG library handle</param>
+        /// <param name="stateHandle">The NvJPEG state handle.</param>
+        /// <param name="imageBytes">The JPEG image bytes.</param>
+        /// <param name="outputFormat">The desired output format.</param>
+        /// <param name="destination">The destination buffer.</param>
+        /// <param name="stream">The accelerator stream.</param>
+        /// <returns>The error code.</returns>
+        public unsafe NvJpegStatus Decode(
+            IntPtr libHandle,
+            IntPtr stateHandle,
+            ReadOnlySpan<byte> imageBytes,
+            NvJpegOutputFormat outputFormat,
+            in NvJpegImage destination,
+            CudaStream stream)
+        {
+            var imageInterop = destination.ToInterop();
+
+            fixed (byte* imageBytesPtr = imageBytes)
+            {
+                return Decode(
+                    libHandle,
+                    stateHandle,
+                    imageBytesPtr,
+                    (ulong)imageBytes.Length,
+                    outputFormat,
+                    &imageInterop,
+                    stream?.StreamPtr ?? IntPtr.Zero);
+            }
+        }
+
+        /// <inheritdoc cref="Decode(
+        ///     IntPtr,
+        ///     IntPtr,
+        ///     ReadOnlySpan{byte},
+        ///     NvJpegOutputFormat,
+        ///     in NvJpegImage,
+        ///     CudaStream)"/>
+        public NvJpegStatus Decode(
+            IntPtr libHandle,
+            IntPtr stateHandle,
+            ReadOnlySpan<byte> imageBytes,
+            NvJpegOutputFormat outputFormat,
+            in NvJpegImage destination) =>
+            Decode(
+                libHandle,
+                stateHandle,
+                imageBytes,
+                outputFormat,
+                destination,
+                null);
+
+        #endregion
+    }
+}
---
+++ Src/ILGPU.Algorithms/Runtime/Cuda/NvJpegStructs.cs
@@ -0,0 +1,120 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                   ILGPU.Algorithms
+//                      Copyright (c) 2021 ILGPU Algorithms Project
+//                                    www.ilgpu.net
+//
+// File: NvJpegStructs.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details
+// ---------------------------------------------------------------------------------------
+
+using System;
+
+#pragma warning disable CA1051 // Do not declare visible instance fields
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace ILGPU.Runtime.Cuda
+{
+    [CLSCompliant(false)]
+    public unsafe struct NvJpegImage_Interop
+    {
+        public fixed ulong Channel[NvJpegConstants.NVJPEG_MAX_COMPONENT];
+        public fixed ulong Pitch[NvJpegConstants.NVJPEG_MAX_COMPONENT];
+    }
+
+    [CLSCompliant(false)]
+    public struct NvJpegImage
+    {
+        #region Properties
+
+        public MemoryBuffer1D<byte, Stride1D.Dense>[] Channel;
+        public ulong[] Pitch;
+
+        #endregion
+
+        #region Static
+
+        /// <summary>
+        /// Creates image buffers to hold an image of the specified width, height and
+        /// number of components/channels.
+        /// </summary>
+        /// <param name="accelerator">The accelerator.</param>
+        /// <param name="width">The width (in bytes) per channel.</param>
+        /// <param name="height">The height (in bytes) per channel.</param>
+        /// <param name="numComponents">The number of components/channels.</param>
+        /// <returns>The allocated buffers.</returns>
+        public static NvJpegImage Create(
+            CudaAccelerator accelerator,
+            int width,
+            int height,
+            int numComponents)
+        {
+            if (width <= 0)
+                throw new ArgumentOutOfRangeException(nameof(width));
+            if (height <= 0)
+                throw new ArgumentOutOfRangeException(nameof(height));
+            if (numComponents <= 0 ||
+                numComponents > NvJpegConstants.NVJPEG_MAX_COMPONENT)
+
+
+                throw new ArgumentOutOfRangeException(nameof(numComponents));
+
+            var size = width * height;
+            var outputImage = new NvJpegImage
+            {
+                Channel = new MemoryBuffer1D<byte, Stride1D.Dense>[]
+                {
+                    numComponents >= 1 ? accelerator.Allocate1D<byte>(size) : null,
+                    numComponents >= 2 ? accelerator.Allocate1D<byte>(size) : null,
+                    numComponents >= 3 ? accelerator.Allocate1D<byte>(size) : null,
+                    numComponents >= 4 ? accelerator.Allocate1D<byte>(size) : null
+                },
+                Pitch = new ulong[]
+                {
+                    numComponents >= 1 ? (ulong)width : 0,
+                    numComponents >= 2 ? (ulong)width : 0,
+                    numComponents >= 3 ? (ulong)width : 0,
+                    numComponents >= 4 ? (ulong)width : 0,
+                }
+            };
+
+            return outputImage;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Gets a representation of the image buffer allocation that is suitable for
+        /// the NvJpeg Interop API.
+        /// </summary>
+        /// <returns>The interop data structure.</returns>
+        public unsafe NvJpegImage_Interop ToInterop()
+        {
+            ulong MemoryBufferToUInt64(MemoryBuffer1D<byte, Stride1D.Dense> buffer) =>
+                buffer != null
+                ? (ulong)buffer.View.BaseView.LoadEffectiveAddressAsPtr().ToInt64()
+                : 0L;
+
+            var imageInterop = new NvJpegImage_Interop();
+            for (var i = 0;
+                i < Math.Min(Channel.Length, NvJpegConstants.NVJPEG_MAX_COMPONENT);
+                i++)
+            {
+                imageInterop.Channel[i] = MemoryBufferToUInt64(Channel[i]);
+                imageInterop.Pitch[i] = Pitch[i];
+            }
+
+            return imageInterop;
+        }
+
+        #endregion
+    }
+}
+
+#pragma warning restore CA1051 // Do not declare visible instance fields
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
