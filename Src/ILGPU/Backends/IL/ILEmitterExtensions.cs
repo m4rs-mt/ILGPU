0d9bafb325903d2f2cd1d790f0a39aa8dc178d4e
Marcel Koester <m4rs@m4rs.net>
2023-01-26 20:48:37 +0100

    Added ILEmitter extensions to simplify MSIL code generation.

--- Src/ILGPU/Backends/IL/ILEmitterExtensions.cs
+++ Src/ILGPU/Backends/IL/ILEmitterExtensions.cs
@@ -9,6 +9,8 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
+using ILGPU.IR.Types;
+using ILGPU.IR.Values;
 using System;
 using System.Reflection;
 using System.Reflection.Emit;
@@ -20,12 +22,197 @@ namespace ILGPU.Backends.IL
     /// </summary>
     public static class ILEmitterExtensions
     {
+        #region Static Instance
+
         private static readonly MethodInfo GetHashCodeInfo = typeof(object).GetMethod(
             nameof(object.GetHashCode),
             BindingFlags.Public | BindingFlags.Instance);
         private static readonly MethodInfo EqualsInfo = typeof(object).GetMethod(
             nameof(object.Equals),
             BindingFlags.Public | BindingFlags.Instance);
+
+        /// <summary>
+        /// Caches all constant op codes.
+        /// </summary>
+        private static readonly OpCode[] ConstantOpCodes =
+            new OpCode[]
+            {
+                OpCodes.Ldc_I4_M1,
+                OpCodes.Ldc_I4_0,
+                OpCodes.Ldc_I4_1,
+                OpCodes.Ldc_I4_2,
+                OpCodes.Ldc_I4_3,
+                OpCodes.Ldc_I4_4,
+                OpCodes.Ldc_I4_5,
+                OpCodes.Ldc_I4_6,
+                OpCodes.Ldc_I4_7,
+                OpCodes.Ldc_I4_8,
+            };
+
+        /// <summary>
+        /// Stores the constructor of the <see cref="Half"/> type.
+        /// </summary>
+        private static readonly ConstructorInfo HalfConstructor =
+            typeof(Half).GetConstructor(
+                BindingFlags.NonPublic | BindingFlags.CreateInstance,
+                null,
+                new Type[] { typeof(ushort) },
+                null);
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Emits optimized code to load an integer constant.
+        /// </summary>
+        public static void LoadIntegerConstant<TILEmitter>(
+            this TILEmitter emitter,
+            int constant)
+            where TILEmitter : IILEmitter
+        {
+            if (constant >= -1 && constant < ConstantOpCodes.Length)
+                emitter.Emit(ConstantOpCodes[constant + 1]);
+            else
+                emitter.EmitConstant(constant);
+        }
+
+        /// <summary>
+        /// Calls a compatible shuffle method.
+        /// </summary>
+        public static void LoadConstant<TILEmitter>(
+            this TILEmitter emitter,
+            PrimitiveValue value,
+            ref ILLocal? temporaryHalf)
+            where TILEmitter : IILEmitter
+        {
+            switch (value.BasicValueType)
+            {
+                case BasicValueType.Int1:
+                    if (value.Int1Value)
+                        emitter.Emit(OpCodes.Ldc_I4_0);
+                    else
+                        emitter.Emit(OpCodes.Ldc_I4_1);
+                    break;
+                case BasicValueType.Int16:
+                    emitter.LoadIntegerConstant(value.Int16Value);
+                    break;
+                case BasicValueType.Int32:
+                    emitter.LoadIntegerConstant(value.Int32Value);
+                    break;
+                case BasicValueType.Int64:
+                    emitter.EmitConstant(value.Int64Value);
+                    break;
+                case BasicValueType.Float16:
+                    // Allocate a temporary variable and invoke the half constructor
+                    temporaryHalf ??= emitter.DeclareLocal(typeof(Half));
+                    emitter.Emit(LocalOperation.LoadAddress, temporaryHalf.Value);
+                    emitter.EmitConstant(value.Float16Value.RawValue);
+                    emitter.EmitNewObject(HalfConstructor);
+                    emitter.Emit(LocalOperation.Load, temporaryHalf.Value);
+                    break;
+                case BasicValueType.Float32:
+                    emitter.EmitConstant(value.Float32Value);
+                    break;
+                case BasicValueType.Float64:
+                    emitter.EmitConstant(value.Float64Value);
+                    break;
+                default:
+                    throw new NotSupportedIntrinsicException(
+                        value.BasicValueType.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Loads an object from an address in memory.
+        /// </summary>
+        /// <param name="emitter">The emitter instance.</param>
+        /// <param name="typeToLoad">The manged type to load.</param>
+        public static void LoadObject<TILEmitter>(
+            this TILEmitter emitter,
+            Type typeToLoad)
+            where TILEmitter : IILEmitter =>
+            emitter.Emit(OpCodes.Ldobj, typeToLoad);
+
+        /// <summary>
+        /// Generates code that loads a null value.
+        /// </summary>
+        public static ILLocal LoadNull<TILEmitter>(
+            this TILEmitter emitter,
+            Type type)
+            where TILEmitter : IILEmitter
+        {
+            var nullLocal = emitter.DeclareLocal(type);
+            // Check whether the given value is a reference type
+            if (type.IsClass)
+            {
+                // Emit a null reference
+                emitter.Emit(OpCodes.Ldnull, type);
+                emitter.Emit(LocalOperation.Store, nullLocal);
+            }
+            else
+            {
+                // Emit a new local variable that is initialized with null
+                emitter.Emit(LocalOperation.LoadAddress, nullLocal);
+                emitter.Emit(OpCodes.Initobj, type);
+            }
+            return nullLocal;
+        }
+
+        /// <summary>
+        /// Gets managed field info from a pre-defined converted structure type.
+        /// </summary>
+        /// <param name="type">The managed structure type.</param>
+        /// <param name="fieldIndex">The internal field index.</param>
+        /// <returns>The corresponding field info.</returns>
+        public static FieldInfo GetFieldInfo(Type type, int fieldIndex)
+        {
+            var fieldName = StructureType.GetFieldName(fieldIndex);
+            return type.GetField(fieldName);
+        }
+
+        /// <summary>
+        /// Emits code to load a field.
+        /// </summary>
+        public static void LoadField<TILEmitter>(
+            this TILEmitter emitter,
+            Type type,
+            int fieldIndex)
+            where TILEmitter : IILEmitter
+        {
+            var fieldInfo = GetFieldInfo(type, fieldIndex);
+            emitter.Emit(OpCodes.Ldfld, fieldInfo);
+        }
+
+        /// <summary>
+        /// Emits code to load the address of a field.
+        /// </summary>
+        public static void LoadFieldAddress<TILEmitter>(
+            this TILEmitter emitter,
+            Type type,
+            int fieldIndex)
+            where TILEmitter : IILEmitter
+        {
+            var fieldInfo = GetFieldInfo(type, fieldIndex);
+            emitter.Emit(OpCodes.Ldflda, fieldInfo);
+        }
+
+        /// <summary>
+        /// Emits code to store a value to a field.
+        /// </summary>
+        public static void StoreField<TILEmitter>(
+            this TILEmitter emitter,
+            Type type,
+            int fieldIndex)
+            where TILEmitter : IILEmitter
+        {
+            var fieldInfo = GetFieldInfo(type, fieldIndex);
+            emitter.Emit(OpCodes.Stfld, fieldInfo);
+        }
+
+        #endregion
+
+        #region Hash Code and Equals
 
         /// <summary>
         /// Generates hash code and equals functions for the given fields.
@@ -197,5 +384,7 @@ namespace ILGPU.Backends.IL
 
             return equals;
         }
+
+        #endregion
     }
 }
