7d9caf3e23b0c9864910eda64927c8c3e6fea2f3
Marcel Koester <m4rs@m4rs.net>
2023-03-02 18:36:58 +0100

    Added abstract internal VelocityParameters class to pass automatically generated parameter instances to Velocity kernels.

---
+++ Src/ILGPU/Runtime/Velocity/VelocityParameters.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2022-2023 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: VelocityParameters.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+namespace ILGPU.Runtime.Velocity
+{
+    /// <summary>
+    /// The base class for all velocity parameters.
+    /// </summary>
+    abstract class VelocityParameters
+    {
+        /// <summary>
+        /// Does nothing at the moment
+        /// </summary>
+        protected VelocityParameters() { }
+
+        /// <summary>
+        /// Dumps all parameters to the default console for debugging purposes.
+        /// </summary>
+        public abstract void DumpToConsole();
+    }
+}
+
