ca491eec1e3e93bbbf1b3eb1ea981977dc8552db
Marcel Koester <m4rs@m4rs.net>
2023-03-02 18:34:13 +0100

    Added new VelocityStream to represent streams in Velocity land.

---
+++ Src/ILGPU/Runtime/Velocity/VelocityStream.cs
@@ -0,0 +1,58 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2022-2023 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: VelocityStream.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+namespace ILGPU.Runtime.Velocity
+{
+    /// <summary>
+    /// Represents a velocity stream.
+    /// </summary>
+    sealed class VelocityStream : AcceleratorStream
+    {
+        #region Instance
+
+        /// <summary>
+        /// Constructs a new Velocity stream.
+        /// </summary>
+        /// <param name="accelerator">The associated accelerator.</param>
+        internal VelocityStream(Accelerator accelerator)
+            : base(accelerator)
+        { }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Does not perform any operation.
+        /// </summary>
+        public override void Synchronize() { }
+
+        /// <inheritdoc/>
+        protected override ProfilingMarker AddProfilingMarkerInternal()
+        {
+            using var binding = Accelerator.BindScoped();
+            return new VelocityProfilingMarker(Accelerator);
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        /// <summary>
+        /// Does not perform any operation.
+        /// </summary>
+        protected override void DisposeAcceleratorObject(bool disposing) { }
+
+        #endregion
+    }
+}
+
+
