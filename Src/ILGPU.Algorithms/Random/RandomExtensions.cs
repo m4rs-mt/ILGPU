967b26e093939227c1568f4c12a372fa64ff4d48
Marcel Koester <m4rs@m4rs.net>
2023-06-29 15:12:26 +0200

    Added RNG extensions to support float and double generation and initialization of views with random values.

--- Src/ILGPU.Algorithms/Random/RandomExtensions.cs
+++ Src/ILGPU.Algorithms/Random/RandomExtensions.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                   ILGPU Algorithms
-//                           Copyright (c) 2021 ILGPU Project
+//                        Copyright (c) 2021-2023 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: RandomExtensions.cs
@@ -95,6 +95,48 @@ namespace ILGPU.Algorithms.Random
         /// <param name="maxValue">The maximum values (exclusive)</param>
         /// <returns>A random int in [minValue..maxValue).</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Next<TRandomProvider>(
+            ref TRandomProvider randomProvider,
+            float minValue,
+            float maxValue)
+            where TRandomProvider : struct, IRandomProvider
+        {
+            Debug.Assert(minValue < maxValue, "Values out of range");
+            float dist = maxValue - minValue;
+            return Math.Min(
+                randomProvider.NextFloat() * dist + minValue,
+                maxValue);
+        }
+
+        /// <summary>
+        /// Generates a random int in [minValue..maxValue).
+        /// </summary>
+        /// <param name="randomProvider">The random provider.</param>
+        /// <param name="minValue">The minimum value (inclusive)</param>
+        /// <param name="maxValue">The maximum values (exclusive)</param>
+        /// <returns>A random int in [minValue..maxValue).</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double Next<TRandomProvider>(
+            ref TRandomProvider randomProvider,
+            double minValue,
+            double maxValue)
+            where TRandomProvider : struct, IRandomProvider
+        {
+            Debug.Assert(minValue < maxValue, "Values out of range");
+            double dist = maxValue - minValue;
+            return Math.Min(
+                randomProvider.NextDouble() * dist + minValue,
+                maxValue);
+        }
+
+        /// <summary>
+        /// Generates a random int in [minValue..maxValue).
+        /// </summary>
+        /// <param name="randomProvider">The random provider.</param>
+        /// <param name="minValue">The minimum value (inclusive)</param>
+        /// <param name="maxValue">The maximum values (exclusive)</param>
+        /// <returns>A random int in [minValue..maxValue).</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Next<TRandomProvider>(
             ref TRandomProvider randomProvider,
             int minValue,
@@ -138,6 +180,7 @@ namespace ILGPU.Algorithms.Random
         /// <typeparam name="TRandomProvider">The random provider type.</typeparam>
         /// <param name="accelerator">The current accelerator.</param>
         /// <param name="random">The parent RNG provider.</param>
+        [CLSCompliant(false)]
         public static RNG<TRandomProvider> CreateRNG<TRandomProvider>(
             this Accelerator accelerator,
             System.Random random)
@@ -153,11 +196,73 @@ namespace ILGPU.Algorithms.Random
         /// <param name="maxNumParallelWarps">
         /// The maximum number of parallel warps.
         /// </param>
+        [CLSCompliant(false)]
         public static RNG<TRandomProvider> CreateRNG<TRandomProvider>(
             this Accelerator accelerator,
             System.Random random,
             int maxNumParallelWarps)
             where TRandomProvider : unmanaged, IRandomProvider<TRandomProvider> =>
             RNG.Create<TRandomProvider>(accelerator, random, maxNumParallelWarps);
+
+        /// <summary>
+        /// Initialization kernel used to initialize the actual RNG values on the device.
+        /// </summary>
+        internal static void InitializeRNGKernel<TRandomProvider>(
+            Index1D index,
+            ArrayView<XorShift128Plus> sourceProviders,
+            ArrayView<TRandomProvider> randomProvider)
+            where TRandomProvider : unmanaged, IRandomProvider<TRandomProvider>
+        {
+            var provider = sourceProviders[index];
+            for (LongIndex1D i = index;
+                i < randomProvider.Length;
+                i += GridExtensions.GridStrideLoopStride)
+            {
+                randomProvider[i] = default(TRandomProvider).CreateProvider(ref provider);
+            }
+            
+            // Update provider state for future iterations
+            sourceProviders[index] = provider;
+        }
+
+        /// <summary>
+        /// Initializes a given view with random values.
+        /// </summary>
+        /// <typeparam name="TRandomProvider">
+        /// The RNG provider type to use on the device.
+        /// </typeparam>
+        /// <param name="accelerator">The current accelerator.</param>
+        /// <param name="stream">The current accelerator stream.</param>
+        /// <param name="rngView">The view to fill.</param>
+        /// <param name="random">The source RNG provider.</param>
+        /// <param name="numInitializers">The number of CPU initializers to use.</param>
+        [CLSCompliant(false)]
+        public static void InitRNGView<TRandomProvider>(
+            this Accelerator accelerator,
+            AcceleratorStream stream,
+            ArrayView<TRandomProvider> rngView,
+            System.Random random,
+            int numInitializers = ushort.MaxValue)
+            where TRandomProvider : unmanaged, IRandomProvider<TRandomProvider>
+        {
+            // Initialize a single provider per warp
+            numInitializers = (int)Math.Min(XMath.Clamp(
+                    numInitializers,
+                    accelerator.MaxNumThreadsPerGroup,
+                    rngView.Length),
+                ushort.MaxValue);
+            var providers = new XorShift128Plus[numInitializers];
+            for (int i = 0; i < numInitializers; ++i)
+                providers[i] = default(XorShift128Plus).CreateProvider(random);
+
+            // Initialize all providers in our buffer
+            using var tempProviderBuffer = accelerator.Allocate1D(providers);
+            accelerator.LaunchAutoGrouped(
+                InitializeRNGKernel,
+                stream,
+                new Index1D(providers.Length),
+                tempProviderBuffer.View.AsContiguous(),
+                rngView);
+        }
     }
 }
