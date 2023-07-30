ef45840c26e8068aa51a646c629fcd3a71907788
Marcel Koester <m4rs@m4rs.net>
2023-07-30 20:39:32 +0200

    Refactored IOptimizationFunction to support an additional abstract interface.

--- Src/ILGPU.Algorithms/Optimization/IOptimizationFunction.cs
+++ Src/ILGPU.Algorithms/Optimization/IOptimizationFunction.cs
@@ -13,10 +13,28 @@ using ILGPU.Algorithms.Vectors;
 using System;
 using System.Numerics;
 
-#if NET7_0_OR_GREATER
-
 namespace ILGPU.Algorithms.Optimization
 {
+    /// <summary>
+    /// An abstract optimization function supporting comparisons between evaluation types.
+    /// </summary>
+    /// <typeparam name="TEvalType">The evaluation data type.</typeparam>
+    public interface IBaseOptimizationFunction<TEvalType>
+        where TEvalType : struct, IEquatable<TEvalType>
+    {
+        /// <summary>
+        /// Compares the current evaluation value with the proposed one and returns true
+        /// if the current one is considered better in any way.
+        /// </summary>
+        /// <param name="current">The currently known value.</param>
+        /// <param name="proposed">The proposed evaluation value.</param>
+        /// <returns>
+        /// True if the current value is considered better than the proposed value.
+        /// </returns>
+        bool CurrentIsBetter(TEvalType current, TEvalType proposed);
+    }
+
+#if NET7_0_OR_GREATER
     /// <summary>
     /// A generic optimization function that defines the objective of an optimization
     /// process using evaluation and comparison methods.
@@ -24,7 +42,8 @@ namespace ILGPU.Algorithms.Optimization
     /// <typeparam name="TNumericType">The vectorized numeric type.</typeparam>
     /// <typeparam name="TElementType">The element type of a numeric type.</typeparam>
     /// <typeparam name="TEvalType">The evaluation data type.</typeparam>
-    public interface IOptimizationFunction<TNumericType, TElementType, TEvalType>
+    public interface IOptimizationFunction<TNumericType, TElementType, TEvalType> :
+        IBaseOptimizationFunction<TEvalType>
         where TNumericType : unmanaged, IVectorType<TNumericType, TElementType>
         where TElementType : unmanaged, INumber<TElementType>
         where TEvalType : unmanaged, IEquatable<TEvalType>
@@ -43,18 +62,7 @@ namespace ILGPU.Algorithms.Optimization
             LongIndex1D index,
             Index1D dimension,
             SingleVectorView<TNumericType> positionView);
-
-        /// <summary>
-        /// Compares the current evaluation value with the proposed one and returns true
-        /// if the current one is considered better in any way.
-        /// </summary>
-        /// <param name="current">The currently known value.</param>
-        /// <param name="proposed">The proposed evaluation value.</param>
-        /// <returns>
-        /// True if the current value is considered better than the proposed value.
-        /// </returns>
-        bool CurrentIsBetter(TEvalType current, TEvalType proposed);
     }
+#endif
 }
 
-#endif
