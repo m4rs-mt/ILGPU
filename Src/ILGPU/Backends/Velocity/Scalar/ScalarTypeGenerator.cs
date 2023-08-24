be8de8ed66b844da2788eb16ece88d36f82e204c
Marcel Koester <m4rs@m4rs.net>
2023-08-24 21:16:25 +0200

    Added scalar type generator to support building pseudo-vectorized types.

---
+++ Src/ILGPU/Backends/Velocity/Scalar/ScalarTypeGenerator.cs
@@ -0,0 +1,71 @@
+// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                           Copyright (c) 2023 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: ScalarTypeGenerator.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+using ILGPU.Backends.IL;
+using ILGPU.Runtime.Velocity;
+using System;
+
+namespace ILGPU.Backends.Velocity.Scalar
+{
+    /// <summary>
+    /// A scalar type generator to be used with the Velocity backend.
+    /// </summary>
+    sealed class ScalarTypeGenerator : VelocityTypeGenerator
+    {
+        #region Static
+
+        /// <summary>
+        /// Maps basic types to vectorized basic types.
+        /// </summary>
+        private static readonly Type[] VectorizedBasicTypeMapping = new Type[]
+        {
+            ScalarOperations2.WarpType32, // None/Unknown
+
+            ScalarOperations2.WarpType32, // Int1
+            ScalarOperations2.WarpType32, // Int8
+            ScalarOperations2.WarpType32, // Int16
+            ScalarOperations2.WarpType32, // Int32
+            ScalarOperations2.WarpType64, // Int64
+
+            ScalarOperations2.WarpType32, // Float16
+            ScalarOperations2.WarpType32, // Float32
+            ScalarOperations2.WarpType64, // Float64
+        };
+
+        #endregion
+
+        #region Instance
+
+        /// <summary>
+        /// Constructs a new IL scalar code generator.
+        /// </summary>
+        /// <param name="capabilityContext">The parent capability context.</param>
+        /// <param name="runtimeSystem">The parent runtime system.</param>
+        public ScalarTypeGenerator(
+            VelocityCapabilityContext capabilityContext,
+            RuntimeSystem runtimeSystem)
+            : base(capabilityContext, runtimeSystem, ScalarOperations2.WarpSize)
+        { }
+
+        #endregion
+
+        #region Type System
+
+        public override Type GetVectorizedBasicType(BasicValueType basicValueType)
+        {
+            if (basicValueType == BasicValueType.Float16 && !CapabilityContext.Float16)
+                throw VelocityCapabilityContext.GetNotSupportedFloat16Exception();
+            return VectorizedBasicTypeMapping[(int)basicValueType];
+        }
+
+        #endregion
+    }
+}
