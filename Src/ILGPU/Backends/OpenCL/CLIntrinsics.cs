bfa942db0896fdb4f728f864eb867e955155060f
Marcel Koester <m4rs@m4rs.net>
2023-09-07 14:11:02 +0200

    Refined Atomics. (#1063)
    * Updated Atomic functions to use readonly structures.
    * Fixed intrinsic mapping of Atomic.Min and Atomic.Max functions for FP types.
    * Adapted intrinsic OpenCL bindings.
    * Adapted intrinsic PTX bindings.

--- Src/ILGPU/Backends/OpenCL/CLIntrinsics.cs
+++ Src/ILGPU/Backends/OpenCL/CLIntrinsics.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2020-2021 ILGPU Project
+//                        Copyright (c) 2020-2023 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: CLIntrinsics.cs
@@ -57,10 +57,34 @@ namespace ILGPU.Backends.OpenCL
                     nameof(AtomicAddF32),
                     IntrinsicImplementationMode.Redirect));
             manager.RegisterGenericAtomic(
+                AtomicKind.Min,
+                BasicValueType.Float32,
+                CreateIntrinsic(
+                    nameof(AtomicMinF32),
+                    IntrinsicImplementationMode.Redirect));
+            manager.RegisterGenericAtomic(
+                AtomicKind.Max,
+                BasicValueType.Float32,
+                CreateIntrinsic(
+                    nameof(AtomicMaxF32),
+                    IntrinsicImplementationMode.Redirect));
+            manager.RegisterGenericAtomic(
                 AtomicKind.Add,
                 BasicValueType.Float64,
                 CreateIntrinsic(
                     nameof(AtomicAddF64),
+                    IntrinsicImplementationMode.Redirect));
+            manager.RegisterGenericAtomic(
+                AtomicKind.Min,
+                BasicValueType.Float64,
+                CreateIntrinsic(
+                    nameof(AtomicMinF64),
+                    IntrinsicImplementationMode.Redirect));
+            manager.RegisterGenericAtomic(
+                AtomicKind.Max,
+                BasicValueType.Float64,
+                CreateIntrinsic(
+                    nameof(AtomicMaxF64),
                     IntrinsicImplementationMode.Redirect));
 
             // Group
@@ -74,6 +98,94 @@ namespace ILGPU.Backends.OpenCL
         #endregion
 
         #region Atomics
+
+        /// <summary>
+        /// Represents an atomic min operation in software.
+        /// </summary>
+        private readonly struct MinFloat : IAtomicOperation<float>
+        {
+            public float Operation(float current, float value) =>
+                IntrinsicMath.Min(current, value);
+        }
+
+        /// <summary>
+        /// A software implementation for atomic max on 32-bit floats.
+        /// </summary>
+        /// <param name="target">The target address.</param>
+        /// <param name="value">The value to add.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static float AtomicMinF32(ref float target, float value) =>
+            Atomic.MakeAtomic(
+                ref target,
+                value,
+                new MinFloat(),
+                new CompareExchangeFloat());
+
+        /// <summary>
+        /// Represents an atomic max operation in software.
+        /// </summary>
+        private readonly struct MaxFloat : IAtomicOperation<float>
+        {
+            public float Operation(float current, float value) =>
+                IntrinsicMath.Max(current, value);
+        }
+
+        /// <summary>
+        /// A software implementation for atomic max on 32-bit floats.
+        /// </summary>
+        /// <param name="target">The target address.</param>
+        /// <param name="value">The value to add.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static float AtomicMaxF32(ref float target, float value) =>
+            Atomic.MakeAtomic(
+                ref target,
+                value,
+                new MaxFloat(),
+                new CompareExchangeFloat());
+
+        /// <summary>
+        /// Represents an atomic min operation in software.
+        /// </summary>
+        private readonly struct MinDouble : IAtomicOperation<double>
+        {
+            public double Operation(double current, double value) =>
+                IntrinsicMath.Min(current, value);
+        }
+
+        /// <summary>
+        /// A software implementation for atomic max on 64-bit floats.
+        /// </summary>
+        /// <param name="target">The target address.</param>
+        /// <param name="value">The value to add.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static double AtomicMinF64(ref double target, double value) =>
+            Atomic.MakeAtomic(
+                ref target,
+                value,
+                new MinDouble(),
+                new CompareExchangeDouble());
+
+        /// <summary>
+        /// Represents an atomic max operation in software.
+        /// </summary>
+        private readonly struct MaxDouble : IAtomicOperation<double>
+        {
+            public double Operation(double current, double value) =>
+                IntrinsicMath.Max(current, value);
+        }
+
+        /// <summary>
+        /// A software implementation for atomic max on 64-bit floats.
+        /// </summary>
+        /// <param name="target">The target address.</param>
+        /// <param name="value">The value to add.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static double AtomicMaxF64(ref double target, double value) =>
+            Atomic.MakeAtomic(
+                ref target,
+                value,
+                new MaxDouble(),
+                new CompareExchangeDouble());
 
         /// <summary>
         /// Represents an atomic add operation of type float.
