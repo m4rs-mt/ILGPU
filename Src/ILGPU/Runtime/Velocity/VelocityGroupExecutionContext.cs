304e4bba67359c23bead9a8bf6386363bee52a87
Marcel Koester <m4rs@m4rs.net>
2023-09-08 09:32:05 +0200

    Removed thread-value properties from VelocityGroupExecutionContext.

--- Src/ILGPU/Runtime/Velocity/VelocityGroupExecutionContext.cs
+++ Src/ILGPU/Runtime/Velocity/VelocityGroupExecutionContext.cs
@@ -38,61 +38,18 @@ namespace ILGPU.Runtime.Velocity
         }
 
         /// <summary>
-        /// Returns the current grid index.
-        /// </summary>
-        public int GridIdx { get; private set; }
-
-        /// <summary>
-        /// Returns the current group dimension.
-        /// </summary>
-        public int GroupDim { get; private set; }
-
-        /// <summary>
-        /// Returns the current grid dimension.
-        /// </summary>
-        public int GridDim { get; private set; }
-
-        /// <summary>
-        /// Returns the user-specific total size.
-        /// </summary>
-        public int UserSize { get; private set; }
-
-        /// <summary>
         /// Returns a view to dynamic shared memory (if any).
         /// </summary>
         public ArrayView<byte> DynamicSharedMemory { get; private set; }
 
         /// <summary>
-        /// Returns the linear group .
-        /// </summary>
-        public int GroupOffset => GridIdx * GroupDim;
-
-        /// <summary>
-        /// Resets this execution context.
-        /// </summary>
-        private void Reset()
-        {
-            sharedMemoryPool.Reset();
-            localMemoryPool.Reset();
-        }
-
-        /// <summary>
         /// Sets up the current thread grid information for the current thread group.
         /// </summary>
-        public void SetupThreadGrid(
-            int gridIdx,
-            int groupDim,
-            int gridDim,
-            int userSize,
-            int dynamicSharedMemoryLength)
+        public void SetupThreadGrid(int dynamicSharedMemoryLength)
         {
-            GridIdx = gridIdx;
-            GridDim = gridDim;
-            GroupDim = groupDim;
-            UserSize = userSize;
-
             // Reset everything
-            Reset();
+            sharedMemoryPool.Reset();
+            localMemoryPool.Reset();
 
             // Allocate dynamic shared memory
             if (dynamicSharedMemoryLength > 0)
