d49fd60b89f0e04487af372179f3f2a7ae06c7fa
MoFtZ <MoFtZ@users.noreply.github.com>
2023-12-02 02:01:54 +1100

    Fixed marshalling ArrayView.Empty as kernel parameter. (#1131)

--- Src/ILGPU/Backends/PointerViews/Utilities.cs
+++ Src/ILGPU/Backends/PointerViews/Utilities.cs
@@ -92,7 +92,7 @@ namespace ILGPU.Backends.PointerViews
         /// <returns>The underlying native pointer.</returns>
         private static IntPtr GetNativePtr<T>(in ArrayView<T> view)
             where T : unmanaged =>
-            view.Buffer.NativePtr;
+            view.Buffer?.NativePtr ?? IntPtr.Zero;
 
         /// <summary>
         /// Gets the native-pointer method for the given element type.
