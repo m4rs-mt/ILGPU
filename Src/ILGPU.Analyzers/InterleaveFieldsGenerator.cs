2092eb76952018a9f381b7ddbcbf2113ee2e3655
MoFtZ <MoFtZ@users.noreply.github.com>
2024-01-08 17:28:36 +1100

    Updated copyright year.

--- Src/ILGPU.Analyzers/InterleaveFieldsGenerator.cs
+++ Src/ILGPU.Analyzers/InterleaveFieldsGenerator.cs
@@ -1,9 +1,9 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                           Copyright (c) 2023 ILGPU Project
+//                        Copyright (c) 2023-2024 ILGPU Project
 //                                    www.ilgpu.net
 //
-// File: SourceGenerator.cs
+// File: InterleaveFieldsGenerator.cs
 //
 // This file is part of ILGPU and is distributed under the University of Illinois Open
 // Source License. See LICENSE.txt for details.
--- Tools/CopyrightUpdateTool/Parsers/SourceCodeCopyrightParser.cs
+++ Tools/CopyrightUpdateTool/Parsers/SourceCodeCopyrightParser.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2022-2023 ILGPU Project
+//                        Copyright (c) 2022-2024 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: SourceCodeCopyrightParser.cs
