7e4cc85e7d16a613cf08abc4bbea76b07f95339e
Yey007 <yeeeeeeey007@gmail.com>
2024-02-20 15:30:22 -0500

    Update style

--- Samples/RefTypeAnalyzer/Program.cs
+++ Samples/RefTypeAnalyzer/Program.cs
@@ -22,24 +22,26 @@ class Program
     {
         return new MyRefType().Hello;
     }
-    
+
+    // TODO: tests needed: normal, using stuff from the class context, arrays, arrays of
+    // ref types, constructor calls, method/function calls
     static void Kernel(Index1D index, ArrayView<int> input, ArrayView<int> output)
     {
         // This is disallowed, since MyRefType is a reference type
         var refType = new MyRefType();
         output[index] = input[index] + refType.Hello;
-        
+
         // Allocating arrays of unmanaged types is fine
         MyUnmanagedType[] array = [new MyUnmanagedType()];
         int[] ints = [0, 1, 2];
-        
+
         // But arrays of reference types are still disallowed
         MyRefType[] refs = [new MyRefType()];
-        
+
         // Any functions that may be called are also analyzed
         int result = AnotherFunction();
     }
-    
+
     static void Main(string[] args)
     {
         using var context = Context.CreateDefault();
@@ -48,11 +50,14 @@ class Program
 
         using var input = accelerator.Allocate1D<int>(1024);
         using var output = accelerator.Allocate1D<int>(1024);
-        
-        var kernel = accelerator.LoadAutoGroupedStreamKernel<Index1D, ArrayView<int>, ArrayView<int>>(Kernel);
+
+        var kernel =
+            accelerator
+                .LoadAutoGroupedStreamKernel<Index1D, ArrayView<int>, ArrayView<int>>(
+                    Kernel);
 
         kernel(input.IntExtent, input.View, output.View);
-        
+
         accelerator.Synchronize();
     }
 }
--- Src/ILGPU.Analyzers/KernelAnalyzer.cs
+++ Src/ILGPU.Analyzers/KernelAnalyzer.cs
@@ -10,14 +10,15 @@ namespace ILGPU.Analyzers
 {
     public abstract class KernelAnalyzer : DiagnosticAnalyzer
     {
-        private readonly ImmutableHashSet<string> kernelLoadNames = ImmutableHashSet.Create(
-            "LoadKernel",
-            "LoadAutoGroupedKernel",
-            "LoadImplicitlyGroupedKernel",
-            "LoadStreamKernel",
-            "LoadAutoGroupedStreamKernel",
-            "LoadImplicitlyGroupedStreamKernel"
-        );
+        private readonly ImmutableHashSet<string> _kernelLoadNames =
+            ImmutableHashSet.Create(
+                "LoadKernel",
+                "LoadAutoGroupedKernel",
+                "LoadImplicitlyGroupedKernel",
+                "LoadStreamKernel",
+                "LoadAutoGroupedStreamKernel",
+                "LoadImplicitlyGroupedStreamKernel"
+                );
 
         /// <summary>
         /// Called for every operation potentially reachable from a kernel.
@@ -26,8 +27,8 @@ namespace ILGPU.Analyzers
         /// The analysis context used to report diagnostics.
         /// </param>
         /// <param name="operation">
-        /// The operation. Operations for subsequent calls may be parents or descendants of
-        /// an operation for a previous call.
+        /// The operation. Operations for subsequent calls may be parents or descendants
+        /// of an operation for a previous call.
         /// </param>
         protected abstract void AnalyzeKernelOperation(
             OperationAnalysisContext context,
@@ -51,21 +52,23 @@ namespace ILGPU.Analyzers
             var methodSymbol = invocationOperation.TargetMethod;
 
             if (methodSymbol.MethodKind != MethodKind.Ordinary
-                || !kernelLoadNames.Contains(methodSymbol.Name))
+                || !_kernelLoadNames.Contains(methodSymbol.Name))
                 return;
 
             var kernelArg = invocationOperation.Arguments.FirstOrDefault(x =>
                 x.Parameter?.Type.TypeKind == TypeKind.Delegate);
 
+            // TODO: support expressions that return delegate (probably requires dataflow)
             if (kernelArg?.Value is
                 IDelegateCreationOperation
-                delegateOp) // TODO: support expressions that return delegate (probably requires dataflow)
+                delegateOp)
             {
                 var semanticModel = context.Operation.SemanticModel;
 
                 var bodyOp = delegateOp.Target switch
                 {
-                    IMethodReferenceOperation refOp => GetMethodBody(semanticModel, refOp.Method),
+                    IMethodReferenceOperation refOp => GetMethodBody(semanticModel,
+                        refOp.Method),
                     IAnonymousFunctionOperation anonymousOp => anonymousOp.Body,
                     _ => null
                 };
@@ -75,7 +78,8 @@ namespace ILGPU.Analyzers
         }
 
         // TODO: iterative using stack
-        private void RecursivelyAnalyzeKernelOperations(OperationAnalysisContext context, IOperation? bodyOp,
+        private void RecursivelyAnalyzeKernelOperations(OperationAnalysisContext context,
+            IOperation? bodyOp,
             HashSet<IOperation>? seen = null)
         {
             seen ??= new HashSet<IOperation>();
@@ -90,24 +94,29 @@ namespace ILGPU.Analyzers
                 seen.Add(descendant);
                 AnalyzeKernelOperation(context, descendant);
 
-                // Okay, so this doesn't cover every possible way other code can be called through a kernel.
-                // But this is in general quite difficult to do. We can improve things over time as necessary, perhaps.
-                // Thankfully, if we accept a degree of false positives, we probably don't need to solve the halting problem :)
+                // Okay, so this doesn't cover every possible way other code can be called
+                // through a kernel. But this is in general quite difficult to do. We can
+                // improve things over time as necessary, perhaps. Thankfully, if we
+                // accept a degree of false positives, we probably don't need to solve the
+                // halting problem :)
                 if (descendant is IInvocationOperation kernelMethodCallOperation)
                 {
-                    var innerBodyOp = GetMethodBody(semanticModel, kernelMethodCallOperation.TargetMethod);
+                    var innerBodyOp = GetMethodBody(semanticModel,
+                        kernelMethodCallOperation.TargetMethod);
                     RecursivelyAnalyzeKernelOperations(context, innerBodyOp, seen);
                 }
             }
         }
 
-        private IOperation? GetMethodBody(SemanticModel? model, IMethodSymbol symbol) => symbol switch
-        {
-            { IsPartialDefinition: false } => model?.GetOperation(
-                symbol.DeclaringSyntaxReferences[0].GetSyntax()),
-            { PartialImplementationPart: not null } => model?.GetOperation(
-                symbol.PartialImplementationPart.DeclaringSyntaxReferences[0].GetSyntax()),
-            _ => null
-        };
+        private IOperation? GetMethodBody(SemanticModel? model, IMethodSymbol symbol) =>
+            symbol switch
+            {
+                { IsPartialDefinition: false } => model?.GetOperation(
+                    symbol.DeclaringSyntaxReferences[0].GetSyntax()),
+                { PartialImplementationPart: not null } => model?.GetOperation(
+                    symbol.PartialImplementationPart.DeclaringSyntaxReferences[0]
+                        .GetSyntax()),
+                _ => null
+            };
     }
 }
