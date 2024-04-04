716c3fbac0a16f8cef9b4394a086d1f5a52aaaa2
Isabelle Santin <isabelle@chosenfewsoftware.com>
2024-04-04 14:10:43 -0500

    Beginnings of IR bytecode representation

---
+++ Src/ILGPU/Backends/IR/IRByteCodeBuilder.cs
@@ -0,0 +1,28 @@
+﻿using ILGPU.IR;
+using ILGPU.IR.Values;
+using System;
+using System.IO;
+
+namespace ILGPU.Backends.IR
+{
+    class IRByteCodeBuilder
+    {
+        private readonly BinaryWriter _writer;
+
+        public IRByteCodeBuilder()
+        {
+            _writer = new BinaryWriter(new MemoryStream());
+        }
+
+        public IRByteCodeBuilder(byte[] buffer)
+        {
+            _writer = new BinaryWriter(new MemoryStream(buffer));
+        }
+
+        public void EmitValue(Value value, Action<BinaryWriter> callback)
+        {
+            _writer.Write((int)value.ValueKind);
+            callback(_writer);
+        }
+    }
+}
