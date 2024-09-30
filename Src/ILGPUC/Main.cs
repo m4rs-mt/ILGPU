6c082b1fc35224d3584ee663f09a99aaf3dbc7b2
Marcel Koester <m4rs@m4rs.net>
2024-09-30 22:39:09 +0000

    Created main entry point for ILGPUC command line compiler.

---
+++ Src/ILGPUC/Main.cs
@@ -0,0 +1,29 @@
+// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                           Copyright (c) 2025 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: Program.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+using System;
+
+namespace ILGPU;
+
+/// <summary>
+/// Main ILGPUC program class.
+/// </summary>
+class ILGPUC
+{
+    /// <summary>
+    /// Main entry point of the ILGPUC command line compiler.
+    /// </summary>
+    /// <param name="args">The command line args passed to ILGPUC.</param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("ILGPUC command line compiler");
+    }
+}
