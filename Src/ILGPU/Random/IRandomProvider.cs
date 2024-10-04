36b8791c1746a4af8d61a842c379c7ee79cc6c3c
Marcel Koester <m4rs@m4rs.net>
2024-10-04 01:42:47 +0000

    Refined IRandomProvider interfaces to support modern .Net features and abstract period shifts.

--- Src/ILGPU/Random/IRandomProvider.cs
+++ Src/ILGPU/Random/IRandomProvider.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU Algorithms
-//                        Copyright (c) 2021-2023 ILGPU Project
+//                                        ILGPU
+//                        Copyright (c) 2021-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: IRandomProvider.cs
@@ -9,158 +9,75 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
 
-namespace ILGPU.Algorithms.Random
+namespace ILGPU.Random;
+
+/// <summary>
+/// Specifies an abstract RNG provider.
+/// </summary>
+public interface IRandomProvider
 {
     /// <summary>
-    /// Specifies an abstract RNG provider.
+    /// Generates a random int in [0..int.MaxValue].
     /// </summary>
-    public interface IRandomProvider
-    {
-        /// <summary>
-        /// Generates a random int in [0..int.MaxValue].
-        /// </summary>
-        /// <returns>A random int in [0..int.MaxValue].</returns>
-        [SuppressMessage(
-            "Naming",
-            "CA1716:Identifiers should not match keywords",
-            Justification = "Like the method System.Random.Next()")]
-        int Next();
-
-        /// <summary>
-        /// Generates a random long in [0..long.MaxValue].
-        /// </summary>
-        /// <returns>A random long in [0..long.MaxValue].</returns>
-        long NextLong();
-
-        /// <summary>
-        /// Generates a random float in [0..1).
-        /// </summary>
-        /// <returns>A random float in [0..1).</returns>
-        float NextFloat();
-
-        /// <summary>
-        /// Generates a random double in [0..1).
-        /// </summary>
-        /// <returns>A random double in [0..1).</returns>
-        double NextDouble();
-    }
+    /// <returns>A random int in [0..int.MaxValue].</returns>
+    [SuppressMessage(
+        "Naming",
+        "CA1716:Identifiers should not match keywords",
+        Justification = "Like the method System.Random.Next()")]
+    int Next();
 
     /// <summary>
-    /// An abstract RNG provider that supports period shifts.
+    /// Generates a random long in [0..long.MaxValue].
     /// </summary>
-    /// <typeparam name="TProvider">The implementing provider type.</typeparam>
-    public interface IRandomProvider<TProvider> : IRandomProvider
-        where TProvider : struct, IRandomProvider<TProvider>
-    {
-        /// <summary>
-        /// Shifts the current period.
-        /// </summary>
-        /// <param name="shift">The shift amount.</param>
-        void ShiftPeriod(int shift);
+    /// <returns>A random long in [0..long.MaxValue].</returns>
+    long NextLong();
 
-        /// <summary>
-        /// Instantiates a new provider using the internal random state.
-        /// </summary>
-        /// <returns>The next provider instance.</returns>
-        TProvider NextProvider();
+    /// <summary>
+    /// Generates a random float in [0..1).
+    /// </summary>
+    /// <returns>A random float in [0..1).</returns>
+    float NextFloat();
 
-        /// <summary>
-        /// Instantiates a new provider using the given random.
-        /// </summary>
-        /// <param name="random">The parent RNG instance.</param>
-        /// <returns>The next provider instance.</returns>
-        TProvider CreateProvider(System.Random random);
+    /// <summary>
+    /// Generates a random double in [0..1).
+    /// </summary>
+    /// <returns>A random double in [0..1).</returns>
+    double NextDouble();
 
-        /// <summary>
-        /// Instantiates a new provider using the given random.
-        /// </summary>
-        /// <param name="random">The parent RNG instance.</param>
-        /// <returns>The next provider instance.</returns>
-        TProvider CreateProvider<TRandomProvider>(ref TRandomProvider random)
-            where TRandomProvider : struct, IRandomProvider<TRandomProvider>;
-    }
+    /// <summary>
+    /// Shifts the current period.
+    /// </summary>
+    /// <param name="shift">The shift amount.</param>
+    void ShiftPeriod(int shift);
+}
 
-    namespace Operations
-    {
-        /// <summary>
-        /// An operation that can be executed using a provider instance.
-        /// </summary>
-        /// <typeparam name="T">The operation return type.</typeparam>
-        /// <typeparam name="TRandomProvider">The provider type.</typeparam>
-        public interface IRandomProviderOperation<T, TRandomProvider>
-            where T : struct
-            where TRandomProvider : struct, IRandomProvider
-        {
-            /// <summary>
-            /// Applies the current operation to the RNG provider.
-            /// </summary>
-            /// <param name="randomProvider">The RNG provider to use.</param>
-            /// <returns>The return value.</returns>
-            T Apply(ref TRandomProvider randomProvider);
-        }
+/// <summary>
+/// An abstract RNG provider that supports period shifts.
+/// </summary>
+/// <typeparam name="TSelf">The implementing provider type.</typeparam>
+public interface IRandomProvider<TSelf> : IRandomProvider
+    where TSelf : struct, IRandomProvider<TSelf>
+{
+    /// <summary>
+    /// Instantiates a new provider using the internal random state.
+    /// </summary>
+    /// <returns>The next provider instance.</returns>
+    TSelf NextProvider();
 
-        /// <summary>
-        /// Invokes <see cref="IRandomProvider.Next"/> on a RNG state.
-        /// </summary>
-        /// <typeparam name="TRandomProvider">The provider type.</typeparam>
-        public readonly struct NextIntOperation<TRandomProvider> :
-            IRandomProviderOperation<int, TRandomProvider>
-            where TRandomProvider : struct, IRandomProvider
-        {
-            /// <inheritdoc cref="IRandomProviderOperation{T, TRandomProvider}.
-            /// Apply(ref TRandomProvider)"/>"
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public int Apply(ref TRandomProvider randomProvider) =>
-                randomProvider.Next();
-        }
+    /// <summary>
+    /// Instantiates a new provider using the given random.
+    /// </summary>
+    /// <param name="random">The parent RNG instance.</param>
+    /// <returns>The next provider instance.</returns>
+    static abstract TSelf CreateProvider(System.Random random);
 
-        /// <summary>
-        /// Invokes <see cref="IRandomProvider.NextLong"/> on a RNG state.
-        /// </summary>
-        /// <typeparam name="TRandomProvider">The provider type.</typeparam>
-        public readonly struct NextLongOperation<TRandomProvider> :
-            IRandomProviderOperation<long, TRandomProvider>
-            where TRandomProvider : struct, IRandomProvider
-        {
-            /// <inheritdoc cref="IRandomProviderOperation{T, TRandomProvider}.
-            /// Apply(ref TRandomProvider)"/>"
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public long Apply(ref TRandomProvider randomProvider) =>
-                randomProvider.NextLong();
-        }
-
-        /// <summary>
-        /// Invokes <see cref="IRandomProvider.NextFloat"/> on a RNG state.
-        /// </summary>
-        /// <typeparam name="TRandomProvider">The provider type.</typeparam>
-        public readonly struct NextFloatOperation<TRandomProvider> :
-            IRandomProviderOperation<float, TRandomProvider>
-            where TRandomProvider : struct, IRandomProvider
-        {
-            /// <inheritdoc cref="IRandomProviderOperation{T, TRandomProvider}.
-            /// Apply(ref TRandomProvider)"/>"
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public float Apply(ref TRandomProvider randomProvider) =>
-                randomProvider.NextFloat();
-        }
-
-        /// <summary>
-        /// Invokes <see cref="IRandomProvider.NextDouble"/> on a RNG state.
-        /// </summary>
-        /// <typeparam name="TRandomProvider">The provider type.</typeparam>
-        public readonly struct NextDoubleOperation<TRandomProvider> :
-            IRandomProviderOperation<double, TRandomProvider>
-            where TRandomProvider : struct, IRandomProvider
-        {
-            /// <inheritdoc cref="IRandomProviderOperation{T, TRandomProvider}.
-            /// Apply(ref TRandomProvider)"/>"
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public double Apply(ref TRandomProvider randomProvider) =>
-                randomProvider.NextDouble();
-        }
-    }
+    /// <summary>
+    /// Instantiates a new provider using the given random.
+    /// </summary>
+    /// <param name="random">The parent RNG instance.</param>
+    /// <returns>The next provider instance.</returns>
+    static abstract TSelf CreateProvider<TRandomProvider>(ref TRandomProvider random)
+        where TRandomProvider : struct, IRandomProvider<TRandomProvider>;
 }
