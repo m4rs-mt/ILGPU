5c0027b83efa04558d9573375bb6910deab46b9e
Marcel Koester <m4rs@m4rs.net>
2024-10-10 23:22:44 +0000

    Moved BinaryLog logic to ILGPUC and refined validation logic.

---
+++ Src/ILGPUC/Intrinsic/Math/BinaryLog.cs
@@ -0,0 +1,91 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2024-2025 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: BinaryLog.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+namespace ILGPU.Intrinsic.Math;
+
+/// <summary>
+/// Contains software implementation for Log with two parameters.
+/// </summary>
+static class BinaryLog
+{
+    /// <summary>
+    /// Implements Log with two parameters.
+    /// </summary>
+    public static double Log(double value, double newBase)
+    {
+        if (value < 0.0 |
+            newBase < 0.0 |
+            (value != 1.0 & newBase == 0.0) ||
+            (value != 1.0 & newBase == double.PositiveInfinity) |
+            XMath.IsNaN(value) | XMath.IsNaN(newBase) |
+            newBase > 1.0 - 1e-9)
+        {
+            return double.NaN;
+        }
+
+        if (value == 0.0)
+        {
+            if (0.0 < newBase & newBase < 1.0)
+                return double.PositiveInfinity;
+            else if (newBase > 1.0)
+                return double.NegativeInfinity;
+        }
+
+        if (value == double.PositiveInfinity)
+        {
+            if (0.0 < newBase & newBase < 1.0)
+                return double.NegativeInfinity;
+            else if (newBase > 1.0)
+                return double.PositiveInfinity;
+        }
+
+        if (value == 1.0 & (newBase == 0.0 | newBase == double.PositiveInfinity))
+            return 0.0;
+
+        return XMath.Log(value) * XMath.Rcp(XMath.Log(newBase));
+    }
+
+    /// <summary>
+    /// Implements Log with two parameters.
+    /// </summary>
+    public static float Log(float value, float newBase)
+    {
+        if (value < 0.0f | newBase < 0.0f |
+            (value != 1.0f & newBase == 0.0f) |
+            (value != 1.0f & newBase == float.PositiveInfinity) |
+            XMath.IsNaN(value) || XMath.IsNaN(newBase) ||
+            newBase > 1.0f - 1e-6f)
+        {
+            return float.NaN;
+        }
+
+        if (value == 0.0f)
+        {
+            if (0.0f < newBase & newBase < 1.0f)
+                return float.PositiveInfinity;
+            else if (newBase > 1.0f)
+                return float.NegativeInfinity;
+        }
+
+        if (value == float.PositiveInfinity)
+        {
+            if (0.0f < newBase & newBase < 1.0f)
+                return float.NegativeInfinity;
+            else if (newBase > 1.0f)
+                return float.PositiveInfinity;
+        }
+
+        if (value == 1.0f & (newBase == 0.0f | newBase == float.PositiveInfinity))
+            return 0.0f;
+
+        return XMath.Log(value) * XMath.Rcp(XMath.Log(newBase));
+    }
+}
