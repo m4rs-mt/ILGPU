b873f7ee11d193e6263150ce9da7ac3a7c43a2d6
Marcel Koester <m4rs@m4rs.net>
2024-10-12 12:49:32 +0000

    Updated XMath to include sign and CopySign methods for various numeric types with intrinsic optimizations.

--- Src/ILGPU/XMath/Sign.cs
+++ Src/ILGPU/XMath/Sign.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU Algorithms
-//                        Copyright (c) 2019-2021 ILGPU Project
+//                                        ILGPU
+//                        Copyright (c) 2019-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: Sign.cs
@@ -9,34 +9,100 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
+using ILGPU.Intrinsic;
 using ILGPU.Util;
+using System;
 using System.Runtime.CompilerServices;
 
-namespace ILGPU.Algorithms
+namespace ILGPU;
+
+partial class XMath
 {
-    partial class XMath
-    {
-        /// <summary>
-        /// Computes the sign of the provided value.
-        /// Sign will return 0 for NaN, Infitity or 0 values.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>-1 for negative value, 1 for positive values, and 0 for
-        /// 0, NaN or Infinity.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Sign(double value) =>
-            Utilities.Select(value < 0.0, -1, Utilities.Select(value > 0.0, 1, 0));
+    /// <summary>
+    /// Computes the sign of the provided value.
+    /// Sign will return 0 for NaN, Infinity or 0 values.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>-1 for negative value, 1 for positive values, and 0 for
+    /// 0, NaN or Infinity.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Sign(sbyte value) =>
+        Utilities.Select(value < 0, -1, Utilities.Select(value > 0, 1, 0));
 
-        /// <summary>
-        /// Computes the sign of the provided value.
-        /// Sign will return 0 for NaN, Infitity or 0 values.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>-1 for negative value, 1 for positive values, and 0 for
-        /// 0, NaN or Infinity.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Sign(float value) =>
-            Utilities.Select(value < 0.0f, -1, Utilities.Select(value > 0.0f, 1, 0));
+    /// <summary>
+    /// Computes the sign of the provided value.
+    /// Sign will return 0 for NaN, Infinity or 0 values.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>-1 for negative value, 1 for positive values, and 0 for
+    /// 0, NaN or Infinity.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Sign(short value) =>
+        Utilities.Select(value < 0, -1, Utilities.Select(value > 0, 1, 0));
 
-    }
+    /// <summary>
+    /// Computes the sign of the provided value.
+    /// Sign will return 0 for NaN, Infinity or 0 values.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>-1 for negative value, 1 for positive values, and 0 for
+    /// 0, NaN or Infinity.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Sign(int value) =>
+        Utilities.Select(value < 0, -1, Utilities.Select(value > 0, 1, 0));
+
+    /// <summary>
+    /// Computes the sign of the provided value.
+    /// Sign will return 0 for NaN, Infinity or 0 values.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>-1 for negative value, 1 for positive values, and 0 for
+    /// 0, NaN or Infinity.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Sign(long value) =>
+        Utilities.Select(value < 0, -1, Utilities.Select(value > 0, 1, 0));
+
+    /// <summary>
+    /// Computes the sign of the provided value.
+    /// Sign will return 0 for NaN, Infinity or 0 values.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>-1 for negative value, 1 for positive values, and 0 for
+    /// 0, NaN or Infinity.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Sign(double value) =>
+        Utilities.Select(value < 0.0, -1, Utilities.Select(value > 0.0, 1, 0));
+
+    /// <summary>
+    /// Computes the sign of the provided value.
+    /// Sign will return 0 for NaN, Infinity or 0 values.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>-1 for negative value, 1 for positive values, and 0 for
+    /// 0, NaN or Infinity.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Sign(float value) =>
+        Utilities.Select(value < 0.0f, -1, Utilities.Select(value > 0.0f, 1, 0));
+
+    /// <summary>
+    /// Returns a value with the magnitude of x and the sign of y.
+    /// </summary>
+    /// <param name="x">A number whose magnitude is used in the result.</param>
+    /// <param name="y">A number whose sign is the used in the result.</param>
+    /// <returns>A value with the magnitude of x and the sign of y.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double CopySign(double x, double y) =>
+        Math.CopySign(x, y);
+
+    /// <summary>
+    /// Returns a value with the magnitude of x and the sign of y.
+    /// </summary>
+    /// <param name="x">A number whose magnitude is used in the result.</param>
+    /// <param name="y">A number whose sign is the used in the result.</param>
+    /// <returns>A value with the magnitude of x and the sign of y.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float CopySign(float x, float y) =>
+        MathF.CopySign(x, y);
 }
