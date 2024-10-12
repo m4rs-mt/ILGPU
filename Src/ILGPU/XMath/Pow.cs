2b651749623d75d2192150d88188de62edd3eeb9
Marcel Koester <m4rs@m4rs.net>
2024-10-12 14:40:21 +0000

    Updated XMath functions to include Pow, Rcp, Rem, and Exp operations with intrinsic support.

--- Src/ILGPU/XMath/Pow.cs
+++ Src/ILGPU/XMath/Pow.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU Algorithms
-//                        Copyright (c) 2019-2021 ILGPU Project
+//                                        ILGPU
+//                        Copyright (c) 2019-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: Pow.cs
@@ -9,66 +9,85 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
+using ILGPU.Intrinsic;
+using System;
 using System.Runtime.CompilerServices;
 
-namespace ILGPU.Algorithms
+namespace ILGPU;
+
+partial class XMath
 {
-    partial class XMath
-    {
-        /// <summary>
-        /// Computes basis^exp.
-        /// </summary>
-        /// <param name="base">The basis.</param>
-        /// <param name="exp">The exponent.</param>
-        /// <returns>pow(basis, exp).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Pow(double @base, double exp) =>
-            IntrinsicMath.CPUOnly.Pow(@base, exp);
+    /// <summary>
+    /// Computes basis^exp.
+    /// </summary>
+    /// <param name="base">The basis.</param>
+    /// <param name="exp">The exponent.</param>
+    /// <returns>pow(basis, exp).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Pow(double @base, double exp) => Math.Pow(@base, exp);
 
-        /// <summary>
-        /// Computes basis^exp.
-        /// </summary>
-        /// <param name="base">The basis.</param>
-        /// <param name="exp">The exponent.</param>
-        /// <returns>pow(basis, exp).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Pow(float @base, float exp) =>
-            IntrinsicMath.CPUOnly.Pow(@base, exp);
+    /// <summary>
+    /// Computes basis^exp.
+    /// </summary>
+    /// <param name="base">The basis.</param>
+    /// <param name="exp">The exponent.</param>
+    /// <returns>pow(basis, exp).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Pow(float @base, float exp) => MathF.Pow(@base, exp);
 
-        /// <summary>
-        /// Computes exp(value).
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>exp(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Exp(double value) =>
-            IntrinsicMath.CPUOnly.Exp(value);
+    /// <summary>
+    /// Computes exp(value).
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>exp(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Exp(double value) => Math.Exp(value);
 
-        /// <summary>
-        /// Computes exp(value).
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>exp(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Exp(float value) =>
-            IntrinsicMath.CPUOnly.Exp(value);
+    /// <summary>
+    /// Computes exp(value).
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>exp(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Exp(float value) => MathF.Exp(value);
 
-        /// <summary>
-        /// Computes 2^value.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>2^value.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Exp2(double value) =>
-            IntrinsicMath.CPUOnly.Exp2(value);
+    /// <summary>
+    /// Computes exp(value).
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>exp(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Half Exp(Half value) => (Half)MathF.Exp(value);
 
-        /// <summary>
-        /// Computes 2^value.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>2^value.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Exp2(float value) =>
-            IntrinsicMath.CPUOnly.Exp2(value);
-    }
+    /// <summary>
+    /// Computes 2^value.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>2^value.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Exp2(double value) => Math.Pow(value, 2.0);
+
+    /// <summary>
+    /// Computes 2^value.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>2^value.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Exp2(float value) => MathF.Pow(value, 2.0f);
+
+    /// <summary>
+    /// Computes 2^value.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>2^value.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Half Exp2(Half value) => (Half)MathF.Pow(value, 2.0f);
 }
