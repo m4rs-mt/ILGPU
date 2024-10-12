f8ea0d0dfe7e156998123f028d4001cadad8fa7b
Marcel Koester <m4rs@m4rs.net>
2024-10-12 13:57:39 +0000

    Refined trigonometric functions in XMath to support both float and double types with intrinsic optimizations.

--- Src/ILGPU/XMath/Trig.cs
+++ Src/ILGPU/XMath/Trig.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU Algorithms
-//                        Copyright (c) 2019-2021 ILGPU Project
+//                                        ILGPU
+//                        Copyright (c) 2019-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: Trig.cs
@@ -9,220 +9,285 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
+using ILGPU.Intrinsic;
+using System;
 using System.Runtime.CompilerServices;
 
-namespace ILGPU.Algorithms
+namespace ILGPU;
+
+partial class XMath
 {
-    partial class XMath
+    /// <summary>
+    /// Computes sin(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>sin(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Sin(double value) => Math.Sin(value);
+
+    /// <summary>
+    /// Computes sin(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>sin(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Sin(float value) => MathF.Sin(value);
+
+    /// <summary>
+    /// Computes asin(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>asin(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Asin(double value) => Math.Asin(value);
+
+    /// <summary>
+    /// Computes asin(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>asin(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Asin(float value) => MathF.Asin(value);
+
+    /// <summary>
+    /// Computes asinh(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>asinh(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Asinh(float value) => MathF.Asinh(value);
+
+    /// <summary>
+    /// Computes asinh(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>asinh(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Asinh(double value) => Math.Asinh(value);
+
+    /// <summary>
+    /// Computes sinh(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>sinh(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Sinh(double value) => Math.Sinh(value);
+
+    /// <summary>
+    /// Computes sinh(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>sinh(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Sinh(float value) => MathF.Sinh(value);
+
+    /// <summary>
+    /// Computes cos(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>cos(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Cos(double value) => Math.Cos(value);
+
+    /// <summary>
+    /// Computes cos(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>cos(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Cos(float value) => MathF.Cos(value);
+
+    /// <summary>
+    /// Computes cosh(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>cosh(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Cosh(double value) => Math.Cosh(value);
+
+    /// <summary>
+    /// Computes cosh(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>cosh(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Cosh(float value) => MathF.Cosh(value);
+
+    /// <summary>
+    /// Computes acos(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>acos(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Acos(double value) => Math.Acos(value);
+
+    /// <summary>
+    /// Computes acos(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>acos(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Acos(float value) => MathF.Acos(value);
+
+    /// <summary>
+    /// Computes acosh(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>acosh(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Acosh(double value) => Math.Acosh(value);
+
+    /// <summary>
+    /// Computes acosh(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>acosh(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Acosh(float value) => MathF.Acosh(value);
+
+    /// <summary>
+    /// Computes tan(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>tan(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Tan(double value) => Math.Tan(value);
+
+    /// <summary>
+    /// Computes tan(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>tan(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Tan(float value) => MathF.Tan(value);
+
+    /// <summary>
+    /// Computes tanh(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>tanh(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Tanh(double value) => Math.Tanh(value);
+
+    /// <summary>
+    /// Computes tanh(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>tanh(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Tanh(float value) => MathF.Tanh(value);
+
+    /// <summary>
+    /// Computes tanh(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>tanh(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Half Tanh(Half value) => HalfExtensions.TanhFP32(value);
+
+    /// <summary>
+    /// Computes atan(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>atan(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Atan(double value) => Math.Atan(value);
+
+    /// <summary>
+    /// Computes atan(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>atan(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Atan(float value) => MathF.Atan(value);
+
+    /// <summary>
+    /// Computes atanh(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>atanh(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Atanh(double value) => Math.Atanh(value);
+
+    /// <summary>
+    /// Computes atanh(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>atanh(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Atanh(float value) => MathF.Atanh(value);
+
+    /// <summary>
+    /// Computes atan2(y, x).
+    /// </summary>
+    /// <param name="y">The y value in radians.</param>
+    /// <param name="x">The x value in radians.</param>
+    /// <returns>atan2(y, x).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Atan2(double y, double x) => Math.Atan2(y, x);
+
+    /// <summary>
+    /// Computes atan2(y, x).
+    /// </summary>
+    /// <param name="y">The y value in radians.</param>
+    /// <param name="x">The x value in radians.</param>
+    /// <returns>atan2(y, x).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Atan2(float y, float x) => MathF.Atan2(y, x);
+
+    /// <summary>
+    /// Computes sin(value) and cos(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <param name="sin">The result of sin(value).</param>
+    /// <param name="cos">The result of cos(value).</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void SinCos(double value, out double sin, out double cos)
     {
-        /// <summary>
-        /// Computes sin(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>sin(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Sin(double value) =>
-            IntrinsicMath.CPUOnly.Sin(value);
-
-        /// <summary>
-        /// Computes sin(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>sin(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Sin(float value) =>
-            IntrinsicMath.CPUOnly.Sin(value);
-
-        /// <summary>
-        /// Computes sinh(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>sinh(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Sinh(double value) =>
-            IntrinsicMath.CPUOnly.Sinh(value);
-
-        /// <summary>
-        /// Computes asin(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>asin(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Asin(double value) =>
-            IntrinsicMath.CPUOnly.Asin(value);
-
-        /// <summary>
-        /// Computes asin(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>asin(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Asin(float value) =>
-            IntrinsicMath.CPUOnly.Asin(value);
-
-        /// <summary>
-        /// Computes sinh(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>sinh(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Sinh(float value) =>
-            IntrinsicMath.CPUOnly.Sinh(value);
-
-        /// <summary>
-        /// Computes cos(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>cos(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Cos(double value) =>
-            IntrinsicMath.CPUOnly.Cos(value);
-
-        /// <summary>
-        /// Computes cos(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>cos(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Cos(float value) =>
-            IntrinsicMath.CPUOnly.Cos(value);
-
-        /// <summary>
-        /// Computes cosh(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>cosh(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Cosh(double value) =>
-            IntrinsicMath.CPUOnly.Cosh(value);
-
-        /// <summary>
-        /// Computes cosh(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>cosh(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Cosh(float value) =>
-            IntrinsicMath.CPUOnly.Cosh(value);
-
-        /// <summary>
-        /// Computes acos(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>acos(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Acos(double value) =>
-            IntrinsicMath.CPUOnly.Acos(value);
-
-        /// <summary>
-        /// Computes acos(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>acos(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Acos(float value) =>
-            IntrinsicMath.CPUOnly.Acos(value);
-
-        /// <summary>
-        /// Computes tan(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>tan(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Tan(double value) =>
-            IntrinsicMath.CPUOnly.Tan(value);
-
-        /// <summary>
-        /// Computes tan(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>tan(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Tan(float value) =>
-            IntrinsicMath.CPUOnly.Tan(value);
-
-        /// <summary>
-        /// Computes tanh(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>tanh(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Tanh(double value) =>
-            IntrinsicMath.CPUOnly.Tanh(value);
-
-        /// <summary>
-        /// Computes tanh(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>tanh(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Tanh(float value) =>
-            IntrinsicMath.CPUOnly.Tanh(value);
-
-        /// <summary>
-        /// Computes atan(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>atan(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Atan(double value) =>
-            IntrinsicMath.CPUOnly.Atan(value);
-
-        /// <summary>
-        /// Computes atan(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <returns>atan(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Atan(float value) =>
-            IntrinsicMath.CPUOnly.Atan(value);
-
-        /// <summary>
-        /// Computes atan2(y, x).
-        /// </summary>
-        /// <param name="y">The y value in radians.</param>
-        /// <param name="x">The x value in radians.</param>
-        /// <returns>atan2(y, x).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Atan2(double y, double x) =>
-            IntrinsicMath.CPUOnly.Atan2(y, x);
-
-        /// <summary>
-        /// Computes atan2(y, x).
-        /// </summary>
-        /// <param name="y">The y value in radians.</param>
-        /// <param name="x">The x value in radians.</param>
-        /// <returns>atan2(y, x).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Atan2(float y, float x) =>
-            IntrinsicMath.CPUOnly.Atan2(y, x);
-
-        /// <summary>
-        /// Computes sin(value) and cos(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <param name="sin">The result of sin(value).</param>
-        /// <param name="cos">The result of cos(value).</param>
-        /// <returns>tanh(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void SinCos(double value, out double sin, out double cos)
-        {
-            sin = Sin(value);
-            cos = Cos(value);
-        }
-
-        /// <summary>
-        /// Computes sin(value) and cos(value).
-        /// </summary>
-        /// <param name="value">The value in radians.</param>
-        /// <param name="sin">The result of sin(value).</param>
-        /// <param name="cos">The result of cos(value).</param>
-        /// <returns>tanh(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void SinCos(float value, out float sin, out float cos)
-        {
-            sin = Sin(value);
-            cos = Cos(value);
-        }
+        sin = Sin(value);
+        cos = Cos(value);
     }
+
+    /// <summary>
+    /// Computes sin(value) and cos(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>The sin/cos result of the given value.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static (double, double) SinCos(double value) => (Sin(value), Cos(value));
+
+    /// <summary>
+    /// Computes sin(value) and cos(value).
+    /// </summary>
+    /// <param name="value">The value in radians.</param>
+    /// <returns>The sin/cos result of the given value.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static (float, float) SinCos(float value) => (Sin(value), Cos(value));
 }
