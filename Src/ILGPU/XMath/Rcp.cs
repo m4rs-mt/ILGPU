85ee30bd91f12353fa10e0a267e179156d716b71
Marcel Koester <m4rs@m4rs.net>
2024-10-12 15:28:49 +0000

    Updated XMath functions to include reciprocal operations for both double and float types.

--- Src/ILGPU/XMath/Rcp.cs
+++ Src/ILGPU/XMath/Rcp.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU Algorithms
-//                        Copyright (c) 2019-2021 ILGPU Project
+//                                        ILGPU
+//                        Copyright (c) 2019-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: Rcp.cs
@@ -9,28 +9,28 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
+using ILGPU.Intrinsic;
 using System.Runtime.CompilerServices;
 
-namespace ILGPU.Algorithms
-{
-    partial class XMath
-    {
-        /// <summary>
-        /// Computes 1.0 / value.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>1.0 / value.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Rcp(double value) =>
-            IntrinsicMath.CPUOnly.Rcp(value);
+namespace ILGPU;
 
-        /// <summary>
-        /// Computes 1.0f / value.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>1.0f / value.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Rcp(float value) =>
-            IntrinsicMath.CPUOnly.Rcp(value);
-    }
+partial class XMath
+{
+    /// <summary>
+    /// Computes 1.0 / value.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>1.0 / value.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Rcp(double value) => 1.0 / value;
+
+    /// <summary>
+    /// Computes 1.0f / value.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>1.0f / value.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Rcp(float value) => 1.0f / value;
 }
