c50545caa9adcfda65b6ea8f5679b48edebabe58
Marcel Koester <m4rs@m4rs.net>
2024-10-13 12:12:38 +0000

    Refined Abs function in XMath to support various numeric types and applied intrinsic optimizations.

--- Src/ILGPU/XMath/Abs.cs
+++ Src/ILGPU/XMath/Abs.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU Algorithms
-//                        Copyright (c) 2019-2023 ILGPU Project
+//                                        ILGPU
+//                        Copyright (c) 2019-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: Abs.cs
@@ -9,65 +9,74 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
+using ILGPU.Intrinsic;
 using System;
 using System.Runtime.CompilerServices;
 
-namespace ILGPU.Algorithms
+namespace ILGPU;
+
+partial class XMath
 {
-    partial class XMath
-    {
-        /// <summary>
-        /// Computes |value|.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>|value|.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Abs(double value) =>
-            IntrinsicMath.Abs(value);
+    /// <summary>
+    /// Computes |value|.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>|value|.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Abs(double value) => Math.Abs(value);
 
-        /// <summary>
-        /// Computes |value|.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>|value|.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Abs(float value) =>
-            IntrinsicMath.Abs(value);
+    /// <summary>
+    /// Computes |value|.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>|value|.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Abs(float value) => Math.Abs(value);
 
-        /// <summary>
-        /// Computes |value|.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>|value|.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static sbyte Abs(sbyte value) =>
-            IntrinsicMath.Abs(value);
+    /// <summary>
+    /// Computes |value|.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>|value|.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Half Abs(Half value) => Half.Abs(value);
 
-        /// <summary>
-        /// Computes |value|.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>|value|.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static short Abs(short value) =>
-            IntrinsicMath.Abs(value);
+    /// <summary>
+    /// Computes |value|.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>|value|.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static sbyte Abs(sbyte value) => (sbyte)Abs((int)value);
 
-        /// <summary>
-        /// Computes |value|.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>|value|.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Abs(int value) =>
-            IntrinsicMath.Abs(value);
+    /// <summary>
+    /// Computes |value|.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>|value|.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static short Abs(short value) => (short)Abs((int)value);
 
-        /// <summary>
-        /// Computes |value|.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>|value|.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static long Abs(long value) =>
-            IntrinsicMath.Abs(value);
-    }
+    /// <summary>
+    /// Computes |value|.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>|value|.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Abs(int value) => Math.Abs(value);
+
+    /// <summary>
+    /// Computes |value|.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>|value|.</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static long Abs(long value) => Math.Abs(value);
 }
