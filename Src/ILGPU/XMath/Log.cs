20d53a72ec03aba0e2c1a5f23d10b3977b3d3b08
Marcel Koester <m4rs@m4rs.net>
2024-10-13 12:43:01 +0000

    Refined XMath to include intrinsic changes and updated Log, Abs, NaN functions.

--- Src/ILGPU/XMath/Log.cs
+++ Src/ILGPU/XMath/Log.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU Algorithms
-//                        Copyright (c) 2019-2021 ILGPU Project
+//                                        ILGPU
+//                        Copyright (c) 2019-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: Log.cs
@@ -9,84 +9,85 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
+using ILGPU.Intrinsic;
+using System;
 using System.Runtime.CompilerServices;
 
-namespace ILGPU.Algorithms
+namespace ILGPU;
+
+partial class XMath
 {
-    partial class XMath
-    {
-        /// <summary>
-        /// Computes log_newBase(value) to base newBase.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <param name="newBase">The desired base.</param>
-        /// <returns>log_newBase(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Log(double value, double newBase) =>
-            IntrinsicMath.CPUOnly.Log(value, newBase);
+    /// <summary>
+    /// Computes log_newBase(value) to base newBase.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <param name="newBase">The desired base.</param>
+    /// <returns>log_newBase(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Log(double value, double newBase) => Math.Log(value, newBase);
 
-        /// <summary>
-        /// Computes log_newBase(value) to base newBase.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <param name="newBase">The desired base.</param>
-        /// <returns>log_newBase(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Log(float value, float newBase) =>
-            IntrinsicMath.CPUOnly.Log(value, newBase);
+    /// <summary>
+    /// Computes log_newBase(value) to base newBase.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <param name="newBase">The desired base.</param>
+    /// <returns>log_newBase(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Log(float value, float newBase) => MathF.Log(value, newBase);
 
-        /// <summary>
-        /// Computes log(value) to base e.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>log(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Log(double value) =>
-            IntrinsicMath.CPUOnly.Log(value);
+    /// <summary>
+    /// Computes log(value) to base e.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>log(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Log(double value) => Math.Log(value);
 
-        /// <summary>
-        /// Computes log(value) to base e.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>log(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Log(float value) =>
-            IntrinsicMath.CPUOnly.Log(value);
+    /// <summary>
+    /// Computes log(value) to base e.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>log(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Log(float value) => MathF.Log(value);
 
-        /// <summary>
-        /// Computes log10(value) to base 10.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>log10(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Log10(double value) =>
-            IntrinsicMath.CPUOnly.Log10(value);
+    /// <summary>
+    /// Computes log10(value) to base 10.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>log10(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Log10(double value) => Math.Log10(value);
 
-        /// <summary>
-        /// Computes log10(value) to base 10.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>log10(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Log10(float value) =>
-            IntrinsicMath.CPUOnly.Log10(value);
+    /// <summary>
+    /// Computes log10(value) to base 10.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>log10(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Log10(float value) => MathF.Log10(value);
 
-        /// <summary>
-        /// Computes log2(value) to base 2.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>log2(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Log2(double value) =>
-            IntrinsicMath.CPUOnly.Log2(value);
+    /// <summary>
+    /// Computes log2(value) to base 2.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>log2(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Log2(double value) => Math.Log2(value);
 
-        /// <summary>
-        /// Computes log2(value) to base 2.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns>log2(value).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Log2(float value) =>
-            IntrinsicMath.CPUOnly.Log2(value);
-    }
+    /// <summary>
+    /// Computes log2(value) to base 2.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>log2(value).</returns>
+    [MathIntrinsic]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Log2(float value) => MathF.Log2(value);
 }
