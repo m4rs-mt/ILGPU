67783ac9681ec7504331ed6ee3c57818db18ee2d
Marcel Koester <m4rs@m4rs.net>
2024-10-27 11:19:35 +0000

    Moved memory classes to SharedMemory.cs and added methods for shared memory allocation and management.

--- Src/ILGPU/SharedMemory.cs
+++ Src/ILGPU/SharedMemory.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2017-2021 ILGPU Project
+//                        Copyright (c) 2017-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: SharedMemory.cs
@@ -9,49 +9,41 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPU.Frontend.Intrinsic;
-using ILGPU.Runtime.CPU;
-using System.Runtime.CompilerServices;
+using ILGPU.Intrinsic;
 
-namespace ILGPU
+namespace ILGPU;
+
+/// <summary>
+/// Contains methods to allocate and managed shared memory.
+/// </summary>
+public static partial class SharedMemory
 {
     /// <summary>
-    /// Contains methods to allocate and managed shared memory.
+    /// Allocates a single element in shared memory.
     /// </summary>
-    public static partial class SharedMemory
-    {
-        /// <summary>
-        /// Allocates a single element in shared memory.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <returns>An allocated element in shared memory.</returns>
-        [SharedMemoryIntrinsic(SharedMemoryIntrinsicKind.AllocateElement)]
-        public static ref T Allocate<T>()
-            where T : unmanaged =>
-            ref Allocate<T>(1)[0];
+    /// <typeparam name="T">The element type.</typeparam>
+    /// <returns>An allocated element in shared memory.</returns>
+    [SharedMemoryIntrinsic]
+    public static ref T Allocate<T>() where T : unmanaged =>
+        throw new InvalidKernelOperationException();
 
-        /// <summary>
-        /// Allocates a chunk of shared memory with the specified number of elements.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="extent">The extent (number of elements to allocate).</param>
-        /// <returns>An allocated region of shared memory.</returns>
-        [SharedMemoryIntrinsic(SharedMemoryIntrinsicKind.Allocate)]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ArrayView<T> Allocate<T>(int extent)
-            where T : unmanaged =>
-            CPURuntimeGroupContext.Current.AllocateSharedMemory<T>(extent);
+    /// <summary>
+    /// Allocates a chunk of shared memory with the specified number of elements.
+    /// </summary>
+    /// <typeparam name="T">The element type.</typeparam>
+    /// <param name="extent">The extent (number of elements to allocate).</param>
+    /// <returns>An allocated region of shared memory.</returns>
+    [SharedMemoryIntrinsic]
+    public static ArrayView<T> Allocate<T>(int extent) where T : unmanaged =>
+        throw new InvalidKernelOperationException();
 
-        /// <summary>
-        /// Gets a chunk of dynamically allocated shared memory as typed memory view
-        /// with the element type <typeparamref name="T"/>.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <returns>A view to a dynamically allocated region of shared memory.</returns>
-        [SharedMemoryIntrinsic(SharedMemoryIntrinsicKind.AllocateDynamic)]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ArrayView<T> GetDynamic<T>()
-            where T : unmanaged =>
-            CPURuntimeGroupContext.Current.AllocateSharedMemoryDynamic<T>();
-    }
+    /// <summary>
+    /// Gets a chunk of dynamically allocated shared memory as typed memory view
+    /// with the element type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The element type.</typeparam>
+    /// <returns>A view to a dynamically allocated region of shared memory.</returns>
+    [SharedMemoryIntrinsic]
+    public static ArrayView<T> GetDynamic<T>() where T : unmanaged =>
+        throw new InvalidKernelOperationException();
 }
