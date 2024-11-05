f8bf35e30dc32133f7e06b76dae8ed4d26de640e
Marcel Koester <m4rs@m4rs.net>
2024-11-05 23:53:17 +0000

    Refined AcceleratorStream class to support asynchronous operations and improved kernel configuration methods.

--- Src/ILGPU/Runtime/AcceleratorStream.cs
+++ Src/ILGPU/Runtime/AcceleratorStream.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2017-2023 ILGPU Project
+//                        Copyright (c) 2017-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: AcceleratorStream.cs
@@ -10,76 +10,185 @@
 // ---------------------------------------------------------------------------------------
 
 using ILGPU.Resources;
+using ILGPU.Runtime.Extensions;
+using ILGPU.Util;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
-namespace ILGPU.Runtime
+namespace ILGPU.Runtime;
+
+/// <summary>
+/// Flags for accelerator stream instances.
+/// </summary>
+[Flags]
+public enum AcceleratorStreamFlags
 {
     /// <summary>
-    /// Represents an abstract kernel stream for asynchronous processing.
+    /// Specifies an accelerator stream that must be assumed to run in sync.
     /// </summary>
-    /// <remarks>Members of this class are not thread safe.</remarks>
-    [SuppressMessage(
-        "Microsoft.Naming",
-        "CA1711:IdentifiersShouldNotHaveIncorrectSuffix")]
-    public abstract class AcceleratorStream : AcceleratorObject
+    None = 0,
+
+    /// <summary>
+    /// Specified an async stream to allow for full concurrency.
+    /// </summary>
+    Async = 1,
+}
+
+/// <summary>
+/// Represents an abstract kernel stream for asynchronous processing.
+/// </summary>
+/// <remarks>Members of this class are not thread safe.</remarks>
+[SuppressMessage(
+    "Microsoft.Naming",
+    "CA1711:IdentifiersShouldNotHaveIncorrectSuffix")]
+public abstract partial class AcceleratorStream : AcceleratorObject
+{
+    #region Instance
+
+    private readonly Action _synchronizeAction;
+    private readonly Dictionary<Guid, AcceleratorExtension> _extensionCache = [];
+
+    /// <summary>
+    /// Constructs a new accelerator stream.
+    /// </summary>
+    /// <param name="accelerator">The associated accelerator.</param>
+    /// <param name="flags">The associated flags.</param>
+    protected AcceleratorStream(Accelerator accelerator, AcceleratorStreamFlags flags)
+        : base(accelerator)
     {
-        #region Instance
-
-        private readonly Action synchronizeAction;
-
-        /// <summary>
-        /// Constructs a new accelerator stream.
-        /// </summary>
-        /// <param name="accelerator">The associated accelerator.</param>
-        protected AcceleratorStream(Accelerator accelerator)
-            : base(accelerator)
-        {
-            synchronizeAction = () => Synchronize();
-        }
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Synchronizes all queued operations.
-        /// </summary>
-        public abstract void Synchronize();
-
-        /// <summary>
-        /// Synchronizes all queued operations asynchronously.
-        /// </summary>
-        /// <returns>A task object to wait for.</returns>
-        public Task SynchronizeAsync() => Task.Run(synchronizeAction);
-
-        /// <summary>
-        /// Makes the associated accelerator the current one for this thread and
-        /// returns a <see cref="ScopedAcceleratorBinding"/> object that allows
-        /// to easily recover the old binding.
-        /// </summary>
-        /// <returns>A scoped binding object.</returns>
-        public ScopedAcceleratorBinding BindScoped() => Accelerator.BindScoped();
-
-        /// <summary>
-        /// Adds a profiling marker into the stream.
-        /// </summary>
-        /// <returns>The profiling marker.</returns>
-        public ProfilingMarker AddProfilingMarker() =>
-            Accelerator.Context.Properties.EnableProfiling
-            ? AddProfilingMarkerInternal()
-            : throw new NotSupportedException(
-                RuntimeErrorMessages.NotSupportedProfilingMarker);
-
-        /// <summary>
-        /// Adds a profiling marker into the stream.
-        /// </summary>
-        /// <returns>The profiling marker.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        protected abstract ProfilingMarker AddProfilingMarkerInternal();
-
-        #endregion
+        _synchronizeAction = Synchronize;
+        OptimalKernelSize = accelerator.OptimalKernelSize;
+        NumWarpsPerOptimalGroupSize = XMath.DivRoundUp(
+            OptimalKernelSize.GroupSize,
+            accelerator.WarpSize);
+        WarpSize = accelerator.WarpSize;
+        Flags = flags;
     }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Returns the associated flags.
+    /// </summary>
+    public AcceleratorStreamFlags Flags { get; }
+
+    /// <summary>
+    /// Returns a kernel extent (a grouped index) with the maximum number of groups
+    /// using the maximum number of threads per group to launch common grid-stride
+    /// loop kernels.
+    /// </summary>
+    public KernelSize OptimalKernelSize { get; }
+
+    /// <summary>
+    /// Returns the warp size of the underlying accelerator.
+    /// </summary>
+    public int WarpSize { get; }
+
+    /// <summary>
+    /// Returns the number of warps for kernel launches using
+    /// <see cref="OptimalKernelSize"/>.
+    /// </summary>
+    public int NumWarpsPerOptimalGroupSize { get; }
+
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// Synchronizes all queued operations.
+    /// </summary>
+    public abstract void Synchronize();
+
+    /// <summary>
+    /// Synchronizes all queued operations asynchronously.
+    /// </summary>
+    /// <returns>A task object to wait for.</returns>
+    public Task SynchronizeAsync() => Task.Run(_synchronizeAction);
+
+    /// <summary>
+    /// Makes the associated accelerator the current one for this thread and
+    /// returns a <see cref="ScopedAcceleratorBinding"/> object that allows
+    /// to easily recover the old binding.
+    /// </summary>
+    /// <returns>A scoped binding object.</returns>
+    public ScopedAcceleratorBinding BindScoped() => Accelerator.AsNotNull().BindScoped();
+
+    /// <summary>
+    /// Adds a profiling marker into the stream.
+    /// </summary>
+    /// <returns>The profiling marker.</returns>
+    public ProfilingMarker AddProfilingMarker() =>
+        Accelerator.AsNotNull().Context.Properties.EnableProfiling
+        ? AddProfilingMarkerInternal()
+        : throw new NotSupportedException(
+            RuntimeErrorMessages.NotSupportedProfilingMarker);
+
+    /// <summary>
+    /// Adds a profiling marker into the stream.
+    /// </summary>
+    /// <returns>The profiling marker.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    protected abstract ProfilingMarker AddProfilingMarkerInternal();
+
+    /// <summary>
+    /// Gets a registered accelerator extension.
+    /// </summary>
+    /// <typeparam name="T">The extension type.</typeparam>
+    /// <returns>The extension instance.</returns>
+    public T GetExtension<T>() where T : IAcceleratorExtension
+    {
+        if (!_extensionCache.TryGetValue(T.Id, out var extension))
+        {
+            extension = Accelerator.AsNotNull().GetAcceleratorExtension<T>();
+            _extensionCache.Add(T.Id, extension);
+        }
+        return extension.GetAsAbstractExtension<T>();
+    }
+
+    /// <summary>
+    /// Computes number of groups with optimal group size.
+    /// </summary>
+    /// <param name="numDataElements">
+    /// The number of parallel data elements to process.
+    /// </param>
+    /// <returns>The number of groups to launch.</returns>
+    public long ComputeNumGroupsWithOptimalGroupSize(long numDataElements) =>
+        numDataElements < 1
+        ? throw new ArgumentOutOfRangeException(nameof(numDataElements))
+        : OptimalKernelSize.ComputeNumGroups(numDataElements);
+
+    /// <summary>
+    /// Computes a kernel config using optimal group size.
+    /// </summary>
+    /// <param name="numDataElements">
+    /// The number of parallel data elements to process.
+    /// </param>
+    /// <returns>The kernel configuration for launch.</returns>
+    public KernelConfig ComputeKernelConfig(long numDataElements) => new(
+        ComputeNumGroupsWithOptimalGroupSize(numDataElements),
+        OptimalKernelSize.GroupSize);
+
+    /// <summary>
+    /// Returns a kernel extent (a grouped index) with the maximum number of groups
+    /// using the maximum number of threads per group to launch common grid-stride
+    /// loop kernels.
+    /// </summary>
+    /// <param name="numDataElements">
+    /// The number of parallel data elements to process.
+    /// </param>
+    /// <param name="numIterationsPerGroup">
+    /// The number of loop iterations per group.
+    /// </param>
+    public KernelConfig ComputeGridStrideKernelConfig(
+        long numDataElements,
+        out int numIterationsPerGroup) =>
+        OptimalKernelSize.ComputeGridStrideKernelConfig(
+            numDataElements,
+            out numIterationsPerGroup);
+
+    #endregion
 }
