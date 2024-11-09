ca01df1721d6feb164fb40733dc17821aa19a03c
Marcel Koester <m4rs@m4rs.net>
2024-11-09 10:59:46 +0000

    Refined Scan and Reduce implementations to support static properties and improved queries.

--- Src/ILGPU/ScanReduce/IScanReduceOperation.cs
+++ Src/ILGPU/ScanReduce/IScanReduceOperation.cs
@@ -9,89 +9,107 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-namespace ILGPU.ScanReduce
+namespace ILGPU.ScanReduce;
+
+/// <summary>
+/// Implements a scan or a reduction operation.
+/// </summary>
+/// <typeparam name="T">The underlying type of the scan operation.</typeparam>
+public interface IScanReduceOperation<T> where T : struct
 {
     /// <summary>
-    /// Implements a scan or a reduction operation.
+    /// Returns the identity value (the neutral element of the operation), such that
+    /// Apply(Apply(Identity, left), right) == Apply(left, right).
     /// </summary>
-    /// <typeparam name="T">The underlying type of the scan operation.</typeparam>
-    public interface IScanReduceOperation<T>
-        where T : struct
+    static abstract T Identity { get; }
+
+    /// <summary>
+    /// Applies the current operation.
+    /// </summary>
+    /// <param name="first">The first operand.</param>
+    /// <param name="second">The second operand.</param>
+    /// <returns>The result of the operation.</returns>
+    static abstract T Apply(T first, T second);
+
+    /// <summary>
+    /// Performs an atomic operation of the form target =
+    /// AtomicUpdate(target.Value, value).
+    /// </summary>
+    /// <param name="target">The target address to update.</param>
+    /// <param name="value">The value.</param>
+    static abstract void AtomicApply(ref T target, T value);
+}
+
+/// <summary>
+/// Holds the left and the right boundary of a scan operation.
+/// </summary>
+/// <typeparam name="T">The element type.</typeparam>
+/// <remarks>
+/// Constructs a new scan-boundaries instance.
+/// </remarks>
+/// <param name="LeftBoundary">The left boundary.</param>
+/// <param name="RightBoundary">The right boundary.</param>
+public readonly record struct ScanBoundaries<T>(T LeftBoundary, T RightBoundary)
+    where T : struct
+{
+    /// <summary>
+    /// Returns the string representation of these boundary values.
+    /// </summary>
+    /// <returns>The string representation of these boundary values.</returns>
+    public override string ToString() => $"[{LeftBoundary}, {RightBoundary}]";
+}
+
+/// <summary>
+/// Represents the scan operation type.
+/// </summary>
+public enum ScanKind
+{
+    /// <summary>
+    /// An inclusive scan operation.
+    /// </summary>
+    Inclusive,
+
+    /// <summary>
+    /// An exclusive scan operation.
+    /// </summary>
+    Exclusive
+}
+
+/// <summary>
+/// Represents an interface to parameterize scan operations.
+/// </summary>
+public interface IScanPredicate
+{
+    /// <summary>
+    /// Returns the scan kind of the current operation.
+    /// </summary>
+    static abstract ScanKind ScanKind { get; }
+}
+
+/// <summary>
+/// Contains pre-defined scan predicates.
+/// </summary>
+public static class ScanPredicates
+{
+    /// <summary>
+    /// Represents an inclusive scan predicate.
+    /// </summary>
+    public readonly struct InclusiveScan : IScanPredicate
     {
         /// <summary>
-        /// Returns the associated OpenCL command suffix for the internal code generator
-        /// to build the final OpenCL command to use.
+        /// Returns <see cref="ScanKind.Inclusive"/>.
         /// </summary>
-        string CLCommand { get; }
-
-        /// <summary>
-        /// Returns the identity value (the neutral element of the operation), such that
-        /// Apply(Apply(Identity, left), right) == Apply(left, right).
-        /// </summary>
-        T Identity { get; }
-
-        /// <summary>
-        /// Applies the current operation.
-        /// </summary>
-        /// <param name="first">The first operand.</param>
-        /// <param name="second">The second operand.</param>
-        /// <returns>The result of the operation.</returns>
-        T Apply(T first, T second);
-
-        /// <summary>
-        /// Performs an atomic operation of the form target =
-        /// AtomicUpdate(target.Value, value).
-        /// </summary>
-        /// <param name="target">The target address to update.</param>
-        /// <param name="value">The value.</param>
-        void AtomicApply(ref T target, T value);
+        public static ScanKind ScanKind => ScanKind.Inclusive;
     }
 
     /// <summary>
-    /// Holds the left and the right boundary of a scan operation.
+    /// Represents an exclusive scan predicate.
     /// </summary>
-    /// <typeparam name="T">The element type.</typeparam>
-    public readonly struct ScanBoundaries<T>
-        where T : struct
+    public readonly struct ExclusiveScan : IScanPredicate
     {
-        #region Instance
-
         /// <summary>
-        /// Constructs a new scan-boundaries instance.
+        /// Returns <see cref="ScanKind.Exclusive"/>.
         /// </summary>
-        /// <param name="leftBoundary">The left boundary.</param>
-        /// <param name="rightBoundary">The right boundary.</param>
-        public ScanBoundaries(T leftBoundary, T rightBoundary)
-        {
-            LeftBoundary = leftBoundary;
-            RightBoundary = rightBoundary;
-        }
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// The left boundary.
-        /// </summary>
-        public T LeftBoundary { get; }
-
-        /// <summary>
-        /// The right boundary.
-        /// </summary>
-        public T RightBoundary { get; }
-
-        #endregion
-
-        #region Object
-
-        /// <summary>
-        /// Returns the string representation of these boundary values.
-        /// </summary>
-        /// <returns>The string representation of these boundary values.</returns>
-        public override string ToString() =>
-            $"[{LeftBoundary}, {RightBoundary}]";
-
-        #endregion
+        public static ScanKind ScanKind => ScanKind.Exclusive;
     }
 }
