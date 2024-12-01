09fe2147e011e021a7a675d0d7222891d8e18feb
Marcel Koester <m4rs@m4rs.net>
2024-12-01 14:07:49 +0000

    Refined ILGPU utilities by introducing a new RawString record struct for improved string handling.

--- Src/ILGPU/Util/RawString.cs
+++ Src/ILGPU/Util/RawString.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                           Copyright (c) 2021 ILGPU Project
+//                        Copyright (c) 2023-2024 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: RawString.cs
@@ -13,53 +13,38 @@ using System;
 
 // disable: max_line_length
 
-namespace ILGPU.Util
+namespace ILGPU.Util;
+
+/// <summary>
+/// Helper to coerce the C# compiler into preferencing the FormattableString
+/// overload of a function, and to also accept a regular string.
+///
+/// https://www.damirscorner.com/blog/posts/20180921-FormattableStringAsMethodParameter.html
+/// </summary>
+/// <param name="Value">The string value.</param>
+public record struct RawString(string Value)
 {
+    private RawString(char value) : this(value.ToString()) { }
+
     /// <summary>
-    /// Helper to coerce the C# compiler into preferencing the FormattableString
-    /// overload of a function, and to also accept a regular string.
-    ///
-    /// https://www.damirscorner.com/blog/posts/20180921-FormattableStringAsMethodParameter.html
+    /// Implicit conversion from char.
     /// </summary>
-    public struct RawString
-    {
-        private RawString(char value)
-        {
-            Value = value.ToString();
-        }
+    /// <param name="value">The char value.</param>
+    public static implicit operator RawString(char value) => new(value);
 
-        private RawString(string value)
-        {
-            Value = value;
-        }
+    /// <summary>
+    /// Implicit conversion from string.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    public static implicit operator RawString(string value) => new(value);
 
-        /// <summary>
-        /// The string value.
-        /// </summary>
-        public string Value { get; }
-
-        /// <summary>
-        /// Implicit conversion from char.
-        /// </summary>
-        /// <param name="value">The char value.</param>
-        public static implicit operator RawString(char value) =>
-            new RawString(value);
-
-        /// <summary>
-        /// Implicit conversion from string.
-        /// </summary>
-        /// <param name="value">The string value.</param>
-        public static implicit operator RawString(string value) =>
-            new RawString(value);
-
-        /// <summary>
-        /// Implicit conversion from <see cref="FormattableString"/>.
-        /// </summary>
-        /// <remarks>
-        /// This should not be used, but is necessary to coerce the C# compiler.
-        /// </remarks>
-        /// <param name="value">The string value.</param>
-        public static implicit operator RawString(FormattableString value) =>
-            new RawString(value.ToString());
-    }
+    /// <summary>
+    /// Implicit conversion from <see cref="FormattableString"/>.
+    /// </summary>
+    /// <remarks>
+    /// This should not be used, but is necessary to coerce the C# compiler.
+    /// </remarks>
+    /// <param name="value">The string value.</param>
+    public static implicit operator RawString(FormattableString value) =>
+        new(value.ToString());
 }
