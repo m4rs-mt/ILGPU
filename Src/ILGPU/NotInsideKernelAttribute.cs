93232a674cc2e73fc5c710cba113d9c9b08e0961
Marcel Koester <m4rs@m4rs.net>
2024-12-08 16:29:57 +0000

    Refined NotInsideKernelAttribute to support static property checks and improved error message generation.

--- Src/ILGPU/NotInsideKernelAttribute.cs
+++ Src/ILGPU/NotInsideKernelAttribute.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                           Copyright (c) 2021 ILGPU Project
+//                        Copyright (c) 2021-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: NotInsideKernelAttribute.cs
@@ -11,34 +11,31 @@
 
 using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
-namespace ILGPU
+namespace ILGPU;
+
+/// <summary>
+/// Marks methods and constructors that are not supported within kernels.
+/// </summary>
+/// <remarks>
+/// This attribute is not required but helps us to generate better error messages.
+/// </remarks>
+[AttributeUsage(
+    AttributeTargets.Method | AttributeTargets.Constructor,
+    AllowMultiple = false)]
+public sealed class NotInsideKernelAttribute : Attribute
 {
     /// <summary>
-    /// Marks methods and constructors that are not supported within kernels.
+    /// The static type reference.
     /// </summary>
-    /// <remarks>
-    /// This attribute is not required but helps us to generate better error messages.
-    /// </remarks>
-    [AttributeUsage(
-        AttributeTargets.Method | AttributeTargets.Constructor,
-        AllowMultiple = false)]
-    public sealed class NotInsideKernelAttribute : Attribute
-    {
-        /// <summary>
-        /// The static type reference.
-        /// </summary>
-        internal static readonly Type AttributeType = typeof(NotInsideKernelAttribute);
+    internal static readonly Type AttributeType = typeof(NotInsideKernelAttribute);
 
-        /// <summary>
-        /// Returns true if the method is annotated with the attribute
-        /// <see cref="NotInsideKernelAttribute"/>.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsDefined(MethodBase method) =>
-            method.IsDefined(AttributeType);
+    /// <summary>
+    /// Returns true if the method is annotated with the attribute
+    /// <see cref="NotInsideKernelAttribute"/>.
+    /// </summary>
+    public static bool IsDefined(MethodBase method) =>
+        method.IsDefined(AttributeType);
 
-        // ...
-    }
+    // ...
 }
