33c92b80bd9c3efa8b8e5f874c83e9e44469cd1a
Marcel Koester <m4rs@m4rs.net>
2024-12-09 21:08:40 +0000

    Refined CapabilityNotSupportedException to include additional constructors for enhanced exception handling.

--- Src/ILGPU/CapabilityNotSupportedException.cs
+++ Src/ILGPU/CapabilityNotSupportedException.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2020-2023 ILGPU Project
+//                        Copyright (c) 2020-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: CapabilityNotSupportedException.cs
@@ -10,66 +10,43 @@
 // ---------------------------------------------------------------------------------------
 
 using System;
-using System.Runtime.Serialization;
 
-namespace ILGPU
+namespace ILGPU;
+
+/// <summary>
+/// The exception that is thrown when a capability is not supported by an accelerator.
+/// </summary>
+[Serializable]
+public sealed class CapabilityNotSupportedException : Exception
 {
     /// <summary>
-    /// The exception that is thrown when a capability is not supported by an accelerator.
+    /// Initializes a new instance of the CapabilityNotSupportedException class.
     /// </summary>
-    [Serializable]
-    public sealed class CapabilityNotSupportedException : Exception
-    {
-        /// <summary>
-        /// Initializes a new instance of the CapabilityNotSupportedException class.
-        /// </summary>
-        public CapabilityNotSupportedException()
-        { }
+    public CapabilityNotSupportedException()
+    { }
 
-        /// <summary>
-        /// Initializes a new instance of the CapabilityNotSupportedException class
-        /// with a specified error message.
-        /// </summary>
-        /// <param name="message">The message that describes the error.</param>
-        public CapabilityNotSupportedException(string message)
-            : base(message)
-        { }
+    /// <summary>
+    /// Initializes a new instance of the CapabilityNotSupportedException class
+    /// with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    public CapabilityNotSupportedException(string message)
+        : base(message)
+    { }
 
-        /// <summary>
-        /// Initializes a new instance of the CapabilityNotSupportedException class
-        /// with a specified error message and a reference to the inner exception
-        /// that is the cause of this exception.
-        /// </summary>
-        /// <param name="message">
-        /// The error message that explains the reason for the exception.
-        /// </param>
-        /// <param name="innerException">
-        /// The exception that is the cause of the current exception, or a null reference
-        /// if no inner exception is specified.
-        /// </param>
-        public CapabilityNotSupportedException(string message, Exception innerException)
-            : base(message, innerException)
-        { }
-
-        /// <summary>
-        /// Initializes a new instance of the CapabilityNotSupportedException class with
-        /// serialized data.
-        /// </summary>
-        /// <param name="serializationInfo">
-        /// The System.Runtime.Serialization.SerializationInfo that holds the serialized
-        /// object data about the exception being thrown.
-        /// </param>
-        /// <param name="streamingContext">
-        /// The System.Runtime.Serialization.StreamingContext that contains contextual
-        /// information about the source or destination.
-        /// </param>
-#if NET8_0_OR_GREATER
-        [Obsolete("SYSLIB0050: Formatter-based serialization is obsolete")]
-#endif
-        private CapabilityNotSupportedException(
-            SerializationInfo serializationInfo,
-            StreamingContext streamingContext)
-            : base(serializationInfo, streamingContext)
-        { }
-    }
+    /// <summary>
+    /// Initializes a new instance of the CapabilityNotSupportedException class
+    /// with a specified error message and a reference to the inner exception
+    /// that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">
+    /// The error message that explains the reason for the exception.
+    /// </param>
+    /// <param name="innerException">
+    /// The exception that is the cause of the current exception, or a null reference
+    /// if no inner exception is specified.
+    /// </param>
+    public CapabilityNotSupportedException(string message, Exception innerException)
+        : base(message, innerException)
+    { }
 }
