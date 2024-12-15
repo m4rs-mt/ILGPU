9c6d2025efa868838eda6bceeb99e529e76e7462
Marcel Koester <m4rs@m4rs.net>
2024-12-15 12:04:07 +0000

    Created DebugKernel classes to support newly introduced DebugAccelerator.

--- Src/ILGPU/Runtime/Debugging/DebugKernel.cs
+++ Src/ILGPU/Runtime/Debugging/DebugKernel.cs
@@ -1,85 +1,77 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2017-2023 ILGPU Project
+//                        Copyright (c) 2017-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
-// File: CPUKernel.cs
+// File: DebugKernel.cs
 //
 // This file is part of ILGPU and is distributed under the University of Illinois Open
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPU.Backends;
-using ILGPU.Util;
-using System;
-using System.Reflection;
+namespace ILGPU.Runtime.Debugging;
 
-namespace ILGPU.Runtime.CPU
+/// <summary>
+/// Wraps kernel groups to be invoked.
+/// </summary>
+/// <param name="kernelConfig">The current kernel config.</param>
+/// <param name="gridIndex">The current grid index of the current group.</param>
+/// <param name="context">The current kernel context.</param>
+public delegate void DebugKernelGroupHandler(
+    in KernelConfig kernelConfig,
+    long gridIndex,
+    DebugKernelContext context);
+
+/// <summary>
+/// Represents a single debug kernel.
+/// </summary>
+public sealed class DebugCompiledKernel(
+    CompiledKernelData data,
+    DebugKernelGroupHandler groupHandler) : CompiledKernel(data)
 {
     /// <summary>
-    /// Represents a single CPU kernel.
+    /// Returns the underlying group handler.
     /// </summary>
-    public sealed class CPUKernel : Kernel
+    public DebugKernelGroupHandler GroupHandler { get; } = groupHandler;
+}
+
+/// <summary>
+/// An abstract debug kernel context to be used during kernel launch.
+/// </summary>
+public abstract class DebugKernelContext;
+
+/// <summary>
+/// Represents a single debug kernel.
+/// </summary>
+public sealed class DebugKernel : Kernel
+{
+    private readonly DebugKernelGroupHandler _handler;
+
+    /// <summary>
+    /// Loads a compiled kernel into the given Cuda context as kernel program.
+    /// </summary>
+    /// <param name="accelerator">The associated accelerator.</param>
+    /// <param name="kernel">The source kernel.</param>
+    internal DebugKernel(DebugAccelerator accelerator, CompiledKernel kernel)
+        : base(accelerator, kernel)
     {
-        #region Static
-
-        /// <summary>
-        /// Represents the <see cref="KernelExecutionDelegate"/> property getter.
-        /// </summary>
-        internal static readonly MethodInfo GetKernelExecutionDelegate =
-            typeof(CPUKernel).GetProperty(
-                nameof(KernelExecutionDelegate),
-                BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
-            .ThrowIfNull()
-            .GetGetMethod(true)
-            .ThrowIfNull();
-
-        #endregion
-
-        #region Instance
-
-        /// <summary>
-        /// Loads a compiled kernel into the given Cuda context as kernel program.
-        /// </summary>
-        /// <param name="accelerator">The associated accelerator.</param>
-        /// <param name="kernel">The source kernel.</param>
-        /// <param name="launcher">The launcher method for the given kernel.</param>
-        /// <param name="kernelExecutionDelegate">The execution method.</param>
-        internal CPUKernel(
-            CPUAccelerator accelerator,
-            CompiledKernel kernel,
-            MethodInfo launcher,
-            CPUKernelExecutionHandler kernelExecutionDelegate)
-            : base(accelerator, kernel, launcher)
-        {
-            KernelExecutionDelegate = kernelExecutionDelegate
-                ?? throw new ArgumentNullException(nameof(kernelExecutionDelegate));
-        }
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Returns the associated CPU runtime.
-        /// </summary>
-        public CPUAccelerator CPUAccelerator =>
-            Accelerator.AsNotNullCast<CPUAccelerator>();
-
-        /// <summary>
-        /// Returns the associated kernel-execution delegate.
-        /// </summary>
-        internal CPUKernelExecutionHandler KernelExecutionDelegate { get; }
-
-        #endregion
-
-        #region IDisposable
-
-        /// <summary>
-        /// Does not perform any operation.
-        /// </summary>
-        protected override void DisposeAcceleratorObject(bool disposing) { }
-
-        #endregion
+        _handler = ((DebugCompiledKernel)kernel).GroupHandler;
     }
+
+    /// <summary>
+    /// Invokes the group processing for the current thread.
+    /// </summary>
+    /// <param name="kernelConfig">The current kernel config.</param>
+    /// <param name="gridIndex">The grid index to use.</param>
+    /// <param name="context">The current kernel context.</param>
+    public void InvokeGroup(
+        in KernelConfig kernelConfig,
+        long gridIndex,
+        DebugKernelContext context) =>
+        _handler(kernelConfig, gridIndex, context);
+
+    /// <summary>
+    /// Does not perform any operation.
+    /// </summary>
+    protected override void DisposeAcceleratorObject(bool disposing) { }
 }
