0a8946ba451d06e27fdeca65c4478dccf3269e1e
Marcel Koester <m4rs@m4rs.net>
2024-12-15 12:43:54 +0000

    Created DebugStream class to support newly introduced DebugAccelerator.

--- Src/ILGPU/Runtime/Debugging/DebugStream.cs
+++ Src/ILGPU/Runtime/Debugging/DebugStream.cs
@@ -1,56 +1,62 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2017-2023 ILGPU Project
+//                        Copyright (c) 2017-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
-// File: CPUStream.cs
+// File: DebugStream.cs
 //
 // This file is part of ILGPU and is distributed under the University of Illinois Open
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-namespace ILGPU.Runtime.CPU
+using ILGPU.Util;
+
+#pragma warning disable CA1711 // Identifiers should not have incorrect suffix
+
+namespace ILGPU.Runtime.Debugging;
+
+/// <summary>
+/// Represents a debug stream.
+/// </summary>
+sealed class DebugStream : AcceleratorStream
 {
+    #region Instance
+
     /// <summary>
-    /// Represents a CPU stream.
+    /// Constructs a new debug stream.
     /// </summary>
-    sealed class CPUStream : AcceleratorStream
+    /// <param name="accelerator">The associated accelerator.</param>
+    internal DebugStream(Accelerator accelerator)
+        : base(accelerator, AcceleratorStreamFlags.None)
+    { }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Does not perform any operation.
+    /// </summary>
+    public override void Synchronize() { }
+
+    /// <inheritdoc/>
+    protected unsafe override ProfilingMarker AddProfilingMarkerInternal()
     {
-        #region Instance
-
-        /// <summary>
-        /// Constructs a new CPU stream.
-        /// </summary>
-        /// <param name="accelerator">The associated accelerator.</param>
-        internal CPUStream(Accelerator accelerator)
-            : base(accelerator)
-        { }
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Does not perform any operation.
-        /// </summary>
-        public override void Synchronize() { }
-
-        /// <inheritdoc/>
-        protected unsafe override ProfilingMarker AddProfilingMarkerInternal()
-        {
-            using var binding = Accelerator.BindScoped();
-            return new CPUProfilingMarker(Accelerator);
-        }
-
-        #endregion
-
-        #region IDisposable
-
-        /// <summary>
-        /// Does not perform any operation.
-        /// </summary>
-        protected override void DisposeAcceleratorObject(bool disposing) { }
-
-        #endregion
+        var accelerator = Accelerator.AsNotNull();
+        using var binding = accelerator.BindScoped();
+        return new DebugProfilingMarker(accelerator);
     }
+
+    #endregion
+
+    #region IDisposable
+
+    /// <summary>
+    /// Does not perform any operation.
+    /// </summary>
+    protected override void DisposeAcceleratorObject(bool disposing) { }
+
+    #endregion
 }
+
+#pragma warning restore CA1711 // Identifiers should not have incorrect suffix
