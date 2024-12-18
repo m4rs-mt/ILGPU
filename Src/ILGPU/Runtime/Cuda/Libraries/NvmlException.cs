774ddd1a74c7ab7e85d8d28429ce5d65f41cfeb4
Marcel Koester <m4rs@m4rs.net>
2024-12-18 02:02:02 +0000

    Refined NvmlException class to handle NVML errors more effectively.

--- Src/ILGPU/Runtime/Cuda/Libraries/NvmlException.cs
+++ Src/ILGPU/Runtime/Cuda/Libraries/NvmlException.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU Algorithms
-//                        Copyright (c) 2021-2023 ILGPU Project
+//                                        ILGPU
+//                        Copyright (c) 2021-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: NvmlException.cs
@@ -11,97 +11,71 @@
 
 using System;
 using System.Runtime.CompilerServices;
-using System.Runtime.Serialization;
-using System.Security.Permissions;
 
-namespace ILGPU.Runtime.Cuda
+namespace ILGPU.Runtime.Cuda.Libraries;
+
+/// <summary>
+/// Represents an exception that can be thrown by the NVML library.
+/// </summary>
+[Serializable]
+public sealed class NvmlException : Exception
 {
+    #region Instance
+
     /// <summary>
-    /// Represents an exception that can be thrown by the NVML library.
+    /// Constructs a new NVML exception.
     /// </summary>
-    [Serializable]
-    public sealed class NvmlException : Exception
+    public NvmlException()
+        : this(NvmlReturn.NVML_ERROR_UNKNOWN)
+    { }
+
+    /// <summary>
+    /// Constructs a new NVML exception.
+    /// </summary>
+    /// <param name="errorCode">The NVML runtime error.</param>
+    public NvmlException(NvmlReturn errorCode)
+        : base()
     {
-        #region Instance
-
-        /// <summary>
-        /// Constructs a new NVML exception.
-        /// </summary>
-        public NvmlException()
-            : this(NvmlReturn.NVML_ERROR_UNKNOWN)
-        { }
-
-        /// <summary>
-        /// Constructs a new NVML exception.
-        /// </summary>
-        /// <param name="errorCode">The NVML runtime error.</param>
-        public NvmlException(NvmlReturn errorCode)
-            : base()
-        {
-            Error = errorCode;
-        }
-
-        /// <summary cref="Exception(string)"/>
-        public NvmlException(string message)
-            : base(message)
-        {
-            Error = NvmlReturn.NVML_ERROR_UNKNOWN;
-        }
-
-        /// <summary cref="Exception(string, Exception)"/>
-        public NvmlException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-            Error = NvmlReturn.NVML_ERROR_UNKNOWN;
-        }
-
-        /// <summary cref="Exception(SerializationInfo, StreamingContext)"/>
-#if NET8_0_OR_GREATER
-        [Obsolete("SYSLIB0050: Formatter-based serialization is obsolete")]
-#endif
-        private NvmlException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-            Error = (NvmlReturn)info.GetInt32("Error");
-        }
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Returns the error.
-        /// </summary>
-        public NvmlReturn Error { get; }
-
-        #endregion
-
-        #region Methods
-
-        /// <summary cref="Exception.GetObjectData(SerializationInfo, StreamingContext)"/>
-#if NET8_0_OR_GREATER
-        [Obsolete("SYSLIB0050: Formatter-based serialization is obsolete")]
-#endif
-        public override void GetObjectData(
-            SerializationInfo info,
-            StreamingContext context)
-        {
-            base.GetObjectData(info, context);
-
-            info.AddValue("Error", (int)Error);
-        }
-
-        /// <summary>
-        /// Checks the given status and throws an exception in case of an error.
-        /// </summary>
-        /// <param name="errorCode">The NVML error code to check.</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ThrowIfFailed(NvmlReturn errorCode)
-        {
-            if (errorCode != NvmlReturn.NVML_SUCCESS)
-                throw new NvmlException(errorCode);
-        }
-
-        #endregion
+        Error = errorCode;
     }
+
+    /// <summary cref="Exception(string)"/>
+    public NvmlException(string message)
+        : base(message)
+    {
+        Error = NvmlReturn.NVML_ERROR_UNKNOWN;
+    }
+
+    /// <summary cref="Exception(string, Exception)"/>
+    public NvmlException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+        Error = NvmlReturn.NVML_ERROR_UNKNOWN;
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Returns the error.
+    /// </summary>
+    public NvmlReturn Error { get; }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Checks the given status and throws an exception in case of an error.
+    /// </summary>
+    /// <param name="errorCode">The NVML error code to check.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfFailed(NvmlReturn errorCode)
+    {
+        if (errorCode != NvmlReturn.NVML_SUCCESS)
+            throw new NvmlException(errorCode);
+    }
+
+    #endregion
 }
