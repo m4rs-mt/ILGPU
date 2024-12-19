635ba18c4b5a2a2f559e839431b85c62390ca298
Marcel Koester <m4rs@m4rs.net>
2024-12-19 00:26:50 +0000

    Refined NvJpeg API implementation to support image state creation and decoding functionalities.

--- Src/ILGPU/Runtime/Cuda/Libraries/NvJpegLibrary.cs
+++ Src/ILGPU/Runtime/Cuda/Libraries/NvJpegLibrary.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU Algorithms
-//                        Copyright (c) 2021-2023 ILGPU Project
+//                                        ILGPU
+//                        Copyright (c) 2021-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: NvJpegLibrary.cs
@@ -13,111 +13,110 @@ using ILGPU.Runtime.Cuda.API;
 using ILGPU.Util;
 using System;
 
-namespace ILGPU.Runtime.Cuda
+namespace ILGPU.Runtime.Cuda.Libraries;
+
+/// <summary>
+/// Represents an NvJpeg library.
+/// </summary>
+public sealed partial class NvJpegLibrary : DisposeBase
 {
     /// <summary>
-    /// Represents an NvJpeg library.
+    /// Constructs a new instance to wrap an NvJpeg library.
     /// </summary>
-    public sealed partial class NvJpegLibrary : DisposeBase
+    public NvJpegLibrary(NvJpegAPI api, IntPtr libHandle)
     {
-        /// <summary>
-        /// Constructs a new instance to wrap an NvJpeg library.
-        /// </summary>
-        public NvJpegLibrary(NvJpegAPI api, IntPtr libHandle)
-        {
-            API = api;
-            LibHandle = libHandle;
-        }
+        API = api;
+        LibHandle = libHandle;
+    }
 
-        /// <summary>
-        /// The underlying API wrapper.
-        /// </summary>
-        public NvJpegAPI API { get; }
+    /// <summary>
+    /// The underlying API wrapper.
+    /// </summary>
+    public NvJpegAPI API { get; }
 
-        /// <summary>
-        /// The native handle.
-        /// </summary>
-        public IntPtr LibHandle { get; private set; }
+    /// <summary>
+    /// The native handle.
+    /// </summary>
+    public IntPtr LibHandle { get; private set; }
 
-        /// <inheritdoc />
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                NvJpegException.ThrowIfFailed(
-                    API.Destroy(LibHandle));
-                LibHandle = IntPtr.Zero;
-            }
-            base.Dispose(disposing);
-        }
-
-        /// <summary>
-        /// Creates a new NvJpeg state instance.
-        /// </summary>
-        public NvJpegState CreateState()
+    /// <inheritdoc />
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
         {
             NvJpegException.ThrowIfFailed(
-                API.JpegStateCreate(LibHandle, out IntPtr stateHandle));
-            return new NvJpegState(API, stateHandle);
+                API.Destroy(LibHandle));
+            LibHandle = IntPtr.Zero;
         }
-
-        /// <inheritdoc cref="NvJpegAPI.GetImageInfo(
-        ///     IntPtr,
-        ///     ReadOnlySpan{byte},
-        ///     out int,
-        ///     out NvJpegChromaSubsampling,
-        ///     out int[],
-        ///     out int[])"/>
-        public unsafe NvJpegStatus GetImageInfo(
-            ReadOnlySpan<byte> imageBytes,
-            out int numComponents,
-            out NvJpegChromaSubsampling subsampling,
-            out int[] widths,
-            out int[] heights) =>
-            API.GetImageInfo(
-                LibHandle,
-                imageBytes,
-                out numComponents,
-                out subsampling,
-                out widths,
-                out heights);
-
-        /// <inheritdoc cref="NvJpegAPI.Decode(
-        ///     IntPtr,
-        ///     IntPtr,
-        ///     ReadOnlySpan{byte},
-        ///     NvJpegOutputFormat,
-        ///     in NvJpegImage, CudaStream)"/>
-        public unsafe NvJpegStatus Decode(
-            NvJpegState state,
-            ReadOnlySpan<byte> imageBytes,
-            NvJpegOutputFormat outputFormat,
-            in NvJpegImage destination,
-            CudaStream stream) =>
-            API.Decode(
-                LibHandle,
-                state.StateHandle,
-                imageBytes,
-                outputFormat,
-                destination,
-                stream);
-
-        /// <inheritdoc cref="NvJpegAPI.Decode(
-        ///     IntPtr,
-        ///     IntPtr,
-        ///     ReadOnlySpan{byte},
-        ///     NvJpegOutputFormat,
-        ///     in NvJpegImage)"/>
-        public NvJpegStatus Decode(
-            NvJpegState state,
-            ReadOnlySpan<byte> imageBytes,
-            NvJpegOutputFormat outputFormat,
-            in NvJpegImage destination) =>
-            API.Decode(
-                LibHandle,
-                state.StateHandle,
-                imageBytes,
-                outputFormat,
-                destination);
+        base.Dispose(disposing);
     }
+
+    /// <summary>
+    /// Creates a new NvJpeg state instance.
+    /// </summary>
+    public NvJpegState CreateState()
+    {
+        NvJpegException.ThrowIfFailed(
+            API.JpegStateCreate(LibHandle, out IntPtr stateHandle));
+        return new NvJpegState(API, stateHandle);
+    }
+
+    /// <inheritdoc cref="NvJpegAPI.GetImageInfo(
+    ///     IntPtr,
+    ///     ReadOnlySpan{byte},
+    ///     out int,
+    ///     out NvJpegChromaSubsampling,
+    ///     out int[],
+    ///     out int[])"/>
+    public unsafe NvJpegStatus GetImageInfo(
+        ReadOnlySpan<byte> imageBytes,
+        out int numComponents,
+        out NvJpegChromaSubsampling subsampling,
+        out int[] widths,
+        out int[] heights) =>
+        API.GetImageInfo(
+            LibHandle,
+            imageBytes,
+            out numComponents,
+            out subsampling,
+            out widths,
+            out heights);
+
+    /// <inheritdoc cref="NvJpegAPI.Decode(
+    ///     IntPtr,
+    ///     IntPtr,
+    ///     ReadOnlySpan{byte},
+    ///     NvJpegOutputFormat,
+    ///     in NvJpegImage, CudaStream)"/>
+    public unsafe NvJpegStatus Decode(
+        NvJpegState state,
+        ReadOnlySpan<byte> imageBytes,
+        NvJpegOutputFormat outputFormat,
+        in NvJpegImage destination,
+        CudaStream stream) =>
+        API.Decode(
+            LibHandle,
+            state.StateHandle,
+            imageBytes,
+            outputFormat,
+            destination,
+            stream);
+
+    /// <inheritdoc cref="NvJpegAPI.Decode(
+    ///     IntPtr,
+    ///     IntPtr,
+    ///     ReadOnlySpan{byte},
+    ///     NvJpegOutputFormat,
+    ///     in NvJpegImage)"/>
+    public NvJpegStatus Decode(
+        NvJpegState state,
+        ReadOnlySpan<byte> imageBytes,
+        NvJpegOutputFormat outputFormat,
+        in NvJpegImage destination) =>
+        API.Decode(
+            LibHandle,
+            state.StateHandle,
+            imageBytes,
+            outputFormat,
+            destination);
 }
