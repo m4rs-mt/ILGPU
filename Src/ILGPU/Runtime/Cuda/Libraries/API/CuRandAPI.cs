a4259c02d9c600a192b0097cf77d5c777489ec9f
Marcel Koester <m4rs@m4rs.net>
2024-12-19 02:46:34 +0000

    Refined CuRand API to support creation of API wrappers using the latest installed version and improved exception handling.

--- Src/ILGPU/Runtime/Cuda/Libraries/API/CuRandAPI.cs
+++ Src/ILGPU/Runtime/Cuda/Libraries/API/CuRandAPI.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU Algorithms
-//                        Copyright (c) 2021-2023 ILGPU Project
+//                                        ILGPU
+//                        Copyright (c) 2021-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: CuRandAPI.cs
@@ -9,65 +9,65 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
+using ILGPU.Runtime.Cuda.Libraries;
 using System;
 
-namespace ILGPU.Runtime.Cuda.API
+namespace ILGPU.Runtime.Cuda.API;
+
+/// <summary>
+/// An implementation of the cuRAND API.
+/// </summary>
+public abstract partial class CuRandAPI
 {
+    #region Static
+
     /// <summary>
-    /// An implementation of the cuRAND API.
+    /// Creates a new API wrapper.
     /// </summary>
-    public abstract partial class CuRandAPI
+    /// <param name="version">The cuRand version to use.</param>
+    /// <returns>The created API wrapper.</returns>
+    public static CuRandAPI Create(CuRandAPIVersion? version) =>
+        version.HasValue
+        ? CreateInternal(version.Value)
+            ?? throw new DllNotFoundException(nameof(CuRandAPI))
+        : CreateLatest();
+
+    /// <summary>
+    /// Creates a new API wrapper using the latest installed version.
+    /// </summary>
+    /// <returns>The created API wrapper.</returns>
+    private static CuRandAPI CreateLatest()
     {
-        #region Static
-
-        /// <summary>
-        /// Creates a new API wrapper.
-        /// </summary>
-        /// <param name="version">The cuRand version to use.</param>
-        /// <returns>The created API wrapper.</returns>
-        public static CuRandAPI Create(CuRandAPIVersion? version) =>
-            version.HasValue
-            ? CreateInternal(version.Value)
-                ?? throw new DllNotFoundException(nameof(CuRandAPI))
-            : CreateLatest();
-
-        /// <summary>
-        /// Creates a new API wrapper using the latest installed version.
-        /// </summary>
-        /// <returns>The created API wrapper.</returns>
-        private static CuRandAPI CreateLatest()
+        Exception? firstException = null;
+        var versions = Enum.GetValues<CuRandAPIVersion>();
+        for (var i = versions.Length - 1; i >= 0; i--)
         {
-            Exception? firstException = null;
-            var versions = Enum.GetValues<CuRandAPIVersion>();
-            for (var i = versions.Length - 1; i >= 0; i--)
+            var version = versions[i];
+            var api = CreateInternal(version);
+            if (api != null)
             {
-                var version = versions[i];
-                var api = CreateInternal(version);
-                if (api != null)
+                try
                 {
-                    try
-                    {
-                        var status = api.GetVersion(out _);
-                        if (status == CuRandStatus.CURAND_STATUS_SUCCESS)
-                            return api;
-                    }
-                    catch (Exception ex) when (
-                        ex is DllNotFoundException ||
-                        ex is EntryPointNotFoundException)
-                    {
-                        firstException ??= ex;
-                    }
+                    var status = api.GetVersion(out _);
+                    if (status == CuRandStatus.CURAND_STATUS_SUCCESS)
+                        return api;
+                }
+                catch (Exception ex) when (
+                    ex is DllNotFoundException ||
+                    ex is EntryPointNotFoundException)
+                {
+                    firstException ??= ex;
                 }
             }
-
-            throw firstException ?? new DllNotFoundException(nameof(CuRandAPI));
         }
 
-        /// <summary>
-        /// Constructs a new cuRAND API instance.
-        /// </summary>
-        protected CuRandAPI() { }
-
-        #endregion
+        throw firstException ?? new DllNotFoundException(nameof(CuRandAPI));
     }
+
+    /// <summary>
+    /// Constructs a new cuRAND API instance.
+    /// </summary>
+    protected CuRandAPI() { }
+
+    #endregion
 }
