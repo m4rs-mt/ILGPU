5777ff85887517c33b3007b829ab72d8602004f1
Marcel Koester <m4rs@m4rs.net>
2024-12-19 22:44:57 +0000

    Refined CuRandException class to handle different error scenarios and improved exception handling.

--- Src/ILGPU/Runtime/Cuda/Libraries/CuRandException.cs
+++ Src/ILGPU/Runtime/Cuda/Libraries/CuRandException.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU Algorithms
-//                        Copyright (c) 2021-2023 ILGPU Project
+//                                        ILGPU
+//                        Copyright (c) 2021-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: CuRandException.cs
@@ -11,97 +11,71 @@
 
 using System;
 using System.Runtime.CompilerServices;
-using System.Runtime.Serialization;
-using System.Security.Permissions;
 
-namespace ILGPU.Runtime.Cuda
+namespace ILGPU.Runtime.Cuda.Libraries;
+
+/// <summary>
+/// Represents an CuRand exception that can be thrown by the CuRand library.
+/// </summary>
+[Serializable]
+public sealed class CuRandException : Exception
 {
+    #region Instance
+
     /// <summary>
-    /// Represents an CuRand exception that can be thrown by the CuRand library.
+    /// Constructs a new CuRand exception.
     /// </summary>
-    [Serializable]
-    public sealed class CuRandException : Exception
+    public CuRandException()
+        : this(CuRandStatus.CURAND_STATUS_NOT_INITIALIZED)
+    { }
+
+    /// <summary>
+    /// Constructs a new CuRand exception.
+    /// </summary>
+    /// <param name="errorCode">The CuRand runtime error.</param>
+    public CuRandException(CuRandStatus errorCode)
+        : base()
     {
-        #region Instance
-
-        /// <summary>
-        /// Constructs a new CuRand exception.
-        /// </summary>
-        public CuRandException()
-            : this(CuRandStatus.CURAND_STATUS_NOT_INITIALIZED)
-        { }
-
-        /// <summary>
-        /// Constructs a new CuRand exception.
-        /// </summary>
-        /// <param name="errorCode">The CuRand runtime error.</param>
-        public CuRandException(CuRandStatus errorCode)
-            : base()
-        {
-            Error = errorCode;
-        }
-
-        /// <summary cref="Exception(string)"/>
-        public CuRandException(string message)
-            : base(message)
-        {
-            Error = CuRandStatus.CURAND_STATUS_NOT_INITIALIZED;
-        }
-
-        /// <summary cref="Exception(string, Exception)"/>
-        public CuRandException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-            Error = CuRandStatus.CURAND_STATUS_NOT_INITIALIZED;
-        }
-
-        /// <summary cref="Exception(SerializationInfo, StreamingContext)"/>
-#if NET8_0_OR_GREATER
-        [Obsolete("SYSLIB0050: Formatter-based serialization is obsolete")]
-#endif
-        private CuRandException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-            Error = (CuRandStatus)info.GetInt32("Error");
-        }
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Returns the error.
-        /// </summary>
-        public CuRandStatus Error { get; }
-
-        #endregion
-
-        #region Methods
-
-        /// <summary cref="Exception.GetObjectData(SerializationInfo, StreamingContext)"/>
-#if NET8_0_OR_GREATER
-        [Obsolete("SYSLIB0050: Formatter-based serialization is obsolete")]
-#endif
-        public override void GetObjectData(
-            SerializationInfo info,
-            StreamingContext context)
-        {
-            base.GetObjectData(info, context);
-
-            info.AddValue("Error", (int)Error);
-        }
-
-        /// <summary>
-        /// Checks the given status and throws an exception in case of an error.
-        /// </summary>
-        /// <param name="errorCode">The CuRand error code to check.</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ThrowIfFailed(CuRandStatus errorCode)
-        {
-            if (errorCode != CuRandStatus.CURAND_STATUS_SUCCESS)
-                throw new CuRandException(errorCode);
-        }
-
-        #endregion
+        Error = errorCode;
     }
+
+    /// <summary cref="Exception(string)"/>
+    public CuRandException(string message)
+        : base(message)
+    {
+        Error = CuRandStatus.CURAND_STATUS_NOT_INITIALIZED;
+    }
+
+    /// <summary cref="Exception(string, Exception)"/>
+    public CuRandException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+        Error = CuRandStatus.CURAND_STATUS_NOT_INITIALIZED;
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Returns the error.
+    /// </summary>
+    public CuRandStatus Error { get; }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Checks the given status and throws an exception in case of an error.
+    /// </summary>
+    /// <param name="errorCode">The CuRand error code to check.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfFailed(CuRandStatus errorCode)
+    {
+        if (errorCode != CuRandStatus.CURAND_STATUS_SUCCESS)
+            throw new CuRandException(errorCode);
+    }
+
+    #endregion
 }
