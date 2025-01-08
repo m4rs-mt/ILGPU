a5e23d06d25071a969d560ca44851a7031c0fed5
Marcel Koester <m4rs@m4rs.net>
2025-01-08 01:02:35 +0000

    Refined CuBlasException class to support error status and improved exception handling.

--- Src/ILGPU/Runtime/Cuda/Libraries/CuBlasException.cs
+++ Src/ILGPU/Runtime/Cuda/Libraries/CuBlasException.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU Algorithms
-//                        Copyright (c) 2020-2023 ILGPU Project
+//                                        ILGPU
+//                        Copyright (c) 2020-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: CuBlasException.cs
@@ -11,97 +11,71 @@
 
 using System;
 using System.Runtime.CompilerServices;
-using System.Runtime.Serialization;
-using System.Security.Permissions;
 
-namespace ILGPU.Runtime.Cuda
+namespace ILGPU.Runtime.Cuda.Libraries;
+
+/// <summary>
+/// Represents an CuBlas exception that can be thrown by the CuBlas library.
+/// </summary>
+[Serializable]
+public sealed class CuBlasException : Exception
 {
+    #region Instance
+
     /// <summary>
-    /// Represents an CuBlas exception that can be thrown by the CuBlas library.
+    /// Constructs a new CuBlas exception.
     /// </summary>
-    [Serializable]
-    public sealed class CuBlasException : Exception
+    public CuBlasException()
+        : this(CuBlasStatus.CUBLAS_STATUS_NOT_SUPPORTED)
+    { }
+
+    /// <summary>
+    /// Constructs a new CuBlas exception.
+    /// </summary>
+    /// <param name="errorCode">The CuBlas runtime error.</param>
+    public CuBlasException(CuBlasStatus errorCode)
+        : base()
     {
-        #region Instance
-
-        /// <summary>
-        /// Constructs a new CuBlas exception.
-        /// </summary>
-        public CuBlasException()
-            : this(CuBlasStatus.CUBLAS_STATUS_NOT_SUPPORTED)
-        { }
-
-        /// <summary>
-        /// Constructs a new CuBlas exception.
-        /// </summary>
-        /// <param name="errorCode">The CuBlas runtime error.</param>
-        public CuBlasException(CuBlasStatus errorCode)
-            : base()
-        {
-            Error = errorCode;
-        }
-
-        /// <summary cref="Exception(string)"/>
-        public CuBlasException(string message)
-            : base(message)
-        {
-            Error = CuBlasStatus.CUBLAS_STATUS_NOT_SUPPORTED;
-        }
-
-        /// <summary cref="Exception(string, Exception)"/>
-        public CuBlasException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-            Error = CuBlasStatus.CUBLAS_STATUS_NOT_SUPPORTED;
-        }
-
-        /// <summary cref="Exception(SerializationInfo, StreamingContext)"/>
-#if NET8_0_OR_GREATER
-        [Obsolete("SYSLIB0050: Formatter-based serialization is obsolete")]
-#endif
-        private CuBlasException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-            Error = (CuBlasStatus)info.GetInt32("Error");
-        }
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Returns the error.
-        /// </summary>
-        public CuBlasStatus Error { get; }
-
-        #endregion
-
-        #region Methods
-
-        /// <summary cref="Exception.GetObjectData(SerializationInfo, StreamingContext)"/>
-#if NET8_0_OR_GREATER
-        [Obsolete("SYSLIB0050: Formatter-based serialization is obsolete")]
-#endif
-        public override void GetObjectData(
-            SerializationInfo info,
-            StreamingContext context)
-        {
-            base.GetObjectData(info, context);
-
-            info.AddValue("Error", (int)Error);
-        }
-
-        /// <summary>
-        /// Checks the given status and throws an exception in case of an error.
-        /// </summary>
-        /// <param name="errorCode">The CuBlas error code to check.</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ThrowIfFailed(CuBlasStatus errorCode)
-        {
-            if (errorCode != CuBlasStatus.CUBLAS_STATUS_SUCCESS)
-                throw new CuBlasException(errorCode);
-        }
-
-        #endregion
+        Error = errorCode;
     }
+
+    /// <summary cref="Exception(string)"/>
+    public CuBlasException(string message)
+        : base(message)
+    {
+        Error = CuBlasStatus.CUBLAS_STATUS_NOT_SUPPORTED;
+    }
+
+    /// <summary cref="Exception(string, Exception)"/>
+    public CuBlasException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+        Error = CuBlasStatus.CUBLAS_STATUS_NOT_SUPPORTED;
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Returns the error.
+    /// </summary>
+    public CuBlasStatus Error { get; }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Checks the given status and throws an exception in case of an error.
+    /// </summary>
+    /// <param name="errorCode">The CuBlas error code to check.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfFailed(CuBlasStatus errorCode)
+    {
+        if (errorCode != CuBlasStatus.CUBLAS_STATUS_SUCCESS)
+            throw new CuBlasException(errorCode);
+    }
+
+    #endregion
 }
