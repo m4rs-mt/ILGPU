2059fba5449c96d24754ea3131766878ef4454d4
Marcel Koester <m4rs@m4rs.net>
2025-01-08 23:19:08 +0000

    Refined CuBlas API to include pointer mode handling with automatic and manual modes.

--- Src/ILGPU/Runtime/Cuda/Libraries/CuBlasPointerModeHandler.cs
+++ Src/ILGPU/Runtime/Cuda/Libraries/CuBlasPointerModeHandler.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
-//                                   ILGPU Algorithms
-//                        Copyright (c) 2020-2021 ILGPU Project
+//                                        ILGPU
+//                        Copyright (c) 2020-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: CuBlasPointerModeHandler.cs
@@ -11,82 +11,79 @@
 
 using System.Runtime.CompilerServices;
 
-namespace ILGPU.Runtime.Cuda
+namespace ILGPU.Runtime.Cuda.Libraries;
+
+/// <summary>
+/// An abstract handler to adapt the current pointer mode of a
+/// <see cref="CuBlas{TPointerModeHandler}"/> instance.
+/// </summary>
+/// <typeparam name="THandler">The handler type itself.</typeparam>
+public interface ICuBlasPointerModeHandler<THandler>
+    where THandler : struct, ICuBlasPointerModeHandler<THandler>
 {
     /// <summary>
-    /// An abstract handler to adapt the current pointer mode of a
-    /// <see cref="CuBlas{TPointerModeHandler}"/> instance.
+    /// Updates the pointer mode to be compatible with the given one.
     /// </summary>
-    /// <typeparam name="THandler">The handler type itself.</typeparam>
-    public interface ICuBlasPointerModeHandler<THandler>
-        where THandler : struct, ICuBlasPointerModeHandler<THandler>
+    /// <param name="parent">The parent instance to use.</param>
+    /// <param name="pointerMode">The new pointer mode to use.</param>
+    void UpdatePointerMode(CuBlas<THandler> parent, CuBlasPointerMode pointerMode);
+}
+
+/// <summary>
+/// A utility class that holds pre-defined pointer mode handlers that can be used in
+/// combination with the type <see cref="CuBlas{TPointerModeHandler}"/>.
+/// </summary>
+public static class CuBlasPointerModeHandlers
+{
+    #region Nested Types
+
+    /// <summary>
+    /// A custom handler type that automatically updates the pointer mode to be
+    /// compatible with the requested pointer mode.
+    /// </summary>
+    public readonly struct AutomaticMode : ICuBlasPointerModeHandler<AutomaticMode>
     {
-        /// <summary>
-        /// Updates the pointer mode to be compatible with the given one.
-        /// </summary>
-        /// <param name="parent">The parent instance to use.</param>
-        /// <param name="pointerMode">The new pointer mode to use.</param>
-        void UpdatePointerMode(CuBlas<THandler> parent, CuBlasPointerMode pointerMode);
+        /// <summary cref="ICuBlasPointerModeHandler{THandler}.UpdatePointerMode(
+        /// CuBlas{THandler}, CuBlasPointerMode)"/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void UpdatePointerMode(
+            CuBlas<AutomaticMode> parent,
+            CuBlasPointerMode pointerMode) =>
+            // Setup the internal pointer mode ot the desired one
+            parent.PointerMode = pointerMode;
     }
 
     /// <summary>
-    /// A utility class that holds pre-defined pointer mode handlers that can be used in
-    /// combination with the type <see cref="CuBlas{TPointerModeHandler}"/>.
+    /// A custom handler type that does not automatically update the pointer mode.
     /// </summary>
-    public static class CuBlasPointerModeHandlers
+    public readonly struct ManualMode : ICuBlasPointerModeHandler<ManualMode>
     {
-        #region Nested Types
-
-        /// <summary>
-        /// A custom handler type that automatically updates the pointer mode to be
-        /// compatible with the requested pointer mode.
-        /// </summary>
-        public readonly struct AutomaticMode : ICuBlasPointerModeHandler<AutomaticMode>
+        /// <summary cref="ICuBlasPointerModeHandler{THandler}.UpdatePointerMode(
+        /// CuBlas{THandler}, CuBlasPointerMode)"/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void UpdatePointerMode(
+            CuBlas<ManualMode> parent,
+            CuBlasPointerMode pointerMode)
         {
-            /// <summary cref="ICuBlasPointerModeHandler{THandler}.UpdatePointerMode(
-            /// CuBlas{THandler}, CuBlasPointerMode)"/>
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void UpdatePointerMode(
-                CuBlas<AutomaticMode> parent,
-                CuBlasPointerMode pointerMode)
-            {
-                // Setup the internal pointer mode ot the desired one
-                parent.PointerMode = pointerMode;
-            }
+            // Do nothing
         }
-
-        /// <summary>
-        /// A custom handler type that does not automatically update the pointer mode.
-        /// </summary>
-        public readonly struct ManualMode : ICuBlasPointerModeHandler<ManualMode>
-        {
-            /// <summary cref="ICuBlasPointerModeHandler{THandler}.UpdatePointerMode(
-            /// CuBlas{THandler}, CuBlasPointerMode)"/>
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void UpdatePointerMode(
-                CuBlas<ManualMode> parent,
-                CuBlasPointerMode pointerMode)
-            {
-                // Do nothing
-            }
-        }
-
-        #endregion
-
-        #region Static
-
-        /// <summary>
-        /// Returns an automatic pointer mode handler that switches the underlying pointer
-        /// mode of the <see cref="CuBlas{TPointerModeHandler}"/> class automatically.
-        /// </summary>
-        public static AutomaticMode Automatic { get; }
-
-        /// <summary>
-        /// Returns a manual pointer mode handler that does not change the underlying
-        /// pointer mode of the <see cref="CuBlas{TPointerModeHandler}"/> class.
-        /// </summary>
-        public static ManualMode Manual { get; }
-
-        #endregion
     }
+
+    #endregion
+
+    #region Static
+
+    /// <summary>
+    /// Returns an automatic pointer mode handler that switches the underlying pointer
+    /// mode of the <see cref="CuBlas{TPointerModeHandler}"/> class automatically.
+    /// </summary>
+    public static AutomaticMode Automatic { get; }
+
+    /// <summary>
+    /// Returns a manual pointer mode handler that does not change the underlying
+    /// pointer mode of the <see cref="CuBlas{TPointerModeHandler}"/> class.
+    /// </summary>
+    public static ManualMode Manual { get; }
+
+    #endregion
 }
