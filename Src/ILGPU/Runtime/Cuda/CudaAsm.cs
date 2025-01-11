67314803ee2d22028cfa81162f90ad34fb46e174
Marcel Koester <m4rs@m4rs.net>
2025-01-11 00:56:46 +0000

    Refined CudaAsm to support inline PTX assembly instructions and added interfaces for parameter handling.

--- Src/ILGPU/Runtime/Cuda/CudaAsm.cs
+++ Src/ILGPU/Runtime/Cuda/CudaAsm.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2021-2022 ILGPU Project
+//                        Copyright (c) 2021-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: CudaAsm.cs
@@ -9,92 +9,90 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPU.Frontend.Intrinsic;
+using ILGPU.Intrinsic;
 using System;
 using System.Runtime.CompilerServices;
 
 #pragma warning disable CA1051 // Do not declare visible instance fields
 #pragma warning disable CA1724 // Type names should not match namespaces
 
-namespace ILGPU.Runtime.Cuda
+namespace ILGPU.Runtime.Cuda;
+
+/// <summary>
+/// Provides library calls for inline PTX assembly instructions.
+/// </summary>
+public static partial class CudaAsm
 {
     /// <summary>
-    /// Provides library calls for inline PTX assembly instructions.
+    /// Returns true if running on a Cuda accelerator.
     /// </summary>
-    public static partial class CudaAsm
+    public static bool IsSupported
     {
-        /// <summary>
-        /// Returns true if running on a Cuda accelerator.
-        /// </summary>
-        public static bool IsSupported
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => Accelerator.CurrentType == AcceleratorType.Cuda;
-        }
-
-        /// <summary>
-        /// Writes the inline PTX assembly instructions into the kernel.
-        /// </summary>
-        /// <param name="asm">The PTX assembly instruction string.</param>
-        [LanguageIntrinsic(LanguageIntrinsicKind.EmitPTX)]
-        public static void Emit(string asm) =>
-            throw new NotImplementedException();
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => Accelerator.CurrentType == AcceleratorType.Cuda;
     }
 
     /// <summary>
-    /// Base interface used by <see cref="CudaAsm.EmitRef{T0}(string, ref T0)"/> to
-    /// ensure only valid generic arguments are supplied.
+    /// Writes the inline PTX assembly instructions into the kernel.
     /// </summary>
-    public interface ICudaAsmEmitParameter
-    { }
+    /// <param name="asm">The PTX assembly instruction string.</param>
+    [LanguageIntrinsic]
+    public static void Emit(string asm) =>
+        throw new NotImplementedException();
+}
+
+/// <summary>
+/// Base interface used by <see cref="CudaAsm.EmitRef{T0}(string, ref T0)"/> to
+/// ensure only valid generic arguments are supplied.
+/// </summary>
+public interface ICudaAsmEmitParameter
+{ }
+
+/// <summary>
+/// Input parameter for the PTX instructions.
+/// </summary>
+public struct Input<T> : ICudaAsmEmitParameter
+    where T : struct
+{
+    /// <summary>
+    /// Holds the input value.
+    /// </summary>
+    public T Value;
 
     /// <summary>
-    /// Input parameter for the PTX instructions.
+    /// Wraps the value as an input parameter.
     /// </summary>
-    public struct Input<T> : ICudaAsmEmitParameter
-        where T : struct
-    {
-        /// <summary>
-        /// Holds the input value.
-        /// </summary>
-        public T Value;
+    public static implicit operator Input<T>(T v) =>
+        new Input<T>() { Value = v };
+}
 
-        /// <summary>
-        /// Wraps the value as an input parameter.
-        /// </summary>
-        public static implicit operator Input<T>(T v) =>
-            new Input<T>() { Value = v };
-    }
+/// <summary>
+/// Output parameter for the PTX instructions.
+/// </summary>
+public struct Output<T> : ICudaAsmEmitParameter
+    where T : struct
+{
+    /// <summary>
+    /// Filled in with the output value.
+    /// </summary>
+    public T Value;
+}
+
+/// <summary>
+/// Input/Output parameter for the PTX instructions.
+/// </summary>
+public struct Ref<T> : ICudaAsmEmitParameter
+    where T : struct
+{
+    /// <summary>
+    /// Holds the input value. Filled in with the output value.
+    /// </summary>
+    public T Value;
 
     /// <summary>
-    /// Output parameter for the PTX instructions.
+    /// Wraps the value as a reference parameter.
     /// </summary>
-    public struct Output<T> : ICudaAsmEmitParameter
-        where T : struct
-    {
-        /// <summary>
-        /// Filled in with the output value.
-        /// </summary>
-        public T Value;
-    }
-
-    /// <summary>
-    /// Input/Output parameter for the PTX instructions.
-    /// </summary>
-    public struct Ref<T> : ICudaAsmEmitParameter
-        where T : struct
-    {
-        /// <summary>
-        /// Holds the input value. Filled in with the output value.
-        /// </summary>
-        public T Value;
-
-        /// <summary>
-        /// Wraps the value as a reference parameter.
-        /// </summary>
-        public static implicit operator Ref<T>(T v) =>
-            new Ref<T>() { Value = v };
-    }
+    public static implicit operator Ref<T>(T v) => new() { Value = v };
 }
 
 #pragma warning restore CA1724 // Type names should not match namespaces
