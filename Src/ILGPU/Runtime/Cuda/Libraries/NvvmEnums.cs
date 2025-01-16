8f261fbaf89f6210df90c6f12b4cf4c9a884897b
Marcel Koester <m4rs@m4rs.net>
2025-01-16 22:20:33 +0000

    Moved NvvmEnums.cs to Src/ILGPU/Runtime/Cuda/Libraries/NvvmEnums.cs.

--- Src/ILGPU/Runtime/Cuda/Libraries/NvvmEnums.cs
+++ Src/ILGPU/Runtime/Cuda/Libraries/NvvmEnums.cs
@@ -11,7 +11,7 @@
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
-namespace ILGPU.Runtime.Cuda.Nvvm;
+namespace ILGPU.Runtime.Cuda.Libraries;
 
 public enum NvvmResult
 {
