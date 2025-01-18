7196c8bd4ee7e8e1c54d12ea4529ef7eecceb02a
Marcel Koester <m4rs@m4rs.net>
2025-01-18 00:10:55 +0000

    Refined BasicValueType and introduced ArithmeticBasicValueType enums to support additional value types.

--- Src/ILGPUC/BasicValueType.cs
+++ Src/ILGPUC/BasicValueType.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2017-2021 ILGPU Project
+//                        Copyright (c) 2017-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: BasicValueType.cs
@@ -9,127 +9,126 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-namespace ILGPU
+namespace ILGPUC;
+
+/// <summary>
+/// Represents a basic value type.
+/// </summary>
+enum BasicValueType
 {
     /// <summary>
-    /// Represents a basic value type.
+    /// Represent a non-basic value type.
     /// </summary>
-    public enum BasicValueType
-    {
-        /// <summary>
-        /// Represent a non-basic value type.
-        /// </summary>
-        None,
-
-        /// <summary>
-        /// Represents an 1-bit integer.
-        /// </summary>
-        Int1,
-
-        /// <summary>
-        /// Represents an 8-bit integer.
-        /// </summary>
-        Int8,
-
-        /// <summary>
-        /// Represents a 16-bit integer.
-        /// </summary>
-        Int16,
-
-        /// <summary>
-        /// Represents a 32-bit integer.
-        /// </summary>
-        Int32,
-
-        /// <summary>
-        /// Represents a 64-bit integer.
-        /// </summary>
-        Int64,
-
-        /// <summary>
-        /// Represents a 16-bit float.
-        /// </summary>
-        Float16,
-
-        /// <summary>
-        /// Represents a 32-bit float.
-        /// </summary>
-        Float32,
-
-        /// <summary>
-        /// Represents a 64-bit float.
-        /// </summary>
-        Float64,
-    }
+    None,
 
     /// <summary>
-    /// Represents an arithmetic basic value type.
+    /// Represents an 1-bit integer.
     /// </summary>
-    public enum ArithmeticBasicValueType
-    {
-        /// <summary>
-        /// Represent a non-arithmetic value type.
-        /// </summary>
-        None,
+    Int1,
 
-        /// <summary>
-        /// Represents an 1-bit integer.
-        /// </summary>
-        UInt1,
+    /// <summary>
+    /// Represents an 8-bit integer.
+    /// </summary>
+    Int8,
 
-        /// <summary>
-        /// Represents an 8-bit integer.
-        /// </summary>
-        Int8,
+    /// <summary>
+    /// Represents a 16-bit integer.
+    /// </summary>
+    Int16,
 
-        /// <summary>
-        /// Represents a 16-bit integer.
-        /// </summary>
-        Int16,
+    /// <summary>
+    /// Represents a 32-bit integer.
+    /// </summary>
+    Int32,
 
-        /// <summary>
-        /// Represents a 32-bit integer.
-        /// </summary>
-        Int32,
+    /// <summary>
+    /// Represents a 64-bit integer.
+    /// </summary>
+    Int64,
 
-        /// <summary>
-        /// Represents a 64-bit integer.
-        /// </summary>
-        Int64,
+    /// <summary>
+    /// Represents a 16-bit float.
+    /// </summary>
+    Float16,
 
-        /// <summary>
-        /// Represents a 16-bit float.
-        /// </summary>
-        Float16,
+    /// <summary>
+    /// Represents a 32-bit float.
+    /// </summary>
+    Float32,
 
-        /// <summary>
-        /// Represents a 32-bit float.
-        /// </summary>
-        Float32,
+    /// <summary>
+    /// Represents a 64-bit float.
+    /// </summary>
+    Float64,
+}
 
-        /// <summary>
-        /// Represents a 64-bit float.
-        /// </summary>
-        Float64,
+/// <summary>
+/// Represents an arithmetic basic value type.
+/// </summary>
+enum ArithmeticBasicValueType
+{
+    /// <summary>
+    /// Represent a non-arithmetic value type.
+    /// </summary>
+    None,
 
-        /// <summary>
-        /// Represents an 8-bit unsigned integer.
-        /// </summary>
-        UInt8,
+    /// <summary>
+    /// Represents an 1-bit integer.
+    /// </summary>
+    UInt1,
 
-        /// <summary>
-        /// Represents a 16-bit unsigned integer.
-        /// </summary>
-        UInt16,
+    /// <summary>
+    /// Represents an 8-bit integer.
+    /// </summary>
+    Int8,
 
-        /// <summary>
-        /// Represents a 32-bit unsigned integer.
-        /// </summary>
-        UInt32,
+    /// <summary>
+    /// Represents a 16-bit integer.
+    /// </summary>
+    Int16,
 
-        /// <summary>
-        /// Represents a 64-bit unsigned integer.
-        /// </summary>
-        UInt64,
-    }
+    /// <summary>
+    /// Represents a 32-bit integer.
+    /// </summary>
+    Int32,
+
+    /// <summary>
+    /// Represents a 64-bit integer.
+    /// </summary>
+    Int64,
+
+    /// <summary>
+    /// Represents a 16-bit float.
+    /// </summary>
+    Float16,
+
+    /// <summary>
+    /// Represents a 32-bit float.
+    /// </summary>
+    Float32,
+
+    /// <summary>
+    /// Represents a 64-bit float.
+    /// </summary>
+    Float64,
+
+    /// <summary>
+    /// Represents an 8-bit unsigned integer.
+    /// </summary>
+    UInt8,
+
+    /// <summary>
+    /// Represents a 16-bit unsigned integer.
+    /// </summary>
+    UInt16,
+
+    /// <summary>
+    /// Represents a 32-bit unsigned integer.
+    /// </summary>
+    UInt32,
+
+    /// <summary>
+    /// Represents a 64-bit unsigned integer.
+    /// </summary>
+    UInt64,
 }
