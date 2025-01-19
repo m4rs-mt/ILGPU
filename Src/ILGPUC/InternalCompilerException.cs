84e39f3728ca9150a684a26ecbc60a12d6e0435c
Marcel Koester <m4rs@m4rs.net>
2025-01-19 13:27:05 +0000

    Refined InternalCompilerException class to enhance error handling and initialization methods.

--- Src/ILGPUC/InternalCompilerException.cs
+++ Src/ILGPUC/InternalCompilerException.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2020-2023 ILGPU Project
+//                        Copyright (c) 2020-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: InternalCompilerException.cs
@@ -10,66 +10,43 @@
 // ---------------------------------------------------------------------------------------
 
 using System;
-using System.Runtime.Serialization;
 
-namespace ILGPU
+namespace ILGPUC;
+
+/// <summary>
+/// The exception that is thrown when an internal compiler error has been detected.
+/// </summary>
+[Serializable]
+sealed class InternalCompilerException : Exception
 {
     /// <summary>
-    /// The exception that is thrown when an internal compiler error has been detected.
+    /// Initializes a new instance of the InternalCompilerException class.
     /// </summary>
-    [Serializable]
-    public sealed class InternalCompilerException : Exception
-    {
-        /// <summary>
-        /// Initializes a new instance of the InternalCompilerException class.
-        /// </summary>
-        public InternalCompilerException()
-        { }
+    public InternalCompilerException()
+    { }
 
-        /// <summary>
-        /// Initializes a new instance of the InternalCompilerException class
-        /// with a specified error message.
-        /// </summary>
-        /// <param name="message">The message that describes the error.</param>
-        public InternalCompilerException(string message)
-            : base(message)
-        { }
+    /// <summary>
+    /// Initializes a new instance of the InternalCompilerException class
+    /// with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    public InternalCompilerException(string message)
+        : base(message)
+    { }
 
-        /// <summary>
-        /// Initializes a new instance of the InternalCompilerException class
-        /// with a specified error message and a reference to the inner exception
-        /// that is the cause of this exception.
-        /// </summary>
-        /// <param name="message">
-        /// The error message that explains the reason for the exception.
-        /// </param>
-        /// <param name="innerException">
-        /// The exception that is the cause of the current exception, or a null reference
-        /// if no inner exception is specified.
-        /// </param>
-        public InternalCompilerException(string message, Exception innerException)
-            : base(message, innerException)
-        { }
-
-        /// <summary>
-        /// Initializes a new instance of the InternalCompilerException class with
-        /// serialized data.
-        /// </summary>
-        /// <param name="serializationInfo">
-        /// The System.Runtime.Serialization.SerializationInfo that holds the serialized
-        /// object data about the exception being thrown.
-        /// </param>
-        /// <param name="streamingContext">
-        /// The System.Runtime.Serialization.StreamingContext that contains contextual
-        /// information about the source or destination.
-        /// </param>
-#if NET8_0_OR_GREATER
-        [Obsolete("SYSLIB0050: Formatter-based serialization is obsolete")]
-#endif
-        private InternalCompilerException(
-            SerializationInfo serializationInfo,
-            StreamingContext streamingContext)
-            : base(serializationInfo, streamingContext)
-        { }
-    }
+    /// <summary>
+    /// Initializes a new instance of the InternalCompilerException class
+    /// with a specified error message and a reference to the inner exception
+    /// that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">
+    /// The error message that explains the reason for the exception.
+    /// </param>
+    /// <param name="innerException">
+    /// The exception that is the cause of the current exception, or a null reference
+    /// if no inner exception is specified.
+    /// </param>
+    public InternalCompilerException(string message, Exception innerException)
+        : base(message, innerException)
+    { }
 }
