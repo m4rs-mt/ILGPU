d6aa919d0b627052562139cb11e43979aef26cc9
Marcel Koester <m4rs@m4rs.net>
2025-01-24 20:27:50 +0000

    Added IsLauncherAttribute and related attributes to mark methods for kernel launches and restrictions in ILGPU.

--- Src/ILGPU/CodeGeneration/LauncherAttributes.cs
+++ Src/ILGPU/CodeGeneration/LauncherAttributes.cs
@@ -17,7 +17,13 @@ namespace ILGPU.CodeGeneration;
 /// Marks methods that act as wrappers around kernel launches.
 /// </summary>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-public sealed class IsLauncherAttribute : Attribute;
+public sealed class IsLauncherAttribute(bool isGrouped) : Attribute
+{
+    /// <summary>
+    /// Returns true if the current launcher is a grouped launcher.
+    /// </summary>
+    public bool IsGrouped { get; } = isGrouped;
+}
 
 /// <summary>
 /// Marks methods that will be directly replaced with launcher invocations.
