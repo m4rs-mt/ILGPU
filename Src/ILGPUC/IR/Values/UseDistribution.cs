dd7bfbd486ddc89816cd6c3b589f896a6f5540a6
Marcel Koester <m4rs@m4rs.net>
2025-01-26 14:28:38 +0000

    Refined implementation of UseDistribution to improve use distribution analysis and added support for type-based statistics.

--- Src/ILGPUC/IR/Values/UseDistribution.cs
+++ Src/ILGPUC/IR/Values/UseDistribution.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2018-2021 ILGPU Project
+//                        Copyright (c) 2018-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: UseDistribution.cs
@@ -9,90 +9,90 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
+using ILGPU;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 
-namespace ILGPU.IR.Values
+namespace ILGPUC.IR.Values;
+
+/// <summary>
+/// An analysis class to resolve information about the global use-relation
+/// within a given context.
+/// </summary>
+/// <remarks>
+/// This class is typically used for internal debugging and tracking of memory
+/// allocations.
+/// </remarks>
+sealed class UseDistribution
 {
+    #region Instance
+
     /// <summary>
-    /// An analysis class to resolve information about the global use-relation
-    /// within a given context.
+    /// Constructs a new use distribution.
     /// </summary>
-    /// <remarks>
-    /// This class is typically used for internal debugging and tracking of memory
-    /// allocations.
-    /// </remarks>
-    public sealed class UseDistribution
+    /// <param name="context">The target context.</param>
+    public UseDistribution(IRContext context)
     {
-        #region Instance
+        if (context == null)
+            throw new ArgumentNullException(nameof(context));
 
-        /// <summary>
-        /// Constructs a new use distribution.
-        /// </summary>
-        /// <param name="context">The target context.</param>
-        public UseDistribution(IRContext context)
+        var groupedUses = new Dictionary<int, int>();
+        var usesPerType = new Dictionary<Type, (int, int)>();
+
+        foreach (var method in context.Methods)
         {
-            if (context == null)
-                throw new ArgumentNullException(nameof(context));
-
-            var groupedUses = new Dictionary<int, int>();
-            var usesPerType = new Dictionary<Type, (int, int)>();
-
-            foreach (var method in context.Methods)
+            foreach (Value value in method.Blocks.Values)
             {
-                foreach (Value value in method.Blocks.Values)
-                {
-                    if (!groupedUses.TryGetValue(value.AllNumUses, out int count))
-                        count = 0;
-                    groupedUses[value.AllNumUses] = count + 1;
+                if (!groupedUses.TryGetValue(value.AllNumUses, out int count))
+                    count = 0;
+                groupedUses[value.AllNumUses] = count + 1;
 
-                    var type = value.GetType();
-                    if (!usesPerType.TryGetValue(type, out var entry))
-                        entry = (0, 0);
+                var type = value.GetType();
+                if (!usesPerType.TryGetValue(type, out var entry))
+                    entry = (0, 0);
 
-                    usesPerType[type] =
-                        (IntrinsicMath.Max(value.AllNumUses, entry.Item1),
-                        entry.Item2 + 1);
-                }
+                usesPerType[type] =
+                    (XMath.Max(value.AllNumUses, entry.Item1),
+                    entry.Item2 + 1);
             }
-
-            var groupedUsesList = new List<(int, int)>(groupedUses.Count);
-            foreach (var entry in groupedUses)
-                groupedUsesList.Add((entry.Key, entry.Value));
-            groupedUsesList.Sort((x, y) => y.Item1.CompareTo(x.Item1));
-            Uses = groupedUsesList.ToImmutableArray();
-
-            var groupedUsesPerTypeList = new List<(int, Type, int)>(usesPerType.Count);
-            foreach (var entry in usesPerType)
-            {
-                groupedUsesPerTypeList.Add(
-                    (entry.Value.Item1, entry.Key, entry.Value.Item2));
-            }
-            groupedUsesPerTypeList.Sort((x, y) => y.Item1.CompareTo(x.Item1));
-
-            Uses = groupedUsesList.ToImmutableArray();
-            UsesPerType = groupedUsesPerTypeList.ToImmutableArray();
         }
 
-        #endregion
+        var groupedUsesList = new List<(int, int)>(groupedUses.Count);
+        foreach (var entry in groupedUses)
+            groupedUsesList.Add((entry.Key, entry.Value));
+        groupedUsesList.Sort((x, y) => y.Item1.CompareTo(x.Item1));
+        Uses = groupedUsesList.ToImmutableArray();
 
-        #region Properties
+        var groupedUsesPerTypeList = new List<(int, Type, int)>(usesPerType.Count);
+        foreach (var entry in usesPerType)
+        {
+            groupedUsesPerTypeList.Add(
+                (entry.Value.Item1, entry.Key, entry.Value.Item2));
+        }
+        groupedUsesPerTypeList.Sort((x, y) => y.Item1.CompareTo(x.Item1));
 
-        /// <summary>
-        /// Returns the use distribution of all global nodes.
-        /// </summary>
-        /// <remarks>Tuple layout: (number of uses, number of nodes).</remarks>
-        public ImmutableArray<(int, int)> Uses { get; }
-
-        /// <summary>
-        /// Returns the use distribution of all node types.
-        /// </summary>
-        /// <remarks>
-        /// Tuple layout: (max number of uses, type, number of nodes with this type).
-        /// </remarks>
-        public ImmutableArray<(int, Type, int)> UsesPerType { get; }
-
-        #endregion
+        Uses = groupedUsesList.ToImmutableArray();
+        UsesPerType = groupedUsesPerTypeList.ToImmutableArray();
     }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Returns the use distribution of all global nodes.
+    /// </summary>
+    /// <remarks>Tuple layout: (number of uses, number of nodes).</remarks>
+    public ImmutableArray<(int, int)> Uses { get; }
+
+    /// <summary>
+    /// Returns the use distribution of all node types.
+    /// </summary>
+    /// <remarks>
+    /// Tuple layout: (max number of uses, type, number of nodes with this type).
+    /// </remarks>
+    public ImmutableArray<(int, Type, int)> UsesPerType { get; }
+
+    #endregion
 }
