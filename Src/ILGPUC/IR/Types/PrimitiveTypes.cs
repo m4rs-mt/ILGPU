244a0bac2ca1d9128c5f153d898ae7721c579945
Marcel Koester <m4rs@m4rs.net>
2025-01-27 23:02:57 +0000

    Refined PrimitiveType class to support static properties and improved type size determination logic.

--- Src/ILGPUC/IR/Types/PrimitiveTypes.cs
+++ Src/ILGPUC/IR/Types/PrimitiveTypes.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2018-2024 ILGPU Project
+//                        Copyright (c) 2018-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: PrimitiveTypes.cs
@@ -12,196 +12,166 @@
 using System;
 using System.Collections.Immutable;
 
-namespace ILGPU.IR.Types
+namespace ILGPUC.IR.Types;
+
+/// <summary>
+/// Represents a primitive type.
+/// </summary>
+sealed class PrimitiveType : TypeNode
 {
-    /// <summary>
-    /// Represents a primitive type.
-    /// </summary>
-    public sealed class PrimitiveType : TypeNode
-    {
-        #region Static
-
-        /// <summary>
-        /// Contains default size information about built-in types.
-        /// </summary>
-        private static readonly ImmutableArray<int> BasicTypeInformation =
-            ImmutableArray.Create(
-                -1, // None
-                4,
-                1,
-                2,
-                4,
-                8,
-                2,
-                4,
-                8);
-
-        /// <summary>
-        /// Maps integer-based type size values to <see cref="BasicValueType"/> entries.
-        /// </summary>
-        private static readonly ImmutableArray<BasicValueType> BasicSizeInformation =
-            ImmutableArray.Create(
-                BasicValueType.None,
-                BasicValueType.Int8,
-                BasicValueType.Int16,
-                BasicValueType.None,
-                BasicValueType.Int32,
-                BasicValueType.None,
-                BasicValueType.None,
-                BasicValueType.None,
-                BasicValueType.Int64);
-
-        /// <summary>
-        /// Determines the <see cref="BasicValueType"/> that corresponds to the given
-        /// type size in bytes (if any).
-        /// </summary>
-        /// <param name="size">The size in bytes.</param>
-        /// <returns>The basic value type (if any).</returns>
-        public static BasicValueType GetBasicValueTypeBySize(int size) =>
-            size > BasicSizeInformation.Length
-            ? BasicValueType.None
-            : BasicSizeInformation[size];
-
-        #endregion
-
-        #region Instance
-
-        /// <summary>
-        /// Constructs a new primitive type.
-        /// </summary>
-        /// <param name="typeContext">The parent type context.</param>
-        /// <param name="basicValueType">The basic value type.</param>
-        internal PrimitiveType(IRTypeContext typeContext, BasicValueType basicValueType)
-            : base(typeContext)
-        {
-            BasicValueType = basicValueType;
-
-            Size = Alignment = BasicTypeInformation[(int)basicValueType];
-        }
-
-        #endregion
-
-        #region Properties
-
-        /// <inheritdoc/>
-        public override bool IsPrimitiveType => true;
-
-        /// <inheritdoc/>
-        public override TypeKind TypeKind => TypeKind.Primitive;
-
-        /// <summary>
-        /// Returns the associated basic value type.
-        /// </summary>
-        public override BasicValueType BasicValueType { get; }
-
-        /// <summary>
-        /// Returns true if this type represents a bool type.
-        /// </summary>
-        public bool IsBool => BasicValueType == BasicValueType.Int1;
-
-        /// <summary>
-        /// Returns true if this type represents a 32 bit type.
-        /// </summary>
-        public bool Is32Bit =>
-            BasicValueType == BasicValueType.Int32 ||
-            BasicValueType == BasicValueType.Float32;
-
-        /// <summary>
-        /// Returns true if this type represents a 64 bit type.
-        /// </summary>
-        public bool Is64Bit =>
-            BasicValueType == BasicValueType.Int64 ||
-            BasicValueType == BasicValueType.Float64;
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Returns the corresponding managed basic value type.
-        /// </summary>
-        protected override Type GetManagedType<TTypeProvider>(
-            TTypeProvider typeProvider) =>
-            typeProvider.GetPrimitiveType(this);
-
-        /// <summary cref="TypeNode.Write{T}(T)"/>
-        protected internal override void Write<T>(T writer) =>
-            writer.Write(nameof(BasicValueType), BasicValueType);
-
-        #endregion
-
-        #region Object
-
-        /// <summary cref="Node.ToPrefixString"/>
-        protected override string ToPrefixString() =>
-            BasicValueType.ToString();
-
-        /// <summary cref="TypeNode.GetHashCode"/>
-        public override int GetHashCode() =>
-            base.GetHashCode() ^ 0x2AB11613 ^ (int)BasicValueType;
-
-        /// <summary cref="TypeNode.Equals(object)"/>
-        public override bool Equals(object? obj) =>
-            obj is PrimitiveType primitiveType &&
-            primitiveType.BasicValueType == BasicValueType;
-
-        #endregion
-    }
+    #region Static
 
     /// <summary>
-    /// Represents a string type.
+    /// Contains default size information about built-in types.
     /// </summary>
-    public sealed class StringType : TypeNode
+    private static readonly ImmutableArray<int> BasicTypeInformation =
+        ImmutableArray.Create(
+            -1, // None
+            4,
+            1,
+            2,
+            4,
+            8,
+            2,
+            4,
+            8);
+
+    /// <summary>
+    /// Maps integer-based type size values to <see cref="BasicValueType"/> entries.
+    /// </summary>
+    private static readonly ImmutableArray<BasicValueType> BasicSizeInformation =
+        ImmutableArray.Create(
+            BasicValueType.None,
+            BasicValueType.Int8,
+            BasicValueType.Int16,
+            BasicValueType.None,
+            BasicValueType.Int32,
+            BasicValueType.None,
+            BasicValueType.None,
+            BasicValueType.None,
+            BasicValueType.Int64);
+
+    /// <summary>
+    /// Determines the <see cref="BasicValueType"/> that corresponds to the given
+    /// type size in bytes (if any).
+    /// </summary>
+    /// <param name="size">The size in bytes.</param>
+    /// <returns>The basic value type (if any).</returns>
+    public static BasicValueType GetBasicValueTypeBySize(int size) =>
+        size > BasicSizeInformation.Length
+        ? BasicValueType.None
+        : BasicSizeInformation[size];
+
+    #endregion
+
+    #region Instance
+
+    /// <summary>
+    /// Constructs a new primitive type.
+    /// </summary>
+    /// <param name="typeContext">The parent type context.</param>
+    /// <param name="basicValueType">The basic value type.</param>
+    internal PrimitiveType(IRTypeContext typeContext, BasicValueType basicValueType)
+        : base(typeContext)
     {
-        #region Instance
+        BasicValueType = basicValueType;
 
-        /// <summary>
-        /// Constructs a new string type.
-        /// </summary>
-        /// <param name="typeContext">The parent type context.</param>
-        internal StringType(IRTypeContext typeContext)
-            : base(typeContext)
-        { }
-
-        #endregion
-
-        #region Properties
-
-        /// <inheritdoc/>
-        public override bool IsStringType => true;
-
-        /// <inheritdoc/>
-        public override TypeKind TypeKind => TypeKind.String;
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Returns the corresponding managed basic value type.
-        /// </summary>
-        protected override Type GetManagedType<TTypeProvider>(
-            TTypeProvider typeProvider) =>
-            typeof(string);
-
-        /// <summary cref="TypeNode.Write{T}(T)"/>
-        protected internal override void Write<T>(T writer) { }
-
-        #endregion
-
-        #region Object
-
-        /// <summary cref="Node.ToPrefixString"/>
-        protected override string ToPrefixString() => "string";
-
-        /// <summary cref="TypeNode.GetHashCode"/>
-        public override int GetHashCode() =>
-            base.GetHashCode() ^ 0x3D12C251;
-
-        /// <summary cref="TypeNode.Equals(object)"/>
-        public override bool Equals(object? obj) =>
-            obj is StringType && base.Equals(obj);
-
-        #endregion
+        Size = Alignment = BasicTypeInformation[(int)basicValueType];
     }
+
+    #endregion
+
+    #region Properties
+
+    /// <inheritdoc/>
+    public override bool IsPrimitiveType => true;
+
+    /// <inheritdoc/>
+    public override TypeKind TypeKind => TypeKind.Primitive;
+
+    /// <summary>
+    /// Returns the associated basic value type.
+    /// </summary>
+    public override BasicValueType BasicValueType { get; }
+
+    /// <summary>
+    /// Returns true if this type represents a bool type.
+    /// </summary>
+    public bool IsBool => BasicValueType == BasicValueType.Int1;
+
+    /// <summary>
+    /// Returns true if this type represents a 32 bit type.
+    /// </summary>
+    public bool Is32Bit =>
+        BasicValueType == BasicValueType.Int32 ||
+        BasicValueType == BasicValueType.Float32;
+
+    /// <summary>
+    /// Returns true if this type represents a 64 bit type.
+    /// </summary>
+    public bool Is64Bit =>
+        BasicValueType == BasicValueType.Int64 ||
+        BasicValueType == BasicValueType.Float64;
+
+    #endregion
+
+    #region Object
+
+    /// <summary cref="Node.ToPrefixString"/>
+    protected override string ToPrefixString() =>
+        BasicValueType.ToString();
+
+    /// <summary cref="TypeNode.GetHashCode"/>
+    public override int GetHashCode() =>
+        base.GetHashCode() ^ 0x2AB11613 ^ (int)BasicValueType;
+
+    /// <summary cref="TypeNode.Equals(object)"/>
+    public override bool Equals(object? obj) =>
+        obj is PrimitiveType primitiveType &&
+        primitiveType.BasicValueType == BasicValueType;
+
+    #endregion
+}
+
+/// <summary>
+/// Represents a string type.
+/// </summary>
+sealed class StringType : TypeNode
+{
+    #region Instance
+
+    /// <summary>
+    /// Constructs a new string type.
+    /// </summary>
+    /// <param name="typeContext">The parent type context.</param>
+    internal StringType(IRTypeContext typeContext)
+        : base(typeContext)
+    { }
+
+    #endregion
+
+    #region Properties
+
+    /// <inheritdoc/>
+    public override bool IsStringType => true;
+
+    /// <inheritdoc/>
+    public override TypeKind TypeKind => TypeKind.String;
+
+    #endregion
+
+    #region Object
+
+    /// <summary cref="Node.ToPrefixString"/>
+    protected override string ToPrefixString() => "string";
+
+    /// <summary cref="TypeNode.GetHashCode"/>
+    public override int GetHashCode() =>
+        base.GetHashCode() ^ 0x3D12C251;
+
+    /// <summary cref="TypeNode.Equals(object)"/>
+    public override bool Equals(object? obj) =>
+        obj is StringType && base.Equals(obj);
+
+    #endregion
 }
