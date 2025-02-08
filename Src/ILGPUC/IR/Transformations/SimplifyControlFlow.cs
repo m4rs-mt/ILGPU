df2cfbee93e67849572a8997e926d2ad75316947
Marcel Koester <m4rs@m4rs.net>
2025-02-08 15:42:49 +0000

    Refined SimplifyControlFlow transformation to efficiently merge sequential jump chains.

--- Src/ILGPUC/IR/Transformations/SimplifyControlFlow.cs
+++ Src/ILGPUC/IR/Transformations/SimplifyControlFlow.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2018-2021 ILGPU Project
+//                        Copyright (c) 2018-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: SimplifyControlFlow.cs
@@ -9,100 +9,97 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPU.IR.Analyses.ControlFlowDirection;
-using ILGPU.IR.Analyses.TraversalOrders;
-using System.Runtime.CompilerServices;
+using ILGPUC.IR.Analyses.ControlFlowDirection;
+using ILGPUC.IR.Analyses.TraversalOrders;
 
-namespace ILGPU.IR.Transformations
+namespace ILGPUC.IR.Transformations;
+
+/// <summary>
+/// Merges multiple sequential branches (a call/branch chain) into a single block.
+/// </summary>
+sealed class SimplifyControlFlow : UnorderedTransformation
 {
+    #region Static
+
     /// <summary>
-    /// Merges multiple sequential branches (a call/branch chain) into a single block.
+    /// Tries to merge a sequence of jumps.
     /// </summary>
-    public sealed class SimplifyControlFlow : UnorderedTransformation
+    /// <param name="builder">The current method builder.</param>
+    /// <param name="root">The block where to start merging.</param>
+    /// <param name="visited">The collection of visited nodes.</param>
+    /// <returns>True, if something could be merged.</returns>
+    private static bool MergeChain(
+        Method.Builder builder,
+        BasicBlock root,
+        ref BasicBlockSet visited)
     {
-        #region Static
+        if (root.Successors.Length != 1 || visited.Contains(root))
+            return false;
 
-        /// <summary>
-        /// Tries to merge a sequence of jumps.
-        /// </summary>
-        /// <param name="builder">The current method builder.</param>
-        /// <param name="root">The block where to start merging.</param>
-        /// <param name="visited">The collection of visited nodes.</param>
-        /// <returns>True, if something could be merged.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static bool MergeChain(
-            Method.Builder builder,
-            BasicBlock root,
-            ref BasicBlockSet visited)
+        // Mark node as seen
+        visited.Add(root);
+
+        // Init initial builder and successors list
+        var rootBlockBuilder = builder[root];
+        var successors = root.Successors;
+        bool result = false;
+
+        do
         {
-            if (root.Successors.Length != 1 || visited.Contains(root))
-                return false;
+            var nextBlock = successors[0];
 
-            // Mark node as seen
-            visited.Add(root);
+            // We cannot merge jump targets in div. control-flow or in the case
+            // of a block that we have already seen
+            if (nextBlock.Predecessors.Length > 1 || visited.Contains(nextBlock))
+                break;
 
-            // Init initial builder and successors list
-            var rootBlockBuilder = builder[root];
-            var successors = root.Successors;
-            bool result = false;
+            // Mark next block as seen
+            visited.Add(nextBlock);
 
-            do
-            {
-                var nextBlock = successors[0];
+            // Merge block
+            successors = nextBlock.Successors;
+            rootBlockBuilder.MergeBlock(nextBlock);
 
-                // We cannot merge jump targets in div. control-flow or in the case
-                // of a block that we have already seen
-                if (nextBlock.Predecessors.Length > 1 || visited.Contains(nextBlock))
-                    break;
-
-                // Mark next block as seen
-                visited.Add(nextBlock);
-
-                // Merge block
-                successors = nextBlock.Successors;
-                rootBlockBuilder.MergeBlock(nextBlock);
-
-                // Return true as we have changed the IR
-                result = true;
-            }
-            while (successors.Length == 1);
-
-            // Return the success status
-            return result;
+            // Return true as we have changed the IR
+            result = true;
         }
+        while (successors.Length == 1);
 
-        #endregion
-
-        #region Instance
-
-        /// <summary>
-        /// Constructs a new transformation to merge sequential jump chains.
-        /// </summary>
-        public SimplifyControlFlow() { }
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Applies the control-flow simplification transformation.
-        /// </summary>
-        protected override bool PerformTransformation(Method.Builder builder)
-        {
-            // We change the control-flow structure during the transformation but
-            // need to get information about previous predecessors and successors
-            builder.AcceptControlFlowUpdates(accept: true);
-
-            BasicBlockCollection<ReversePostOrder, Forwards> blocks =
-                builder.SourceBlocks;
-
-            var visited = blocks.CreateSet();
-            bool result = false;
-            foreach (var block in blocks)
-                result |= MergeChain(builder, block, ref visited);
-            return result;
-        }
-
-        #endregion
+        // Return the success status
+        return result;
     }
+
+    #endregion
+
+    #region Instance
+
+    /// <summary>
+    /// Constructs a new transformation to merge sequential jump chains.
+    /// </summary>
+    public SimplifyControlFlow() { }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Applies the control-flow simplification transformation.
+    /// </summary>
+    protected override bool PerformTransformation(Method.Builder builder)
+    {
+        // We change the control-flow structure during the transformation but
+        // need to get information about previous predecessors and successors
+        builder.AcceptControlFlowUpdates(accept: true);
+
+        BasicBlockCollection<ReversePostOrder, Forwards> blocks =
+            builder.SourceBlocks;
+
+        var visited = blocks.CreateSet();
+        bool result = false;
+        foreach (var block in blocks)
+            result |= MergeChain(builder, block, ref visited);
+        return result;
+    }
+
+    #endregion
 }
