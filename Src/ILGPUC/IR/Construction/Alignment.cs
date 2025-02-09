116a001a70dada651ee07e9b18574565c3056697
Marcel Koester <m4rs@m4rs.net>
2025-02-09 12:43:41 +0000

    Refined implementation of Alignment methods in IRBuilder to support view and pointer alignment operations.

--- Src/ILGPUC/IR/Construction/Alignment.cs
+++ Src/ILGPUC/IR/Construction/Alignment.cs
@@ -1,7 +1,7 @@
 
 // ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2018-2021 ILGPU Project
+//                        Copyright (c) 2018-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: Alignment.cs
@@ -10,56 +10,54 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPU.IR.Types;
-using ILGPU.IR.Values;
-using ILGPU.Util;
+using ILGPUC.IR.Values;
+using ILGPUC.Util;
 
-namespace ILGPU.IR.Construction
+namespace ILGPUC.IR.Construction;
+
+partial class IRBuilder
 {
-    partial class IRBuilder
+    /// <summary>
+    /// Creates a node that aligns the given view or pointer to a given number of
+    /// bytes.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="viewOrPointer">The source view or pointer.</param>
+    /// <param name="alignmentInBytes">The alignment in bytes.</param>
+    /// <returns>The created node.</returns>
+    public ValueReference CreateAlignTo(
+        Location location,
+        Value viewOrPointer,
+        Value alignmentInBytes)
     {
-        /// <summary>
-        /// Creates a node that aligns the given view or pointer to a given number of
-        /// bytes.
-        /// </summary>
-        /// <param name="location">The current location.</param>
-        /// <param name="viewOrPointer">The source view or pointer.</param>
-        /// <param name="alignmentInBytes">The alignment in bytes.</param>
-        /// <returns>The created node.</returns>
-        public ValueReference CreateAlignTo(
-            Location location,
-            Value viewOrPointer,
-            Value alignmentInBytes)
-        {
-            location.Assert(viewOrPointer.Type.IsViewOrPointerType);
-            location.Assert(alignmentInBytes.Type.BasicValueType.IsInt());
+        location.Assert(viewOrPointer.Type.IsViewOrPointerType);
+        location.Assert(alignmentInBytes.Type.BasicValueType.IsInt());
 
-            return Append(new AlignTo(
-                GetInitializer(location),
-                viewOrPointer,
-                alignmentInBytes));
-        }
+        return Append(new AlignTo(
+            GetInitializer(location),
+            viewOrPointer,
+            alignmentInBytes));
+    }
 
-        /// <summary>
-        /// Creates a node that treats the given view or pointer as an aligned
-        /// view/pointer that is aligned to a given number of bytes.
-        /// </summary>
-        /// <param name="location">The current location.</param>
-        /// <param name="viewOrPointer">The source view or pointer.</param>
-        /// <param name="alignmentInBytes">The alignment in bytes.</param>
-        /// <returns>The created node.</returns>
-        public ValueReference CreateAsAligned(
-            Location location,
-            Value viewOrPointer,
-            Value alignmentInBytes)
-        {
-            location.Assert(viewOrPointer.Type.IsViewOrPointerType);
-            location.Assert(alignmentInBytes.Type.BasicValueType.IsInt());
+    /// <summary>
+    /// Creates a node that treats the given view or pointer as an aligned
+    /// view/pointer that is aligned to a given number of bytes.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="viewOrPointer">The source view or pointer.</param>
+    /// <param name="alignmentInBytes">The alignment in bytes.</param>
+    /// <returns>The created node.</returns>
+    public ValueReference CreateAsAligned(
+        Location location,
+        Value viewOrPointer,
+        Value alignmentInBytes)
+    {
+        location.Assert(viewOrPointer.Type.IsViewOrPointerType);
+        location.Assert(alignmentInBytes.Type.BasicValueType.IsInt());
 
-            return Append(new AsAligned(
-                GetInitializer(location),
-                viewOrPointer,
-                alignmentInBytes));
-        }
+        return Append(new AsAligned(
+            GetInitializer(location),
+            viewOrPointer,
+            alignmentInBytes));
     }
 }
