ae6ccc55832bb26e986776ed6ee0ecb19d51d65f
Marcel Koester <m4rs@m4rs.net>
2025-02-12 19:47:28 +0000

    Refined implementation of Predicate to handle constant and boolean expressions more efficiently.

--- Src/ILGPUC/IR/Construction/Predicate.cs
+++ Src/ILGPUC/IR/Construction/Predicate.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2020-2023 ILGPU Project
+//                        Copyright (c) 2020-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: Predicate.cs
@@ -9,97 +9,96 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPU.IR.Types;
-using ILGPU.IR.Values;
 using ILGPU.Util;
+using ILGPUC.IR.Types;
+using ILGPUC.IR.Values;
 
-namespace ILGPU.IR.Construction
+namespace ILGPUC.IR.Construction;
+
+partial class IRBuilder
 {
-    partial class IRBuilder
+    /// <summary>
+    /// Creates a conditional if predicate.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="condition">The condition.</param>
+    /// <param name="trueValue">The true value.</param>
+    /// <param name="falseValue">The false value.</param>
+    /// <returns>A node that represents the predicate operation.</returns>
+    public ValueReference CreatePredicate(
+        Location location,
+        Value condition,
+        Value trueValue,
+        Value falseValue)
     {
-        /// <summary>
-        /// Creates a conditional if predicate.
-        /// </summary>
-        /// <param name="location">The current location.</param>
-        /// <param name="condition">The condition.</param>
-        /// <param name="trueValue">The true value.</param>
-        /// <param name="falseValue">The false value.</param>
-        /// <returns>A node that represents the predicate operation.</returns>
-        public ValueReference CreatePredicate(
-            Location location,
-            Value condition,
-            Value trueValue,
-            Value falseValue)
+        location.Assert(condition.Type.BasicValueType == BasicValueType.Int1);
+
+        if (trueValue.Type != falseValue.Type)
         {
-            location.Assert(condition.Type.BasicValueType == BasicValueType.Int1);
+            falseValue = CreateConvert(
+                location,
+                falseValue,
+                trueValue.Type.AsNotNullCast<PrimitiveType>());
+        }
 
-            if (trueValue.Type != falseValue.Type)
+        // Match simple constant predicates
+        if (condition is PrimitiveValue constant)
+            return constant.Int1Value ? trueValue : falseValue;
+
+        // Match bool predicates that can be represented via simple expressions
+        if (trueValue.BasicValueType == BasicValueType.Int1)
+        {
+            // Check for two cases: condition ? True : falseValue
+            //                       --> condition | falseValue
+            //                      condition ? False : falseValue
+            //                       --> !condition & falseValue
+            if (trueValue is PrimitiveValue truePrimitive)
             {
-                falseValue = CreateConvert(
-                    location,
-                    falseValue,
-                    trueValue.Type.AsNotNullCast<PrimitiveType>());
-            }
-
-            // Match simple constant predicates
-            if (condition is PrimitiveValue constant)
-                return constant.Int1Value ? trueValue : falseValue;
-
-            // Match bool predicates that can be represented via simple expressions
-            if (trueValue.BasicValueType == BasicValueType.Int1)
-            {
-                // Check for two cases: condition ? True : falseValue
-                //                       --> condition | falseValue
-                //                      condition ? False : falseValue
-                //                       --> !condition & falseValue
-                if (trueValue is PrimitiveValue truePrimitive)
+                var kind = BinaryArithmeticKind.Or;
+                // Check for: condition ? False ...
+                if (!truePrimitive.Int1Value)
                 {
-                    var kind = BinaryArithmeticKind.Or;
-                    // Check for: condition ? False ...
-                    if (!truePrimitive.Int1Value)
-                    {
-                        kind = BinaryArithmeticKind.And;
-                        condition = CreateArithmetic(
-                            location,
-                            condition,
-                            UnaryArithmeticKind.Not);
-                    }
-                    return CreateArithmetic(
+                    kind = BinaryArithmeticKind.And;
+                    condition = CreateArithmetic(
                         location,
                         condition,
-                        falseValue,
-                        kind,
-                        ArithmeticFlags.Unsigned);
+                        UnaryArithmeticKind.Not);
                 }
-                // Move constants to the left
-                else if (falseValue is PrimitiveValue)
-                {
-                    return CreatePredicate(
+                return CreateArithmetic(
+                    location,
+                    condition,
+                    falseValue,
+                    kind,
+                    ArithmeticFlags.Unsigned);
+            }
+            // Move constants to the left
+            else if (falseValue is PrimitiveValue)
+            {
+                return CreatePredicate(
+                    location,
+                    CreateArithmetic(
                         location,
-                        CreateArithmetic(
-                            location,
-                            condition,
-                            UnaryArithmeticKind.Not),
-                        falseValue,
-                        trueValue);
-                }
-
-                // If we arrive here we cannot merge any constants
+                        condition,
+                        UnaryArithmeticKind.Not),
+                    falseValue,
+                    trueValue);
             }
 
-            // Match negated predicates
-            return condition is UnaryArithmeticValue unary &&
-                unary.Kind == UnaryArithmeticKind.Not
-                ? CreatePredicate(
-                    location,
-                    unary.Value,
-                    falseValue,
-                    trueValue)
-                : Append(new Predicate(
-                    GetInitializer(location),
-                    condition,
-                    trueValue,
-                    falseValue));
+            // If we arrive here we cannot merge any constants
         }
+
+        // Match negated predicates
+        return condition is UnaryArithmeticValue unary &&
+            unary.Kind == UnaryArithmeticKind.Not
+            ? CreatePredicate(
+                location,
+                unary.Value,
+                falseValue,
+                trueValue)
+            : Append(new Predicate(
+                GetInitializer(location),
+                condition,
+                trueValue,
+                falseValue));
     }
 }
