240da8643e295f87cd54928bbdfa318dc83198b0
Marcel Koester <m4rs@m4rs.net>
2025-02-12 01:31:55 +0000

    Refined implementation of Methods.cs to enhance IRBuilder functionality and improve method call creation.

--- Src/ILGPUC/IR/Construction/Methods.cs
+++ Src/ILGPUC/IR/Construction/Methods.cs
@@ -9,71 +9,68 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPU.IR.Types;
-using ILGPU.IR.Values;
-using System.Runtime.CompilerServices;
-using ValueList = ILGPU.Util.InlineList<ILGPU.IR.Values.ValueReference>;
+using ILGPUC.IR.Types;
+using ILGPUC.IR.Values;
+using ValueList = ILGPU.Util.InlineList<ILGPUC.IR.Values.ValueReference>;
 
-namespace ILGPU.IR.Construction
+namespace ILGPUC.IR.Construction;
+
+partial class IRBuilder
 {
-    partial class IRBuilder
+    /// <summary>
+    /// Creates a new call node builder.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="target">The jump target.</param>
+    /// <returns>A call builder.</returns>
+    public MethodCall.Builder CreateCall(Location location, Method target) =>
+        new(this, location, target);
+
+    /// <summary>
+    /// Creates a new method call.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="target">The method target.</param>
+    /// <param name="values">The argument values.</param>
+    /// <returns>The created method call value.</returns>
+    internal MethodCall CreateCall(
+        Location location,
+        Method target,
+        ref ValueList values)
     {
-        /// <summary>
-        /// Creates a new call node builder.
-        /// </summary>
-        /// <param name="location">The current location.</param>
-        /// <param name="target">The jump target.</param>
-        /// <returns>A call builder.</returns>
-        public MethodCall.Builder CreateCall(Location location, Method target) =>
-            new MethodCall.Builder(this, location, target);
+        location.AssertNotNull(target);
+        return Append(new MethodCall(
+            GetInitializer(location),
+            target,
+            ref values));
+    }
 
-        /// <summary>
-        /// Creates a new method call.
-        /// </summary>
-        /// <param name="location">The current location.</param>
-        /// <param name="target">The method target.</param>
-        /// <param name="values">The argument values.</param>
-        /// <returns>The created method call value.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal MethodCall CreateCall(
-            Location location,
-            Method target,
-            ref ValueList values)
-        {
-            location.AssertNotNull(target);
-            return Append(new MethodCall(
-                GetInitializer(location),
-                target,
-                ref values));
-        }
+    /// <summary>
+    /// Creates a new phi node builder.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="type">The given node type.</param>
+    /// <returns>The created phi builder.</returns>
+    public PhiValue.Builder CreatePhi(Location location, TypeNode type) =>
+        CreatePhi(location, type, 0);
 
-        /// <summary>
-        /// Creates a new phi node builder.
-        /// </summary>
-        /// <param name="location">The current location.</param>
-        /// <param name="type">The given node type.</param>
-        /// <returns>The created phi builder.</returns>
-        public PhiValue.Builder CreatePhi(Location location, TypeNode type) =>
-            CreatePhi(location, type, 0);
+    /// <summary>
+    /// Creates a new phi node builder.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="type">The given node type.</param>
+    /// <param name="capacity">The initial capacity.</param>
+    /// <returns>The created phi builder.</returns>
+    public PhiValue.Builder CreatePhi(
+        Location location,
+        TypeNode type,
+        int capacity)
+    {
+        location.AssertNotNull(type);
 
-        /// <summary>
-        /// Creates a new phi node builder.
-        /// </summary>
-        /// <param name="location">The current location.</param>
-        /// <param name="type">The given node type.</param>
-        /// <param name="capacity">The initial capacity.</param>
-        /// <returns>The created phi builder.</returns>
-        public PhiValue.Builder CreatePhi(
-            Location location,
-            TypeNode type,
-            int capacity)
-        {
-            location.AssertNotNull(type);
-
-            var phiNode = CreatePhiValue(new PhiValue(
-                GetInitializer(location),
-                type));
-            return new PhiValue.Builder(phiNode, capacity);
-        }
+        var phiNode = CreatePhiValue(new PhiValue(
+            GetInitializer(location),
+            type));
+        return new PhiValue.Builder(phiNode, capacity);
     }
 }
