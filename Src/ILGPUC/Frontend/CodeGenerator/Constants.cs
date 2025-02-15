f4bac5aeae972ee2cf770cd151276a6a06de84f3
Marcel Koester <m4rs@m4rs.net>
2025-02-15 15:42:50 +0000

    Refined frontend code generator to support loading various primitive types and strings.

--- Src/ILGPUC/Frontend/CodeGenerator/Constants.cs
+++ Src/ILGPUC/Frontend/CodeGenerator/Constants.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2018-2021 ILGPU Project
+//                        Copyright (c) 2018-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: Constants.cs
@@ -9,43 +9,42 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-namespace ILGPU.Frontend
+namespace ILGPUC.Frontend;
+
+partial class CodeGenerator
 {
-    partial class CodeGenerator
-    {
-        /// <summary>
-        /// Loads an int.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        private void Load(int value) =>
-            Block.Push(Builder.CreatePrimitiveValue(Location, value));
+    /// <summary>
+    /// Loads an int.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    private void Load(int value) =>
+        Block.Push(Builder.CreatePrimitiveValue(Location, value));
 
-        /// <summary>
-        /// Loads a long.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        private void Load(long value) =>
-            Block.Push(Builder.CreatePrimitiveValue(Location, value));
+    /// <summary>
+    /// Loads a long.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    private void Load(long value) =>
+        Block.Push(Builder.CreatePrimitiveValue(Location, value));
 
-        /// <summary>
-        /// Loads a float.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        private void Load(float value) =>
-            Block.Push(Builder.CreatePrimitiveValue(Location, value));
+    /// <summary>
+    /// Loads a float.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    private void Load(float value) =>
+        Block.Push(Builder.CreatePrimitiveValue(Location, value));
 
-        /// <summary>
-        /// Loads a double.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        private void Load(double value) =>
-            Block.Push(Builder.CreatePrimitiveValue(Location, value));
+    /// <summary>
+    /// Loads a double.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    private void Load(double value) =>
+        Block.Push(Builder.CreatePrimitiveValue(Location, value));
 
-        /// <summary>
-        /// Loads a string.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        private void LoadString(string value) =>
-            Block.Push(Builder.CreatePrimitiveValue(Location, value));
-    }
+    /// <summary>
+    /// Loads a string.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    private void LoadString(string value) =>
+        Block.Push(Builder.CreatePrimitiveValue(Location, value));
 }
