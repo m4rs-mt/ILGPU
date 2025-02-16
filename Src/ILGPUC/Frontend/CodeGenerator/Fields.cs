7850fa7eed99ef409ff3aa12470d2f46207beb73
Marcel Koester <m4rs@m4rs.net>
2025-02-16 12:19:25 +0000

    Refined implementation of frontend code generator Fields to support static properties and improved queries.

--- Src/ILGPUC/Frontend/CodeGenerator/Fields.cs
+++ Src/ILGPUC/Frontend/CodeGenerator/Fields.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2018-2023 ILGPU Project
+//                        Copyright (c) 2018-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: Fields.cs
@@ -9,169 +9,169 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPU.IR;
-using ILGPU.IR.Types;
-using ILGPU.IR.Values;
 using ILGPU.Util;
+using ILGPUC.IR;
+using ILGPUC.IR.Types;
+using ILGPUC.IR.Values;
+using ILGPUC.Util;
 using System.Reflection;
 
-namespace ILGPU.Frontend
+namespace ILGPUC.Frontend;
+
+partial class CodeGenerator
 {
-    partial class CodeGenerator
+    /// <summary>
+    /// Helper function to compute field reference for the specified field of a type.
+    /// </summary>
+    /// <param name="type">The type node.</param>
+    /// <param name="field">The field.</param>
+    /// <returns>The target field span.</returns>
+    private FieldSpan ComputeFieldSpan(TypeNode type, FieldInfo field)
     {
-        /// <summary>
-        /// Helper function to compute field refence for the specified field of a type.
-        /// </summary>
-        /// <param name="type">The type node.</param>
-        /// <param name="field">The field.</param>
-        /// <returns>The target field span.</returns>
-        private FieldSpan ComputeFieldSpan(TypeNode type, FieldInfo field)
-        {
-            var typeInfo = TypeContext.GetTypeInfo(field.FieldType);
-            var parentInfo = TypeContext.GetTypeInfo(field.DeclaringType.AsNotNull());
-            var fieldIndex = parentInfo.GetAbsoluteIndex(field);
+        var typeInfo = TypeContext.GetTypeInfo(field.FieldType);
+        var parentInfo = TypeContext.GetTypeInfo(field.DeclaringType.AsNotNull());
+        var fieldIndex = parentInfo.GetAbsoluteIndex(field);
 
-            if (type.IsStructureType)
-            {
-                var structureType = type.As<StructureType>(Location);
-                fieldIndex = structureType.RemapFieldIndex(fieldIndex);
-            }
-            return new FieldSpan(fieldIndex, typeInfo.NumFlattendedFields);
+        if (type.IsStructureType)
+        {
+            var structureType = type.As<StructureType>(Location);
+            fieldIndex = structureType.RemapFieldIndex(fieldIndex);
         }
+        return new FieldSpan(fieldIndex, typeInfo.NumFlattenedFields);
+    }
 
-        /// <summary>
-        /// Loads the value of a field specified by the given metadata token.
-        /// </summary>
-        /// <param name="field">The field.</param>
-        private void MakeLoadField(FieldInfo field)
+    /// <summary>
+    /// Loads the value of a field specified by the given metadata token.
+    /// </summary>
+    /// <param name="field">The field.</param>
+    private void MakeLoadField(FieldInfo field)
+    {
+        if (field == null)
+            throw Location.GetInvalidOperationException();
+
+        var fieldValue = Block.Pop();
+        if (fieldValue.Type.IsPointerType)
         {
-            if (field == null)
-                throw Location.GetInvalidOperationException();
+            // Load field from address
+            Block.Push(fieldValue);
+            MakeLoadFieldAddress(field);
+            var fieldAddress = Block.Pop();
+            var fieldType = Builder.CreateType(field.FieldType);
+            Block.Push(CreateLoad(
+                fieldAddress,
+                fieldType,
+                field.FieldType.ToTargetUnsignedFlags()));
+        }
+        else
+        {
+            // Load field from value
+            var fieldSpan = ComputeFieldSpan(fieldValue.Type, field);
 
-            var fieldValue = Block.Pop();
-            if (fieldValue.Type.IsPointerType)
+            // Check whether we have to get multiple elements
+            var getField = Builder.CreateGetField(
+                Location,
+                fieldValue,
+                fieldSpan);
+            if (fieldSpan.Span == 1)
             {
-                // Load field from address
-                Block.Push(fieldValue);
-                MakeLoadFieldAddress(field);
-                var fieldAddress = Block.Pop();
-                var fieldType = Builder.CreateType(field.FieldType);
-                Block.Push(CreateLoad(
-                    fieldAddress,
-                    fieldType,
+                Block.Push(LoadOntoEvaluationStack(
+                    getField,
                     field.FieldType.ToTargetUnsignedFlags()));
             }
             else
             {
-                // Load field from value
-                var fieldSpan = ComputeFieldSpan(fieldValue.Type, field);
-
-                // Check whether we have to get multiple elements
-                var getField = Builder.CreateGetField(
-                    Location,
-                    fieldValue,
-                    fieldSpan);
-                if (fieldSpan.Span == 1)
-                {
-                    Block.Push(LoadOntoEvaluationStack(
-                        getField,
-                        field.FieldType.ToTargetUnsignedFlags()));
-                }
-                else
-                {
-                    Block.Push(getField);
-                }
+                Block.Push(getField);
             }
         }
+    }
 
-        /// <summary>
-        /// Loads the address of a field specified by the given metadata token.
-        /// </summary>
-        /// <param name="field">The field.</param>
-        private void MakeLoadFieldAddress(FieldInfo field)
-        {
-            if (field == null)
-                throw Location.GetInvalidOperationException();
-            var targetPointerType = Builder.CreatePointerType(
-                Builder.CreateType(field.DeclaringType.AsNotNull()),
-                MemoryAddressSpace.Generic);
-            var address = Block.Pop(
-                targetPointerType,
-                ConvertFlags.None);
+    /// <summary>
+    /// Loads the address of a field specified by the given metadata token.
+    /// </summary>
+    /// <param name="field">The field.</param>
+    private void MakeLoadFieldAddress(FieldInfo field)
+    {
+        if (field == null)
+            throw Location.GetInvalidOperationException();
+        var targetPointerType = Builder.CreatePointerType(
+            Builder.CreateType(field.DeclaringType.AsNotNull()),
+            MemoryAddressSpace.Generic);
+        var address = Block.Pop(
+            targetPointerType,
+            ConvertFlags.None);
 
-            var fieldSpan = ComputeFieldSpan(targetPointerType.ElementType, field);
-            var fieldAddress = Builder.CreateLoadFieldAddress(
-                Location,
-                address,
-                fieldSpan);
-            Block.Push(fieldAddress);
-        }
+        var fieldSpan = ComputeFieldSpan(targetPointerType.ElementType, field);
+        var fieldAddress = Builder.CreateLoadFieldAddress(
+            Location,
+            address,
+            fieldSpan);
+        Block.Push(fieldAddress);
+    }
 
-        /// <summary>
-        /// Loads a static field value and returns the created IR node.
-        /// </summary>
-        /// <param name="field">The field.</param>
-        /// <returns>The loaded field value.</returns>
-        private ValueReference CreateLoadStaticFieldValue(FieldInfo field)
-        {
-            if (field == null)
-                throw Location.GetInvalidOperationException();
-            VerifyStaticFieldLoad(field);
+    /// <summary>
+    /// Loads a static field value and returns the created IR node.
+    /// </summary>
+    /// <param name="field">The field.</param>
+    /// <returns>The loaded field value.</returns>
+    private ValueReference CreateLoadStaticFieldValue(FieldInfo field)
+    {
+        if (field == null)
+            throw Location.GetInvalidOperationException();
+        VerifyStaticFieldLoad(field);
 
-            var fieldValue = field.GetValue(null);
-            return fieldValue == null ?
-                Builder.CreateObjectValue(Location, field.FieldType) :
-                Builder.CreateObjectValue(Location, fieldValue);
-        }
+        var fieldValue = field.GetValue(null);
+        return fieldValue == null ?
+            Builder.CreateObjectValue(Location, field.FieldType) :
+            Builder.CreateObjectValue(Location, fieldValue);
+    }
 
-        /// <summary>
-        /// Loads a static field value.
-        /// </summary>
-        /// <param name="field">The field.</param>
-        private void MakeLoadStaticField(FieldInfo field) =>
-            Block.Push(CreateLoadStaticFieldValue(field));
+    /// <summary>
+    /// Loads a static field value.
+    /// </summary>
+    /// <param name="field">The field.</param>
+    private void MakeLoadStaticField(FieldInfo field) =>
+        Block.Push(CreateLoadStaticFieldValue(field));
 
-        /// <summary>
-        /// Loads the address of a static field specified by the given metadata token.
-        /// </summary>
-        /// <param name="field">The field.</param>
-        private void MakeLoadStaticFieldAddress(FieldInfo field)
-        {
-            var fieldValue = CreateLoadStaticFieldValue(field);
-            var tempAlloca = CreateTempAlloca(fieldValue.Type);
-            Builder.CreateStore(Location, tempAlloca, fieldValue);
-            Block.Push(tempAlloca);
-        }
+    /// <summary>
+    /// Loads the address of a static field specified by the given metadata token.
+    /// </summary>
+    /// <param name="field">The field.</param>
+    private void MakeLoadStaticFieldAddress(FieldInfo field)
+    {
+        var fieldValue = CreateLoadStaticFieldValue(field);
+        var tempAlloca = CreateTempAlloca(fieldValue.Type);
+        Builder.CreateStore(Location, tempAlloca, fieldValue);
+        Block.Push(tempAlloca);
+    }
 
-        /// <summary>
-        /// Stores a value to a field.
-        /// </summary>
-        /// <param name="field">The field.</param>
-        private void MakeStoreField(FieldInfo field)
-        {
-            var fieldType = Builder.CreateType(field.FieldType);
-            var value = Block.Pop(
-                fieldType,
-                field.FieldType.ToTargetUnsignedFlags());
-            MakeLoadFieldAddress(field);
-            var address = Block.Pop();
-            CreateStore(address, value);
-        }
+    /// <summary>
+    /// Stores a value to a field.
+    /// </summary>
+    /// <param name="field">The field.</param>
+    private void MakeStoreField(FieldInfo field)
+    {
+        var fieldType = Builder.CreateType(field.FieldType);
+        var value = Block.Pop(
+            fieldType,
+            field.FieldType.ToTargetUnsignedFlags());
+        MakeLoadFieldAddress(field);
+        var address = Block.Pop();
+        CreateStore(address, value);
+    }
 
-        /// <summary>
-        /// Stores a value to a static field.
-        /// </summary>
-        /// <param name="field">The field.</param>
-        private void MakeStoreStaticField(FieldInfo field)
-        {
-            VerifyStaticFieldStore(field);
+    /// <summary>
+    /// Stores a value to a static field.
+    /// </summary>
+    /// <param name="field">The field.</param>
+    private void MakeStoreStaticField(FieldInfo field)
+    {
+        VerifyStaticFieldStore(field);
 
-            // Consume the current value from the stack but do not emit a global store,
-            // since we don't have any valid target address.
-            // TODO: Stores to static fields could be automatically propagated to the
-            // .Net runtime after kernel invocation. However, this remains as a future
-            // feature.
-            Block.Pop();
-        }
+        // Consume the current value from the stack but do not emit a global store,
+        // since we don't have any valid target address.
+        // TODO: Stores to static fields could be automatically propagated to the
+        // .Net runtime after kernel invocation. However, this remains as a future
+        // feature.
+        Block.Pop();
     }
 }
