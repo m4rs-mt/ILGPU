5bcfcbfbca58ae38e2ff1dee841f7b165f2ef2dd
Marcel Koester <m4rs@m4rs.net>
2025-02-16 14:49:08 +0000

    Refined AddressSpace.cs to introduce MemoryAddressSpace enum and enhance address space management.

--- Src/ILGPUC/IR/AddressSpace.cs
+++ Src/ILGPUC/IR/AddressSpace.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2018-2023 ILGPU Project
+//                        Copyright (c) 2018-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: AddressSpace.cs
@@ -12,128 +12,125 @@
 using System;
 using System.Collections.Immutable;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
-namespace ILGPU.IR
+namespace ILGPUC.IR;
+
+/// <summary>
+/// Represents an address space.
+/// </summary>
+enum MemoryAddressSpace : int
 {
     /// <summary>
-    /// Represents an address space.
+    /// The generic address space (any space).
     /// </summary>
-    public enum MemoryAddressSpace : int
+    Generic = 0,
+
+    /// <summary>
+    /// Represents the global address space.
+    /// </summary>
+    Global = 1,
+
+    /// <summary>
+    /// Represents the shared address space.
+    /// </summary>
+    Shared = 2,
+
+    /// <summary>
+    /// Represents the local address space.
+    /// </summary>
+    Local = 3
+}
+
+/// <summary>
+/// Represents the base interface for all address spaces.
+/// </summary>
+interface IAddressSpace { }
+
+/// <summary>
+/// Represents an address-space annotation.
+/// </summary>
+[AttributeUsage(AttributeTargets.Struct)]
+sealed class AddressSpaceAttribute : Attribute
+{
+    /// <summary>
+    /// Constructs a new address-space attribute.
+    /// </summary>
+    /// <param name="addressSpace">The target address space.</param>
+    internal AddressSpaceAttribute(MemoryAddressSpace addressSpace)
     {
-        /// <summary>
-        /// The generic address space (any space).
-        /// </summary>
-        Generic = 0,
-
-        /// <summary>
-        /// Represents the global address space.
-        /// </summary>
-        Global = 1,
-
-        /// <summary>
-        /// Represents the shared address space.
-        /// </summary>
-        Shared = 2,
-
-        /// <summary>
-        /// Represents the local address space.
-        /// </summary>
-        Local = 3
+        AddressSpace = addressSpace;
     }
 
     /// <summary>
-    /// Represents the base interface for all address spaces.
+    /// Returns the associated address space.
     /// </summary>
-    public interface IAddressSpace { }
+    public MemoryAddressSpace AddressSpace { get; }
+}
+
+/// <summary>
+/// Extensions to encode ILGPU address space information in the .Net type
+/// system environment.
+/// </summary>
+static class AddressSpaces
+{
+    /// <summary>
+    /// A readonly array of all address spaces.
+    /// </summary>
+    public static readonly ImmutableArray<MemoryAddressSpace> Spaces =
+        ImmutableArray.Create(
+            MemoryAddressSpace.Generic,
+            MemoryAddressSpace.Global,
+            MemoryAddressSpace.Local,
+            MemoryAddressSpace.Shared);
 
     /// <summary>
-    /// Represents an address-space annotation.
+    /// Represents the generic address space.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Struct)]
-    public sealed class AddressSpaceAttribute : Attribute
-    {
-        /// <summary>
-        /// Constructs a new address-space attribute.
-        /// </summary>
-        /// <param name="addressSpace">The target address space.</param>
-        internal AddressSpaceAttribute(MemoryAddressSpace addressSpace)
-        {
-            AddressSpace = addressSpace;
-        }
-
-        /// <summary>
-        /// Returns the associated address space.
-        /// </summary>
-        public MemoryAddressSpace AddressSpace { get; }
-    }
+    [AddressSpace(MemoryAddressSpace.Generic)]
+    internal struct Generic : IAddressSpace { }
 
     /// <summary>
-    /// Extensions to encode ILGPU address space information in the .Net type
-    /// system environment.
+    /// Represents the global address space.
     /// </summary>
-    public static class AddressSpaces
-    {
-        /// <summary>
-        /// A readonly array of all address spaces.
-        /// </summary>
-        public static readonly ImmutableArray<MemoryAddressSpace> Spaces =
-            ImmutableArray.Create(
-                MemoryAddressSpace.Generic,
-                MemoryAddressSpace.Global,
-                MemoryAddressSpace.Local,
-                MemoryAddressSpace.Shared);
+    [AddressSpace(MemoryAddressSpace.Global)]
+    internal struct Global : IAddressSpace { }
 
-        /// <summary>
-        /// Represents the generic address space.
-        /// </summary>
-        [AddressSpace(MemoryAddressSpace.Generic)]
-        internal struct Generic : IAddressSpace { }
+    /// <summary>
+    /// Represents the shared address space.
+    /// </summary>
+    [AddressSpace(MemoryAddressSpace.Shared)]
+    internal struct Shared : IAddressSpace { }
 
-        /// <summary>
-        /// Represents the global address space.
-        /// </summary>
-        [AddressSpace(MemoryAddressSpace.Global)]
-        internal struct Global : IAddressSpace { }
+    /// <summary>
+    /// Represents the local address space.
+    /// </summary>
+    [AddressSpace(MemoryAddressSpace.Local)]
+    internal struct Local : IAddressSpace { }
 
-        /// <summary>
-        /// Represents the shared address space.
-        /// </summary>
-        [AddressSpace(MemoryAddressSpace.Shared)]
-        internal struct Shared : IAddressSpace { }
-
-        /// <summary>
-        /// Represents the local address space.
-        /// </summary>
-        [AddressSpace(MemoryAddressSpace.Local)]
-        internal struct Local : IAddressSpace { }
-
-        /// <summary>
-        /// Resolves the managed type for the given address space.
-        /// </summary>
-        /// <param name="space">The address space.</param>
-        /// <returns>The .Net representation of the given address space.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Type GetManagedType(this MemoryAddressSpace space) =>
-            space switch
-            {
-                MemoryAddressSpace.Global => typeof(Global),
-                MemoryAddressSpace.Local => typeof(Local),
-                MemoryAddressSpace.Shared => typeof(Shared),
-                _ => typeof(Generic),
-            };
-
-        /// <summary>
-        /// Resolves the address-space type for the given .Net type.
-        /// </summary>
-        /// <param name="type">The type.</param>
-        /// <returns>The resolved address space.</returns>
-        public static MemoryAddressSpace GetAddressSpaceType(this Type type)
+    /// <summary>
+    /// Resolves the managed type for the given address space.
+    /// </summary>
+    /// <param name="space">The address space.</param>
+    /// <returns>The .Net representation of the given address space.</returns>
+    public static Type GetManagedType(this MemoryAddressSpace space) =>
+        space switch
         {
-            var attr = type.GetCustomAttribute<AddressSpaceAttribute>();
-            return attr != null
-                ? attr.AddressSpace
-                : MemoryAddressSpace.Generic;
-        }
+            MemoryAddressSpace.Global => typeof(Global),
+            MemoryAddressSpace.Local => typeof(Local),
+            MemoryAddressSpace.Shared => typeof(Shared),
+            _ => typeof(Generic),
+        };
+
+    /// <summary>
+    /// Resolves the address-space type for the given .Net type.
+    /// </summary>
+    /// <param name="type">The type.</param>
+    /// <returns>The resolved address space.</returns>
+    public static MemoryAddressSpace GetAddressSpaceType(this Type type)
+    {
+        var attr = type.GetCustomAttribute<AddressSpaceAttribute>();
+        return attr != null
+            ? attr.AddressSpace
+            : MemoryAddressSpace.Generic;
     }
 }
