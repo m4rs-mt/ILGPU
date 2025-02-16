e1eda6d272a69d6ef596943b22084b793a4e3e8e
Marcel Koester <m4rs@m4rs.net>
2025-02-16 13:22:12 +0000

    Refined Variable handling in CodeGenerator to support both argument and local variable references.

--- Src/ILGPUC/Frontend/CodeGenerator/Variables.cs
+++ Src/ILGPUC/Frontend/CodeGenerator/Variables.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2018-2021 ILGPU Project
+//                        Copyright (c) 2018-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: Variables.cs
@@ -9,76 +9,64 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPU.IR;
+using ILGPUC.IR;
 using System.Diagnostics;
 
-namespace ILGPU.Frontend
+namespace ILGPUC.Frontend;
+
+partial class CodeGenerator
 {
-    partial class CodeGenerator
+    /// <summary>
+    /// Loads a variable. This can be an argument or a local reference.
+    /// </summary>
+    /// <param name="var">The variable reference.</param>
+    private void LoadVariable(VariableRef var)
     {
-        /// <summary>
-        /// Loads a variable. This can be an argument or a local reference.
-        /// </summary>
-        /// <param name="var">The variable reference.</param>
-        private void LoadVariable(VariableRef var)
-        {
-            Debug.Assert(
-                var.RefType == VariableRefType.Argument ||
-                var.RefType == VariableRefType.Local);
-            var addressOrValue = Block.GetValue(var);
-            var type = variableTypes[var];
-            if (variables.Contains(var))
-            {
-                Block.Push(CreateLoad(
-                    addressOrValue,
-                    type.Item1,
-                    type.Item2));
-            }
-            else
-            {
-                Block.Push(LoadOntoEvaluationStack(
-                    addressOrValue,
-                    type.Item2));
-            }
-        }
+        Debug.Assert(
+            var.RefType == VariableRefType.Argument ||
+            var.RefType == VariableRefType.Local);
+        var addressOrValue = Block.GetValue(var);
+        var type = _variableTypes[var];
+        if (_variables.Contains(var))
+            Block.Push(CreateLoad(addressOrValue, type.Type, type.Flags));
+        else
+            Block.Push(LoadOntoEvaluationStack(addressOrValue, type.Flags));
+    }
 
-        /// <summary>
-        /// Loads a variable address. This can be an argument or a local reference.
-        /// </summary>
-        /// <param name="var">The variable reference.</param>
-        private void LoadVariableAddress(VariableRef var)
+    /// <summary>
+    /// Loads a variable address. This can be an argument or a local reference.
+    /// </summary>
+    /// <param name="var">The variable reference.</param>
+    private void LoadVariableAddress(VariableRef var)
+    {
+        Location.Assert(
+            var.RefType == VariableRefType.Argument ||
+            var.RefType == VariableRefType.Local);
+        // Check whether we can load the address of a non-SSA value
+        Location.Assert(_variables.Contains(var));
+        var address = Block.GetValue(var);
+        Block.Push(address);
+    }
+
+    /// <summary>
+    /// Stores a value to the given variable slot.
+    /// </summary>
+    /// <param name="var">The variable reference.</param>
+    private void StoreVariable(VariableRef var)
+    {
+        Location.Assert(
+            var.RefType == VariableRefType.Argument ||
+            var.RefType == VariableRefType.Local);
+        var (type, flags) = _variableTypes[var];
+        var storeValue = Block.Pop(type, flags);
+        if (_variables.Contains(var))
         {
-            Location.Assert(
-                var.RefType == VariableRefType.Argument ||
-                var.RefType == VariableRefType.Local);
-            // Check whether we can load the address of a non-SSA value
-            Location.Assert(variables.Contains(var));
             var address = Block.GetValue(var);
-            Block.Push(address);
+            Builder.CreateStore(Location, address, storeValue);
         }
-
-        /// <summary>
-        /// Stores a value to the given variable slot.
-        /// </summary>
-        /// <param name="var">The variable reference.</param>
-        private void StoreVariable(VariableRef var)
+        else
         {
-            Location.Assert(
-                var.RefType == VariableRefType.Argument ||
-                var.RefType == VariableRefType.Local);
-            var variableType = variableTypes[var];
-            var storeValue = Block.Pop(
-                variableType.Item1,
-                variableType.Item2);
-            if (variables.Contains(var))
-            {
-                var address = Block.GetValue(var);
-                Builder.CreateStore(Location, address, storeValue);
-            }
-            else
-            {
-                Block.SetValue(var, storeValue);
-            }
+            Block.SetValue(var, storeValue);
         }
     }
 }
