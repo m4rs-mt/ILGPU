62f70a3aea9057de54af955720c0116accfffd77
Marcel Koester <m4rs@m4rs.net>
2025-02-21 22:17:46 +0000

    Migrated atomic intrinsic implementations to use new builder methods.

---
+++ Src/ILGPUC/Frontend/Intrinsic/Atomic.cs
@@ -0,0 +1,51 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2018-2025 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: Atomic.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+using ILGPUC.IR.Values;
+
+namespace ILGPUC.Frontend.Intrinsic;
+
+partial class Intrinsics
+{
+    /// <summary>
+    /// Handles atomic operations.
+    /// </summary>
+    /// <param name="context">The current invocation context.</param>
+    /// <param name="kind">Atomic kind to be used.</param>
+    /// <param name="flags">Atomic flags to be used.</param>
+    /// <returns>The resulting value.</returns>
+    private static ValueReference Atomic_Operation(
+        ref InvocationContext context,
+        AtomicKind kind,
+        AtomicFlags flags) =>
+        context.Builder.CreateAtomic(
+            context.Location,
+            context.Pull(),
+            context.Pull(),
+            kind,
+            flags);
+
+    /// <summary>
+    /// Handles atomic ands.
+    /// </summary>
+    /// <param name="context">The current invocation context.</param>
+    /// <param name="flags">Atomic flags to be used.</param>
+    /// <returns>The resulting value.</returns>
+    private static ValueReference Atomic_CompareExchange(
+        ref InvocationContext context,
+        AtomicFlags flags) =>
+        context.Builder.CreateAtomicCAS(
+            context.Location,
+            context.Pull(),
+            context.Pull(),
+            context.Pull(),
+            flags);
+}
