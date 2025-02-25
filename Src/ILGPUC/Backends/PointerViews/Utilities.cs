30f7f23f02cf3580f8b3d37c549ae602e930cff8
Marcel Koester <m4rs@m4rs.net>
2025-02-25 19:41:45 +0000

    Refined native view implementations to support additional element types and improve method handling.

--- Src/ILGPUC/Backends/PointerViews/Utilities.cs
+++ Src/ILGPUC/Backends/PointerViews/Utilities.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2018-2023 ILGPU Project
+//                        Copyright (c) 2018-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: Utilities.cs
@@ -9,97 +9,97 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
+using ILGPU;
 using ILGPU.Util;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
 
-namespace ILGPU.Backends.PointerViews
+namespace ILGPUC.Backends.PointerViews;
+
+/// <summary>
+/// General extensions for pointer-based array view implementations.
+/// </summary>
+static class ViewImplementation
 {
     /// <summary>
-    /// General extensions for pointer-based array view implementations.
+    /// The generic implementation type.
     /// </summary>
-    public static class ViewImplementation
+    public static readonly Type ImplementationType = typeof(ViewImplementation<>);
+
+    /// <summary>
+    /// Returns a specialized implementation type.
+    /// </summary>
+    /// <param name="elementType">The view element type.</param>
+    /// <returns>The implement type.</returns>
+    public static Type GetImplementationType(Type elementType) =>
+        ImplementationType.MakeGenericType(elementType);
+
+    /// <summary>
+    /// Append all implementation-specific element types.
+    /// </summary>
+    /// <typeparam name="TCollection">The target collection type.</typeparam>
+    /// <param name="collection">The target element collection.</param>
+    public static void AppendImplementationTypes<TCollection>(TCollection collection)
+        where TCollection : ICollection<Type>
     {
-        /// <summary>
-        /// The generic implementation type.
-        /// </summary>
-        public static readonly Type ImplementationType = typeof(ViewImplementation<>);
+        collection.Add(typeof(void*));
+        collection.Add(typeof(long));
+    }
 
-        /// <summary>
-        /// Returns a specialized implementation type.
-        /// </summary>
-        /// <param name="elementType">The view element type.</param>
-        /// <returns>The implement type.</returns>
-        public static Type GetImplementationType(Type elementType) =>
-            ImplementationType.MakeGenericType(elementType);
-
-        /// <summary>
-        /// Append all implementation-specific element types.
-        /// </summary>
-        /// <typeparam name="TCollection">The target collection type.</typeparam>
-        /// <param name="collection">The target element collection.</param>
-        public static void AppendImplementationTypes<TCollection>(TCollection collection)
-            where TCollection : ICollection<Type>
+    /// <summary>
+    /// Returns a specialized view constructor.
+    /// </summary>
+    /// <param name="implType">The view implementation type.</param>
+    /// <returns>The resolved view constructor.</returns>
+    public static ConstructorInfo GetViewConstructor(Type implType) =>
+        implType.GetConstructor(new Type[]
         {
-            collection.Add(typeof(void*));
-            collection.Add(typeof(long));
-        }
-
-        /// <summary>
-        /// Returns a specialized view constructor.
-        /// </summary>
-        /// <param name="implType">The view implementation type.</param>
-        /// <returns>The resolved view constructor.</returns>
-        public static ConstructorInfo GetViewConstructor(Type implType) =>
-            implType.GetConstructor(new Type[]
-            {
                 typeof(ArrayView<>).MakeGenericType(
                     implType.GetGenericArguments()[0])
-            })
-            .ThrowIfNull();
+        })
+        .ThrowIfNull();
 
-        /// <summary>
-        /// Returns the pointer field of a view implementation.
-        /// </summary>
-        /// <param name="implType">The view implementation type.</param>
-        /// <returns>The resolved field.</returns>
-        public static FieldInfo GetPtrField(Type implType) =>
-            implType.GetField(nameof(ViewImplementation<int>.Ptr)).ThrowIfNull();
+    /// <summary>
+    /// Returns the pointer field of a view implementation.
+    /// </summary>
+    /// <param name="implType">The view implementation type.</param>
+    /// <returns>The resolved field.</returns>
+    public static FieldInfo GetPtrField(Type implType) =>
+        implType.GetField(nameof(ViewImplementation<int>.Ptr)).ThrowIfNull();
 
-        /// <summary>
-        /// Returns the length field of a view implementation.
-        /// </summary>
-        /// <param name="implType">The view implementation type.</param>
-        /// <returns>The resolved field.</returns>
-        public static FieldInfo GetLengthField(Type implType) =>
-            implType.GetField(nameof(ViewImplementation<int>.Length)).ThrowIfNull();
+    /// <summary>
+    /// Returns the length field of a view implementation.
+    /// </summary>
+    /// <param name="implType">The view implementation type.</param>
+    /// <returns>The resolved field.</returns>
+    public static FieldInfo GetLengthField(Type implType) =>
+        implType.GetField(nameof(ViewImplementation<int>.Length)).ThrowIfNull();
 
-        /// <summary>
-        /// The method handle of the <see cref="GetNativePtrMethod(Type)"/> method.
-        /// </summary>
-        private static readonly MethodInfo GetNativePtrMethodInfo =
-            typeof(ViewImplementation).GetMethod(
-                nameof(GetNativePtr),
-                BindingFlags.NonPublic | BindingFlags.Static)
-            .ThrowIfNull();
+    /// <summary>
+    /// The method handle of the <see cref="GetNativePtrMethod(Type)"/> method.
+    /// </summary>
+    private static readonly MethodInfo GetNativePtrMethodInfo =
+        typeof(ViewImplementation).GetMethod(
+            nameof(GetNativePtr),
+            BindingFlags.NonPublic | BindingFlags.Static)
+        .ThrowIfNull();
 
-        /// <summary>
-        /// Gets the associated native pointer that is stored inside the given view.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="view">The view type.</param>
-        /// <returns>The underlying native pointer.</returns>
-        private static IntPtr GetNativePtr<T>(in ArrayView<T> view)
-            where T : unmanaged =>
-            view.Buffer?.NativePtr ?? IntPtr.Zero;
+    /// <summary>
+    /// Gets the associated native pointer that is stored inside the given view.
+    /// </summary>
+    /// <typeparam name="T">The element type.</typeparam>
+    /// <param name="view">The view type.</param>
+    /// <returns>The underlying native pointer.</returns>
+    private static IntPtr GetNativePtr<T>(in ArrayView<T> view)
+        where T : unmanaged =>
+        view.Buffer?.NativePtr ?? IntPtr.Zero;
 
-        /// <summary>
-        /// Gets the native-pointer method for the given element type.
-        /// </summary>
-        /// <param name="elementType">The element type.</param>
-        /// <returns>The instantiated native method.</returns>
-        public static MethodInfo GetNativePtrMethod(Type elementType) =>
-            GetNativePtrMethodInfo.MakeGenericMethod(elementType);
-    }
+    /// <summary>
+    /// Gets the native-pointer method for the given element type.
+    /// </summary>
+    /// <param name="elementType">The element type.</param>
+    /// <returns>The instantiated native method.</returns>
+    public static MethodInfo GetNativePtrMethod(Type elementType) =>
+        GetNativePtrMethodInfo.MakeGenericMethod(elementType);
 }
