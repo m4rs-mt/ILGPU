499f4234a6bc050fb739cc88c83a5548906d7db4
Marcel Koester <m4rs@m4rs.net>
2025-02-25 20:58:49 +0000

    Refined native view implementations for improved performance and code clarity.

--- Src/ILGPUC/Backends/PointerViews/ViewImplementation.cs
+++ Src/ILGPUC/Backends/PointerViews/ViewImplementation.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2018-2023 ILGPU Project
+//                        Copyright (c) 2018-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: ViewImplementation.cs
@@ -9,114 +9,114 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
+using ILGPU;
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-namespace ILGPU.Backends.PointerViews
+namespace ILGPUC.Backends.PointerViews;
+
+/// <summary>
+/// Represents an array view that is implemented with the help of
+/// native pointers.
+/// </summary>
+/// <typeparam name="T">The element type.</typeparam>
+[StructLayout(LayoutKind.Sequential)]
+unsafe readonly struct ViewImplementation<T>
+    where T : unmanaged
 {
+    #region Instance
+
     /// <summary>
-    /// Represents an array view that is implemented with the help of
-    /// native pointers.
+    /// The base pointer.
     /// </summary>
-    /// <typeparam name="T">The element type.</typeparam>
-    [StructLayout(LayoutKind.Sequential)]
-    public unsafe readonly struct ViewImplementation<T>
-        where T : unmanaged
+    [SuppressMessage(
+        "Microsoft.Design",
+        "CA1051: DoNotDeclareVisibleInstanceFields",
+        Justification = "Implementation type that simplifies code generation")]
+    public readonly void* Ptr;
+
+    /// <summary>
+    /// The length.
+    /// </summary>
+    [SuppressMessage(
+        "Microsoft.Design",
+        "CA1051: DoNotDeclareVisibleInstanceFields",
+        Justification = "Implementation type that simplifies code generation")]
+    public readonly long Length;
+
+    /// <summary>
+    /// Constructs a new array view implementation.
+    /// </summary>
+    /// <param name="ptr">The base pointer.</param>
+    /// <param name="length">The length information.</param>
+    public ViewImplementation(void* ptr, long length)
     {
-        #region Instance
-
-        /// <summary>
-        /// The base pointer.
-        /// </summary>
-        [SuppressMessage(
-            "Microsoft.Design",
-            "CA1051: DoNotDeclareVisibleInstanceFields",
-            Justification = "Implementation type that simplifies code generation")]
-        public readonly void* Ptr;
-
-        /// <summary>
-        /// The length.
-        /// </summary>
-        [SuppressMessage(
-            "Microsoft.Design",
-            "CA1051: DoNotDeclareVisibleInstanceFields",
-            Justification = "Implementation type that simplifies code generation")]
-        public readonly long Length;
-
-        /// <summary>
-        /// Constructs a new array view implementation.
-        /// </summary>
-        /// <param name="ptr">The base pointer.</param>
-        /// <param name="length">The length information.</param>
-        public ViewImplementation(void* ptr, long length)
-        {
-            Ptr = ptr;
-            Length = length;
-        }
-
-        /// <summary>
-        /// Constructs a new array view implementation.
-        /// </summary>
-        /// <param name="source">The abstract source view.</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public ViewImplementation(ArrayView<T> source)
-            : this(
-                  source.IsValid
-                  ? Unsafe.AsPointer(ref source.LoadEffectiveAddress())
-                  : null,
-                  source.Length)
-        { }
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Access the element at the given index.
-        /// </summary>
-        /// <param name="index">The element index.</param>
-        /// <returns>The element at the given index.</returns>
-        public ref T this[Index1D index]
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => ref LoadElementAddress(index);
-        }
-
-        /// <summary>
-        /// Access the element at the given index.
-        /// </summary>
-        /// <param name="index">The element index.</param>
-        /// <returns>The element at the given index.</returns>
-        public ref T this[LongIndex1D index]
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => ref LoadElementAddress(index);
-        }
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Access the element at the given index.
-        /// </summary>
-        /// <param name="index">The element index.</param>
-        /// <returns>The element at the given index.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public ref T LoadElementAddress(Index1D index) =>
-            ref LoadElementAddress((LongIndex1D)index);
-
-        /// <summary>
-        /// Access the element at the given index.
-        /// </summary>
-        /// <param name="index">The element index.</param>
-        /// <returns>The element at the given index.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public ref T LoadElementAddress(LongIndex1D index) =>
-            ref Unsafe.Add(ref Unsafe.AsRef<T>(Ptr), new IntPtr(index));
-
-        #endregion
+        Ptr = ptr;
+        Length = length;
     }
+
+    /// <summary>
+    /// Constructs a new array view implementation.
+    /// </summary>
+    /// <param name="source">The abstract source view.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ViewImplementation(ArrayView<T> source)
+        : this(
+              source.IsValid
+              ? Unsafe.AsPointer(ref source.LoadEffectiveAddress())
+              : null,
+              source.Length)
+    { }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Access the element at the given index.
+    /// </summary>
+    /// <param name="index">The element index.</param>
+    /// <returns>The element at the given index.</returns>
+    public ref T this[Index1D index]
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => ref LoadElementAddress(index);
+    }
+
+    /// <summary>
+    /// Access the element at the given index.
+    /// </summary>
+    /// <param name="index">The element index.</param>
+    /// <returns>The element at the given index.</returns>
+    public ref T this[LongIndex1D index]
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => ref LoadElementAddress(index);
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Access the element at the given index.
+    /// </summary>
+    /// <param name="index">The element index.</param>
+    /// <returns>The element at the given index.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ref T LoadElementAddress(Index1D index) =>
+        ref LoadElementAddress((LongIndex1D)index);
+
+    /// <summary>
+    /// Access the element at the given index.
+    /// </summary>
+    /// <param name="index">The element index.</param>
+    /// <returns>The element at the given index.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ref T LoadElementAddress(LongIndex1D index) =>
+        ref Unsafe.Add(ref Unsafe.AsRef<T>(Ptr), new IntPtr(index));
+
+    #endregion
 }
