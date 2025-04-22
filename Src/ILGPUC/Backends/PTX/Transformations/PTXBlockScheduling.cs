480331637aae656685d3f47fd45a002e2908d2b0
Marcel Koester <m4rs@m4rs.net>
2025-04-22 21:48:11 +0000

    Refined PTXBlockScheduling to optimize block scheduling and avoid negated predicate branches.

--- Src/ILGPUC/Backends/PTX/Transformations/PTXBlockScheduling.cs
+++ Src/ILGPUC/Backends/PTX/Transformations/PTXBlockScheduling.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2020-2023 ILGPU Project
+//                        Copyright (c) 2020-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: PTXBlockScheduling.cs
@@ -9,57 +9,56 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPU.Backends.PTX.Analyses;
-using ILGPU.IR;
-using ILGPU.IR.Transformations;
-using ILGPU.IR.Values;
+using ILGPUC.Backends.PTX.Analyses;
+using ILGPUC.IR;
+using ILGPUC.IR.Transformations;
+using ILGPUC.IR.Values;
 
-namespace ILGPU.Backends.PTX.Transformations
+namespace ILGPUC.Backends.PTX.Transformations;
+
+/// <summary>
+/// Adapts the actual block branch order in a way to avoid negated predicated
+/// branches and which maximizes the number of implicit block branches.
+/// </summary>
+sealed class PTXBlockScheduling : UnorderedTransformation
 {
     /// <summary>
-    /// Adapts the actual block branch order in a way to avoid negated predicated
-    /// branches and which maximizes the number of implicit block branches.
+    /// Applies the PTX-specific block schedule to the given builder.
     /// </summary>
-    sealed class PTXBlockScheduling : UnorderedTransformation
+    protected override void PerformTransformation(
+        IRContext context,
+        Method.Builder builder)
     {
-        /// <summary>
-        /// Applies the PTX-specific block schedule to the given builder.
-        /// </summary>
-        protected override bool PerformTransformation(Method.Builder builder)
+        // We change the control-flow structure during the transformation but
+        // need to get information about previous predecessors and successors
+        builder.AcceptControlFlowUpdates(accept: true);
+
+        // Compute the intended block scheduling order and assign all blocks to their
+        // traversal index
+        var schedule = builder.SourceBlocks.CreatePTXScheduleToOptimize();
+
+        // Ensure that all conditional if branches (which can fall through in the
+        // true case) will be inverted to have "optimistic" branch checks in the
+        // false case. This circumvents several limitations in the current PTX
+        // assembler implementation.
+        foreach (var block in schedule)
         {
-            // We change the control-flow structure during the transformation but
-            // need to get information about previous predecessors and successors
-            builder.AcceptControlFlowUpdates(accept: true);
+            if (block.Terminator is not IfBranch ifBranch)
+                continue;
 
-            // Compute the intended block scheduling order and assign all blocks to their
-            // traversal index
-            var schedule = builder.SourceBlocks.CreatePTXScheduleToOptimize();
+            // Ensure that all branches are not inverted at this point
+            block.Assert(!ifBranch.IsInverted);
 
-            // Ensure that all conditional if branches (which can fall through in the
-            // true case) will be inverted to have "optimistic" branch checks in the
-            // false case. This circumvents several limitations in the current PTX
-            // assembler implementation.
-            foreach (var block in schedule)
-            {
-                if (!(block.Terminator is IfBranch ifBranch))
-                    continue;
+            // Ignore if branches that can jump to arbitrary locations and branches
+            // that are already in the desired state (with respect to the other false
+            // branch target).
+            if (!schedule.IsImplicitSuccessor(block, ifBranch.TrueTarget))
+                continue;
 
-                // Ensure that all branches are not inverted at this point
-                block.Assert(!ifBranch.IsInverted);
-
-                // Ignore if branches that can jump to arbitrary locations and branches
-                // that are already in the desired state (with respect to the other false
-                // branch target).
-                if (!schedule.IsImplicitSuccessor(block, ifBranch.TrueTarget))
-                    continue;
-
-                // Invert the current if such that the false target becomes the true
-                // branch target and the condition is properly inverted to avoid the
-                // generation of negated predicate checks
-                ifBranch.Invert(builder[block]);
-            }
-
-            return true;
+            // Invert the current if such that the false target becomes the true
+            // branch target and the condition is properly inverted to avoid the
+            // generation of negated predicate checks
+            ifBranch.Invert(builder[block]);
         }
     }
 }
