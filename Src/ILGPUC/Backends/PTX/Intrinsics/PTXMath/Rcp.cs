d762c86891ae3283fbd8ac9c314b8aa89b14a406
Marcel Koester <m4rs@m4rs.net>
2025-04-23 22:11:02 +0000

    Refined PTXMath to support all intrinsic math functions using inline assembly where appropriate.

---
+++ Src/ILGPUC/Backends/PTX/Intrinsics/PTXMath/Rcp.cs
@@ -0,0 +1,49 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2019-2025 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: Rcp.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+using ILGPU;
+using ILGPU.Runtime.Cuda;
+using LD = ILGPU.Runtime.Cuda.CudaLibDevice;
+
+namespace ILGPUC.Backends.PTX.Intrinsics;
+
+partial class PTXMath
+{
+    /// <inheritdoc cref="XMath.Rcp(double)"/>
+    public static double RcpFloat64(double value)
+    {
+        if (LD.IsSupported)
+            return LD.RcpRoundZero(value);
+
+        CudaAsm.Emit(
+            XMath.OptimizePerformance
+            ? "rcp.approx.ftz.f64 {0}, {1}"
+            : "rcp.rn.f64 {0}, {1}",
+            out double result,
+            value);
+        return result;
+    }
+
+    /// <inheritdoc cref="XMath.Rcp(float)"/>
+    public static float RcpFloat32(float value)
+    {
+        if (LD.IsSupported)
+            return LD.RcpRoundZero(value);
+
+        CudaAsm.Emit(
+            XMath.OptimizePerformance
+            ? "rcp.approx.ftz.f32 {0}, {1}"
+            : "rcp.rn.f32 {0}, {1}",
+            out float result,
+            value);
+        return result;
+    }
+}
