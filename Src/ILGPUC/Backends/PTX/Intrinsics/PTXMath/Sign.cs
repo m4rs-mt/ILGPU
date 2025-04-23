d985a0f766c68f4d9a71074c253539450606f2d7
Marcel Koester <m4rs@m4rs.net>
2025-04-23 23:23:47 +0000

    Refined PTXMath to support intrinsic copy sign functions using inline assembly.

---
+++ Src/ILGPUC/Backends/PTX/Intrinsics/PTXMath/Sign.cs
@@ -0,0 +1,32 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2024-2025 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: Sign.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+using ILGPU;
+using ILGPU.Runtime.Cuda;
+
+namespace ILGPUC.Backends.PTX.Intrinsics;
+
+partial class PTXMath
+{
+    /// <inheritdoc cref="XMath.CopySign(double, double)" />
+    public static double CopySignFloat64(double x, double y)
+    {
+        CudaAsm.Emit("copysign.f64 {0}, {1}, {2}", out double result, x, y);
+        return result;
+    }
+
+    /// <inheritdoc cref="XMath.CopySign(float, float)" />
+    public static float CopySignFloat32(float x, float y)
+    {
+        CudaAsm.Emit("copysign.f32 {0}, {1}, {2}", out float result, x, y);
+        return result;
+    }
+}
