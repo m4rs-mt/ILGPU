25ce528b62914d11eaf71e002ee8896adc2f3fd2
Marcel Koester <m4rs@m4rs.net>
2025-05-03 14:01:39 +0000

    Refined NotSupportedIntrinsicException to enhance exception handling for unsupported intrinsics.

--- Src/ILGPUC/Backends/NotSupportedIntrinsicException.cs
+++ Src/ILGPUC/Backends/NotSupportedIntrinsicException.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------------------------------
 //                                        ILGPU
-//                        Copyright (c) 2019-2023 ILGPU Project
+//                        Copyright (c) 2019-2025 ILGPU Project
 //                                    www.ilgpu.net
 //
 // File: NotSupportedIntrinsicException.cs
@@ -9,69 +9,55 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPU.IR;
 using ILGPU.Resources;
+using ILGPUC.IR;
 using System;
-using System.Runtime.Serialization;
 
-namespace ILGPU.Backends
+namespace ILGPUC.Backends;
+
+/// <summary>
+/// An exception that is thrown in case of a not support intrinsic.
+/// </summary>
+[Serializable]
+sealed class NotSupportedIntrinsicException : Exception
 {
     /// <summary>
-    /// An exception that is thrown in case of a not support intrinsic.
+    /// Constructs a new intrinsic exception.
     /// </summary>
-    [Serializable]
-    public sealed class NotSupportedIntrinsicException : Exception
-    {
-        /// <summary>
-        /// Constructs a new intrinsic exception.
-        /// </summary>
-        public NotSupportedIntrinsicException()
-            : base(ErrorMessages.NotSupportedIntrinsicImplementation0)
-        { }
+    public NotSupportedIntrinsicException()
+        : base(ErrorMessages.NotSupportedIntrinsicImplementation0)
+    { }
 
-        /// <summary>
-        /// Constructs a new intrinsic exception.
-        /// </summary>
-        /// <param name="intrinsicMethod">
-        /// The IR method that could not be implemented.
-        /// </param>
-        public NotSupportedIntrinsicException(Method intrinsicMethod)
-            : this(
-                  intrinsicMethod.HasSource
-                  ? intrinsicMethod.Source.Name
-                  : intrinsicMethod.Name)
-        { }
+    /// <summary>
+    /// Constructs a new intrinsic exception.
+    /// </summary>
+    /// <param name="intrinsicMethod">
+    /// The IR method that could not be implemented.
+    /// </param>
+    public NotSupportedIntrinsicException(Method intrinsicMethod)
+        : this(
+              intrinsicMethod.HasSource
+              ? intrinsicMethod.Source.Name
+              : intrinsicMethod.Name)
+    { }
 
-        /// <summary>
-        /// Constructs a new intrinsic exception.
-        /// </summary>
-        /// <param name="intrinsicName">The name of the not supported intrinsic.</param>
-        public NotSupportedIntrinsicException(string intrinsicName)
-            : base(
-                  string.Format(
-                      ErrorMessages.NotSupportedIntrinsicImplementation1,
-                      intrinsicName))
-        { }
+    /// <summary>
+    /// Constructs a new intrinsic exception.
+    /// </summary>
+    /// <param name="intrinsicName">The name of the not supported intrinsic.</param>
+    public NotSupportedIntrinsicException(string intrinsicName)
+        : base(
+              string.Format(
+                  ErrorMessages.NotSupportedIntrinsicImplementation1,
+                  intrinsicName))
+    { }
 
-        /// <summary>
-        /// Constructs a new intrinsic exception.
-        /// </summary>
-        /// <param name="message">The detailed error message.</param>
-        /// <param name="innerException">The inner exception.</param>
-        public NotSupportedIntrinsicException(string message, Exception innerException)
-            : base(message, innerException)
-        { }
-
-        /// <summary>
-        /// Constructs a new intrinsic exception.
-        /// </summary>
-#if NET8_0_OR_GREATER
-        [Obsolete("SYSLIB0050: Formatter-based serialization is obsolete")]
-#endif
-        private NotSupportedIntrinsicException(
-            SerializationInfo info,
-            StreamingContext context)
-            : base(info, context)
-        { }
-    }
+    /// <summary>
+    /// Constructs a new intrinsic exception.
+    /// </summary>
+    /// <param name="message">The detailed error message.</param>
+    /// <param name="innerException">The inner exception.</param>
+    public NotSupportedIntrinsicException(string message, Exception innerException)
+        : base(message, innerException)
+    { }
 }
