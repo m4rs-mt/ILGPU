66c33d9a9e801b1c33a9144b5f52f1542faec3a4
Marcel Koester <m4rs@m4rs.net>
2025-05-06 23:02:22 +0000

    Implemented atomic operations and compare-exchange functionality in the Frontend.Intrinsic namespace.

--- Src/ILGPUC/Frontend/Intrinsic/Atomic.cs
+++ Src/ILGPUC/Frontend/Intrinsic/Atomic.cs
@@ -16,36 +16,45 @@ namespace ILGPUC.Frontend.Intrinsic;
 partial class Intrinsics
 {
     /// <summary>
+    /// Determines atomic flags for internal operations.
+    /// </summary>
+    private static AtomicFlags DetermineAtomicFlags(ref InvocationContext context) =>
+        context.HasUnsignedArguments
+        ? AtomicFlags.Unsigned
+        : AtomicFlags.None;
+
+    /// <summary>
     /// Handles atomic operations.
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <param name="kind">Atomic kind to be used.</param>
-    /// <param name="flags">Atomic flags to be used.</param>
     /// <returns>The resulting value.</returns>
     private static ValueReference Atomic_Operation(
         ref InvocationContext context,
-        AtomicKind kind,
-        AtomicFlags flags) =>
-        context.Builder.CreateAtomic(
+        GenericAtomicKind kind)
+    {
+        var flags = DetermineAtomicFlags(ref context);
+        return context.Builder.CreateAtomic(
             context.Location,
             context.Pull(),
             context.Pull(),
             kind,
             flags);
+    }
 
     /// <summary>
     /// Handles atomic ands.
     /// </summary>
     /// <param name="context">The current invocation context.</param>
-    /// <param name="flags">Atomic flags to be used.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Atomic_CompareExchange(
-        ref InvocationContext context,
-        AtomicFlags flags) =>
-        context.Builder.CreateAtomicCAS(
+    private static ValueReference Atomic_CompareExchange(ref InvocationContext context)
+    {
+        var flags = DetermineAtomicFlags(ref context);
+        return context.Builder.CreateAtomicCAS(
             context.Location,
             context.Pull(),
             context.Pull(),
             context.Pull(),
             flags);
+    }
 }
