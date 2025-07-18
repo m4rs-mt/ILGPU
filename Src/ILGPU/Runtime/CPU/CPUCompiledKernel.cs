541eb5a5994bfafaa4787e5fbe8f84448fd8aa29
Marcel Koester <m4rs@m4rs.net>
2025-07-18 22:43:21 +0100

    Added CPU kernel, compiled kernel, and runtime view (part 1/2).

---
+++ Src/ILGPU/Runtime/CPU/CPUCompiledKernel.cs
@@ -0,0 +1,45 @@
+// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                           Copyright (c) 2025 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: CPUCompiledKernel.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+using System;
+
+namespace ILGPU.Runtime.CPU;
+
+/// <summary>
+/// Represents a compiled CSharp/Debug kernel for vectorized CPU execution.
+/// </summary>
+public class CPUCompiledKernel(
+    Guid guid,
+    string kernelName,
+    CompiledKernelType kernelType,
+    CompiledKernelSharedMemoryMode sharedMemoryMode,
+    int simdWidth,
+    string kernelClassName) :
+    CompiledKernel(guid, kernelName, kernelType, sharedMemoryMode)
+{
+    /// <summary>
+    /// Returns the SIMD width used for vectorized execution.
+    /// </summary>
+    public int SimdWidth { get; } = simdWidth;
+
+    /// <summary>
+    /// Returns the kernel class name for calling the entry point.
+    /// </summary>
+    public string KernelClassName { get; } = kernelClassName;
+
+    /// <inheritdoc/>
+    public override AcceleratorCapabilities RequiredCapabilities =>
+        CPUAcceleratorCapabilities.Default;
+
+    /// <inheritdoc/>
+    public sealed override string GetSourceAsString() =>
+        throw new NotSupportedException();
+}
