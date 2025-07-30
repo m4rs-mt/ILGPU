65cec0685ad73848c6b3cc04a882113d41d23821
Marcel Koester <m4rs@m4rs.net>
2025-07-30 20:22:35 +0100

    Refined ScanReduce core (Reduction, Scanner) and renamed operation-type interface to ScanReduceTypes (part 1/2).

--- Src/ILGPU/ScanReduce/ScanReduceTypes.cs
+++ Src/ILGPU/ScanReduce/ScanReduceTypes.cs
@@ -3,7 +3,7 @@
 //                           Copyright (c) 2026 ILGPU Project
 //                                    www.ilgpu.net
 //
-// File: IScanReduceOperation.cs
+// File: ScanReduceTypes.cs
 //
 // This file is part of ILGPU and is distributed under the University of Illinois Open
 // Source License. See LICENSE.txt for details.
@@ -12,52 +12,13 @@
 namespace ILGPU.ScanReduce;
 
 /// <summary>
-/// Implements a scan or a reduction operation.
-/// </summary>
-/// <typeparam name="T">The underlying type of the scan operation.</typeparam>
-public interface IScanReduceOperation<T> where T : struct
-{
-    /// <summary>
-    /// Returns the identity value (the neutral element of the operation), such that
-    /// Apply(Apply(Identity, left), right) == Apply(left, right).
-    /// </summary>
-    static abstract T Identity { get; }
-
-    /// <summary>
-    /// Applies the current operation.
-    /// </summary>
-    /// <param name="first">The first operand.</param>
-    /// <param name="second">The second operand.</param>
-    /// <returns>The result of the operation.</returns>
-    static abstract T Apply(T first, T second);
-
-    /// <summary>
-    /// Performs an atomic operation of the form target =
-    /// AtomicUpdate(target.Value, value).
-    /// </summary>
-    /// <param name="target">The target address to update.</param>
-    /// <param name="value">The value.</param>
-    static abstract void AtomicApply(ref T target, T value);
-}
-
-/// <summary>
-/// Holds the left and the right boundary of a scan operation.
+/// Represents an atomic binary operation applied to a reference target.
+/// Used by the lambda-based reduction API.
 /// </summary>
 /// <typeparam name="T">The element type.</typeparam>
-/// <remarks>
-/// Constructs a new scan-boundaries instance.
-/// </remarks>
-/// <param name="LeftBoundary">The left boundary.</param>
-/// <param name="RightBoundary">The right boundary.</param>
-public readonly record struct ScanBoundaries<T>(T LeftBoundary, T RightBoundary)
-    where T : struct
-{
-    /// <summary>
-    /// Returns the string representation of these boundary values.
-    /// </summary>
-    /// <returns>The string representation of these boundary values.</returns>
-    public override string ToString() => $"[{LeftBoundary}, {RightBoundary}]";
-}
+/// <param name="target">The target reference to update atomically.</param>
+/// <param name="value">The value to apply.</param>
+public delegate void AtomicApplyAction<T>(ref T target, T value);
 
 /// <summary>
 /// Represents the scan operation type.
