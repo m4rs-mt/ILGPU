c5239b356e7682e7ef204d9aa464cea6d82f8079
Marcel Koester <m4rs@m4rs.net>
2025-08-20 19:28:00 +0100

    Added IR handle, kind, object, and padding type values (part 1/2).

---
+++ Src/ILGPUC/IR/ModuleValues/HandleType.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2019-2025 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: HandleType.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+namespace ILGPUC.IR.ModuleValues;
+
+/// <summary>
+/// Represents a .Net runtime-specific handle type.
+/// </summary>
+sealed partial class HandleType : ObjectType
+{
+    /// <summary>
+    /// Constructs a new .Net runtime-specific handle type.
+    /// </summary>
+    /// <param name="initializer">The value initializer.</param>
+    public HandleType(in ModuleValueInitializer initializer)
+        : base(initializer, initializer.Builder.KindType)
+    {
+        Seal();
+    }
+
+    /// <inheritdoc/>
+    public override TypeValue? Rewrite<TRewriter>(in TRewriter rewriter) =>
+        rewriter.ModuleBuilder.HandleType;
+
+    /// <summary>
+    /// Returns the string "handle".
+    /// </summary>
+    protected override string ToPrefixString() => "handle";
+
+    /// <summary cref="TypeValue.GetHashCode"/>
+    public override int GetHashCode() => nameof(HandleType).GetHashCode(
+        System.StringComparison.Ordinal);
+
+    /// <summary cref="TypeValue.Equals(object?)"/>
+    public override bool Equals(object? obj) => obj is HandleType;
+}
