d66629bb8369f466c43cbda1e358378eff3f3f54
Marcel Koester <m4rs@m4rs.net>
2025-09-20 09:08:21 +0100

    Added IR basic-block memory value and construction helper.

---
+++ Src/ILGPUC/IR/BasicBlockValues/Construction/Memory.cs
@@ -0,0 +1,233 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2018-2025 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: Memory.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+using ILGPUC.IR.ModuleValues;
+using ILGPUC.IR.PureValues;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ILGPUC.IR.BasicBlockValues.Construction;
+
+partial class BasicBlockBuilder
+{
+    /// <summary>
+    /// Creates a <see cref="GCInit"/> node that marks the point in this block where the
+    /// managed object backed by <paramref name="global"/> is initialized.
+    /// </summary>
+    /// <param name="location">The current source location.</param>
+    /// <param name="global">
+    /// The module-level <see cref="Global"/> that holds the per-thread Local memory
+    /// for the object being initialized.
+    /// </param>
+    /// <returns>The new <see cref="GCInit"/> node.</returns>
+    public GCInit? CreateGCInit(Location location, Global? global)
+    {
+        if (global is null) return null;
+        return Append(new GCInit(GetInitializer(location), global));
+    }
+
+    /// <summary>
+    /// Creates a local allocation.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="type">The type of the allocation.</param>
+    /// <param name="arrayLength">
+    /// The array length (number of elements to allocate or undefined).
+    /// </param>
+    /// <returns>A node that represents the alloca operation.</returns>
+    public Alloca? CreateAlloca(
+        Location location,
+        TypeValue type,
+        Value? arrayLength = null)
+    {
+        arrayLength ??= CreatePrimitiveValue(location, 1);
+
+        if (arrayLength is PrimitiveValue primitiveValue)
+        {
+            if (primitiveValue.HasIntValue(0L))
+                return null;
+
+            var box = primitiveValue.Box;
+            return CreateAlloca(location, type, box);
+        }
+
+        return Append(new Alloca(
+            GetInitializer(location),
+                arrayLengthValue: arrayLength,
+                arrayLength: null,
+                type));
+    }
+
+    /// <summary>
+    /// Creates a local allocation.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="type">The type of the allocation.</param>
+    /// <param name="arrayLength">
+    /// The array length (number of elements to allocate or undefined).
+    /// </param>
+    /// <returns>A node that represents the alloca operation.</returns>
+    public Alloca? CreateAlloca(
+        Location location,
+        TypeValue type,
+        PrimitiveValueBox? arrayLength)
+    {
+        if (!arrayLength.HasValue) return null;
+        if (arrayLength.Value.HasIntValue(0L)) return null;
+
+        return Append(new Alloca(
+            GetInitializer(location),
+                arrayLengthValue: null,
+                arrayLength: arrayLength,
+                type));
+    }
+
+    /// <summary>
+    /// Creates a load operation.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="source">The source address.</param>
+    /// <returns>A node that represents the load operation.</returns>
+    [return: NotNullIfNotNull(nameof(source))]
+    public BasicBlockValue? CreateLoad(Location location, Value? source)
+    {
+        if (source is null) return null;
+
+        location.Assert(source.Type is PointerType);
+
+        return Append(new Load(GetInitializer(location), source));
+    }
+
+    /// <summary>
+    /// Creates a store operation.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="target">The target address.</param>
+    /// <param name="value">The value to store.</param>
+    /// <returns>A node that represents the store operation.</returns>
+    public BasicBlockValue? CreateStore(Location location, Value? target, Value? value)
+    {
+        if (target is null || value is null) return null;
+
+        location.Assert(
+            target.Type is PointerType pointerType &&
+            pointerType.ElementType.Equals(value.Type));
+
+        return Append(new Store(GetInitializer(location), target, value));
+    }
+
+    /// <summary>
+    /// Creates a memory barrier.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="kind">The type of the memory barrier.</param>
+    /// <returns>A node that represents the memory barrier.</returns>
+    public BasicBlockValue CreateMemoryBarrier(
+        Location location,
+        MemoryBarrierKind kind) =>
+        Append(new MemoryBarrier(GetInitializer(location), kind));
+
+    /// <summary>
+    /// Creates a new atomic operation.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="target">The target address.</param>
+    /// <param name="value">The target value.</param>
+    /// <param name="kind">The operation kind.</param>
+    /// <param name="flags">The operation flags.</param>
+    /// <returns>A node that represents the atomic operation.</returns>
+    public Value? CreateAtomic(
+        Location location,
+        Value? target,
+        Value? value,
+        GenericAtomicKind kind,
+        AtomicFlags flags)
+    {
+        if (target is null || value is null) return null;
+
+        location.Assert(
+            target.Type is PointerType type &&
+            type.ElementType.Equals(value.Type));
+
+        return Append(new GenericAtomic(
+            GetInitializer(location),
+            target,
+            value,
+            kind,
+            flags));
+    }
+
+    /// <summary>
+    /// Creates a new atomic compare-and-swap operation
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="target">The parent memory operation.</param>
+    /// <param name="value">The target value.</param>
+    /// <param name="compareValue">The comparison value.</param>
+    /// <param name="flags">The operation flags.</param>
+    /// <returns>
+    /// A node that represents the atomic compare-and-swap operation.
+    /// </returns>
+    public Value? CreateAtomicCAS(
+        Location location,
+        Value? target,
+        Value? value,
+        Value? compareValue,
+        AtomicFlags flags)
+    {
+        if (target is null || value is null || compareValue is null) return null;
+
+        location.Assert(
+            target.Type is PointerType type &&
+            type.ElementType.Equals(value.Type) &&
+            value.Type.Equals(compareValue.Type));
+
+        return Append(new AtomicCAS(
+            GetInitializer(location),
+            target,
+            value,
+            compareValue,
+            flags));
+    }
+
+    /// <summary>
+    /// Creates a custom atomic operation driven by a user-provided binary
+    /// operation lambda. Lowered to a CAS loop by
+    /// <see cref="IR.Transformations.LowerCustomAtomic"/>.
+    /// </summary>
+    /// <param name="location">The current location.</param>
+    /// <param name="target">The pointer to the atomic target.</param>
+    /// <param name="value">The value passed to the operation lambda.</param>
+    /// <param name="operation">
+    /// The IR <see cref="Method"/> representing the user's binary operation.
+    /// </param>
+    /// <param name="flags">The operation flags.</param>
+    /// <returns>A node that represents the custom atomic operation.</returns>
+    public Value? CreateCustomAtomic(
+        Location location,
+        Value? target,
+        Value? value,
+        Value? operation,
+        AtomicFlags flags)
+    {
+        if (target is null || value is null || operation is null) return null;
+
+        location.Assert(
+            target.Type is PointerType type &&
+            type.ElementType.Equals(value.Type));
+
+        return Append(new CustomAtomic(
+            GetInitializer(location),
+            target,
+            value,
+            operation,
+            flags));
+    }
+}
---
+++ Src/ILGPUC/IR/BasicBlockValues/Memory.cs
@@ -0,0 +1,231 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2018-2025 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: Memory.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+using ILGPU.Util;
+using ILGPUC.IR.ModuleValues;
+using ILGPUC.IR.PureValues;
+
+namespace ILGPUC.IR.BasicBlockValues;
+
+/// <summary>
+/// Represents an abstract value operating on memory.
+/// </summary>
+/// <param name="initializer">The value initializer.</param>
+/// <param name="type">
+/// The type to use (not part of the generic initializer provided).
+/// </param>
+abstract class MemoryValue(in BasicBlockValueInitializer initializer, TypeValue type) :
+    BasicBlockValue(initializer, type);
+
+/// <summary>
+/// Represents an allocation operation on the stack.
+/// </summary>
+sealed partial class Alloca : MemoryValue, IAllocationValue
+{
+    /// <summary>
+    /// Constructs a new alloc node.
+    /// </summary>
+    /// <param name="initializer">The value initializer.</param>
+    /// <param name="arrayLengthValue">The array length value to allocate.</param>
+    /// <param name="arrayLength">The array length to allocate.</param>
+    /// <param name="allocType">The allocation type.</param>
+    internal Alloca(
+        in BasicBlockValueInitializer initializer,
+        Value? arrayLengthValue,
+        PrimitiveValueBox? arrayLength,
+        TypeValue allocType)
+        : base(
+            initializer,
+            initializer.ModuleBuilder.CreatePointerType(
+                allocType,
+                MemoryAddressSpace.Local))
+    {
+        ArrayLength = arrayLength;
+
+        if (!arrayLength.HasValue)
+            Seal(arrayLengthValue.AsNotNull());
+        else
+            Seal();
+    }
+
+    /// <summary>
+    /// Returns the allocation type.
+    /// </summary>
+    public TypeValue AllocType => GetTypeAs<PointerType>().ElementType;
+
+    /// <summary>
+    /// Returns the address space of this allocation.
+    /// </summary>
+    public MemoryAddressSpace AddressSpace => MemoryAddressSpace.Local;
+
+    /// <summary>
+    /// Returns the array length value (if applicable).
+    /// </summary>
+    public Value<Method>? ArrayLengthValue =>
+        !ArrayLength.HasValue ? GetValue<Value<Method>>(0) : null;
+
+    /// <summary>
+    /// Returns the current <see cref="ArrayLengthValue"/>.
+    /// </summary>
+    Value? IAllocationValue.ArrayLengthValue => ArrayLengthValue;
+
+    /// <summary>
+    /// Returns the underlying array length (if any).
+    /// </summary>
+    public PrimitiveValueBox? ArrayLength { get; }
+
+    /// <inheritdoc cref="IBasicBlockValue.Rewrite{TRewriter}(in TRewriter)"/>
+    public override Value? Rewrite<TRewriter>(in TRewriter rewriter)
+    {
+        if (this.IsStaticAllocation(out var arrayLength))
+        {
+            return rewriter.Builder.CreateAlloca(
+                Location,
+                rewriter.RewriteAs<TypeValue>(AllocType),
+                arrayLength);
+        }
+        else
+        {
+            return rewriter.Builder.CreateAlloca(
+                Location,
+                rewriter.RewriteAs<TypeValue>(AllocType),
+                rewriter.RewriteAs<Value<Method>>(ArrayLengthValue.AsNotNull()));
+        }
+    }
+
+    /// <inheritdoc cref="Value.ToPrefixString"/>
+    protected override string ToPrefixString() => "alloc";
+
+    /// <inheritdoc cref="Value.ToArgString()"/>
+    protected override string ToArgString() =>
+        this.IsStaticAllocation(out var arrayLength)
+        ? $"{Type} [{arrayLength}]"
+        : $"{Type} [{ArrayLengthValue.AsNotNull().ToReferenceString()}]";
+}
+
+/// <summary>
+/// Represents a memory barrier that hinders reordering of memory operations with side
+/// effects.
+/// </summary>
+sealed partial class MemoryBarrier : MemoryValue
+{
+    /// <summary>
+    /// Constructs a new memory barrier.
+    /// </summary>
+    /// <param name="initializer">The value initializer.</param>
+    /// <param name="kind">The barrier kind.</param>
+    public MemoryBarrier(
+        in BasicBlockValueInitializer initializer,
+        MemoryBarrierKind kind)
+        : base(initializer, initializer.ModuleBuilder.VoidType)
+    {
+        Kind = kind;
+
+        Seal();
+    }
+
+    /// <inheritdoc/>
+    public override Value? Rewrite<TRewriter>(in TRewriter rewriter) =>
+        rewriter.Builder.CreateMemoryBarrier(Location, Kind);
+
+    /// <inheritdoc cref="Value.ToPrefixString"/>
+    protected override string ToPrefixString() => "memBarrier";
+
+    /// <inheritdoc cref="Value.ToArgString()"/>
+    protected override string ToArgString() => Kind.ToString();
+}
+
+/// <summary>
+/// Represents a load operation with side effects.
+/// </summary>
+sealed partial class Load : MemoryValue
+{
+    /// <summary>
+    /// Constructs a new load operation.
+    /// </summary>
+    /// <param name="initializer">The value initializer.</param>
+    /// <param name="source">The source view.</param>
+    public Load(in BasicBlockValueInitializer initializer, Value source)
+        : base(initializer, source.GetTypeAs<AddressSpaceType>().ElementType)
+    {
+        Seal(source);
+    }
+
+    /// <summary>
+    /// Returns the source view.
+    /// </summary>
+    public Value Source => GetValue<Value>(0);
+
+    /// <summary>
+    /// Returns the source address space this load reads from.
+    /// </summary>
+    public MemoryAddressSpace SourceAddressSpace =>
+        Source.GetTypeAs<AddressSpaceType>().AddressSpace;
+
+    /// <inheritdoc/>
+    public override Value? Rewrite<TRewriter>(in TRewriter rewriter) =>
+        rewriter.Builder.CreateLoad(Location, rewriter.Rewrite(Source));
+
+    /// <inheritdoc cref="Value.ToPrefixString"/>
+    protected override string ToPrefixString() => "ld";
+
+    /// <inheritdoc cref="Value.ToArgString()"/>
+    protected override string ToArgString() => Source.ToReferenceString();
+}
+
+/// <summary>
+/// Represents a store operation with side effects.
+/// </summary>
+sealed partial class Store : MemoryValue
+{
+    /// <summary>
+    /// Constructs a new store operation.
+    /// </summary>
+    /// <param name="initializer">The value initializer.</param>
+    /// <param name="target">The target view.</param>
+    /// <param name="value">The value to store.</param>
+    public Store(in BasicBlockValueInitializer initializer, Value target, Value value)
+        : base(initializer, initializer.ModuleBuilder.VoidType)
+    {
+        Seal(target, value);
+    }
+
+    /// <summary>
+    /// Returns the target view.
+    /// </summary>
+    public Value Target => GetValue<Value>(0);
+
+    /// <summary>
+    /// Returns the target address space this store writes to.
+    /// </summary>
+    public MemoryAddressSpace TargetAddressSpace =>
+        Target.GetTypeAs<AddressSpaceType>().AddressSpace;
+
+    /// <summary>
+    /// Returns the value to store.
+    /// </summary>
+    public Value Value => GetValue<Value>(1);
+
+    /// <inheritdoc/>
+    public override Value? Rewrite<TRewriter>(in TRewriter rewriter)
+    {
+        var newTarget = rewriter.Rewrite(Target);
+        var newValue = rewriter.Rewrite(Value);
+        return rewriter.Builder.CreateStore(Location, newTarget, newValue);
+    }
+
+    /// <inheritdoc cref="Value.ToPrefixString"/>
+    protected override string ToPrefixString() => "st";
+
+    /// <inheritdoc cref="Value.ToArgString()"/>
+    protected override string ToArgString() =>
+        $"{Target.ToReferenceString()} -> {Value.ToReferenceString()}";
+}
