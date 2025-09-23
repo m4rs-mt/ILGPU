32e1867bf48611aa9efdb13c5863f01363ac9722
Marcel Koester <m4rs@m4rs.net>
2025-09-23 19:28:12 +0100

    Added IR basic-block class values and terminator construction helper.

---
+++ Src/ILGPUC/IR/BasicBlockValues/ClassValues.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2020-2025 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: ClassValues.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+using ILGPUC.IR.ModuleValues;
+
+namespace ILGPUC.IR.BasicBlockValues;
+
+/// <summary>
+/// An abstract class value.
+/// </summary>
+/// <param name="initializer">The value initializer.</param>
+/// <param name="type">
+/// The type to use (not part of the generic initializer provided).
+/// </param>
+abstract class ClassValue(in BasicBlockValueInitializer initializer, TypeValue type) :
+    MemoryValue(initializer, type);
+
+/// <summary>
+/// Represents an operation on object values.
+/// </summary>
+/// <param name="initializer">The value initializer.</param>
+/// <param name="type">
+/// The type to use (not part of the generic initializer provided).
+/// </param>
+abstract class ClassOperationValue(
+    in BasicBlockValueInitializer initializer,
+    TypeValue type) :
+    MemoryValue(initializer, type)
+{
+    /// <summary>
+    /// Returns the object value to load from.
+    /// </summary>
+    public Value ObjectValue => GetValue<Value>(0);
+
+    /// <summary>
+    /// Returns the object type.
+    /// </summary>
+    public ObjectType ObjectType => ObjectValue.GetTypeAs<ObjectType>();
+}
---
+++ Src/ILGPUC/IR/BasicBlockValues/Construction/Terminators.cs
@@ -0,0 +1,208 @@
+﻿// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                        Copyright (c) 2018-2025 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: Terminators.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+using ILGPU.Util;
+using ILGPUC.IR.MethodValues;
+using ILGPUC.IR.PureValues;
+using ILGPUC.IR.Rewriting;
+using System;
+
+namespace ILGPUC.IR.BasicBlockValues.Construction;
+
+partial class BasicBlockBuilder
+{
+    /// <summary>
+    /// Builder for switch termination.
+    /// </summary>
+    /// <remarks>
+    /// Creates a new switch termination builder.
+    /// </remarks>
+    internal struct SwitchBuilder(
+        BasicBlockBuilder builder,
+        Value condition,
+        int capacity)
+    {
+        private BasicBlock? _defaultTarget = null;
+        private InlineList<BasicBlock> _caseTargets =
+            InlineList<BasicBlock>.Create(capacity);
+
+        /// <summary>
+        /// Gets the switch condition.
+        /// </summary>
+        public readonly Value Condition => condition;
+
+        /// <summary>
+        /// Adds the default target.
+        /// </summary>
+        public void AddDefault(BasicBlock target) => _defaultTarget = target;
+
+        /// <summary>
+        /// Adds a case target.
+        /// </summary>
+        public void AddCase(BasicBlock target) => _caseTargets.Add(target);
+
+        /// <summary>
+        /// Seals the switch termination.
+        /// </summary>
+        public readonly void Seal() =>
+            builder.CreateSwitchTermination(
+                condition,
+                _defaultTarget,
+                _caseTargets.AsReadOnlySpan());
+    }
+
+    /// <summary>
+    /// Sets up return termination for this block.
+    /// </summary>
+    /// <param name="returnValue">The return value (or null for void returns).</param>
+    public BasicBlock CreateReturnTermination(Value? returnValue = null)
+    {
+        returnValue ??= ModuleBuilder.UndefinedValue;
+
+        SetTermination(BlockTerminationKind.Return, value: returnValue);
+        return BasicBlock;
+    }
+
+    /// <summary>
+    /// Sets up unconditional termination for this block.
+    /// </summary>
+    /// <param name="target">The target block.</param>
+    /// <returns>True if termination was set, false if target was null.</returns>
+    public BasicBlock CreateUnconditionalTermination(BasicBlock? target)
+    {
+        if (target is null)
+            return BasicBlock;
+
+        SetTermination(
+            BlockTerminationKind.Unconditional,
+            value: null,
+            successorCapacity: 1)
+            .AddSuccessor(target);
+
+        return BasicBlock;
+    }
+
+    /// <summary>
+    /// Sets up conditional (if-branch) termination for this block using no specific
+    /// flags.
+    /// </summary>
+    /// <param name="condition">The branch condition.</param>
+    /// <param name="trueTarget">The true target block.</param>
+    /// <param name="falseTarget">The false target block.</param>
+    /// <returns>
+    /// True if conditional termination was set, false if folded to unconditional.
+    /// </returns>
+    public BasicBlock CreateConditionalTermination(
+        Value? condition,
+        BasicBlock? trueTarget,
+        BasicBlock? falseTarget)
+    {
+        if (condition is null || trueTarget is null)
+            return BasicBlock;
+
+        // Simplify unnecessary if branches and fold them to unconditional branches
+        if (trueTarget == falseTarget || falseTarget is null)
+            return CreateUnconditionalTermination(trueTarget);
+
+        if (condition is PrimitiveValue primitiveValue)
+        {
+            bool isTrue = primitiveValue.Int1Value;
+            return CreateUnconditionalTermination(isTrue ? trueTarget : falseTarget);
+        }
+
+        // Create conditional termination
+        SetTermination(
+            BlockTerminationKind.Conditional,
+            condition,
+            successorCapacity: 2)
+            .AddSuccessor(trueTarget)
+            .AddSuccessor(falseTarget);
+        return BasicBlock;
+    }
+
+    /// <summary>
+    /// Creates a switch termination builder.
+    /// </summary>
+    /// <param name="value">The selection value.</param>
+    /// <param name="capacity">The expected number of cases to append.</param>
+    /// <returns>The created switch builder.</returns>
+    public SwitchBuilder CreateSwitchTermination(Value value, int capacity = 2) =>
+        new(this, value, capacity);
+
+    /// <summary>
+    /// Sets up switch termination for this block.
+    /// </summary>
+    /// <param name="value">The switch value.</param>
+    /// <param name="defaultTarget">The default case target.</param>
+    /// <param name="caseTargets">The case targets.</param>
+    /// <returns>
+    /// True if switch termination was set, false if folded to simpler form.
+    /// </returns>
+    public BasicBlock CreateSwitchTermination(
+        Value? value,
+        BasicBlock? defaultTarget,
+        ReadOnlySpan<BasicBlock> caseTargets)
+    {
+        if (defaultTarget is null)
+            return BasicBlock;
+        if (value is null)
+            return CreateUnconditionalTermination(defaultTarget);
+
+        value = CreateConvert(
+            value.Location,
+            value,
+            ModuleBuilder.GetPrimitiveType(BasicValueType.Int32)).AsNotNull();
+
+        if (value is PrimitiveValue primitiveValue)
+        {
+            int caseValue = primitiveValue.Int32Value;
+            var target = caseValue < 0 || caseValue >= caseTargets.Length
+                ? defaultTarget
+                : caseTargets[caseValue];
+            return CreateUnconditionalTermination(target);
+        }
+
+        // Two targets can be simplified to conditional
+        if (caseTargets.Length == 1)
+        {
+            return CreateConditionalTermination(
+                CreateCompare(
+                    value.Location,
+                    value,
+                    CreatePrimitiveValue(value.Location, 0),
+                    CompareKind.Equal),
+                defaultTarget,
+                caseTargets[0]);
+        }
+
+        // Full switch termination
+        var builder = SetTermination(
+            BlockTerminationKind.Switch,
+            value: value,
+            successorCapacity: 1 + caseTargets.Length);
+        builder.AddSuccessor(defaultTarget.AsNotNull());
+        builder.AddSuccessors(caseTargets);
+
+        return BasicBlock;
+    }
+
+    /// <summary>
+    /// Sets up pending termination for this block (used during building).
+    /// </summary>
+    /// <param name="targets">The pending target blocks.</param>
+    public void CreatePendingTermination(ReadOnlySpan<BasicBlock> targets)
+    {
+        var builder = SetTermination(
+            BlockTerminationKind.Pending,
+            successorCapacity: targets.Length);
+        builder.AddSuccessors(targets);
+    }
+}
