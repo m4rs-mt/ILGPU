c508ef452212010db12bd52ff9ed91e454f6da2f
Marcel Koester <m4rs@m4rs.net>
2025-09-28 17:00:29 +0100

    Refined IR analysis-value framework and CFG analysis.

--- Src/ILGPUC/IR/Analyses/CFG.cs
+++ Src/ILGPUC/IR/Analyses/CFG.cs
@@ -9,338 +9,159 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPUC.IR.Analyses.ControlFlowDirection;
-using ILGPUC.IR.Analyses.TraversalOrders;
+using ILGPU.Util;
+using ILGPUC.IR.MethodValues;
+using ILGPUC.IR.ModuleValues;
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 namespace ILGPUC.IR.Analyses;
-
-/// <summary>
-/// Represents an abstract interface for all CFG nodes.
-/// </summary>
-interface ICFGNode
-{
-    /// <summary>
-    /// Returns the associated function value.
-    /// </summary>
-    BasicBlock Block { get; }
-
-    /// <summary>
-    /// Returns the zero-based node index that can be used
-    /// for fast lookups using arrays.
-    /// </summary>
-    int TraversalIndex { get; }
-}
 
 /// <summary>
 /// Represents a control-flow graph (CFG).
 /// </summary>
 /// <typeparam name="TOrder">The underlying block order.</typeparam>
 /// <typeparam name="TDirection">The control-flow direction.</typeparam>
-[SuppressMessage(
-    "Microsoft.Naming",
-    "CA1710: IdentifiersShouldHaveCorrectSuffix",
-    Justification = "This is the correct name of the current entity")]
-sealed class CFG<TOrder, TDirection> :
-    IReadOnlyCollection<CFG<TOrder, TDirection>.Node>
-    where TOrder : struct, ITraversalOrder
+sealed class CFG<TOrder, TDirection>
+    where TOrder : struct, ITraversalOrder<BasicBlock>
     where TDirection : struct, IControlFlowDirection
 {
-    #region Nested Types
+    #region Nested Types and Helpers
 
     /// <summary>
     /// Represents a single node in the scope of a control-flow graph.
     /// </summary>
-    internal readonly struct Node : ICFGNode, ILocation
+    /// <remarks>
+    /// Constructs a new node.
+    /// </remarks>
+    /// <param name="cfg">The parent graph.</param>
+    /// <param name="block">The associated block.</param>
+    /// <param name="traversalIndex">The traversal index.</param>
+    internal readonly struct Node(
+        CFG<TOrder, TDirection> cfg,
+        BasicBlock block,
+        int traversalIndex) : ILocation
     {
-        #region Nested Types
-
-        /// <summary>
-        /// Enumerates all CFG nodes.
-        /// </summary>
-        internal ref struct Enumerator
-        {
-            private ReadOnlySpan<BasicBlock>.Enumerator nestedEnumerator;
-
-            internal Enumerator(
-                CFG<TOrder, TDirection> cfg,
-                ReadOnlySpan<BasicBlock>.Enumerator enumerator)
-            {
-                nestedEnumerator = enumerator;
-                CFG = cfg;
-            }
-
-            /// <summary>
-            /// Returns the parent graph.
-            /// </summary>
-            public CFG<TOrder, TDirection> CFG { get; }
-
-            /// <summary>
-            /// Returns the current CFG node.
-            /// </summary>
-            public Node Current => CFG[nestedEnumerator.Current];
-
-            /// <summary>
-            /// Moves the enumerator to the next node.
-            /// </summary>
-            /// <returns>True, if the enumerator could be moved.</returns>
-            public bool MoveNext() => nestedEnumerator.MoveNext();
-        }
-
-        /// <summary>
-        /// Represents a node collection of attached nodes.
-        /// </summary>
-        internal readonly ref struct NodeCollection
-        {
-            #region Instance
-
-            private readonly ReadOnlySpan<BasicBlock> links;
-
-            internal NodeCollection(
-                CFG<TOrder, TDirection> cfg,
-                ReadOnlySpan<BasicBlock> collection)
-            {
-                CFG = cfg;
-                links = collection;
-            }
-
-            #endregion
-
-            #region Properties
-
-            /// <summary>
-            /// Returns the parent graph.
-            /// </summary>
-            public CFG<TOrder, TDirection> CFG { get; }
-
-            /// <summary>
-            /// Returns the number of nodes.
-            /// </summary>
-            public int Count => links.Length;
-
-            /// <summary>
-            /// Returns the i-th node.
-            /// </summary>
-            /// <param name="index">The relative node index.</param>
-            /// <returns>The resolved node.</returns>
-            public Node this[int index] => CFG[links[index]];
-
-            #endregion
-
-            #region IEnumerable
-
-            /// <summary>
-            /// Returns a node enumerator to iterate over all attached nodes.
-            /// </summary>
-            /// <returns>The resulting node enumerator.</returns>
-            public Enumerator GetEnumerator() =>
-                new Enumerator(CFG, links.GetEnumerator());
-
-            #endregion
-        }
-
-        #endregion
-
-        #region Instance
-
-        /// <summary>
-        /// Constructs a new node.
-        /// </summary>
-        /// <param name="cfg">The parent graph.</param>
-        /// <param name="block">The associated block.</param>
-        /// <param name="traversalIndex">The traversal index.</param>
-        internal Node(
-            CFG<TOrder, TDirection> cfg,
-            BasicBlock block,
-            int traversalIndex)
-        {
-            CFG = cfg;
-            Block = block;
-            TraversalIndex = traversalIndex;
-        }
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Returns the parent graph.
-        /// </summary>
-        public CFG<TOrder, TDirection> CFG { get; }
-
         /// <summary>
         /// Returns the associated block.
         /// </summary>
-        public BasicBlock Block { get; }
+        public BasicBlock Block { get; } = block;
 
         /// <summary>
         /// Returns the zero-based traversal index that has been assigned during
         /// traversal of all input blocks.
         /// </summary>
-        public int TraversalIndex { get; }
+        public int TraversalIndex { get; } = traversalIndex;
 
         /// <summary>
         /// Returns the predecessors of this node.
         /// </summary>
-        public NodeCollection Predecessors => new NodeCollection(
-            CFG,
-            GetPredecessors());
+        public NodeCollection Predecessors =>
+            new(cfg, Block.GetPredecessors<TDirection>());
 
         /// <summary>
         /// Returns the successors of this node.
         /// </summary>
-        public NodeCollection Successors => new NodeCollection(
-            CFG,
-            GetSuccessors());
+        public NodeCollection Successors =>
+            new(cfg, Block.GetSuccessors<TDirection>());
 
         /// <summary>
         /// Returns the number of predecessors.
         /// </summary>
-        public int NumPredecessors => GetPredecessors().Length;
+        public int NumPredecessors => Predecessors.Count;
 
         /// <summary>
         /// Returns the number of successors.
         /// </summary>
-        public int NumSuccessors => GetSuccessors().Length;
+        public int NumSuccessors => Successors.Count;
 
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Formats an error message to include specific exception information.
-        /// </summary>
-        /// <param name="message">The source error message.</param>
-        /// <returns>The formatted error message.</returns>
-        public string FormatErrorMessage(string message) =>
+        /// <inheritdoc cref="ILocation.FormatErrorMessage(string)"/>
+        string ILocation.FormatErrorMessage(string message) =>
             Block.FormatErrorMessage(message);
 
-        /// <summary>
-        /// Determines the actual predecessors based on the current direction.
-        /// </summary>
-        private ReadOnlySpan<BasicBlock> GetPredecessors() =>
-            Block.GetPredecessors<TDirection>();
+        public static implicit operator BasicBlock(Node node) => node.Block;
+    }
+
+    /// <summary>
+    /// Represents a node collection of attached nodes.
+    /// </summary>
+    /// <param name="cfg">The parent graph.</param>
+    /// <param name="links">The block links.</param>
+    internal readonly ref struct NodeCollection(
+        CFG<TOrder, TDirection> cfg,
+        ReadOnlySpan<BasicBlock> links)
+    {
+        private readonly ReadOnlySpan<BasicBlock> _links = links;
 
         /// <summary>
-        /// Determines the actual successors based on the current direction.
+        /// Returns the number of nodes.
         /// </summary>
-        private ReadOnlySpan<BasicBlock> GetSuccessors() =>
-            Block.GetSuccessors<TDirection>();
-
-        #endregion
-
-        #region Object
+        public int Count => _links.Length;
 
         /// <summary>
-        /// Returns the string representation of this CFG node.
+        /// Returns the i-th node.
         /// </summary>
-        /// <returns>The string representation of this CFG node.</returns>
-        public override string ToString() => Block.ToString();
-
-        #endregion
-
-        #region Operators
+        /// <param name="index">The relative node index.</param>
+        /// <returns>The resolved node.</returns>
+        public Node this[int index] =>
+            new(cfg, _links[index], cfg._numbering[_links[index]]);
 
         /// <summary>
-        /// Converts the given node implicitly to its underlying basic block.
+        /// Returns an enumerator to iterate over all attached nodes.
         /// </summary>
-        /// <param name="node">The node to convert.</param>
-        public static implicit operator BasicBlock(Node node) =>
-            node.Block;
-
-        #endregion
+        /// <returns>The resulting node enumerator.</returns>
+        public Enumerator GetEnumerator() => new(cfg, _links);
     }
 
     /// <summary>
     /// Enumerates all CFG nodes.
     /// </summary>
-    internal struct Enumerator<TNestedEnumerator> : IEnumerator<Node>
-        where TNestedEnumerator : IEnumerator<BasicBlock>
+    /// <param name="cfg">The parent graph.</param>
+    /// <param name="links">The node links.</param>
+    internal ref struct Enumerator(
+        CFG<TOrder, TDirection> cfg,
+        ReadOnlySpan<BasicBlock> links)
     {
-        [SuppressMessage(
-            "Style",
-            "IDE0044:Add readonly modifier",
-            Justification = "This instance variable will be modified")]
-        private TNestedEnumerator nestedEnumerator;
-
-        internal Enumerator(
-            CFG<TOrder, TDirection> cfg,
-            TNestedEnumerator enumerator)
-        {
-            nestedEnumerator = enumerator;
-            CFG = cfg;
-        }
-
-        /// <summary>
-        /// Returns the parent graph.
-        /// </summary>
-        public CFG<TOrder, TDirection> CFG { get; }
+        private ReadOnlySpan<BasicBlock>.Enumerator _enumerator = links.GetEnumerator();
 
         /// <summary>
         /// Returns the current CFG node.
         /// </summary>
-        public Node Current => CFG[nestedEnumerator.Current];
-
-        /// <summary cref="IEnumerator.Current"/>
-        object IEnumerator.Current => Current;
-
-        /// <summary cref="IDisposable.Dispose"/>
-        void IDisposable.Dispose() { }
+        public Node Current => new(
+            cfg,
+            _enumerator.Current,
+            cfg._numbering[_enumerator.Current]);
 
         /// <summary cref="IEnumerator.MoveNext"/>
-        public bool MoveNext() => nestedEnumerator.MoveNext();
-
-        /// <summary cref="IEnumerator.Reset"/>
-        void IEnumerator.Reset() => throw new InvalidOperationException();
+        public bool MoveNext() => _enumerator.MoveNext();
     }
-
-    /// <summary>
-    /// Provides traversal indices.
-    /// </summary>
-    private readonly struct TraversalIndexProvider :
-        IBasicBlockMapValueProvider<int>
-    {
-        /// <summary>
-        /// Returns the current traversal index.
-        /// </summary>
-        public readonly int GetValue(BasicBlock block, int traversalIndex) =>
-            traversalIndex;
-    }
-
-    #endregion
-
-    #region Static
 
     /// <summary>
     /// Creates a new CFG.
     /// </summary>
     /// <param name="blocks">The block collection.</param>
     public static CFG<TOrder, TDirection> Create(
-        in BasicBlockCollection<TOrder, TDirection> blocks) =>
-        new CFG<TOrder, TDirection>(blocks);
+        in BasicBlockCollection<TOrder, TDirection> blocks) => new(blocks);
 
     #endregion
 
-    #region Instance
+    #region CFG
 
-    private readonly BasicBlockMap<int> numbering;
+    private readonly ValueMap<Method, BasicBlock, int> _numbering;
+    private InlineList<BasicBlock> _blocks;
 
     /// <summary>
     /// Constructs a new CFG.
     /// </summary>
     /// <param name="blocks">The block collection.</param>
-    private CFG(in BasicBlockCollection<TOrder, TDirection> blocks)
+    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+    internal CFG(in BasicBlockCollection<TOrder, TDirection> blocks)
     {
-        numbering = blocks.CreateMap(new TraversalIndexProvider());
+        _numbering = blocks.CreateMap((_, index) => index);
+        _blocks = blocks.ToList();
         Blocks = blocks;
     }
-
-    #endregion
-
-    #region Properties
 
     /// <summary>
     /// Returns the underlying blocks.
@@ -348,48 +169,33 @@ sealed class CFG<TOrder, TDirection> :
     public BasicBlockCollection<TOrder, TDirection> Blocks { get; }
 
     /// <summary>
+    /// Returns the underlying blocks as materialized span.
+    /// </summary>
+    public ReadOnlySpan<BasicBlock> BlockSpan => _blocks;
+
+    /// <summary>
     /// Returns the number of nodes in the graph.
     /// </summary>
-    public int Count => numbering.Count;
+    public int Count => _numbering.Count;
 
     /// <summary>
     /// Returns the root node.
     /// </summary>
-    public Node Root => new Node(
-        this,
-        Blocks.EntryBlock,
-        numbering[Blocks.EntryBlock]);
+    public Node Root => new(this, Blocks.EntryBlock, _numbering[Blocks.EntryBlock]);
 
     /// <summary>
     /// Resolves the CFG node for the given basic block.
     /// </summary>
     /// <param name="block">The basic block to resolve.</param>
     /// <returns>The resolved basic block.</returns>
-    public Node this[BasicBlock block] => new Node(
-        this,
-        block,
-        numbering[block]);
-
-    #endregion
-
-    #region IEnumerable
+    public Node this[BasicBlock block] => new(this, block, _numbering[block]);
 
     /// <summary>
-    /// Returns a node enumerator to iterate over all nodes stored in this graph
+    /// Returns an enumerator to iterate over all nodes stored in this graph
     /// using the current order.
     /// </summary>
     /// <returns>The resulting node enumerator.</returns>
-    public Enumerator<BasicBlockCollection<TOrder, TDirection>.Enumerator>
-        GetEnumerator() =>
-        new Enumerator<BasicBlockCollection<TOrder, TDirection>.Enumerator>(
-            this,
-            Blocks.GetEnumerator());
-
-    /// <summary cref="IEnumerable{T}.GetEnumerator"/>
-    IEnumerator<Node> IEnumerable<Node>.GetEnumerator() => GetEnumerator();
-
-    /// <summary cref="IEnumerable.GetEnumerator"/>
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    public Enumerator GetEnumerator() => new(this, _blocks);
 
     #endregion
 }
@@ -408,7 +214,22 @@ static class CFG
     /// <returns>The created CFG.</returns>
     public static CFG<TOrder, TDirection> CreateCFG<TOrder, TDirection>(
         this BasicBlockCollection<TOrder, TDirection> blocks)
-        where TOrder : struct, ITraversalOrder
+        where TOrder : struct, ITraversalOrder<BasicBlock>
         where TDirection : struct, IControlFlowDirection =>
         CFG<TOrder, TDirection>.Create(blocks);
+
+    /// <summary>
+    /// Creates a new CFG based on the given module.
+    /// </summary>
+    /// <typeparam name="TOrder">The underlying block order.</typeparam>
+    /// <typeparam name="TDirection">The control-flow direction.</typeparam>
+    /// <param name="module">The source module.</param>
+    /// <returns>The created CFGs.</returns>
+    public static ValueMap<Module, Method, CFG<TOrder, TDirection>> CreateCFGs<
+        TOrder, TDirection>(this Module module)
+        where TOrder : struct, ITraversalOrder<BasicBlock>
+        where TDirection : struct, IControlFlowDirection =>
+        module.CreateMap<Method, CFG<TOrder, TDirection>>(method =>
+            CFG<TOrder, TDirection>.Create(
+                method.Blocks.ChangeOrder<TOrder, TDirection>()));
 }
