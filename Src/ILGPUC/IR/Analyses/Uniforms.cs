07454c3a84b3eb40e4cc0fc5cf00aebc64242306
Marcel Koester <m4rs@m4rs.net>
2025-10-01 19:20:11 +0100

    Refined IR uniform-value analysis.

--- Src/ILGPUC/IR/Analyses/Uniforms.cs
+++ Src/ILGPUC/IR/Analyses/Uniforms.cs
@@ -9,173 +9,97 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPUC.IR.Analyses.ControlFlowDirection;
-using ILGPUC.IR.Types;
-using ILGPUC.IR.Values;
+using ILGPUC.IR.BasicBlockValues;
+using ILGPUC.IR.MethodValues;
+using ILGPUC.IR.ModuleValues;
+using ILGPUC.IR.PureValues;
 using System;
+using System.Runtime.CompilerServices;
 
 namespace ILGPUC.IR.Analyses;
 
 /// <summary>
+/// The state of a value.
+/// </summary>
+enum UniformKind
+{
+    /// <summary>
+    /// No or insufficient information is available for the value attached.
+    /// </summary>
+    Unknown = 0,
+
+    /// <summary>
+    /// The associated value can be considered uniform.
+    /// </summary>
+    Uniform = 1,
+
+    /// <summary>
+    /// The associated value has to be considered divergent.
+    /// </summary>
+    Divergent = 2,
+}
+
+/// <summary>
+/// Information carried per value.
+/// </summary>
+/// <remarks>
+/// Constructs a new value information instance.
+/// </remarks>
+/// <param name="Kind">The associated value kind.</param>
+readonly record struct UniformValueInfo(UniformKind Kind)
+{
+    /// <summary>
+    /// Returns the string representation of this instance.
+    /// </summary>
+    /// <returns>The string representation of this instance.</returns>
+    public override string ToString() => Kind.ToString();
+
+    /// <summary>
+    /// Converts a value of type <see cref="UniformKind"/> into a
+    /// <see cref="UniformValueInfo"/> instance.
+    /// </summary>
+    /// <param name="kind">The kind to convert to.</param>
+    /// <returns>The created value information instance.</returns>
+    public static implicit operator UniformValueInfo(UniformKind kind) => new(kind);
+}
+
+/// <summary>
 /// An analysis to determine whether values and terminators can be considered uniform.
 /// </summary>
-sealed class Uniforms : GlobalFixPointAnalysis<Uniforms.ValueInfo, Forwards>
+/// <remarks>
+/// Constructs a new analysis implementation.
+/// </remarks>
+sealed class Uniforms() :
+    FixPointAnalysis<UniformValueInfo, Forwards>(defaultValue: UniformKind.Unknown)
 {
     #region Nested Types
 
     /// <summary>
-    /// The state of a value.
-    /// </summary>
-    internal enum UniformKind
-    {
-        /// <summary>
-        /// No or insufficient information is available for the value attached.
-        /// </summary>
-        Unknown = 0,
-
-        /// <summary>
-        /// The associated value can be considered uniform.
-        /// </summary>
-        Uniform = 1,
-
-        /// <summary>
-        /// The associated value has to be considered divergent.
-        /// </summary>
-        Divergent = 2,
-    }
-
-    /// <summary>
-    /// Information carried per value.
-    /// </summary>
-    internal readonly struct ValueInfo : IEquatable<ValueInfo>
-    {
-        #region Instance
-
-        /// <summary>
-        /// Constructs a new value information instance.
-        /// </summary>
-        /// <param name="kind">The associated value kind.</param>
-        public ValueInfo(UniformKind kind)
-        {
-            Kind = kind;
-        }
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Returns the current kind of the current uniform information instance.
-        /// </summary>
-        public UniformKind Kind { get; }
-
-        #endregion
-
-        #region IEquatable
-
-        /// <inheritdoc cref="IEquatable{T}.Equals(T)"/>>
-        public bool Equals(ValueInfo other) =>
-            Kind == other.Kind;
-
-        #endregion
-
-        #region Object
-
-        /// <summary>
-        /// Returns true if the given object is equal to the current instance.
-        /// </summary>
-        /// <param name="obj">The other object.</param>
-        /// <returns>
-        /// True, if the given object is equal to the current instance.
-        /// </returns>
-        public override bool Equals(object? obj) =>
-            obj is ValueInfo other && Equals(other);
-
-        /// <summary>
-        /// Returns the hash code of this instance.
-        /// </summary>
-        /// <returns>The hash code of this instance.</returns>
-        public override int GetHashCode() => (int)Kind;
-
-        /// <summary>
-        /// Returns the string representation of this instance.
-        /// </summary>
-        /// <returns>The string representation of this instance.</returns>
-        public override string ToString() => Kind.ToString();
-
-        #endregion
-
-        #region Operators
-
-        /// <summary>
-        /// Converts a value of type <see cref="UniformKind"/> into a
-        /// <see cref="ValueInfo"/> instance.
-        /// </summary>
-        /// <param name="kind">The kind to convert to.</param>
-        /// <returns>The created value information instance.</returns>
-        public static implicit operator ValueInfo(UniformKind kind) =>
-            new ValueInfo(kind);
-
-        /// <summary>
-        /// Returns true if the first and second information instances are the same.
-        /// </summary>
-        /// <param name="first">The first instance.</param>
-        /// <param name="second">The second instance.</param>
-        /// <returns>True, if the first and second instances are the same.</returns>
-        public static bool operator ==(ValueInfo first, ValueInfo second) =>
-            first.Equals(second);
-
-        /// <summary>
-        /// Returns true if the first and second information instances are not the
-        /// same.
-        /// </summary>
-        /// <param name="first">The first instance.</param>
-        /// <param name="second">The second instance.</param>
-        /// <returns>
-        /// True, if the first and second instances are not the same.
-        /// </returns>
-        public static bool operator !=(ValueInfo first, ValueInfo second) =>
-            !first.Equals(second);
-
-        #endregion
-    }
-
-    /// <summary>
     /// Stores information of a uniform analysis run.
     /// </summary>
-    internal readonly struct Info(GlobalAnalysisValueResult<ValueInfo> analysisResult)
+    /// <param name="result">The result mapping.</param>
+    /// <remarks>
+    /// Constructs a new uniform analysis result.
+    /// </remarks>
+    internal readonly struct Info(FixPointAnalysisResult<UniformValueInfo>? result)
     {
-        #region Static
-
         /// <summary>
         /// Empty allocation information.
         /// </summary>
-        public static readonly Info Empty =
-            new(GlobalAnalysisValueResult<ValueInfo>.Empty);
-
-        #endregion
-
-        #region Properties
+        public static readonly Info Empty = new(null);
 
         /// <summary>
-        /// Stores a method value-alignment mapping.
+        /// Returns uniform information for the given value.
         /// </summary>
-        public GlobalAnalysisValueResult<ValueInfo> AnalysisResult { get; } =
-            analysisResult;
-
-        /// <summary>
-        /// Returns pointer alignment information for the given value.
-        /// </summary>
-        /// <param name="value">The value to get alignment information for.</param>
-        /// <returns>Pointer alignment in bytes (can be 1 byte).</returns>
+        /// <param name="value">The value to get uniform information for.</param>
+        /// <returns>The uniform kind (can be Unknown).</returns>
         public UniformKind this[Value value] =>
-            AnalysisResult.TryGetData(value, out var data)
-                ? data.Data.Kind
-                : UniformKind.Unknown;
+            result?.GetResult(value).Data.Kind ?? UniformKind.Unknown;
 
-        #endregion
-
-        #region Methods
+        /// <summary>
+        /// Returns true if this uniform information object is empty.
+        /// </summary>
+        public bool IsEmpty => !result.HasValue;
 
         /// <summary>
         /// Returns true if the given value can be considered to be uniformly
@@ -186,44 +110,27 @@ sealed class Uniforms : GlobalFixPointAnalysis<Uniforms.ValueInfo, Forwards>
         /// <param name="value">The value to test.</param>
         /// <returns>True, if the given value can be considered uniform.</returns>
         public bool IsUniform(Value value) => this[value] == UniformKind.Uniform;
-
-        #endregion
     }
 
     #endregion
 
-    #region Static
+    #region Analysis
 
     /// <summary>
     /// Creates a new uniforms analysis.
     /// </summary>
-    public static Uniforms Create(Method entryPoint) => new Uniforms();
+    public static Uniforms Create() => new();
 
     /// <summary>
-    /// Applies a new alignment analysis to the given root method.
+    /// Applies a new uniform analysis to the given root method.
     /// </summary>
     /// <param name="entryPoint">The root (entry) method.</param>
     public static Info Apply(Method entryPoint)
     {
-        var analysis = Create(entryPoint);
-        var result = analysis.AnalyzeGlobalMethod(entryPoint, UniformKind.Uniform);
+        var analysis = Create();
+        var result = analysis.AnalyzeModule(entryPoint);
         return new Info(result);
     }
-
-    #endregion
-
-    #region Instance
-
-    /// <summary>
-    /// Constructs a new analysis implementation.
-    /// </summary>
-    public Uniforms()
-        : base(defaultValue: UniformKind.Unknown)
-    { }
-
-    #endregion
-
-    #region Methods
 
     /// <summary>
     /// Returns initial and unconstrained information about whether the given value
@@ -231,61 +138,67 @@ sealed class Uniforms : GlobalFixPointAnalysis<Uniforms.ValueInfo, Forwards>
     /// </summary>
     /// <param name="node">The IR node.</param>
     /// <returns>The uniform state of the given node.</returns>
-    private static ValueInfo IsUniformByDefinition(Value node) =>
+    private static UniformValueInfo GetInitialUniformKind(Value node) =>
         node switch
         {
             // Thread-dependent values are divergent by definition
-            LaneIdxValue _ => UniformKind.Divergent,
+            SubGroupLaneIndexValue _ => UniformKind.Divergent,
+            SubGroupIndexValue _ => UniformKind.Divergent,
             GroupIndexValue _ => UniformKind.Divergent,
             // (Device-wide) constants are uniform by definition
             GridIndexValue _ => UniformKind.Uniform,
-            ConstantNode _ => UniformKind.Uniform,
-            UndefinedValue _ => UniformKind.Uniform,
+            PrimitiveValue => UniformKind.Uniform,
+            NullValue => UniformKind.Uniform,
+            UndefinedValue => UniformKind.Uniform,
             // Method calls can be considered uniform since each thread will perform
             // the same call (since we do not support virtual and jump-table based
             // calls at the moment)
             MethodCall _ => UniformKind.Uniform,
-            // Unconditional branches can be considered uniform
-            UnconditionalBranch _ => UniformKind.Uniform,
-            // Conditional branches are considered to be unknown
-            ConditionalBranch _ => UniformKind.Unknown,
-            // All return terminators must be assumed to be divergent
-            ReturnTerminator _ => UniformKind.Divergent,
+            // Phi values need special handling
+            PhiValue => UniformKind.Unknown,
             // All remaining values have an unknown state
             _ => UniformKind.Unknown
         };
 
     /// <summary>
-    /// Creates initial analysis data using <see cref="IsUniformByDefinition"/>.
-    /// </summary>
-    protected override AnalysisValue<ValueInfo> CreateData(Value node) =>
-        CreateValue(IsUniformByDefinition(node), node.Type);
-
-    /// <summary>
     /// Returns the maximum of the first and the second kind.
     /// </summary>
-    protected override ValueInfo Merge(ValueInfo first, ValueInfo second) =>
+    protected override UniformValueInfo Merge(
+        UniformValueInfo first,
+        UniformValueInfo second) =>
         (UniformKind)Math.Max((int)first.Kind, (int)second.Kind);
 
     /// <summary>
-    /// Returns no type-based information.
+    /// Analyzes a value to compute its uniform information.
     /// </summary>
-    protected override AnalysisValue<ValueInfo>? TryProvide(TypeNode typeNode) =>
-        null;
-
-    /// <summary>
-    /// Merges information about terminators.
-    /// </summary>
-    protected override AnalysisValue<ValueInfo>? TryMerge<TContext>(
+    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+    protected override AnalysisValue<UniformValueInfo> Analyze(
         Value value,
-        TContext context) =>
-        value switch
+        GlobalValueMap<AnalysisValue<UniformValueInfo>> data)
+    {
+        // Get current value or create initial one
+        var current = data.TryGetValue(value, out var existing)
+            ? existing
+            : CreateInitialValue(value.Type);
+
+        // Handle special cases that need custom analysis
+        var specialized = value switch
         {
-            // Conditional branches depend on their condition value
-            ConditionalBranch condBranch => context[condBranch.Condition],
-            // Use the default merge logic for all remaining values
-            _ => null
+            // Phi values: merge all incoming values
+            PhiValue phiValue => MergePhiValue(phiValue, data),
+
+            // For method calls, use the method return value
+            MethodCall call when !call.Target.IsVoid =>
+                data.TryGetValue(call.Target, out var retValue)
+                    ? retValue
+                    : current,
+
+            // Default: use initial uniform kind
+            _ => AnalysisValue.Create(GetInitialUniformKind(value), value.Type)
         };
+
+        return specialized;
+    }
 
     #endregion
 }
