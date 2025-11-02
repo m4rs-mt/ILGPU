1691a01919b124296a07a8c8a3e4e0ea66b7d035
Marcel Koester <m4rs@m4rs.net>
2025-11-02 20:13:28 +0000

    Added IR ValueClass, ValueContext, and ValueId base types (part 1/2).

---
+++ Src/ILGPUC/IR/ValueContext.cs
@@ -0,0 +1,31 @@
+// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                           Copyright (c) 2025 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: ValueContext.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+using System;
+
+namespace ILGPUC.IR;
+
+interface IValueContext
+{
+    bool TryGetReplaced(Value? oldValue, out Value? newValue);
+
+    ReadOnlyMemory<Value> GetValues(Value value);
+
+    int GetNumUses(Value value);
+
+    UseCollection GetUses(Value value);
+}
+
+// abstract class ValueContext : IValueContext
+// {
+//     public abstract bool TryGetReplaced(Value? oldValue, out Value? newValue);
+// }
+
