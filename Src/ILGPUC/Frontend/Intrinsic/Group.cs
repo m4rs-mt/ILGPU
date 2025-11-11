cf080c7946a5195323ffa6bf0095bc2008fe8c20
Marcel Koester <m4rs@m4rs.net>
2025-11-11 21:46:28 +0000

    Refined frontend thread-hierarchy intrinsics (Accelerator, Grid, Group, Warp) (part 2/2).

--- Src/ILGPUC/Frontend/Intrinsic/Group.cs
+++ Src/ILGPUC/Frontend/Intrinsic/Group.cs
@@ -11,7 +11,8 @@
 
 using ILGPU;
 using ILGPUC.IR;
-using ILGPUC.IR.Values;
+using ILGPUC.IR.BasicBlockValues;
+using ILGPUC.IR.PureValues;
 using System.Linq;
 
 namespace ILGPUC.Frontend.Intrinsic;
@@ -23,7 +24,7 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_Barrier(ref InvocationContext context) =>
+    private static Value? Group_Barrier(ref InvocationContext context) =>
         context.Builder.CreateBarrier(context.Location, BarrierKind.GroupLevel);
 
     /// <summary>
@@ -31,7 +32,7 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_BarrierPopCount(ref InvocationContext context) =>
+    private static Value? Group_BarrierPopCount(ref InvocationContext context) =>
         context.Builder.CreateBarrier(
             context.Location,
             PredicateBarrierKind.GroupLevel,
@@ -43,7 +44,7 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_BarrierAnd(ref InvocationContext context) =>
+    private static Value? Group_BarrierAnd(ref InvocationContext context) =>
         context.Builder.CreateBarrier(
             context.Location,
             PredicateBarrierKind.GroupLevel,
@@ -55,7 +56,7 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_BarrierOr(ref InvocationContext context) =>
+    private static Value? Group_BarrierOr(ref InvocationContext context) =>
         context.Builder.CreateBarrier(
             context.Location,
             PredicateBarrierKind.GroupLevel,
@@ -67,17 +68,17 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_Broadcast(ref InvocationContext context)
+    private static Value? Group_Broadcast(ref InvocationContext context)
     {
         var builder = context.Builder;
         var location = context.Location;
 
         var parameters = context.Method.GetParameters();
-        Value sourceLane = parameters[0].ParameterType == typeof(FirstLaneValue<>)
+        var sourceLane = parameters[0].ParameterType == typeof(FirstLaneValue<>)
             ? builder.CreatePrimitiveValue(location, 0)
             : builder.CreateArithmetic(
                 location,
-                builder.CreateGroupDimensionValue(location, DeviceConstantDimension3D.X),
+                builder.CreateGroupDimensionValue(location),
                 builder.CreatePrimitiveValue(location, 1),
                 BinaryArithmeticKind.Sub);
 
@@ -93,8 +94,7 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_BroadcastFromThread(
-        ref InvocationContext context) =>
+    private static Value? Group_BroadcastFromThread(ref InvocationContext context) =>
         context.Builder.CreateBroadcast(
             context.Location,
             context.Pull(),
@@ -106,27 +106,23 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_Dimension(ref InvocationContext context) =>
-        context.Builder.CreateGroupDimensionValue(
-            context.Location,
-            DeviceConstantDimension3D.X);
+    private static Value? Group_Dimension(ref InvocationContext context) =>
+        context.Builder.CreateGroupDimensionValue(context.Location);
 
     /// <summary>
     /// Handles group index operations.
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_Index(ref InvocationContext context) =>
-        context.Builder.CreateGroupIndexValue(
-            context.Location,
-            DeviceConstantDimension3D.X);
+    private static Value? Group_Index(ref InvocationContext context) =>
+        context.Builder.CreateGroupIndexValue(context.Location);
 
     /// <summary>
     /// Handles group memory fence operations.
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_MemoryFence(ref InvocationContext context) =>
+    private static Value? Group_MemoryFence(ref InvocationContext context) =>
         context.Builder.CreateMemoryBarrier(
             context.Location,
             MemoryBarrierKind.GroupLevel);
@@ -136,24 +132,24 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_GetLocalMemoryBuffer(
-        ref InvocationContext context)
+    private static Value? Group_GetLocalMemoryBuffer(ref InvocationContext context)
     {
-        var builder = context.Builder;
+        var builder = context.ModuleBuilder;
 
         // Create local allocation
         var allocaType = context.GetMethodGenericArguments().First();
-        var alloca = builder.CreateStaticAllocaArray(
+        var length = context.Pull();
+        var alloca = builder.CreateGlobal(
             context.Location,
-            context.Pull(),
             builder.CreateType(allocaType),
-            IR.MemoryAddressSpace.Local);
+            MemoryAddressSpace.Local,
+            length);
 
-        // Build view
-        return builder.CreateNewView(
+        // Build view using the same length
+        return context.Builder.CreateNewView(
             context.Location,
             alloca,
-            context.Pull());
+            length);
     }
 
     /// <summary>
@@ -161,18 +157,16 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_GetSharedMemoryElement(
-        ref InvocationContext context)
+    private static Value? Group_GetSharedMemoryElement(ref InvocationContext context)
     {
-        var builder = context.Builder;
+        var builder = context.ModuleBuilder;
 
         // Create shared allocation
         var allocaType = context.GetMethodGenericArguments().First();
-        var alloca = builder.CreateAlloca(
+        return builder.CreateGlobal(
             context.Location,
-            builder.CreateType(allocaType),
-            IR.MemoryAddressSpace.Shared);
-        return alloca;
+            context.ModuleBuilder.CreateType(allocaType),
+            MemoryAddressSpace.Shared);
     }
 
     /// <summary>
@@ -180,24 +174,24 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_GetSharedMemoryBuffer(
-        ref InvocationContext context)
+    private static Value? Group_GetSharedMemoryBuffer(ref InvocationContext context)
     {
-        var builder = context.Builder;
+        var builder = context.ModuleBuilder;
 
         // Create shared allocation
         var allocaType = context.GetMethodGenericArguments().First();
-        var alloca = builder.CreateStaticAllocaArray(
+        var length = context.Pull();
+        var alloca = builder.CreateGlobal(
             context.Location,
-            context.Pull(),
             builder.CreateType(allocaType),
-            IR.MemoryAddressSpace.Shared);
+            MemoryAddressSpace.Shared,
+            length);
 
-        // Build view
-        return builder.CreateNewView(
+        // Build view using the same length
+        return context.Builder.CreateNewView(
             context.Location,
             alloca,
-            context.Pull());
+            length);
     }
 
     /// <summary>
@@ -205,23 +199,21 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_GetSharedMemoryElementPerThread(
+    private static Value? Group_GetSharedMemoryElementPerThread(
         ref InvocationContext context)
     {
         var builder = context.Builder;
 
         // Create group dimension
-        var groupDimension = builder.CreateGroupDimensionValue(
-            context.Location,
-            DeviceConstantDimension3D.X);
+        var groupDimension = builder.CreateGroupDimensionValue(context.Location);
 
         // Create shared allocation
         var allocaType = context.GetMethodGenericArguments().First();
-        var alloca = builder.CreateStaticAllocaArray(
+        var alloca = context.ModuleBuilder.CreateGlobal(
             context.Location,
-            groupDimension,
-            builder.CreateType(allocaType),
-            IR.MemoryAddressSpace.Shared);
+            context.ModuleBuilder.CreateType(allocaType),
+            MemoryAddressSpace.Shared,
+            groupDimension);
 
         // Build view
         return builder.CreateNewView(
@@ -235,15 +227,13 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_GetSharedMemoryElementPerThreadMultiplier(
+    private static Value? Group_GetSharedMemoryElementPerThreadMultiplier(
         ref InvocationContext context)
     {
         var builder = context.Builder;
 
         // Create group dimension
-        var groupDimension = builder.CreateGroupDimensionValue(
-            context.Location,
-            DeviceConstantDimension3D.X);
+        var groupDimension = builder.CreateGroupDimensionValue(context.Location);
 
         // Get multiplier and apply it
         var multiplier = context.Pull();
@@ -255,11 +245,11 @@ partial class Intrinsics
 
         // Create shared allocation
         var allocaType = context.GetMethodGenericArguments().First();
-        var alloca = builder.CreateStaticAllocaArray(
+        var alloca = context.ModuleBuilder.CreateGlobal(
             context.Location,
-            dimension,
-            builder.CreateType(allocaType),
-            IR.MemoryAddressSpace.Shared);
+            context.ModuleBuilder.CreateType(allocaType),
+            MemoryAddressSpace.Shared,
+            dimension);
 
         // Build view
         return builder.CreateNewView(
@@ -273,26 +263,114 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Group_GetSharedMemoryElementPerWarp(
+    private static Value? Group_GetSharedMemoryElementPerWarp(
         ref InvocationContext context)
     {
         var builder = context.Builder;
 
         // Create group dimension
-        var warpDimension = builder.CreateWarpSizeValue(context.Location);
+        var warpDimension = builder.CreateSubGroupDimensionValue(context.Location);
 
         // Create shared allocation
         var allocaType = context.GetMethodGenericArguments().First();
-        var alloca = builder.CreateStaticAllocaArray(
+        var alloca = context.ModuleBuilder.CreateGlobal(
             context.Location,
-            warpDimension,
-            builder.CreateType(allocaType),
-            IR.MemoryAddressSpace.Shared);
+            context.ModuleBuilder.CreateType(allocaType),
+            MemoryAddressSpace.Shared,
+            warpDimension);
 
         // Build view
         return builder.CreateNewView(
             context.Location,
             alloca,
             warpDimension);
+    }
+
+    /// <summary>
+    /// Handles group reduce/all-reduce operations with lambda binary operations.
+    /// Always emits a custom <see cref="GroupReduce"/>; operation recognition
+    /// happens later in <see cref="IR.Transformations.RecognizeCollectiveOps"/>.
+    /// </summary>
+    private static Value? Group_Reduce(
+        ref InvocationContext context,
+        GroupReduceKind kind = GroupReduceKind.Reduce)
+    {
+        var data = context.Pull();
+        var identity = context.Pull();
+        var methodBase = context.PullDelegateMethodBase();
+        var method = methodBase is not null
+            ? context.CodeGenerator.GetMethod(methodBase)
+            : null;
+        return context.Builder.CreateGroupReduce(
+            context.Location, data, (Value?)method, kind, identity);
+    }
+
+    /// <summary>
+    /// Handles group scan operations with lambda binary operations.
+    /// Always emits a custom <see cref="GroupScan"/>; operation recognition
+    /// happens later in <see cref="IR.Transformations.RecognizeCollectiveOps"/>.
+    /// </summary>
+    private static Value? Group_Scan(
+        ref InvocationContext context,
+        GroupScanKind kind = GroupScanKind.Inclusive)
+    {
+        var data = context.Pull();
+        var identity = context.Pull();
+        var methodBase = context.PullDelegateMethodBase();
+        var method = methodBase is not null
+            ? context.CodeGenerator.GetMethod(methodBase)
+            : null;
+        return context.Builder.CreateGroupScan(
+            context.Location,
+            data,
+            (Value?)method,
+            kind,
+            identity);
+    }
+
+    /// <summary>
+    /// Handles group radix sort operations.
+    /// Resolves IRadixSortOperation static abstract members via reflection
+    /// at frontend time, since the concrete operation type is fully known.
+    /// </summary>
+    private static Value? Group_RadixSort(ref InvocationContext context)
+    {
+        var data = context.Pull();
+
+        // Get generic type arguments: [T, TRadixSortOperation]
+        var operationType = context.GetMethodGenericArguments()[1];
+
+        // Resolve NumBits (static abstract int property)
+        var numBits = (int)operationType
+            .GetProperty(
+                "NumBits",
+                System.Reflection.BindingFlags.Public |
+                System.Reflection.BindingFlags.Static)!
+            .GetValue(null)!;
+
+        // Resolve DefaultValue (static abstract T property)
+        var defaultValueObj = operationType
+            .GetProperty(
+                "DefaultValue",
+                System.Reflection.BindingFlags.Public |
+                System.Reflection.BindingFlags.Static)!
+            .GetValue(null)!;
+        var defaultValue = context.Builder.CreatePrimitiveValue(
+            context.Location,
+            defaultValueObj);
+
+        // Resolve ExtractRadixBits method
+        var extractMethodInfo = operationType.GetMethod(
+            "ExtractRadixBits",
+            System.Reflection.BindingFlags.Public |
+            System.Reflection.BindingFlags.Static)!;
+        var extractMethod = context.CodeGenerator.GetMethod(extractMethodInfo);
+
+        return context.Builder.CreateGroupRadixSort(
+            context.Location,
+            data,
+            numBits,
+            defaultValue,
+            extractMethod);
     }
 }
--- Src/ILGPUC/Frontend/Intrinsic/Warp.cs
+++ Src/ILGPUC/Frontend/Intrinsic/Warp.cs
@@ -9,7 +9,8 @@
 // Source License. See LICENSE.txt for details.
 // ---------------------------------------------------------------------------------------
 
-using ILGPUC.IR.Values;
+using ILGPUC.IR;
+using ILGPUC.IR.BasicBlockValues;
 
 namespace ILGPUC.Frontend.Intrinsic;
 
@@ -20,7 +21,7 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Warp_Barrier(ref InvocationContext context) =>
+    private static Value? Warp_Barrier(ref InvocationContext context) =>
         context.Builder.CreateBarrier(context.Location, BarrierKind.WarpLevel);
 
     /// <summary>
@@ -28,7 +29,7 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Warp_BarrierPopCount(ref InvocationContext context) =>
+    private static Value? Warp_BarrierPopCount(ref InvocationContext context) =>
         context.Builder.CreateBarrier(
             context.Location,
             PredicateBarrierKind.WarpLevel,
@@ -40,7 +41,7 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Warp_BarrierAnd(ref InvocationContext context) =>
+    private static Value? Warp_BarrierAnd(ref InvocationContext context) =>
         context.Builder.CreateBarrier(
             context.Location,
             PredicateBarrierKind.WarpLevel,
@@ -52,7 +53,7 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Warp_BarrierOr(ref InvocationContext context) =>
+    private static Value? Warp_BarrierOr(ref InvocationContext context) =>
         context.Builder.CreateBarrier(
             context.Location,
             PredicateBarrierKind.WarpLevel,
@@ -64,7 +65,7 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Warp_Broadcast(ref InvocationContext context) =>
+    private static Value? Warp_Broadcast(ref InvocationContext context) =>
         context.Builder.CreateBroadcast(
             context.Location,
             context.Pull(),
@@ -76,39 +77,24 @@ partial class Intrinsics
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Warp_Dimension(ref InvocationContext context) =>
-        context.Builder.CreateWarpSizeValue(context.Location);
+    private static Value? Warp_Dimension(ref InvocationContext context) =>
+        context.Builder.CreateSubGroupDimensionValue(context.Location);
 
     /// <summary>
     /// Handles warp index operations.
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Warp_WarpIndex(ref InvocationContext context)
-    {
-        var builder = context.Builder;
-        // Get the current group index
-        var groupIndex = builder.CreateGroupIndexValue(
-            context.Location,
-            DeviceConstantDimension3D.X);
-        // Get warp size
-        var warpSize = builder.CreateWarpSizeValue(context.Location);
-
-        // Determine relative index
-        return builder.CreateArithmetic(
-            context.Location,
-            groupIndex,
-            warpSize,
-            BinaryArithmeticKind.Div);
-    }
+    private static Value? Warp_WarpIndex(ref InvocationContext context) =>
+        context.Builder.CreateSubGroupIndexValue(context.Location);
 
     /// <summary>
     /// Handles warp lane index operations.
     /// </summary>
     /// <param name="context">The current invocation context.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Warp_LaneIndex(ref InvocationContext context) =>
-        context.Builder.CreateLaneIdxValue(context.Location);
+    private static Value? Warp_LaneIndex(ref InvocationContext context) =>
+        context.Builder.CreateSubGroupLaneIndexValue(context.Location);
 
     /// <summary>
     /// Handles warp shuffle operations.
@@ -116,7 +102,7 @@ partial class Intrinsics
     /// <param name="context">The current invocation context.</param>
     /// <param name="kind">Kind of the shuffle operation.</param>
     /// <returns>The resulting value.</returns>
-    private static ValueReference Warp_Shuffle(
+    private static Value? Warp_Shuffle(
         ref InvocationContext context,
         ShuffleKind kind = ShuffleKind.Generic) =>
         context.Builder.CreateShuffle(
@@ -124,4 +110,107 @@ partial class Intrinsics
             context.Pull(),
             context.Pull(),
             kind);
+
+    /// <summary>
+    /// Handles warp reduce/all-reduce operations with lambda binary operations.
+    /// The lambda is resolved via delegate devirtualization. The frontend
+    /// always creates a custom <see cref="WarpReduce"/> referencing the
+    /// lambda <see cref="IR.ModuleValues.Method"/>; recognition of the
+    /// underlying binary arithmetic kind happens later in
+    /// <see cref="IR.Transformations.RecognizeCollectiveOps"/> once the
+    /// lambda body has been fully compiled by the IL frontend.
+    /// </summary>
+    /// <param name="context">The current invocation context.</param>
+    /// <param name="kind">Reduce or AllReduce.</param>
+    /// <returns>The resulting value.</returns>
+    private static Value? Warp_Reduce(
+        ref InvocationContext context,
+        WarpReduceKind kind = WarpReduceKind.Reduce)
+    {
+        var data = context.Pull();
+        var methodBase = context.PullDelegateMethodBase();
+        var method = methodBase is not null
+            ? context.CodeGenerator.GetMethod(methodBase)
+            : null;
+        return context.Builder.CreateWarpReduce(
+            context.Location,
+            data,
+            method,
+            kind);
+    }
+
+    /// <summary>
+    /// Handles warp scan operations with lambda binary operations.
+    /// Always emits a custom <see cref="WarpScan"/> referencing the lambda
+    /// method — <see cref="IR.Transformations.RecognizeCollectiveOps"/>
+    /// converts to intrinsic form after the body is compiled.
+    /// </summary>
+    /// <param name="context">The current invocation context.</param>
+    /// <param name="kind">Inclusive or Exclusive.</param>
+    /// <returns>The resulting value.</returns>
+    private static Value? Warp_Scan(
+        ref InvocationContext context,
+        WarpScanKind kind = WarpScanKind.Inclusive)
+    {
+        var data = context.Pull();
+        Value? identity = kind == WarpScanKind.Exclusive
+            ? context.Pull()
+            : null;
+        var methodBase = context.PullDelegateMethodBase();
+        var method = methodBase is not null
+            ? context.CodeGenerator.GetMethod(methodBase)
+            : null;
+        return context.Builder.CreateWarpScan(
+            context.Location,
+            data,
+            method,
+            kind,
+            identity);
+    }
+
+    /// <summary>
+    /// Handles warp radix sort operations.
+    /// Resolves IRadixSortOperation static abstract members via reflection
+    /// at frontend time, since the concrete operation type is fully known.
+    /// </summary>
+    private static Value? Warp_RadixSort(ref InvocationContext context)
+    {
+        var data = context.Pull();
+
+        // Get generic type arguments: [T, TRadixSortOperation]
+        var operationType = context.GetMethodGenericArguments()[1];
+
+        // Resolve NumBits (static abstract int property)
+        var numBits = (int)operationType
+            .GetProperty(
+                "NumBits",
+                System.Reflection.BindingFlags.Public |
+                System.Reflection.BindingFlags.Static)!
+            .GetValue(null)!;
+
+        // Resolve DefaultValue (static abstract T property)
+        var defaultValueObj = operationType
+            .GetProperty(
+                "DefaultValue",
+                System.Reflection.BindingFlags.Public |
+                System.Reflection.BindingFlags.Static)!
+            .GetValue(null)!;
+        var defaultValue = context.Builder.CreatePrimitiveValue(
+            context.Location,
+            defaultValueObj);
+
+        // Resolve ExtractRadixBits method
+        var extractMethodInfo = operationType.GetMethod(
+            "ExtractRadixBits",
+            System.Reflection.BindingFlags.Public |
+            System.Reflection.BindingFlags.Static)!;
+        var extractMethod = context.CodeGenerator.GetMethod(extractMethodInfo);
+
+        return context.Builder.CreateWarpRadixSort(
+            context.Location,
+            data,
+            numBits,
+            defaultValue,
+            extractMethod);
+    }
 }
