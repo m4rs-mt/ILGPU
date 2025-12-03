17cdef153417ee0340f1fd0d36b44a4a98ee543c
Marcel Koester <m4rs@m4rs.net>
2025-12-03 21:04:56 +0000

    Added CudaLauncherEmitter (CUDA backend).

---
+++ Src/ILGPUC/Backends/CUDA/CudaLauncherEmitter.cs
@@ -0,0 +1,25 @@
+// ---------------------------------------------------------------------------------------
+//                                        ILGPU
+//                           Copyright (c) 2025 ILGPU Project
+//                                    www.ilgpu.net
+//
+// File: CudaLauncherEmitter.cs
+//
+// This file is part of ILGPU and is distributed under the University of Illinois Open
+// Source License. See LICENSE.txt for details.
+// ---------------------------------------------------------------------------------------
+
+namespace ILGPUC.Backends.Cuda;
+
+/// <summary>
+/// CUDA-specific launcher emitter using CudaAPI.LaunchKernelWithStruct.
+/// </summary>
+sealed class CudaLauncherEmitter : FlatStructLauncherEmitter
+{
+    public override string[] RequiredUsings => ["using ILGPU.Runtime.Cuda;"];
+
+    protected override string KernelTypeName => "CudaKernel";
+    protected override string StreamTypeName => "CudaStream";
+    protected override string ApiTypeName => "CudaAPI";
+    protected override string ExceptionTypeName => "CudaException";
+}
